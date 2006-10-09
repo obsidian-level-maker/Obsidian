67b2cc43fd8eb8359430739b035de9819a2dd9c7
Andrew Apted <ajapted@users.sf.net>
2006-10-09 11:27:48 +0000

    Nicer way to make the dummy 'con' module.

--- src/run_it.lua
+++ src/run_it.lua
@@ -18,28 +18,40 @@
 
 require 'oblige'
 
-function run_printf(...)
+-- provide a simplified "con" module
+
+con = { }
+
+function con.printf(...)
   io.stderr:write(string.format(...))
 end
 
-con =
-{
-  printf   = run_printf,
-  at_level = do_nothing,
-  progress = do_nothing,
-  ticker   = do_nothing,
-  abort    = function() return false end,
+function con.debugf(...)
+  -- io.stderr:write(string.format(...))
+end
 
-  random    = function() return math.random() end,
-  rand_seed = function(seed) math.randomseed(seed) end,
-}
+function con.at_level() end
+function con.progress() end
+function con.ticker()   end
+
+function con.abort()
+  return false
+end
+
+function con.rand_seed(value)
+  math.randomseed(value)
+end
+
+function con.random()
+  return math.random()
+end
 
 settings =
 {
   seed = arg[1] or (os.time() % 1000)
   ,
   game = "doom2", addon = "none",
-  mode = "sp",    size  = "one",
+  mode = "sp",  size  = "one",
 }
 
 build_cool_shit()
