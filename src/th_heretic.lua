cdd9fbdfd481ed757fa04b09aa5855417d91d023
Andrew Apted <ajapted@users.sf.net>
2006-11-05 08:02:19 +0000

    Began proper support for Hexen line and sector types, which work quite differently from Doom.

--- src/th_heretic.lua
+++ src/th_heretic.lua
@@ -16,6 +16,33 @@
 --
 ----------------------------------------------------------------
 
+HC_LINE_TYPES =  -- NOTE: only includes differences to DOOM
+{
+  A1_scroll_right = { kind=99 },
+  W1_secret_exit  = { kind=105 }, -- FIXME: correct?
+
+  P1_green_door = { kind=33 },
+  PR_green_door = { kind=28 },
+}
+
+HC_SECTOR_TYPES =
+{
+  secret   = { kind=9 },
+  friction = { kind=15 },
+
+  random_off = { kind=1 },
+  blink_fast = { kind=2 },
+  blink_slow = { kind=3 },
+  glow       = { kind=8 },
+
+  damage_5  = { kind=7 },
+  damage_10 = { kind=5 },
+  damage_20 = { kind=16 },
+}
+
+
+----------------------------------------------------------------
+
 HC_THEMES =
 {
   ---- INDOOR ------------
