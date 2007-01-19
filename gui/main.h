b059ed5a982ae75c29d7c468b75e90ec52c7918b
Andrew Apted <ajapted@users.sf.net>
2007-01-19 04:12:23 +0000

    Version bump.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "0.80"
-#define OBLIGE_HEX_VER  0x080
+#define OBLIGE_VERSION  "0.81"
+#define OBLIGE_HEX_VER  0x081
 
 #define DATA_DIR  "./obl_data"
 
