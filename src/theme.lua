adacdd1e0cb69f0b7fe6efbe0d844bcbf27f5c36
Andrew Apted <ajapted@users.sf.net>
2007-02-14 12:54:16 +0000

    Added new file 'theme.lua' for managing themes.

---
+++ src/theme.lua
@@ -0,0 +1,27 @@
+----------------------------------------------------------------
+--  THEME manager
+----------------------------------------------------------------
+--
+--  Oblige Level Maker (C) 2006,2007 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+require 'x_doom1'
+require 'x_doom2'
+require 'x_tnt'
+require 'x_plutonia'
+require 'x_freedoom'
+
+require 'x_heretic'
+require 'x_hexen'
+
