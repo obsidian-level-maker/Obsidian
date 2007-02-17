20d7501acb8f140481c2384905074e44e24a0629
Andrew Apted <ajapted@users.sf.net>
2007-02-17 08:03:20 +0000

    Moved the get_rand_xxx functions into theme.lua

--- src/theme.lua
+++ src/theme.lua
@@ -25,3 +25,66 @@ require 'x_freedoom'
 require 'x_heretic'
 require 'x_hexen'
 
+
+----------------------------------------------------------------
+
+function get_rand_theme()
+  local name,info = rand_table_pair(THEME.themes)
+  return info
+end
+
+function get_rand_exit_theme()
+  local name,info = rand_table_pair(THEME.exits)
+  return info
+end
+
+function get_rand_hallway()
+  local name,info = rand_table_pair(THEME.hallways)
+  return info
+end
+
+function get_rand_crate()
+  local name,info = rand_table_pair(THEME.crates)
+  return info
+end
+
+function get_rand_rail()
+  local name,info = rand_table_pair(THEME.rails)
+  return info
+end
+
+function choose_liquid()
+  local name,info = rand_table_pair(THEME.liquids)
+  return info
+end
+
+function find_liquid(name)
+  local info = THEME.liquids[name]
+
+  if not info then
+    error("Unknown liquid: " .. name)
+  end
+end
+
+function random_light_kind(is_flat)
+  local infos = {}
+  for name,info in pairs(THEME.lights) do
+    if sel(is_flat, info.flat, info.tex) then
+      table.insert(infos,info)
+    end
+  end
+  assert(#infos > 0)
+  return rand_element(infos)
+end
+
+function random_door_kind(w)
+  local names = {}
+  for kind,info in pairs(THEME.doors) do
+    if info.w == w then
+      table.insert(names,kind)
+    end
+  end
+  assert(#names > 0)
+  return rand_element(names)
+end
+
