aa6545fd99ec39b407cf5cd367c84de173850bff
Andrew Apted <ajapted@users.sf.net>
2007-03-07 00:56:38 +0000

    Fixed an off-by-one error with 'y' coord. Added a DumpMap() function to aid debugging. Replaced TABs with spaces.

--- gui/g_wolf.cc
+++ gui/g_wolf.cc
@@ -50,200 +50,239 @@ static u16_t *thing_plane;
 
 static void WF_PutU16(u16_t val, FILE *fp)
 {
-	fputc(val & 0xFF, fp);
-	fputc((val >> 8) & 0xFF, fp);
+  fputc(val & 0xFF, fp);
+  fputc((val >> 8) & 0xFF, fp);
 }
 
 static void WF_PutU32(u32_t val, FILE *fp)
 {
-	fputc(val & 0xFF, fp);
-	fputc((val >> 8) & 0xFF, fp);
-	fputc((val >> 16) & 0xFF, fp);
-	fputc((val >> 24) & 0xFF, fp);
+  fputc(val & 0xFF, fp);
+  fputc((val >> 8) & 0xFF, fp);
+  fputc((val >> 16) & 0xFF, fp);
+  fputc((val >> 24) & 0xFF, fp);
 }
 
 static void WF_PutNString(const char *str, int max_len, FILE *fp)
 {
-	SYS_ASSERT((int)strlen(str) <= max_len);
+  SYS_ASSERT((int)strlen(str) <= max_len);
 
-	while (*str)
-	{
-		fputc(*str++, fp);
-		max_len--;
-	}
+  while (*str)
+  {
+    fputc(*str++, fp);
+    max_len--;
+  }
 
-	for (; max_len > 0; max_len--)
-	{
-		fputc(0, fp);
-	}
+  for (; max_len > 0; max_len--)
+  {
+    fputc(0, fp);
+  }
 }
 
 namespace rle_comp
 {
 /* private */
-	unsigned short value;
-	unsigned short repeat;
+  unsigned short value;
+  unsigned short repeat;
 
-	void Begin()
-	{
-		repeat = 0;
-	}
+  void Begin()
+  {
+    repeat = 0;
+  }
 
-	void Flush()
-	{
-		while (repeat > 3)
-		{
-			int actual = MIN(128, repeat);
+  void Flush()
+  {
+    while (repeat > 3)
+    {
+      int actual = MIN(128, repeat);
 
-			WF_PutU16(RLEW_TAG, map_fp);  // tag
-			WF_PutU16(actual, map_fp);    // count
-			WF_PutU16(value, map_fp);     // value
+      WF_PutU16(RLEW_TAG, map_fp);  // tag
+      WF_PutU16(actual, map_fp);    // count
+      WF_PutU16(value, map_fp);     // value
 
-			repeat -= actual;
-		}
+      repeat -= actual;
+    }
 
-		for (; repeat > 0; repeat--)
-		{
-			WF_PutU16(value, map_fp);
-		}
-	}
+    for (; repeat > 0; repeat--)
+    {
+      WF_PutU16(value, map_fp);
+    }
+  }
 
-	void Add(unsigned short datum)
-	{
-		// don't want no Carmackization...
-		SYS_ASSERT((datum & 0xFF00) != 0xA700);
-		SYS_ASSERT((datum & 0xFF00) != 0xA800);
+  void Add(unsigned short datum)
+  {
+    // don't want no Carmackization...
+    SYS_ASSERT((datum & 0xFF00) != 0xA700);
+    SYS_ASSERT((datum & 0xFF00) != 0xA800);
 
-		// it shouldn't match the RLEW tag either...
-		SYS_ASSERT(datum != RLEW_TAG);
+    // it shouldn't match the RLEW tag either...
+    SYS_ASSERT(datum != RLEW_TAG);
 
 #if 1
-		WF_PutU16(datum, map_fp);
+    WF_PutU16(datum, map_fp);
 #else
-		if (repeat > 0)
-		{
-			if (datum == value)
-			{
-				repeat++;
-				return;
-			}
+    if (repeat > 0)
+    {
+      if (datum == value)
+      {
+        repeat++;
+        return;
+      }
 
-			Flush();
-		}
+      Flush();
+    }
 
-		value = datum;
-		repeat = 1;
+    value = datum;
+    repeat = 1;
 #endif
-	}
+  }
 
 } // namespace rle_comp
 
 
 //------------------------------------------------------------------------
 
+static void DumpMap(void)
+{
+  int x, y;
+
+  for (y = 0; y < 64; y++)
+  {
+    for (x = 0; x < 64; x++)
+    {
+      int tile = solid_plane[y*64+x];
+      int obj  = thing_plane[y*64+x];
+
+      int ch;
+
+      if (tile == NO_TILE)
+        ch = '#';
+      else if (tile < 52)
+        ch = 'A' + (tile / 2);
+      else if (tile < 64)
+        ch = '1' + ((tile - 52) / 2);
+      else if (obj == NO_OBJ)
+        ch = '.';
+      else if (obj >= 19 && obj <= 22)
+        ch = 'p'; // player
+      else if ((obj >= 43 && obj <= 56) || obj == 71)
+        ch = '+'; // pickup
+      else if (obj >= 23 && obj <= 71)
+        ch = '%'; // scenery
+      else if (obj >= 108)
+        ch = 'm'; // monster
+      else
+        ch = '?';
+
+      LogPrintf("%c", ch);
+    }
+
+    LogPrintf("\n");
+  }
+}
+
 static void WriteSolidPlane(int *offset, int *length)
 {
-	*offset = (int)ftell(map_fp);
+  *offset = (int)ftell(map_fp);
 
-	WF_PutU16(64*64*2 + 2, map_fp);  // compressed size (FIXME: could go wrong!)
-	WF_PutU16(64*64*2, map_fp);      // expanded size
+  WF_PutU16(64*64*2 + 2, map_fp);  // compressed size (FIXME: could go wrong!)
+  WF_PutU16(64*64*2, map_fp);      // expanded size
 
-	rle_comp::Begin();
+  rle_comp::Begin();
 
-	for (int i = 0; i < 64*64; i++)
+  for (int i = 0; i < 64*64; i++)
   {
     rle_comp::Add(solid_plane[i]);
   }
 
-	rle_comp::Flush();
+  rle_comp::Flush();
 
-	*length = (int)ftell(map_fp);
-	*length -= *offset;
+  *length = (int)ftell(map_fp);
+  *length -= *offset;
 }
 
 static void WriteThingPlane(int *offset, int *length)
 {
-	*offset = (int)ftell(map_fp);
+  *offset = (int)ftell(map_fp);
 
-	WF_PutU16(64*64*2 + 2, map_fp);  // compressed size (FIXME: could be wrong)
-	WF_PutU16(64*64*2, map_fp);      // expanded size
+  WF_PutU16(64*64*2 + 2, map_fp);  // compressed size (FIXME: could be wrong)
+  WF_PutU16(64*64*2, map_fp);      // expanded size
 
-	rle_comp::Begin();
+  rle_comp::Begin();
 
-	for (int i = 0; i < 64*64; i++)
+  for (int i = 0; i < 64*64; i++)
   {
     rle_comp::Add(thing_plane[i]);
   }
 
-	rle_comp::Flush();
+  rle_comp::Flush();
 
-	*length = (int)ftell(map_fp);
-	*length -= *offset;
+  *length = (int)ftell(map_fp);
+  *length -= *offset;
 }
 
 static void WriteBlankPlane(int *offset, int *length)
 {
-	*offset = (int)ftell(map_fp);
+  *offset = (int)ftell(map_fp);
 
-	WF_PutU16(3*2 + 2, map_fp);  // compressed size + 2
-	WF_PutU16(64*64*2, map_fp);  // expanded size
+  WF_PutU16(3*2 + 2, map_fp);  // compressed size + 2
+  WF_PutU16(64*64*2, map_fp);  // expanded size
 
-	WF_PutU16(RLEW_TAG, map_fp); // tag
-	WF_PutU16(64 * 64, map_fp);  // count
-	WF_PutU16(0, map_fp);        // value
+  WF_PutU16(RLEW_TAG, map_fp); // tag
+  WF_PutU16(64 * 64, map_fp);  // count
+  WF_PutU16(0, map_fp);        // value
 
-	*length = (int)ftell(map_fp);
-	*length -= *offset;
+  *length = (int)ftell(map_fp);
+  *length -= *offset;
 }
 
 static void WriteMap(void)
 {
-	//? SYS_ASSERT(the_world->w() <= 62);
-	//? SYS_ASSERT(the_world->h() <= 62);
+  //? SYS_ASSERT(the_world->w() <= 62);
+  //? SYS_ASSERT(the_world->h() <= 62);
 
-	const char *message = OBLIGE_TITLE " " OBLIGE_VERSION;
+  const char *message = OBLIGE_TITLE " " OBLIGE_VERSION;
 
-	WF_PutNString(message, 64, map_fp);
+  WF_PutNString(message, 64, map_fp);
 
-	int plane_offsets[3];
-	int plane_lengths[3];
+  int plane_offsets[3];
+  int plane_lengths[3];
 
-	WriteSolidPlane(plane_offsets+0, plane_lengths+0);
-	WriteThingPlane(plane_offsets+1, plane_lengths+1);
-	WriteBlankPlane(plane_offsets+2, plane_lengths+2);
+  WriteSolidPlane(plane_offsets+0, plane_lengths+0);
+  WriteThingPlane(plane_offsets+1, plane_lengths+1);
+  WriteBlankPlane(plane_offsets+2, plane_lengths+2);
 
-	map_offset = (int)ftell(map_fp);
-	// FIXME: validate (error check)
+  map_offset = (int)ftell(map_fp);
+  // FIXME: validate (error check)
 
-	WF_PutU32(plane_offsets[0], map_fp);
-	WF_PutU32(plane_offsets[1], map_fp);
-	WF_PutU32(plane_offsets[2], map_fp);
+  WF_PutU32(plane_offsets[0], map_fp);
+  WF_PutU32(plane_offsets[1], map_fp);
+  WF_PutU32(plane_offsets[2], map_fp);
 
-	WF_PutU16(plane_lengths[0], map_fp);
-	WF_PutU16(plane_lengths[1], map_fp);
-	WF_PutU16(plane_lengths[2], map_fp);
+  WF_PutU16(plane_lengths[0], map_fp);
+  WF_PutU16(plane_lengths[1], map_fp);
+  WF_PutU16(plane_lengths[2], map_fp);
 
-	// width and height
-	WF_PutU16(64, map_fp);
-	WF_PutU16(64, map_fp);
+  // width and height
+  WF_PutU16(64, map_fp);
+  WF_PutU16(64, map_fp);
 
-	WF_PutNString("Custom Map", 16, map_fp);  // name
+  WF_PutNString("Custom Map", 16, map_fp);  // name
 
-	WF_PutNString("!ID!", 4, map_fp);  // sanity check ??
+  WF_PutNString("!ID!", 4, map_fp);  // sanity check ??
 }
 
 static void WriteHead(void)
 {
-	WF_PutU16(RLEW_TAG, head_fp);
+  WF_PutU16(RLEW_TAG, head_fp);
 
-	// offset to first map (info struct)
-	WF_PutU32(map_offset, head_fp);
+  // offset to first map (info struct)
+  WF_PutU32(map_offset, head_fp);
 
-	// set remaining offsets to zero (=> no map)
-	for (int lev = 1; lev < 60; lev++)
-	{
-		WF_PutU32(0, head_fp);
-	}
+  // set remaining offsets to zero (=> no map)
+  for (int lev = 1; lev < 60; lev++)
+  {
+    WF_PutU32(0, head_fp);
+  }
 }
 
 
@@ -294,9 +333,11 @@ int add_block(lua_State *L)
   int tile = luaL_checkint(L,3);
   int obj  = luaL_checkint(L,4);
 
+x=x-3; if (x < 1) return 0; //!!!!! TESTING
+
   // adjust and validate coords
   x = x-1;
-  y = 63-y;
+  y = 64-y;
 
   SYS_ASSERT(0 <= x && x <= 63);
   SYS_ASSERT(0 <= y && y <= 63);
@@ -330,18 +371,18 @@ void Wolf_InitLua(lua_State *L)
 
 bool Wolf_Begin(void)
 {
-	map_fp = fopen("GAMEMAPS.OUT", "wb");
+  map_fp = fopen("GAMEMAPS.OUT", "wb");
 
-	if (! map_fp)
+  if (! map_fp)
   {
     DLG_ShowError("Unable to create GAMEMAPS.OUT:\n%s", strerror(errno));
     return false;
   }
 
   // Move to Finisher??
-	head_fp = fopen("MAPHEAD.OUT", "wb");
+  head_fp = fopen("MAPHEAD.OUT", "wb");
 
-	if (! head_fp)
+  if (! head_fp)
   {
     DLG_ShowError("Unable to create MAPHEAD.OUT:\n%s", strerror(errno));
     return false;
@@ -354,13 +395,15 @@ bool Wolf_Finish(void)
 {
   // FIXME
 
-	WriteMap();
-	WriteHead();
+  DumpMap();
 
-	fclose(map_fp);
-	fclose(head_fp);
+  WriteMap();
+  WriteHead();
 
-	map_fp = head_fp = NULL;
+  fclose(map_fp);
+  fclose(head_fp);
+
+  map_fp = head_fp = NULL;
 
   return true;
 }
