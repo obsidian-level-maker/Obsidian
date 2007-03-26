f16b78ccb9622d689502e3303ada71529ca57222
Andrew Apted <ajapted@users.sf.net>
2007-03-26 12:21:53 +0000

    Added 'four_dirs' to the capability table.

--- src/x_wolf.lua
+++ src/x_wolf.lua
@@ -595,6 +595,7 @@ THEME_FACTORIES["wolf3d"] = function()
 
     caps = { blocky_items=true, blocky_doors=true,
              tiered_skills=true, elevator_exits=true,
+             four_dirs=true,
            },
 
     ERROR_TEX  = WF_NO_TILE,
