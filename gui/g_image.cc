6f3172f338f89740a75f1e49ec6121aac948e078
Andrew Apted <ajapted@users.sf.net>
2007-07-02 04:41:04 +0000

    Added 'FindPair' function which may be useful one day.

--- gui/g_image.cc
+++ gui/g_image.cc
@@ -201,6 +201,27 @@ const byte hexen_palette[256*3] =
 };
 
 
+/*
+static int slime_to_nukage_pairs[] =
+{
+  0x01,127, 0x4f,126, 0x4e,125, 0xee,124, 0x0f,123,
+  0x4d,123, 0xed,122, 0x97,122, 0x4c,121, 0x4b,121,
+  0x4a,120,
+
+  -1, -1  // end marker
+};
+
+static int slime_to_blood_pairs[] =
+{
+  0x01,191, 0x4f,190, 0x4e,189, 0xee,188, 0x0f,187,
+  0x4d, 38, 0xed,186, 0x97, 36, 0x4c,185, 0x4b, 33,
+  0x4a,184,
+
+  -1, -1  // end marker
+};
+*/
+
+
 //------------------------------------------------------------------------
 
 static byte pixel_to_doom[256];
@@ -238,6 +259,19 @@ static int FindColor(const byte *palette, const byte *col)
 
   return best_idx;
 }
+
+/*
+static int FindPair(const int *pairs, int index)
+{
+  for (int j = 0; pairs[j] >= 0; j += 2)
+  {
+    if (pairs[j] == index)
+      return pairs[j+1];
+  }
+
+  return index;
+}
+*/
 
 static void CreateMappingTables(void)
 {
