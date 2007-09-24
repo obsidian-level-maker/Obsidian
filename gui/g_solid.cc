e63d12ddb07f5b5a5fb3965c726ba72de9713be5
Andrew Apted <ajapted@users.sf.net>
2007-09-24 11:50:04 +0000

    More skeletal bits.

--- gui/g_solid.cc
+++ gui/g_solid.cc
@@ -33,9 +33,14 @@
 #define FVAL_NONE  -27777.75
 
 
+class merged_area_c;
+
+
 class area_info_c
 {
 public:
+  int time; // increases for each new solid area
+
   std::string t_tex;
   std::string b_tex;
   std::string w_tex;  // default
@@ -69,6 +74,8 @@ public:
  
   double x_offset;
   double y_offset;
+
+//??  merged_area_c *face;
 
 public:
    area_side_c();
@@ -109,6 +116,53 @@ public:
 };
 
 
+std::vector<area_poly_c *> all_polys;
+
+
+//------------------------------------------------------------------------
+
+class merged_area_c
+{
+public:
+  std::vector<area_poly_c *> polys;
+
+public:
+   merged_area_c();
+  ~merged_area_c();
+};
+
+
+std::vector<merged_area_c *> all_merges;
+
+
+static void CSG2_MergeAreas(void)
+{
+  // this takes all the area_polys, figures out what OVERLAPS
+  // (on the 2D map), and performs the CSG operations to create
+  // new area_polys for the overlapping parts.
+  //
+  // also figures out which area_polys are TOUCHING, and ensures
+  // there are vertices where needed (at 'T' junctions).
+
+  // TODO
+}
+
+static void CSG2_WriteDoom(void)
+{
+  // converts the Merged list into the sectors, linedefs (etc)
+  // required for a DOOM level.
+  
+  // TODO
+}
+
+static void CSG2_WriteQuake(void)
+{
+  // converts the Merged list into a QUAKE ".map" file.
+
+  // TODO
+}
+
+
 //------------------------------------------------------------------------
 
 namespace csg2
@@ -130,6 +184,8 @@ int begin_level(lua_State *L)
 int end_level(lua_State *L)
 {
   // TODO
+
+  // FIXME: free all polys
 
   return 0;
 }
