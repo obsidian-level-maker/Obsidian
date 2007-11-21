25abf1db1583a21fc2e612768ced14a7c44109bd
Andrew Apted <ajapted@users.sf.net>
2007-11-21 05:10:05 +0000

    Preliminary code for a Custom Mod list.

---
+++ gui/ui_mods.cc
@@ -0,0 +1,98 @@
+//----------------------------------------------------------------
+//  Mod list
+//----------------------------------------------------------------
+//
+//  Oblige Level Maker (C) 2006,2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------
+
+#include "headers.h"
+#include "hdr_fltk.h"
+
+#include "ui_mods.h"
+#include "ui_window.h"
+
+#include "lib_util.h"
+
+
+//
+// Mods Constructor
+//
+UI_Mods::UI_Mods(int x, int y, int w, int h, const char *label) :
+    Fl_Group(x, y, w, h, label)
+{
+  end(); // cancel begin() in Fl_Group constructor
+ 
+  box(FL_THIN_UP_BOX);
+//  align(FL_ALIGN_INSIDE | FL_ALIGN_LEFT | FL_ALIGN_TOP);
+
+
+  int cy = y + 8;
+
+  Fl_Box *heading = new Fl_Box(FL_FLAT_BOX, x+6, cy, 160, 24, "Custom Mods");
+  heading->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+  heading->labeltype(FL_NORMAL_LABEL);
+  heading->labelfont(FL_HELVETICA_BOLD);
+
+  add(heading);
+
+  cy += 28;
+
+
+  pack = new Fl_Pack(x, cy, 600, 600);
+  pack->end();
+  pack->box(FL_FLAT_BOX);
+  pack->color(FL_BLACK);
+
+
+  scroll = new Fl_Scroll(x, cy, w, h - cy);
+  scroll->end();
+  scroll->type(Fl_Scroll::VERTICAL_ALWAYS);
+
+  scroll->add(pack);
+
+  add(scroll);
+
+
+  resizable(0);  // don't resize our children
+}
+
+
+//
+// Mods Destructor
+//
+UI_Mods::~UI_Mods()
+{
+}
+
+
+void UI_Mods::bump_callback(Fl_Widget *w, void *data)
+{
+  UI_Mods *that = (UI_Mods *)data;
+
+//  that->BumpSeed();
+}
+
+
+void UI_Mods::Locked(bool value)
+{
+  if (value)
+  {
+    pack->deactivate();
+  }
+  else
+  {
+    pack->activate();
+  }
+}
+
