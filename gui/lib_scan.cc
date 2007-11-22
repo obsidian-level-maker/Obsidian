d49f100e318ce02672aec5f55e95e7833496ae76
Andrew Apted <ajapted@users.sf.net>
2007-11-22 13:44:15 +0000

    Check-in of directory scanning code (skeletal).

---
+++ gui/lib_scan.cc
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------
+//  Directory Scanning
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker (C) 2006,2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+
+#include "lib_scan.h"
+#include "lib_util.h"
+
+
+int ScanDirectory(const char *path,
+                  void (* func)(void *privdat, const char *name, int flags),
+                  void *priv_data)
+{
+  // TODO
+}
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
---
+++ gui/lib_scan.h
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------
+//  Directory Scanning
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker (C) 2006,2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __LIB_SCAN_H__
+#define __LIB_SCAN_H__
+
+typedef enum
+{
+  SCAN_F_IsDir    = (1 << 0),
+  SCAN_F_Hidden   = (1 << 1),
+  SCAN_F_ReadOnly = (1 << 2),
+}
+scan_flags_e;
+
+typedef enum
+{
+  SCAN_ERROR = -1,  // general catch-all
+
+  SCAN_ERR_NoExist  = -2,  // could not find given path
+  SCAN_ERR_NotDir   = -3,  // path was not a directory
+}
+scan_error_e;
+
+int ScanDirectory(const char *path,
+                  void (* func)(void *privdat, const char *name, int flags),
+                  void *priv_data);
+// scan the directory with the given path and call the given
+// function (passing the private data pointer to it) for each
+// entry in the directory.  Returns the total number of entries,
+// or a negative value on error (SCAN_ERR_xx value).
+
+#endif /* __LIB_SCAN_H__ */
