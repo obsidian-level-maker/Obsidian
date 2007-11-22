a7d8fb70f4ca629a4ee24fff613bc6aaa92021a7
Andrew Apted <ajapted@users.sf.net>
2007-11-22 13:57:54 +0000

    Plugged in some code for ScanDirectory() based on EDGE EPI code.

--- gui/lib_scan.cc
+++ gui/lib_scan.cc
@@ -21,12 +21,104 @@
 #include "lib_scan.h"
 #include "lib_util.h"
 
+#ifdef LINUX
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#endif
+
 
 int ScanDirectory(const char *path,
                   void (* func)(void *privdat, const char *name, int flags),
                   void *priv_data)
 {
-  // TODO
+  int count = 0;
+
+#ifdef WIN32
+
+		if (! FS_GetCurrDir(tmp, MAX_PATH))
+(::GetCurrentDirectory(bufsize, (LPSTR)dir) != FALSE);
+			return false;
+
+		prev_dir = std::string(tmp);
+	}
+
+	if (! FS_SetCurrDir(dir))
+(::SetCurrentDirectory(dir) != FALSE);
+		return false;
+
+	WIN32_FIND_DATA fdata;
+
+	HANDLE handle = FindFirstFile("*.*", &fdata);
+	if (handle == INVALID_HANDLE_VALUE)
+		return false;
+
+	// Ensure the container is empty
+	fsd->Clear();
+
+	do
+	{
+		filesys_direntry_c tmp_entry;
+
+		tmp_entry.name = std::string(fdata.cFileName);
+		tmp_entry.size = fdata.nFileSizeLow;
+		tmp_entry.is_dir = (fdata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)?true:false; 
+
+		if (! fsd->AddEntry(&tmp_entry))
+		{
+			FindClose(handle);
+			FS_SetCurrDir(prev_dir.c_str());
+(::SetCurrentDirectory(dir) != FALSE);
+			return false;
+		}
+	}
+	while(FindNextFile(handle, &fdata));
+
+	FindClose(handle);
+
+	FS_SetCurrDir(prev_dir.c_str());
+(::SetCurrentDirectory(dir) != FALSE);
+
+
+#else // LINUX
+
+	DIR *handle = opendir(dir);
+	if (handle == NULL)
+		return SCAN_ERR_NoExist;
+
+	for (;;)
+	{
+		const struct dirent *fdata = readdir(handle);
+		if (fdata == NULL)
+			break;
+
+		struct stat finfo;
+
+		if (stat(fdata->d_name, &finfo) != 0)
+			continue;
+
+		filesys_direntry_c tmp_entry;
+
+		tmp_entry.name = std::string(fdata->d_name);
+		tmp_entry.size = finfo.st_size;
+		tmp_entry.is_dir = S_ISDIR(finfo.st_mode) ?true:false;
+
+    (* func)(priv_data, name, flags);
+      
+		if (! fsd->AddEntry(&tmp_entry))
+		{
+			closedir(handle);
+			FS_SetCurrDir(olddir);
+			return false;
+		}
+	}
+
+	closedir(handle);
+
+#endif
+
+  return count;
 }
 
 //--- editor settings ---
