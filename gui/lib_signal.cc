d4f1655b10e39915e04eacb277f972cef9417ba9
Andrew Apted <ajapted@users.sf.net>
2007-12-15 04:08:30 +0000

    Check-in of skeletal API for simple signalling.

---
+++ gui/lib_signal.cc
@@ -0,0 +1,40 @@
+//------------------------------------------------------------------------
+//  Simple Signals
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker (C) 2006,2007 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+
+#include "lib_signal.h"
+
+
+void Signal_Watch(const char *name, signal_notify_f func, void *priv_dat)
+{
+  // FIXME
+}
+
+
+void Signal_DontCare(const char *name, signal_notify_f func)
+{
+  // FIXME
+}
+
+
+void Signal_Raise(const char *name)
+{
+  // FIXME
+}
+
