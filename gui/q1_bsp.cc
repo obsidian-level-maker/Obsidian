914139ca8fc9c1711a9881dc4f07715512af6c2d
Andrew Apted <ajapted@users.sf.net>
2008-02-06 03:49:54 +0000

    Check-in of QUAKE 1 BSP structure defs (q1_bsp.h)

---
+++ gui/q1_bsp.cc
@@ -0,0 +1,44 @@
+//------------------------------------------------------------------------
+//  LEVEL building - QUAKE 1 BSP
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker (C) 2006-2008 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+#include "hdr_fltk.h"
+#include "hdr_lua.h"
+#include "hdr_ui.h"
+
+#include "lib_file.h"
+#include "lib_util.h"
+
+#include "csg_poly.h"
+#include "csg_doom.h"
+#include "csg_quake.h"
+
+#include "q1_bsp.h"
+
+#include "g_glbsp.h"
+#include "g_image.h"
+#include "g_lua.h"
+
+#include "main.h"
+
+
+// TODO
+
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
