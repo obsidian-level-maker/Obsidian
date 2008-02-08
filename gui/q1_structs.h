4ea76c0835f8511687744caace35ca2be5bf6dec
Andrew Apted <ajapted@users.sf.net>
2008-02-08 09:35:26 +0000

    Whitespace and other Tidying.

--- gui/q1_structs.h
+++ gui/q1_structs.h
@@ -33,70 +33,74 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // upper design bounds
 
-#define	MAX_MAP_HULLS		4
+#define MAX_MAP_HULLS   4
 
-#define	MAX_MAP_MODELS		256
-#define	MAX_MAP_BRUSHES		4096
-#define	MAX_MAP_ENTITIES	1024
-#define	MAX_MAP_ENTSTRING	65536
+#define MAX_MAP_MODELS    256
+#define MAX_MAP_BRUSHES   4096
+#define MAX_MAP_ENTITIES  1024
+#define MAX_MAP_ENTSTRING 65536
 
-#define	MAX_MAP_PLANES		16384	/* TYR - was 8192 */
-#define	MAX_MAP_NODES		32767	/* negative shorts are contents */
-#define	MAX_MAP_CLIPNODES	32767	/* negative shorts are contents */
-#define	MAX_MAP_LEAFS		32767	/* negative shorts are contents */
-#define	MAX_MAP_VERTS		65535
-#define	MAX_MAP_FACES		65535
-#define	MAX_MAP_MARKSURFACES	65535
-#define	MAX_MAP_TEXINFO		4096
-#define	MAX_MAP_EDGES		256000
-#define	MAX_MAP_SURFEDGES	512000
-#define	MAX_MAP_TEXTURES	512
-#define	MAX_MAP_MIPTEX		0x200000
-#define	MAX_MAP_LIGHTING	0x100000
-#define	MAX_MAP_VISIBILITY	0x100000
+#define MAX_MAP_PLANES    8192
+#define MAX_MAP_NODES     32767 /* negative shorts are contents */
+#define MAX_MAP_CLIPNODES 32767 /* negative shorts are contents */
+#define MAX_MAP_LEAFS     32767 /* negative shorts are contents */
 
-#define	MAX_MAP_PORTALS		65536
+#define MAX_MAP_VERTS         65535
+#define MAX_MAP_FACES         65535
+#define MAX_MAP_MARKSURFACES  65535
+#define MAX_MAP_TEXINFO        4096
+#define MAX_MAP_TEXTURES        512
+
+#define MAX_MAP_EDGES        256000
+#define MAX_MAP_SURFEDGES    512000
+#define MAX_MAP_MIPTEX     0x200000
+#define MAX_MAP_LIGHTING   0x100000
+#define MAX_MAP_VISIBILITY 0x100000
+
+#define MAX_MAP_PORTALS       65535
 
 // key / value pair sizes
 
-#define	MAX_KEY		32
-#define	MAX_VALUE	1024
+#define MAX_KEY   32
+#define MAX_VALUE 1024
 
 //=============================================================================
 
-#define BSPVERSION	29
+#define BSPVERSION  29
 
 typedef struct
 {
-    s32_t fileofs;
-    s32_t filelen;
+  s32_t file_ofs;
+  s32_t file_len;
 }
 lump_t;
 
-#define	LUMP_ENTITIES		0
-#define	LUMP_PLANES		1
-#define	LUMP_TEXTURES		2
-#define	LUMP_VERTEXES		3
-#define	LUMP_VISIBILITY		4
-#define	LUMP_NODES		5
-#define	LUMP_TEXINFO		6
-#define	LUMP_FACES		7
-#define	LUMP_LIGHTING		8
-#define	LUMP_CLIPNODES		9
-#define	LUMP_LEAFS		10
-#define	LUMP_MARKSURFACES	11
-#define	LUMP_EDGES		12
-#define	LUMP_SURFEDGES		13
-#define	LUMP_MODELS		14
+#define LUMP_ENTITIES       0
+#define LUMP_PLANES         1
+#define LUMP_TEXTURES       2
+#define LUMP_VERTEXES       3
+#define LUMP_VISIBILITY     4
+#define LUMP_NODES          5
+#define LUMP_TEXINFO        6
+#define LUMP_FACES          7
 
-#define	HEADER_LUMPS	15
+#define LUMP_LIGHTING       8
+#define LUMP_CLIPNODES      9
+#define LUMP_LEAFS         10
+#define LUMP_MARKSURFACES  11
+#define LUMP_EDGES         12
+#define LUMP_SURFEDGES     13
+#define LUMP_MODELS        14
+
+#define HEADER_LUMPS  15
 
 typedef struct
 {
   float mins[3], maxs[3];
   float origin[3];
+
   s32_t headnode[MAX_MAP_HULLS];
-  s32_t visleafs;		// not including the solid leaf 0
+  s32_t visleafs;   // not including the solid leaf 0
   s32_t firstface, numfaces;
 }
 dmodel_t;
@@ -111,16 +115,16 @@ dheader_t;
 typedef struct
 {
   s32_t nummiptex;
-  s32_t dataofs[4];		// [nummiptex]
+  s32_t dataofs[4];   // [nummiptex]
 }
 dmiptexlump_t;
 
-#define	MIPLEVELS	4
+#define MIPLEVELS 4
 typedef struct miptex_s
 {
   char name[16];
   u32_t width, height;
-  u32_t offsets[MIPLEVELS];	// four mip maps stored
+  u32_t offsets[MIPLEVELS]; // four mip maps stored
 }
 miptex_t;
 
@@ -132,50 +136,50 @@ dvertex_t;
 
 
 // 0-2 are axial planes
-#define	PLANE_X		0
-#define	PLANE_Y		1
-#define	PLANE_Z		2
+#define PLANE_X   0
+#define PLANE_Y   1
+#define PLANE_Z   2
 
 // 3-5 are non-axial planes snapped to the nearest
-#define	PLANE_ANYX	3
-#define	PLANE_ANYY	4
-#define	PLANE_ANYZ	5
+#define PLANE_ANYX  3
+#define PLANE_ANYY  4
+#define PLANE_ANYZ  5
 
 typedef struct
 {
   float normal[3];
   float dist;
-  s32_t type;	// PLANE_X - PLANE_ANYZ ?remove? trivial to regenerate
+  s32_t type; // PLANE_X - PLANE_ANYZ 
 }
 dplane_t;
 
 
-#define	CONTENTS_EMPTY		-1
-#define	CONTENTS_SOLID		-2
-#define	CONTENTS_WATER		-3
-#define	CONTENTS_SLIME		-4
-#define	CONTENTS_LAVA		-5
-#define	CONTENTS_SKY		-6
-#define	CONTENTS_ORIGIN		-7	/* removed at csg time       */
-#define	CONTENTS_CLIP		-8	/* changed to contents_solid */
+#define CONTENTS_EMPTY  -1
+#define CONTENTS_SOLID  -2
+#define CONTENTS_WATER  -3
+#define CONTENTS_SLIME  -4
+#define CONTENTS_LAVA   -5
+#define CONTENTS_SKY    -6
+#define CONTENTS_ORIGIN -7  /* removed at csg time       */
+#define CONTENTS_CLIP   -8  /* changed to contents_solid */
 
-#define	CONTENTS_CURRENT_0	-9
-#define	CONTENTS_CURRENT_90	-10
-#define	CONTENTS_CURRENT_180	-11
-#define	CONTENTS_CURRENT_270	-12
-#define	CONTENTS_CURRENT_UP	-13
-#define	CONTENTS_CURRENT_DOWN	-14
+#define CONTENTS_CURRENT_0      -9
+#define CONTENTS_CURRENT_90    -10
+#define CONTENTS_CURRENT_180   -11
+#define CONTENTS_CURRENT_270   -12
+#define CONTENTS_CURRENT_UP    -13
+#define CONTENTS_CURRENT_DOWN  -14
 
 
 // !!! if this is changed, it must be changed in asm_i386.h too !!!
 typedef struct
 {
   s32_t planenum;
-  s16_t children[2];	// negative numbers are -(leafs+1), not nodes
-  s16_t mins[3];		// for sphere culling
+  s16_t children[2];  // negative numbers are -(leafs+1), not nodes
+  s16_t mins[3];    // for sphere culling
   s16_t maxs[3];
   u16_t firstface;
-  u16_t numfaces;	// counting both sides
+  u16_t numfaces; // counting both sides
 }
 dnode_t;
 
@@ -210,55 +214,57 @@ dclipnode_t;
 
 typedef struct texinfo_s
 {
-  float vecs[2][4];		// [s/t][xyz offset]
+  float s[4];   // x/y/z/offset
+  float t[4];
+
   s32_t miptex;
   s32_t flags;
 }
 texinfo_t;
 
-#define	TEX_SPECIAL 	1	// sky or slime, no lightmap or 256 subdivision
+#define TEX_SPECIAL   1 // sky or slime, no lightmap or 256 subdivision
 
 // note that edge 0 is never used, because negative edge nums are used for
 // counterclockwise use of the edge in a face
 typedef struct
 {
-  u16_t v[2];	// vertex numbers
+  u16_t v[2]; // vertex numbers
 }
 dedge_t;
 
-#define	MAXLIGHTMAPS	4
+#define MAXLIGHTMAPS  4
 
 typedef struct
 {
   s16_t planenum;
   s16_t side;
 
-  s32_t firstedge;		// we must support > 64k edges
+  s32_t firstedge;    // we must support > 64k edges
   s16_t numedges;
   s16_t texinfo;
 
   // lighting info
   u8_t  styles[MAXLIGHTMAPS];
-  s32_t lightofs;		// start of [numstyles*surfsize] samples
+  s32_t lightofs;   // start of [numstyles*surfsize] samples
 }
 dface_t;
 
 
-#define	AMBIENT_WATER	0
-#define	AMBIENT_SKY	1
-#define	AMBIENT_SLIME	2
-#define	AMBIENT_LAVA	3
+#define AMBIENT_WATER  0
+#define AMBIENT_SKY    1
+#define AMBIENT_SLIME  2
+#define AMBIENT_LAVA   3
 
-#define	NUM_AMBIENTS	4	// automatic ambient sounds
+#define NUM_AMBIENTS   4 // automatic ambient sounds
 
 // leaf 0 is the generic CONTENTS_SOLID leaf, used for all solid areas
 // all other leafs need visibility info
 typedef struct
 {
   s32_t contents;
-  s32_t visofs;			// -1 = no visibility info
+  s32_t visofs;     // -1 = no visibility info
 
-  s16_t mins[3];		// for frustum culling
+  s16_t mins[3];    // for frustum culling
   s16_t maxs[3];
 
   u16_t firstmarksurface;
