7ed87de1ddf7c7a926d13d2d4b80287c91d95e09
Andrew Apted <ajapted@users.sf.net>
2008-02-08 13:48:12 +0000

    Version bump to 1.02, progress with Quake1 output.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "1.01"
-#define OBLIGE_HEX_VER  0x101
+#define OBLIGE_VERSION  "1.02"
+#define OBLIGE_HEX_VER  0x102
 
 extern const char *install_path;
 
