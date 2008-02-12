b3f944846e888ea90e2fff5c67a0ab443a9e7a51
Andrew Apted <ajapted@users.sf.net>
2008-02-12 02:18:31 +0000

    Added "WIP" to the version string.

--- gui/main.h
+++ gui/main.h
@@ -21,7 +21,7 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.03"
+#define OBLIGE_VERSION  "3.03-WIP"
 #define OBLIGE_HEX_VER  0x303
 
 extern const char *install_path;
