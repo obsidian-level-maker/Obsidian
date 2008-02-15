c526d7b919773aad162655953cf873a206f9c6eb
Andrew Apted <ajapted@users.sf.net>
2008-02-15 08:53:09 +0000

    Preliminary code for saving and loading MipTex location data.

--- gui/q1_tex.cc
+++ gui/q1_tex.cc
@@ -21,6 +21,8 @@
 #include "hdr_lua.h"
 #include "hdr_ui.h"
 
+#include <map>
+
 #include "lib_file.h"
 #include "lib_util.h"
 
@@ -31,8 +33,95 @@
 
 #include "main.h"
 
+#ifndef PATH_MAX
+#define PATH_MAX  2048
+#endif
 
-static FILE *bsp_fp;
+
+class miptex_locator_c
+{
+public:
+  std::string filename;
+
+  int offset;
+  int length;
+
+public:
+  miptex_locator_c(std::string& _fn, int _ofs, int _len) :
+      filename(_fn), offset(_ofs), length(_len)
+  { }
+
+  ~miptex_locator_c()
+  { }
+
+  bool operator< (const miptex_locator_c& other) const
+  {
+    int cmp = StringCaseCmp(filename.c_str(), other.filename.c_str());
+
+    if (cmp != 0)
+      return (cmp < 0);
+
+    return offset < other.offset;
+  }
+};
+
+
+typedef std::map<std::string, miptex_locator_c*> miptex_database_t;
+
+static miptex_database_t miptex_db;
+
+
+static bool MiptexDB_Load(const char *filename)
+{
+  FILE *fp = fopen(filename, "r");
+
+  if (! fp)
+  {
+    DebugPrintf("MiptexDB_Load: cannot open file: %s\n", filename);
+    return false;
+  }
+  
+  char * line_buf = new char[PATH_MAX+1];
+
+  while (fgets(line_buf, PATH_MAX, fp))
+  {
+    
+  }
+
+  delete[] line_buf;
+  return true;
+}
+
+
+static bool MiptexDB_Save(const char *filename)
+{
+  FILE *fp = fopen(filename, "w");
+
+  if (! fp)
+  {
+    DebugPrintf("MiptexDB_Save: cannot create file: %s\n", filename);
+    return false;
+  }
+
+  miptex_database_t::iterator MDI;
+
+  for (MDI = miptex_db.begin(); MDI != miptex_db.end(); MDI++)
+  {
+    const std::string& tex_name = MDI->first;
+
+    fprintf(fp, "%s\n", tex_name.c_str());
+
+    miptex_locator_c *loc = MDI->second;
+
+    fprintf(fp, "%s\n",    loc->filename.c_str());
+    fprintf(fp, "%d %d\n", loc->offset, loc->length);
+  }
+
+  fflush(fp);
+  fclose(fp);
+
+  return true;
+}
 
 
 void Quake1_ExtractTextures(void)
