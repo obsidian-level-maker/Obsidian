22097e220fca23b3ec1c65138c369785dc5f9b8d
Andrew Apted <ajapted@users.sf.net>
2008-02-15 05:52:07 +0000

    Implemented PAK_OpenRead(), with loading the pak directory.
    Implemented PAK_CloseRead().

--- gui/q1_pakfile.cc
+++ gui/q1_pakfile.cc
@@ -35,21 +35,116 @@
 
 static FILE *read_fp;
 
+static raw_pak_header_t r_header;
+
+static std::vector<raw_pak_entry_t> r_directory;
+
 
 bool PAK_OpenRead(const char *filename)
 {
-  // TODO: PAK_OpenRead
-  return false;
+  read_fp = fopen(filename, "rb");
+
+  if (! read_fp)
+  {
+    LogPrintf("PAK_OpenRead: cannot open file: %s\n", filename);
+    return false;
+  }
+
+  LogPrintf("Opened PAK file: %s\n", filename);
+
+  if (fread(&r_header, sizeof(r_header), 1, read_fp) != 1)
+  {
+    LogPrintf("PAK_OpenRead: failed reading header\n");
+    fclose(read_fp);
+    return false;
+  }
+
+  if (memcmp(r_header.magic, PAK_MAGIC, 4) != 0)
+  {
+    LogPrintf("PAK_OpenRead: not a PAK file!\n");
+    fclose(read_fp);
+    return false;
+  }
+
+  r_header.dir_start = LE_U32(r_header.dir_start);
+  r_header.entry_num = LE_U32(r_header.entry_num);
+
+  // convert directory length to entry count
+  r_header.entry_num /= sizeof(raw_pak_entry_t);
+
+  /* read directory */
+
+  if (r_header.entry_num == 0)
+  {
+    LogPrintf("PAK_OpenRead: empty PAK file!\n");
+    fclose(read_fp);
+    return false;
+  }
+  if (r_header.entry_num >= 4000)  // sanity check
+  {
+    LogPrintf("PAK_OpenRead: bad header (%d entries?)\n", r_header.entry_num);
+    fclose(read_fp);
+    return false;
+  }
+
+  if (fseek(read_fp, r_header.dir_start, SEEK_SET) != 0)
+  {
+    LogPrintf("PAK_OpenRead: cannot seek to directory (at 0x%08x)\n", r_header.dir_start);
+    fclose(read_fp);
+    return false;
+  }
+
+  r_directory.resize(r_header.entry_num);
+
+  for (int i = 0; i < (int)r_header.entry_num; i++)
+  {
+    raw_pak_entry_t *E = &r_directory[i];
+
+    int res = fread(&E, sizeof(raw_pak_entry_t), 1, read_fp);
+
+    if (res == EOF || res != 1 || ferror(read_fp))
+    {
+      LogPrintf("PAK_OpenRead: hit EOF reading dir-entry %d\n", i);
+
+      // truncate directory
+      r_header.entry_num = i;
+      break;
+    }
+
+    // make sure name is NUL terminated.  For the longest possible
+    // name, this will chop off the last character.  However for
+    // our purposes this should not matter much.
+    E->name[55] = 0;
+
+    E->offset = LE_U32(E->offset);
+    E->length = LE_U32(E->length);
+
+    DebugPrintf("-- %3d: %08x %08x '%s'\n", i, E->offset, E->length, E->name);
+  }
+
+  if (r_header.entry_num == 0)
+  {
+    LogPrintf("PAK_OpenRead: could not read any dir-entries!\n");
+    fclose(read_fp);
+    return false;
+  }
+
+  return true; // OK
 }
 
 void PAK_CloseRead(void)
 {
-  // TODO
+  fclose(read_fp);
+
+  r_directory.resize(0);
+
+  LogPrintf("Closed PAK file\n");
 }
 
 int PAK_FindMaps(std::vector<int>& entries)
 {
   // TODO
+  return 0;
 }
 
 
