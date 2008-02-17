c131833f3e02a0264786bd8eb079c15a4e80f2f4
Andrew Apted <ajapted@users.sf.net>
2008-02-17 04:26:17 +0000

    Version bumped to 3.04, for working Quake1 texture extractor.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.03-WIP"
-#define OBLIGE_HEX_VER  0x303
+#define OBLIGE_VERSION  "3.04-WIP"
+#define OBLIGE_HEX_VER  0x304
 
 extern const char *install_path;
 
