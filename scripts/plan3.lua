bcadbd04944efb766e81e781fa10043ca6029864
Andrew Apted <ajapted@users.sf.net>
2008-03-02 04:16:29 +0000

    Bit more stuff...

--- scripts/plan3.lua
+++ scripts/plan3.lua
@@ -46,16 +46,70 @@ class ROOM
   links : table(RLINK) -- all connections with other rooms
 
   quest : Quest
+
+  is_big : true  -- can contain other rooms
+
+  s_low, s_high, s_size : Vector3  -- coordinates within SEED map
+}
+
+
+class QUEST     -- FIXME: probably doesn't belong here
+{
+  kind : string  -- "key" | "exit"
+  item : string  -- name of key (etc)
+
+  level : integer  -- increases for each main quest
 }
 
 --]]
+
+
+function temp_decide_quests()
+
+  QUESTS = { }
+
+  table.insert(QUESTS,
+  {
+    kind = "key", item = "k_red", level = 1,
+  })
+
+  table.insert(QUESTS,
+  {
+    kind = "key", item = "k_blue", level = 2,
+  })
+
+  table.insert(QUESTS,
+  {
+    kind = "exit", item = "normal", level = 3,
+  })
+end
 
 
 function plan_rooms_sp()
 
   print("plan_rooms_sp...")
 
+  temp_decide_quests()  -- FIXME: pass quest list as input to here
   
+  
+  -- make an initial room.  This is not explicitly the start room or
+  -- the exit room, but will become some room along the first quest
+  -- (and could become the start or exit room).
+
+  ROOMS = {}
+
+  local initial_R =
+  {
+    links = {}, quest = QUESTS[1]
+
+    -- no seed allocation yet
+  }
+
+  table.insert(ROOMS, initial_R)
+
+  if rand_odds(80) then
+    enlarge_room(initial_R)
+  end
 
 end
 
