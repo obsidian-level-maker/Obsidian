2b494420473269bc16f869e43f58b7666b118fdf
Andrew Apted <ajapted@users.sf.net>
2008-03-02 03:45:47 +0000

    Started work on new planning code for SP/COOP maps.

---
+++ scripts/plan3.lua
@@ -0,0 +1,61 @@
+----------------------------------------------------------------
+-- PLANNER 3
+----------------------------------------------------------------
+--
+--  Oblige Level Maker (C) 2006-2008 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+require 'defs'
+require 'util'
+
+
+--[[ CLASS INFORMATION
+----------------------
+
+class RLINK  -- Room Link
+[
+  rooms : table(ROOM)  -- table has two entries ]1] and [2]
+
+  type  : string  -- "neighbour" (the two rooms touch)
+                  -- "contain"   (rooms[2] is inside rooms[1])
+                  -- "teleport"
+
+  connect : string  -- "view" (windows, railings)
+                    -- "fall" (one-way, typically fall-offs)
+                    -- "walk" (two-way, typically an arch or door)
+
+  door : string  -- "arch", "door" etc..
+
+  lock : string  -- optional, for keyed/switched doors
+}
+
+
+class ROOM
+{
+  links : table(RLINK) -- all connections with other rooms
+
+  quest : Quest
+}
+
+--]]
+
+
+function plan_rooms_sp()
+
+  print("plan_rooms_sp...")
+
+  
+
+end
+
