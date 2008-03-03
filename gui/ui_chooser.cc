b98b615f0a54b961f2562967b6e1b01ab6e040b9
Andrew Apted <ajapted@users.sf.net>
2008-03-03 11:44:54 +0000

    Last_file back to 'wad' for time being...

--- gui/ui_chooser.cc
+++ gui/ui_chooser.cc
@@ -51,7 +51,7 @@ void Default_Location(void)
   }
 #endif
 
-  strcat(last_file, "TEST.bsp"); //FIXME !!!! game dependent
+  strcat(last_file, "TEST.wad"); //FIXME !!!! game dependent
 
   DebugPrintf("Default_Location: [%s]\n", last_file);
 }
