c4b0b16a8c40ac479b82f46b9f379ca90c47f45c
Andrew Apted <ajapted@users.sf.net>
2008-05-12 13:07:55 +0000

    Version bump, as some rooms and hallways are now generated.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.06-WIP"
-#define OBLIGE_HEX_VER  0x306
+#define OBLIGE_VERSION  "3.07-WIP"
+#define OBLIGE_HEX_VER  0x307
 
 extern const char *install_path;
 
