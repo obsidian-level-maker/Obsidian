2cba34a9a3813b56b1a6fe960d1d9f4dceb14bc6
Andrew Apted <ajapted@users.sf.net>
2008-05-20 14:23:54 +0000

    Check-in of experimental planner using "Room Fabs".

---
+++ scripts/plan_B.lua
@@ -0,0 +1,40 @@
+----------------------------------------------------------------
+--  PLANNER : EXPERIMENTAL SHIT
+----------------------------------------------------------------
+--
+--  Oblige Level Maker (C) 2006-2008 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+require 'defs'
+require 'util'
+require 'room_fabs'
+
+
+
+function Plan_rooms_sp()
+
+
+  ---===| Plan_rooms_sp |===---
+
+  con.printf("\n--==| Plan_rooms_sp |==--\n\n")
+
+  WW = 32
+  HH = 32
+
+  SEEDS = array_2D(WW, HH)
+
+
+
+end -- Plan_rooms_sp
+
