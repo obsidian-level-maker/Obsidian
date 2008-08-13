51bc87234f481e1ac35d13680b2d76f2848585bf
Andrew Apted <ajapted@users.sf.net>
2008-08-13 05:58:13 +0000

    Version bump in honor of working key/door puzzles (Quest_assign).

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.12-WIP"
-#define OBLIGE_HEX_VER  0x312
+#define OBLIGE_VERSION  "3.14-WIP"
+#define OBLIGE_HEX_VER  0x314
 
 extern const char *install_path;
 
