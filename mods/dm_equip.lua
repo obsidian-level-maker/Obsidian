08a5d058c8e07e8013429d87b6e98501a76bcee0
Andrew Apted <ajapted@users.sf.net>
2008-09-23 06:28:48 +0000

    Added 'DM Equipment' module.

---
+++ mods/dm_equip.lua
@@ -0,0 +1,50 @@
+----------------------------------------------------------------
+--  MODULE: deathmatch equipment
+----------------------------------------------------------------
+--
+--  Oblige Level Maker (C) 2006-2008 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+
+OB_MODULES["dm_equip_doom"] =
+{
+  label = "DM Equipment",
+
+  for_games = { doom2=1, doom1=1 },
+  for_modes = { dm=1, ctf=1 },
+
+  -- TODO: hook functions !!!
+
+  options =
+  {
+    start_weap =
+    {
+      label = "Starting Weapon",
+
+      choices =
+      {
+        { id="kein",   label="NONE" },
+        { id="saw",    label="Chainsaw" },
+        { id="shotty", label="Shotgun" },
+        { id="ssg",    label="SSG" },
+        { id="chain",  label="Chaingun" },
+        { id="launch", label="Rockets" },
+        { id="plasma", label="Plasma" },
+        { id="bfg",    label="BFG" },
+        { id="mixed",  label="Mix It Up" },
+      }
+    }
+  }
+}
+
