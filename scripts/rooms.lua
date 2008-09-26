814aac85ff4201cb2ac86cc9342ea4208021da7e
Andrew Apted <ajapted@users.sf.net>
2008-09-26 14:40:15 +0000

    Room heights: Reimplemented the algorithm which determines the heights along the main path in each arena (by interpolating to fill small gaps and splitting large gaps with a wildcard).

--- scripts/rooms.lua
+++ scripts/rooms.lua
@@ -137,210 +137,20 @@ gui.debugf("Reverted HALLWAY @ (%d,%d)\n", R.lx1,R.ly1)
 end
 
 
---##function OLD_rooms_select_heights()
---##
---##  local function spread(everything)
---##    local changed = false
---##
---##    for _,C in ipairs(PLAN.all_conns) do
---##      if C.lock or C.dest.kind == "hallway" or everything then
---##        if C.src.floor_h and not C.dest.floor_h then
---##          C.dest.floor_h = C.src.floor_h
---##          changed = true
---##        elseif C.dest.floor_h and not C.src.floor_h then
---##          C.src.floor_h = C.dest.floor_h
---##          changed = true
---##        end
---##      end
---##    end
---##
---##    return changed
---##  end
---##
---##  local function dump_along(list, prefix)
---##    for idx,L in ipairs(list) do
---##      gui.debugf("%sfloor_along_path: %d/%d = %s\n", prefix,
---##                 idx, #list, L.h or "UNSET")
---##    end
---##  end
---##
---##  local function find_stretch(list, min_num)
---##    local S, E
---##
---##    for idx = 1,#list do
---##      if list[idx].h then
---##        if S and (E - S + 1) >= min_num then
---##          return S, E
---##        end
---##        S = nil; E = nil
---##      else
---##        if not S then S = idx end
---##        E = idx
---##      end
---##    end
---##
---##    if S and (E - S + 1) >= min_num then
---##      return S, E
---##    end
---##
---##    return nil, nil
---##  end
---##
---##  local function rand_height()
---##    return 64 * (-1 + rand_index_by_probs { 5,1,6,1,4 })
---##  end
---##
---##  local function in_between(h1, h2)
---##    --!!!! FIXME : TEMP CRUD
---##    return int((h1 + h2) / 2)
---##  end
---##
---##  local function floor_along_path(target, path)
---##    local list = {}
---##
---##    for _,C in ipairs(path) do
---##      table.insert(list, { room = C.src })
---##    end
---##
---##    table.insert(list, { room = target })
---##
---##    local N = #list
---##
---##    for idx,L in ipairs(list) do
---##      if L.room.floor_h then
---##        L.h = L.room.floor_h
---##      end
---##    end
---##
---##    gui.debugf("Before:\n"); dump_along(list, "  ")
---##
---##    -- start
---##    if not list[1].h then
---##      if list[2] and list[2].h then
---##        list[1].h = list[2].h
---##      else
---##        list[1].h = rand_height()
---##      end
---##    end
---##
---##    -- target
---##    if not list[N].h then
---##      if list[N-1] and list[N-1].h then
---##        list[N].h = list[N-1].h
---##      else
---##        list[N].h = rand_height()
---##      end
---##    end
---##
---##    -- for large stretches of unset floors, split them
---##    repeat
---##      local S, E = find_stretch(list, 5)
---##      if S then
---##        gui.debugf("Splitting stretch %d..%d\n", S, E)
---##        list[int((S+E)/2)].h = rand_height()
---##      end
---##    until not S
---##
---##    -- fill all remaining gaps
---##    repeat
---##      local S, E = find_stretch(list, 1)
---##      if S then
---##        assert(S > 1 and E < N)
---##        
---##        --FIXME !!!! TERRIBLE
---##        local new_h = in_between(list[S-1].h, list[E+1].h)
---##        for idx = S, E do
---##          list[idx].h = new_h
---##        end
---##      end
---##    until not S
---##
---##---###    for idx = 2,N-1 do
---##---###      if not list[idx].h and list[idx-1].h and list[idx+1].h then
---##---###        list[idx].h = in_between(list[idx-1].h, list[idx+1].h)
---##---###      end
---##---###    end
---##
---##    gui.debugf("After:\n"); dump_along(list, "> ")
---##    gui.debugf("\n")
---##
---##    for _,L in ipairs(list) do
---##      L.room.floor_h = L.h
---##    end
---##  end
---##
---##
---##  ---| OLD_ooms_select_heights |---
---##
---##  -- handle non-room stuff (liquids)
---##  for x = 1,SEED_W do for y = 1,SEED_H do
---##    local S = SEEDS[x][y][1]
---##    if S.room and S.room.kind == "liquid" and not S.room.floor_h then
---##      S.room.floor_h = -40
---##      S.room.ceil_h  = 512
---##    end
---##  end end -- x, y
---##
---##  -- handle outdoor rooms
---##  for _,R in ipairs(PLAN.all_rooms) do
---##    if R.kind == "valley" then
---##      R.floor_h = 0
---##      R.ceil_h  = 512
---##    elseif R.kind == "ground" then
---##      R.floor_h = 128
---##      R.ceil_h  = 512
---##    elseif R.kind == "hill" then
---##      R.floor_h = 256
---##      R.ceil_h  = 512
---##    end
---##
---##    for _,C in ipairs(R.conns) do
---##      if (R == C.src) and (C.dest.kind == "indoor") then
---##        C.dest.floor_h = R.floor_h
---##      end
---##    end
---##  end
---##
---##  for _,A in ipairs(PLAN.all_arenas) do
---##    spread()
---##    floor_along_path(A.target, A.path)
---##  end
---##
---##  while spread() do end
---##  
---##  --!!!!!! TEMP CRUD
---##  for _,R in ipairs(PLAN.all_rooms) do
---##    if not R.floor_h then
---##      R.floor_h = 128
---##    end
---##    if not R.ceil_h then
---##      R.ceil_h  = math.min(512-32, R.floor_h + sel(R.hallway, 96, 256))
---##    end
---##  end
---##
---##  -- update seeds
---##  for x = 1,SEED_W do for y = 1,SEED_H do
---##    local S = SEEDS[x][y][1]
---##    if S.room then
---##      assert(S.room.floor_h)
---##      S.floor_h = S.room.floor_h
---##      S.ceil_h  = S.room.ceil_h
---##    end
---##  end end -- x, y
---##end
+function UPDATE_SEEDS(R) --!!!!
+  -- update seeds
+  for x = 1,SEED_W do for y = 1,SEED_H do
+    local S = SEEDS[x][y][1]
+    if S.room then
+      assert(S.room.floor_h)
+      S.floor_h = S.room.floor_h
+      S.ceil_h  = S.room.ceil_h
+    end
+  end end -- x, y
+end
 
 
 function Rooms_choose_heights()
-
---##  local function flood_connections()
---##    for _,C in ipairs(PLAN.all_conns) do
---##      if C.src_h and not C.dest_h then
---##        C.dest_h = C.src_h
---##      elseif C.dest_h and not C.src_h then
---##        C.src_h = C.dest_h
---##      end
---##    end
---##  end
 
   local function spread_hallway_height(R, seen_conns, h)
     assert(not R.floor_h)
@@ -394,7 +204,7 @@ function Rooms_choose_heights()
     elseif other_h > 128 then
       other_h = other_h - 128
     else
-      other_h = sel(rand_odds(50), 0, 256)
+      other_h = rand_sel(50, 0, 256)
     end
 
     if not A.conn_h then
@@ -408,8 +218,6 @@ function Rooms_choose_heights()
 
   local function do_special_room(R)
     assert(not R.floor_h)
-
-    -- keep it simple : flat
 
     for _,C in ipairs(R.conns) do
       if C.conn_h then
@@ -428,6 +236,157 @@ function Rooms_choose_heights()
     end
   end
 
+  local function dump_along(list, prefix)
+    for idx,C in ipairs(list) do
+      gui.debugf("%sfloor_along_path: %d/%d = %s\n", prefix,
+                 idx, #list, C.conn_h or "UNSET")
+    end
+  end
+
+  local function rand_height()
+    return 64 * (-1 + rand_index_by_probs { 5,1,6,1,4 })
+  end
+
+  local function rand_delta(h)
+    if h < 0   then return 0 end
+    if h > 256 then return 256 end
+
+    while true do
+      local d = (rand_index_by_probs { 2,1,8,1,2 } - 3) * 64
+      if (h+d) >= 0 and (h+d) <= 256 then
+        return h+d
+      end
+    end
+  end
+
+  local function find_stretch(list, min_num)
+    local S, E
+
+    for idx = 1,#list do
+      if list[idx].conn_h then
+        if S and (E - S + 1) >= min_num then
+          return S, E
+        end
+        S = nil; E = nil
+      else
+        if not S then S = idx end
+        E = idx
+      end
+    end
+
+    if S and (E - S + 1) >= min_num then
+      return S, E
+    end
+
+    return nil, nil
+  end
+
+  local function poke_stretch(path, S, E)
+    local s_h = path[S].conn_h
+    local e_h = path[E].conn_h
+
+    assert(s_h and e_h)
+
+    local diff = math.abs(s_h - e_h)
+
+    -- for large stretches of unset floors, split them
+    if (E-S+1) >= 5 then
+      local P = int((S+E)/2)
+
+      if math.min(s_h, e_h) > 160 then
+        path[P].conn_h = rand_sel(50, 64, 0)
+        return
+      end 
+
+      if math.max(s_h, e_h) < 96 then
+        path[P].conn_h = rand_sel(50, 192, 256)
+        return
+      end 
+
+      path[P].conn_h = rand_sel(50, 0, 256)
+      return
+    end
+
+    -- mitigate a big change
+    if (E-S+1) == 3 and diff > 160 then
+      path[S+1].conn_h = s_h + sel(s_h < e_h, 128, -128)
+      return
+    end
+
+    -- roughly iterpolate between S and E, in 64 chunks
+    while (E-S+1) >= 3 do
+      diff = math.abs(s_h - e_h)
+
+      local bump = 0
+      if diff >= 64 and rand_odds(75) then
+        bump = 64
+      end
+
+      if rand_odds(50) then
+        S = S + 1
+        s_h = s_h + sel(s_h < e_h, bump, -bump)
+        path[S].conn_h = s_h
+      else
+        E = E - 1
+        e_h = e_h + sel(e_h < s_h, bump, -bump)
+        path[E].conn_h = e_h
+      end
+    end
+  end
+
+  local function floor_along_path(target, path)
+    local N = #path
+    assert(N >= 1)
+
+    gui.debugf("Before:\n"); dump_along(path, "  ")
+
+    -- start
+    if not path[1].conn_h then
+      if path[2] and path[2].conn_h then
+        path[1].conn_h = rand_delta(path[2].conn_h)
+      else
+        path[1].conn_h = rand_height()
+      end
+    end
+
+    -- target
+    if not path[N].conn_h then
+      if path[N-1] and path[N-1].conn_h then
+        path[N].conn_h = rand_delta(path[N-1].conn_h)
+      else
+        path[N].conn_h = rand_height()
+      end
+    end
+
+--[[
+    repeat
+      local S, E = find_stretch(list, 5)
+      if S then
+        gui.debugf("Splitting stretch %d..%d\n", S, E)
+        path[int((S+E)/2)].conn_h = rand_height()
+      end
+    until not S
+--]]
+
+    -- fill all remaining gaps
+    repeat
+      local S, E = find_stretch(path, 1)
+      if S then
+        assert(S > 1 and E < N)
+        poke_stretch(path, S-1, E+1)
+      end
+    until not S
+
+    gui.debugf("After:\n"); dump_along(path, "> ")
+    gui.debugf("\n")
+
+  end
+
+  local function try_flood_height(R)
+    -- FIXME !!!!
+    R.floor_h = rand_height()
+    return true
+  end
 
   ---| Rooms_choose_heights |---
   
@@ -492,37 +451,43 @@ function Rooms_choose_heights()
     end
   end
 
+  -- MAIN ALGORITHM
+  -- decide heights along the main path in each area.
+  -- we want to avoid up/down/up/down silliness.
+  for _,A in ipairs(PLAN.all_arenas) do
+    floor_along_path(A.target, A.path)
+  end
+
+
+--[[
   -- handle start and exit rooms
   -- (this ensures that at least two rooms have heights which
   --  can flood into the rest of the map)
   -- FIXME for teleporters [a room per separate area]
   do_special_room(PLAN.start_room)
   do_special_room(PLAN.exit_room)
+--]]
 
-  -- final phase: flood heights into unset rooms
-  -- We visit rooms in order of sizes (smallest first) to
-  -- ensure that small rooms don't have too many different
-  -- heights.
+
+  -- final phase: flood heights into all remaining rooms
 
   for loop = 1,99 do
-    local rooms = {}
+    local changed = false
 
     for _,R in ipairs(PLAN.all_rooms) do
       if not R.floor_h then
-        table.insert(rooms, R)
+        if try_flood_height(R) then
+          changed = true
+        end
       end
     end
 
-    if #rooms == 0 then break; end
-
-    table.sort(rooms, function(A, B) return A.lvol < B.lvol end)
-
-    for _,R in ipairs(rooms) do
-      -- !!!!!! FIXME
-      R.floor_h = 0
+    if not changed then
+      break;
     end
   end 
 
+  -- TEMP CRUD
   for _,R in ipairs(PLAN.all_rooms) do
     if not R.ceil_h then
       R.ceil_h = R.floor_h + (R.tallness or 128)
