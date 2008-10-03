5ce7dbf23c561d44a66e20b0704fee9e9ce1a708
Andrew Apted <ajapted@users.sf.net>
2008-10-03 12:16:13 +0000

    Version bump to 3.22, in honor of better quests.lua code, and splitting a large chunk of the plan_sp.lua code into a new file: connect.lua

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.21-WIP"
-#define OBLIGE_HEX_VER  0x321
+#define OBLIGE_VERSION  "3.22-WIP"
+#define OBLIGE_HEX_VER  0x322
 
 extern const char *install_path;
 
