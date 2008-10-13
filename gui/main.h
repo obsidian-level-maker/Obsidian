2b0b7676af4b6f83850fd1f7d68ac76d7fec182e
Andrew Apted <ajapted@users.sf.net>
2008-10-13 08:56:32 +0000

    Version bump to 3.25, in honor of new Fight Simulator code (fight.lua), and major updates to each game's WEAPONS and MONSTERS tables to get the correct parameters.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.24-WIP"
-#define OBLIGE_HEX_VER  0x324
+#define OBLIGE_VERSION  "3.25-WIP"
+#define OBLIGE_HEX_VER  0x325
 
 extern const char *install_path;
 
