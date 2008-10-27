820ee400c735794b7cd48a3b78ae3bc2dadc6846
Andrew Apted <ajapted@users.sf.net>
2008-10-27 11:20:33 +0000

    Added new (empty) files for sky generation: tx_skies.cc/h

---
+++ gui/tx_skies.h
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------
+//  SKY GENERATION
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker (C) 2008 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __OBLIGE_TX_SKIES_H__
+#define __OBLIGE_TX_SKIES_H__
+
+// TODO
+
+#endif /* __OBLIGE_TX_SKIES_H__ */
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
