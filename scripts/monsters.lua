de236a1e2ff5ca127ae477c3bfda7d02a293a5fb
Andrew Apted <ajapted@users.sf.net>
2008-10-29 11:47:47 +0000

    Added empty 'monsters.lua' file, with a few notes about palettes.

---
+++ scripts/monsters.lua
@@ -0,0 +1,52 @@
+----------------------------------------------------------------
+--  MONSTERS/HEALTH/AMMO
+----------------------------------------------------------------
+--
+--  Oblige Level Maker (C) 2008 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+
+
+MONSTER SELECTION
+-----------------
+
+Main usages:
+(a) free-range
+(b) guarding something [keys]
+(c) cages
+(d) traps (triggered closets, teleport in)
+(e) surprises (behind entry door, closests on back path)
+
+Free range monsters make up the bulk of the level, and are
+subject to the palette.  The palette applies to a fair size
+of a map, on "small" setting --> 1 palette only, upto 2 on
+"regular", between 2-3 on "large" maps.
+
+[Palette probably needs to handle "families", e.g. Baron
+and Hellknight, Mummy and Leader, ]
+
+Trap and Surprise monsters can use any monster (actually
+better when different from palette and different from
+previous traps/surprises).
+
+Cages and Guarding monsters should have a smaller and
+longer-term palette, changing about 4 times less often
+than the free-range palette.  MORE PRECISELY: palette
+evolves about same rate IN TERMS OF # MONSTERS ADDED.
+
+--------------------------------------------------------------]]
+
+require 'defs'
+require 'util'
+
