05252f85192bc267aea97e07a03f75ad56c53cf4
Andrew Apted <ajapted@users.sf.net>
2008-11-14 10:26:09 +0000

    Version bump to 3.28, in honor of new Name Generator code (for level names) which is working a treat, and supports three distinct themes: TECH, URBAN and GOTHIC/HELL.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.27-WIP"
-#define OBLIGE_HEX_VER  0x327
+#define OBLIGE_VERSION  "3.28-WIP"
+#define OBLIGE_HEX_VER  0x328
 
 extern const char *install_path;
 
