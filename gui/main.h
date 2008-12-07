9d1f0f1bc4035e46f611d6ae60b3516238e9fb4e
Andrew Apted <ajapted@users.sf.net>
2008-12-07 03:53:59 +0000

    Version bump to 3.30, in honor of much faster CSG code.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.29-WIP"
-#define OBLIGE_HEX_VER  0x329
+#define OBLIGE_VERSION  "3.30-WIP"
+#define OBLIGE_HEX_VER  0x330
 
 extern const char *install_path;
 
