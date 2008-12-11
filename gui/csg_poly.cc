2f4ee0796158a9442207c3e4cf1b82758e77bba6
Andrew Apted <ajapted@users.sf.net>
2008-12-11 03:23:13 +0000

    Tweaked 'along_tries' table.

--- gui/csg_poly.cc
+++ gui/csg_poly.cc
@@ -791,11 +791,11 @@ static merge_region_c *FindIslandParent(merge_region_c *R)
   // need to try a different 'along' value.
   static const double along_tries[] =
   {
-    0.5, 0.7, 0.3, 0.6, 0,4,
-    0,8, 0.2, 0.9, 0.1,
-
-    0.55, 0.45, 0.65, 0.35, 0.75, 0.25,
-    0.85, 0.15, 0.95, 0.05,
+         0.5, 0.55, 0.45,
+    0.6, 0.4, 0.65, 0.35,
+    0.7, 0.3, 0.75, 0.25,
+    0.8, 0.2, 0.85, 0.15,
+    0.9, 0.1, 0.95, 0.05,
 
     -1 // THE END
   };
