9ca55fb391f7d6fd0d1ba0b4616d1b62e2095bf6
Andrew Apted <ajapted@users.sf.net>
2009-03-08 04:42:29 +0000

    Version bump to 3.35, in honor of working Windows and Pictures, Ceiling features, and reworked Monster/Weapon tables of each game.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.34-WIP"
-#define OBLIGE_HEX_VER  0x334
+#define OBLIGE_VERSION  "3.35-WIP"
+#define OBLIGE_HEX_VER  0x335
 
 extern const char *install_path;
 
