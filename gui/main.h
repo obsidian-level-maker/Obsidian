dc873b4a67df8b47c1bbdc5283af3955cfb8b147
Andrew Apted <ajapted@users.sf.net>
2009-03-18 06:36:14 +0000

    Bumn version to 3.37, in honor of working Pickups.

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.36-WIP"
-#define OBLIGE_HEX_VER  0x336
+#define OBLIGE_VERSION  "3.37-WIP"
+#define OBLIGE_HEX_VER  0x337
 
 extern const char *install_path;
 
