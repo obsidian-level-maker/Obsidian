77e2d1b575b325f372ec9c3fe2b543c1d4e7d93e
Andrew Apted <ajapted@users.sf.net>
2009-04-10 08:25:49 +0000

    More MAP30 work, added cyberdemon platforms etc...

--- scripts/arenas.lua
+++ scripts/arenas.lua
@@ -24,17 +24,17 @@ function Arena_add_players(x, y, z, angle)
   local dist = 40
 
   gui.add_entity(tostring(GAME.things["player1"].id),
-                 x, y, z+35, { angle=angle })
+                 x, y, z, { angle=angle })
 
   if GAME.things["player2"] then
     gui.add_entity(tostring(GAME.things["player2"].id),
-                   x - dist, y, z + 35, { angle=angle })
+                   x - dist, y, z, { angle=angle })
 
     gui.add_entity(tostring(GAME.things["player3"].id),
-                   x + dist, y, z + 35, { angle=angle })
+                   x + dist, y, z, { angle=angle })
 
     gui.add_entity(tostring(GAME.things["player4"].id),
-                   x, y - dist, z + 35, { angle=angle })
+                   x, y - dist, z, { angle=angle })
   end
 end
 
@@ -43,6 +43,10 @@ function Arena_Doom_MAP30()
 
   local face_x = 1024 + 128
   local face_y = 4096
+
+  local room_y  = face_y - 1600
+  local room_x1 = face_x - 768
+  local room_x2 = face_x + 768
 
   local function make_face()
 
@@ -111,7 +115,7 @@ function Arena_Doom_MAP30()
     transformed_brush(nil, info, rect_coords(x1,y1, x2,y2), z2, z2+32)
 
     gui.add_entity(tostring(GAME.things["brain_shooter"].id),
-                   face_x, y1+24, z1+35, { angle=270 })
+                   face_x, y1+24, z1, { angle=270 })
 
     z1 = z1 - 96
 
@@ -127,15 +131,15 @@ function Arena_Doom_MAP30()
     transformed_brush(nil, info, rect_coords(x1,y1, x2,y2), z2, z2+32)
 
     gui.add_entity(tostring(GAME.things["brain_boss"].id),
-                   face_x, y2-48, z1+35, { angle=270 })
+                   face_x, y2-48, z1, { angle=270 })
 
   end
 
   local function make_room()
-    local x1 = face_x - 384 * 2
-    local x2 = face_x + 384 * 2
+    local x1 = room_x1
+    local x2 = room_x2
 
-    local y1 = face_y - 2048
+    local y1 = room_y
     local y2 = face_y
 
     local z1 = 0
@@ -166,10 +170,66 @@ function Arena_Doom_MAP30()
     transformed_brush(nil, info, rect_coords(x1,y1, x2,y2), -EXTREME_H, z1)
     transformed_brush(nil, info, rect_coords(x1,y1, x2,y2), z2, EXTREME_H)
 
+    transformed_brush(nil, info, rect_coords(x1,y1, x2,y1+600), -EXTREME_H, 128)
 
-    transformed_brush(nil, info, rect_coords(x1,y1, x2,y1+512), -EXTREME_H, 240)
+    for i=-1,1 do
+      local hx = face_x + i * 256
+      local hy = face_y - 96
+      local item = sel(i == 0, "blue_armor", "mega")
 
-    Arena_add_players(face_x, y1 + 100, z1, 90)
+      gui.add_entity(tostring(GAME.things[item].id), hx, hy, 0)
+    end
+  end
+
+  local function make_cybers()
+    for i = 1,4 do
+      local x1 = sel((i % 2) == 1, room_x1, room_x2-352)
+      local x2 = sel((i % 2) == 1, room_x1+352, room_x2)
+
+      local y1 = sel(i <= 2, face_y-200, room_y)
+      local y2 = sel(i <= 2, face_y, room_y+200)
+      local y3 = sel(i <= 2, y1 - 64, y2 + 64)
+
+      local z = sel(i <= 2, 0, 128)
+
+      local mx = sel((i % 2) == 1, x1+96, x2-96)
+      local my = (y1 + y2) / 2
+
+      local angle = sel((i % 2) == 1, 180, 0)
+
+      local info =
+      {
+        t_face = { texture="FLOOR4_8" },
+        b_face = { texture="FLOOR4_8" },
+        w_face = { texture="METAL1" },
+      }
+
+      local step =
+      {
+        t_face = { texture="FLOOR5_2" },
+        b_face = { texture="FLOOR5_2" },
+        w_face = { texture="BROWN1", y_offset=0, peg=true },
+      }
+      
+      transformed_brush(nil, info, rect_coords(x1,y1, x2,y2), -EXTREME_H, z + 32)
+
+      transformed_brush(nil, step, rect_coords(x1,y3-32, x2,y3+32), -EXTREME_H, z + 16)
+
+      gui.add_entity(tostring(GAME.things["Cyberdemon"].id),
+                     mx, my, z, { angle=angle })
+
+      gui.add_entity(tostring(GAME.things["launch"].id),
+                     mx, my, z, { angle=angle })
+
+      for side = 2,8,2 do
+        local rx, ry = nudge_coord(mx, my, side, 40)
+        gui.add_entity(tostring(GAME.things["rockets"].id),
+                       rx, ry, z, { angle=angle })
+      end
+    end
+  end
+
+  local function make_platform()
   end
 
 
@@ -178,5 +238,8 @@ function Arena_Doom_MAP30()
   make_face()
   make_hole()
   make_room()
+  make_cybers()
+
+  Arena_add_players(face_x, face_y - 512, 0, 90)
 end
 
