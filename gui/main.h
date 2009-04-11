9185289907f5e75928053a0bb613f58f4f3fa77a
Andrew Apted <ajapted@users.sf.net>
2009-04-11 09:50:37 +0000

    Version bump to 3.39, public release time!

--- gui/main.h
+++ gui/main.h
@@ -21,8 +21,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.38-WIP"
-#define OBLIGE_HEX_VER  0x338
+#define OBLIGE_VERSION  "3.39-WIP"
+#define OBLIGE_HEX_VER  0x339
 
 extern const char *install_path;
 
