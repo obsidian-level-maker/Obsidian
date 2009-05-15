37e71581fec0b2711555d89165b181da2e92bac3
Andrew Apted <ajapted@users.sf.net>
2009-05-15 10:45:26 +0000

    Skygen: added lots of new color schemes, courtesy JohnnyRancid.

--- mods/sky_gen.lua
+++ mods/sky_gen.lua
@@ -25,89 +25,160 @@ function Doom_generate_skies()
     { patch="RSKY3" },
   }
 
+  -- star colors --
+
   local STAR_COLS =
   {
     8, 7, 6, 5,
     111, 109, 107, 104, 101,
-    98, 95, 91, 87, 83,
-    4
+    98, 95, 91, 87, 83, 4
   }
+
+  -- cloud colors --
 
   local GREY_CLOUDS =
   {
-    106,104,102,100,
-    98,96,94,92,90,
-    88,86,84,82,80
+    106, 104, 102, 100,
+    98, 96, 94, 92, 90,
+    88, 86, 84, 82, 80
   }
 
   local DARK_CLOUDS =
   {
-    7,6,5,
-    110,109,108,107,106,
-    105,104,103,102,101
+    7, 6, 5,
+    110, 109, 108, 107, 106,
+    105, 104, 103, 102, 101
   }
 
   local BLUE_CLOUDS =
   {
-    245,245,244,244,243,242,241,
-    240,206,205,204,204,203,203
+    245, 245, 244, 244, 243, 242, 241,
+    240, 206, 205, 204, 204, 203, 203
   }
 
   local HELL_CLOUDS =
   {
-    188,185,184,183,182,181,180,
-    179,178,177,176,175,174,173
+    188, 185, 184, 183, 182, 181, 180,
+    179, 178, 177, 176, 175, 174, 173
   }
 
   local ORANGE_CLOUDS =
   {
-    234, 232,
-    222, 220, 218, 216, 214, 211,
+    234, 232, 222, 220, 218, 216, 214, 211
   }
 
-
-  local BROWN_HILLS =
+  local HELLISH_CLOUDS =
   {
-    0, 2, 1,
-    79, 77, 75, 73, 70, 67, 64,
+    0, 0, 0, 0, 0, 47, 191, 190, 191, 47, 0, 0
   }
 
-  local TAN_HILLS =
+  local BROWN_CLOUDS =
   {
-    239, 237,
-    143, 140, 136, 132, 128,
+     2, 1,
+     79, 78, 77, 76, 75, 74, 73,
+     72, 71, 70, 69, 67, 66, 65
   }
 
-  local GREEN_HILLS =
+  local BROWNISH_CLOUDS =
   {
-    0, 7,
-    127, 126, 125, 124, 123, 122, 120, 118, 116, 113
+    239, 238, 237, 236, 143, 142, 141,
+    140, 139, 138, 137, 136, 135, 134,
+    133, 130, 129, 128
   }
 
-  local HELL_HILLS =
+  local YELLOW_CLOUDS =
   {
-    0, 6,
-    47, 45, 43, 41, 39, 37, 35, 33
+    167, 166, 165, 164, 163, 162,
+    161, 160, 228, 227, 225
   }
+
+  local GREEN_CLOUDS =
+  {
+    127, 126, 125, 124, 123, 122, 121,
+    120, 119, 118, 117, 115, 113, 112
+  }
+
+  local JADE_CLOUDS =
+  {
+    12, 11, 10, 9,
+    159, 158, 157, 156, 155, 154, 153, 152
+  }
+
+  local DARKRED_CLOUDS =
+  {
+     47, 46, 45, 44, 43, 42, 41, 40, 39, 37, 36, 34
+  }
+
+  local PEACH_CLOUDS =
+  {
+     68, 66, 64, 62, 60, 58, 57
+  }
+
+  local WHITE_CLOUDS =
+  {
+     99, 98, 97, 96, 95, 94, 93,
+     92, 91, 90, 89, 88, 87, 86,
+     85, 84, 83, 81
+  }
+
+  -- hill colors --
 
   local BLACK_HILLS =
   {
     0, 0, 0
   }
 
+  local BROWN_HILLS =
+  {
+    0, 2, 1, 79, 77, 75, 73, 70, 67, 64
+  }
 
-  local back_gs = { "stars", GREY_CLOUDS, DARK_CLOUDS,
-                    BLUE_CLOUDS, HELL_CLOUDS, ORANGE_CLOUDS }
+  local TAN_HILLS =
+  {
+    239, 237, 143, 140, 136, 132, 128
+  }
 
-  local fore_gs = { "none",  "none", TAN_HILLS, BROWN_HILLS,
-                    GREEN_HILLS, HELL_HILLS }
+  local GREEN_HILLS =
+  {
+    0, 7,
+    127, 126, 125, 124, 123,
+    122, 120, 118, 116, 113
+  }
+
+  local HELL_HILLS =
+  {
+    0, 6, 47, 45, 43, 41, 39, 37, 35, 33
+  }
+
+  local DARKBROWN_HILLS =
+  {
+    8, 7, 2, 1, 79, 78, 77, 76, 75
+  }
+
+  local GREENISH_HILLS =
+  {
+    0, 7, 12, 11, 10, 9, 15, 14, 13,
+    159, 158, 157, 156, 155, 154
+  }
+
+
+  local back_gs = { "stars", "stars",
+                    GREY_CLOUDS, DARK_CLOUDS, BLUE_CLOUDS,
+                    RED_CLOUDS, ORANGE_CLOUDS, HELLISH_CLOUDS,
+                    BROWN_CLOUDS, BROWNISH_CLOUDS, YELLOW_CLOUDS,
+                    GREEN_CLOUDS, JADE_CLOUDS, DARKRED_CLOUDS,
+                    PEACH_CLOUDS, WHITE_CLOUDS,
+                  }
+
+  local fore_gs = { "none",  "none", "none",
+                    TAN_HILLS, BROWN_HILLS, GREEN_HILLS,
+                    HELL_HILLS, DARKBROWN_HILLS, GREENISH_HILLS,
+                    BLACK_HILLS,
+                  }
 
   rand_shuffle(back_gs)
   rand_shuffle(fore_gs)
 
-  if rand_odds(10) then
-    table.insert(fore_gs, rand_irange(1,3), BLACK_HILLS)
-  end
 
   for num,sky in ipairs(sky_list) do
     gui.fsky_create(256, 128, 0)
