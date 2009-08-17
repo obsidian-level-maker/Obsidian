dfc8376210a61b1c8fb8826391ea83f0f23e0a74
Andrew Apted <ajapted@users.sf.net>
2009-08-17 09:59:44 +0000

    Monster Control module: fleshed out the setup() logic.

--- mods/mon_control.lua
+++ mods/mon_control.lua
@@ -18,13 +18,13 @@
 
 MON_CONTROL_CHOICES =
 {
+  "normal", "Normal",
   "scarce", "Scarce",
   "less",   "Less",
-  "normal", "Normal",
   "more",   "More",
   "heaps",  "Heaps",
   "swarms", "Swarms",
-  "insane", "INSANE",
+  "toomany", "TOO MANY",
 }
 
 MON_CONTROL_AMOUNTS =
@@ -35,18 +35,35 @@ MON_CONTROL_AMOUNTS =
   more   = 3,
   heaps  = 15,
   swarms = 60,
-  insane = 600,
+  toomany = 600,
 }
 
 
 function MonControl_setup(self)
 
-  for name,opt = pairs(self.options) do
+  local function adjust(M, factor, what)
+    if M[what] then
+      M[what] = M[what] * factor
+    end
+  end
+
+  for name,opt in pairs(self.options) do
     local M = GAME.monsters[name]
     local factor = MON_CONTROL_AMOUNTS[opt.value]
 
     if M and factor then
-      if not M.prob then M.prob = M.prob 
+      if not M.prob then M.prob = 4 end
+
+      adjust(M, factor, "prob")
+      adjust(M, factor, "cage_prob")
+      adjust(M, factor, "trap_prob")
+      adjust(M, factor, "guard_prob")
+      adjust(M, factor, "crazy_prob")
+
+      if factor > 5 then
+        M.density = 1.0
+        M.always_use = true
+      end
     end
   end -- for opt
 
@@ -84,5 +101,6 @@ OB_MODULES["mon_control"] =
 
     Cyberdemon = { label="Cyberdemon", priority=12, choices=MON_CONTROL_CHOICES },
     Mastermind = { label="Mastermind", priority=11, choices=MON_CONTROL_CHOICES },
+  },
 }
 
