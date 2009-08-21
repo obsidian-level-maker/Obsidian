10d46938155b67263baf9b2c71eed1fd52f2eacb
Andrew Apted <ajapted@users.sf.net>
2009-08-21 12:36:47 +0000

    Vis Viewer: reworked the UI_Canvas code (just the bare essentials).

--- vis_viewer/main.cc
+++ vis_viewer/main.cc
@@ -82,325 +82,79 @@ private:
 	int loc_x, loc_y;
 
 public:
-	UI_Canvas(int X, int Y, int W, int H, const char *label = NULL);
-	virtual ~UI_Canvas();
+	UI_Canvas(int X, int Y, int W, int H, const char *label = NULL) :
+		Fl_Widget(X, Y, W, H, label),
+		loc_x(-1), loc_y(-1)
+	{ }
 
-public:
-	// FLTK virtual method for handling input events.
-	int handle(int event);
-
-	// FLTK virtual method for resizing.
-	void resize(int X, int Y, int W, int H);
-
-	void DrawEverything();
+	virtual ~UI_Canvas()
+	{ }
 
 private:
 	// FLTK virtual method for drawing.
-	void draw();
-
-};
-
-
-UI_Canvas::UI_Canvas(int X, int Y, int W, int H, const char *label) : 
-    Fl_Widget(X, Y, W, H, label),
-    e(NULL), render3d(false),
-	highlight(), selbox_active(false)
-{ }
-
-UI_Canvas::~UI_Canvas()
-{ }
-
-
-void UI_Canvas::resize(int X, int Y, int W, int H)
-{
-	Fl_Widget::resize(X, Y, W, H);
-}
-
-
-void UI_Canvas::draw()
-{
-	fl_push_clip(x(), y(), w(), h());
-
-	fl_color(FL_WHITE);
-	fl_font(FL_COURIER, 14);
-
-	if (render3d)
-		Render3D_Draw(x(), y());
-	else if (e)
-		DrawEverything();
-
-	fl_pop_clip();
-}
-
-
-int UI_Canvas::handle(int event)
-{
-	//// fprintf(stderr, "HANDLE EVENT %d\n", event);
-
-	switch (event)
+	void draw()
 	{
-		case FL_FOCUS:
-			return 1;
+		fl_push_clip(x(), y(), w(), h());
 
-		case FL_KEYDOWN:
-		case FL_SHORTCUT:
+		fl_color(FL_BLACK);
+		fl_rectf(x(), y(), w(), h());
+
+		// ....
+
+		fl_pop_clip();
+	}
+
+	void DrawEverything();
+
+public:
+	// FLTK virtual method for resizing.
+	void resize(int X, int Y, int W, int H)
+	{
+		Fl_Widget::resize(X, Y, W, H);
+	}
+
+	// FLTK virtual method for handling input events.
+	int handle(int event)
+	{
+		switch (event)
 		{
-			//      int result = handle_key();
-			//      handle_mouse();
+			case FL_FOCUS:
+				return 1;
 
-			int key   = Fl::event_key();
-			int state = Fl::event_state();
+			case FL_ENTER:
+				// we greedily grab the focus
+				if (Fl::focus() != this)
+					take_focus(); 
+				return 1;
 
-			switch (key)
-			{
-				case FL_Num_Lock:
-				case FL_Caps_Lock:
-
-				case FL_Shift_L: case FL_Control_L:
-				case FL_Shift_R: case FL_Control_R:
-				case FL_Meta_L:  case FL_Alt_L:
-				case FL_Meta_R:  case FL_Alt_R:
-
-					/* IGNORE */
-					return 1;
-
-				default:
-					/* OK */
-					break;
-			}
-
-			if (key == FL_Tab || key == '\t')
-			{
-				render3d = !render3d;
+			case FL_LEAVE:
 				redraw();
 				return 1;
-			}
 
-			if (key != 0)
-			{
-				if (key < 127 && isalpha(key) && (state & FL_SHIFT))
-					key = toupper(key);
+			case FL_KEYDOWN:
+			case FL_SHORTCUT:
+				// FIXME
+				// HandleKey(Fl::event_key());
+				return 1;
 
-				if (render3d)
-				{
-					if (Render3D_Key(key))
-						redraw();
-				}
-				else
-					EditorKey(key, (state & FL_SHIFT) ? true : false);
-			}
+			case FL_MOVE:
+			case FL_DRAG:
+				// FIXME : change current square
+				// EditorMouseMotion(Fl::event_x(), Fl::event_y(),
+				//		MAPX(Fl::event_x()), MAPY(Fl::event_y()), event == FL_DRAG);
+				return 1;
 
-			return 1; // result;
+			case FL_PUSH:
+			case FL_RELEASE:
+				return 1;
+
+			default:
+				break;
 		}
 
-		case FL_ENTER:
-			// we greedily grab the focus
-			if (Fl::focus() != this)
-				take_focus(); 
-
-			return 1;
-
-		case FL_LEAVE:
-			//      hilite_rad = NULL;
-			//      hilite_thing = NULL;
-			//      dragging = false;
-			//      determine_cursor();
-			//      update_active_obj();
-			redraw();
-			return 1;
-
-		case FL_MOVE:
-		case FL_DRAG:
-			EditorMouseMotion(Fl::event_x(), Fl::event_y(),
-					MAPX(Fl::event_x()), MAPY(Fl::event_y()), event == FL_DRAG);
-			return 1;
-
-		case FL_PUSH:
-			{
-				int state = Fl::event_state();
-				EditorMousePress((state & FL_CTRL) ? true : false);
-			}
-			return 1;
-
-		case FL_RELEASE:
-			EditorMouseRelease();
-			return 1;
-
-		case FL_MOUSEWHEEL:
-			EditorWheel(0 - Fl::event_dy());
-			return 1;
-
-		default:
-			break;
+		return 0;  // unused
 	}
-
-	return 0;  // unused
-}
-
-
-void UI_Canvas::DrawEverything()
-{
-	map_lx = MAPX(x());
-	map_ly = MAPY(y()+h());
-	map_hx = MAPX(x()+w());
-	map_hy = MAPY(y());
-
-	DrawMap(); 
-
-	HighlightSelection(edit.Selected); // FIXME should be widgetized
-
-	if (highlight())
-		HighlightObject(highlight.type, highlight.num, YELLOW);
-
-	if (selbox_active)
-		SelboxDraw();
-}
-
-
-/*
-  draw the actual game map
-*/
-void UI_Canvas::DrawMap()
-{
-	fl_color(FL_BLACK);
-	fl_rectf(x(), y(), w(), h());
-
-	// draw the grid first since it's in the background
-	if (grid.shown)
-		DrawGrid();
-
-	if (e->obj_type != OBJ_THINGS)
-		DrawThings();
-
-	DrawLinedefs();
-
-	if (e->obj_type == OBJ_VERTICES)
-		DrawVertices();
-
-	if (e->obj_type == OBJ_THINGS)
-		DrawThings();
-
-	if (e->obj_type == OBJ_RADTRIGS)
-		DrawRTS();
-
-
-	int n;
-
-	// Draw the things numbers
-	if (e->obj_type == OBJ_THINGS && e->show_object_numbers)
-	{
-		for (n = 0; n < NumThings; n++)
-		{
-			int x = Things[n]->x;
-			int y = Things[n]->y;
-
-			if (Vis(x, y, MAX_RADIUS))
-				DrawObjNum(SCREENX(x) + FONTW, SCREENY(y) + 2, n, THING_NO);
-		}
-	}
-
-	// Draw the sector numbers
-	if (e->obj_type == OBJ_SECTORS && e->show_object_numbers)
-	{
-		int xoffset = - FONTW / 2;
-
-		for (n = 0; n < NumSectors; n++)
-		{
-			int x;
-			int y;
-
-			centre_of_sector(n, &x, &y);
-
-			if (Vis(x, y, MAX_RADIUS))
-				DrawObjNum(SCREENX(x) + xoffset, SCREENY(y) - FONTH / 2, n, SECTOR_NO);
-
-			if (n == 10 || n == 100 || n == 1000 || n == 10000)
-				xoffset -= FONTW / 2;
-		}
-	}
-}
-
-
-
-/*
- *  draw_grid - draw the grid in the background of the edit window
- */
-void UI_Canvas::DrawGrid()
-{
-	int grid_step_1 = 1 * grid.step;    // Map units between dots
-	int grid_step_2 = 8 * grid_step_1;  // Map units between dim lines
-	int grid_step_3 = 8 * grid_step_2;  // Map units between bright lines
-
-	float pixels_1 = grid.step * grid.Scale;
-
-
-	if (pixels_1 < 1.99)
-	{
-		fl_color(GRID_DARK);
-		fl_rectf(x(), y(), w(), h());
-		return;
-	}
-
-
-	fl_color (GRID_BRIGHT);
-	{
-		int gx = (map_lx / grid_step_3) * grid_step_3;
-
-		for (; gx <= map_hx; gx += grid_step_3)
-			DrawMapLine(gx, map_ly-2, gx, map_hy+2);
-
-		int gy = (map_ly / grid_step_3) * grid_step_3;
-
-		for (; gy <=  map_hy; gy += grid_step_3)
-			DrawMapLine(map_lx, gy, map_hx, gy);
-	}
-
-
-	fl_color (GRID_MEDIUM);
-	{
-		int gx = (map_lx / grid_step_2) * grid_step_2;
-
-		for (; gx <= map_hx; gx += grid_step_2)
-			if (gx % grid_step_3 != 0)
-				DrawMapLine(gx, map_ly, gx, map_hy);
-
-		int gy = (map_ly / grid_step_2) * grid_step_2;
-
-		for (; gy <= map_hy; gy += grid_step_2)
-			if (gy % grid_step_3 != 0)
-				DrawMapLine(map_lx, gy, map_hx, gy);
-	}
-
-
-	// POINTS
-
-	if (pixels_1 < 3.99)
-		fl_color(GRID_MEDIUM);
-	//??  else if (pixels_1 > 30.88)
-	//??    fl_color(GRID_BRIGHT);
-	else
-		fl_color(GRID_POINT);
-
-	{
-		int gx = (map_lx / grid_step_1) * grid_step_1;
-		int gy = (map_ly / grid_step_1) * grid_step_1;
-
-		for (int ny = gy; ny <= map_hy; ny += grid_step_1)
-		for (int nx = gx; nx <= map_hx; nx += grid_step_1)
-		{
-			int sx = SCREENX(nx);
-			int sy = SCREENY(ny);
-
-			if (pixels_1 < 30.99)
-				fl_point(sx, sy);
-			else
-			{
-				fl_line(sx-0, sy, sx+1, sy);
-				fl_line(sx, sy-0, sx, sy+1);
-			}
-		}
-	}
-}
+};
 
 
 //--------------------------------------------------------------------
@@ -488,11 +242,6 @@ int main(int argc, char **argv)
 		Fl::scheme("plastic");
 
 	fl_message_font(FL_HELVETICA /* _BOLD */, 18);
-
-	// load icons for file chooser
-#ifndef WIN32
-	Fl_File_Icon::load_system_icons();
-#endif
 
 	try
 	{
