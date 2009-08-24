522147531a84aeed12c8281793eb6666a4c68a16
Andrew Apted <ajapted@users.sf.net>
2009-08-24 07:07:40 +0000

    Level Control module: tweakage and simplification.

--- mods/level_control.lua
+++ mods/level_control.lua
@@ -33,26 +33,12 @@ LEVEL_CONTROL_CHOICES_B =
   "heaps",  "Heaps",
 }
 
-LEVEL_CONTROL_CHOICES_C =
-{
-  "mixed",  "Mix It Up",
-  "few",    "Few",
-  "heaps",  "Heaps",
-}
-
 LEVEL_CONTROL_CHOICES_D =
 {
   "mixed",  "Mix It Up",
   "none",   "NONE",
   "few",    "Few",
   "some",   "Some",
-}
-
-LEVEL_CONTROL_CHOICES_E =
-{
-  "mixed",  "Mix It Up",
-  "none",   "NONE",
-  "heaps",  "Heaps",
 }
 
 LEVEL_CONTROL_CHOICES_F =
@@ -89,24 +75,15 @@ OB_MODULES["level_control"] =
 {
   label = "Level Control",
 
---??  for_games = { doom1=1, doom2=1, freedoom=1 },
---??  for_modes = { sp=1, coop=1 },
-
   begin_level_func = Level_Control_begin_level,
-
-  -- Might want to rename some of the labels below to be clearer on what they are
-  -- Priority was based on the order they are in levels.lua, probably change that too
 
   options =
   {
---  skies      = { label="Skies",    priority = 22, choices=LEVEL_CONTROL_CHOICES_A },
     hallways   = { label="Hallways", priority = 21, choices=LEVEL_CONTROL_CHOICES_A },
     liquids    = { label="Liquids",  priority = 20, choices=LEVEL_CONTROL_CHOICES_A },
-    scenics    = { label="Scenics",  priority = 19, choices=LEVEL_CONTROL_CHOICES_A },
-    subrooms   = { label="Subrooms", priority = 18, choices=LEVEL_CONTROL_CHOICES_B },
-    islands    = { label="Islands",  priority = 17, choices=LEVEL_CONTROL_CHOICES_C },
+    subrooms   = { label="Sub-Rooms",priority = 19, choices=LEVEL_CONTROL_CHOICES_B },
+    scenics    = { label="Scenics",  priority = 18, choices=LEVEL_CONTROL_CHOICES_A },
 
---  junk       = { label="Junk",     priority = 16, choices=LEVEL_CONTROL_CHOICES_A },
     symmetry   = { label="Symmetry", priority = 15, choices=LEVEL_CONTROL_CHOICES_A },
     pillars    = { label="Pillars",  priority = 14, choices=LEVEL_CONTROL_CHOICES_A },
     beams      = { label="Beams",    priority = 13, choices=LEVEL_CONTROL_CHOICES_A },
@@ -117,10 +94,6 @@ OB_MODULES["level_control"] =
     cages      = { label="Cages",    priority =  9, choices=LEVEL_CONTROL_CHOICES_B },
     fences     = { label="Fences",   priority =  8, choices=LEVEL_CONTROL_CHOICES_D },
     crates     = { label="Crates",   priority =  7, choices=LEVEL_CONTROL_CHOICES_B },
-
---  lt_trim    = { label="Lt_Trim",    priority = 6, choices=LEVEL_CONTROL_CHOICES_B },
---  lt_spokes  = { label="Lt_Spokes",  priority = 5, choices=LEVEL_CONTROL_CHOICES_B },
---  lt_swapped = { label="Lt_Swapped", priority = 4, choices=LEVEL_CONTROL_CHOICES_E },
 
     favor_shape = { label ="Favor Shape", priority = 3, choices=LEVEL_CONTROL_CHOICES_F },
     dm_liquid   = { label ="Liquid Type", priority = 2, choices=LEVEL_CONTROL_CHOICES_G },
