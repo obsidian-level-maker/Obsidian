f0d15cf222008a7ff594eaaf11fd66a4d458d445
Andrew Apted <ajapted@users.sf.net>
2009-08-30 09:57:30 +0000

    Modules: merged DOOM Control (mon + weap) into a single file.

--- mods/doom_control.lua
+++ mods/doom_control.lua
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------
---  MODULE: Monster Control
+--  MODULE: Doom Control
 ----------------------------------------------------------------
 --
 --  Copyright (C) 2009 Andrew Apted
@@ -92,6 +92,78 @@ OB_MODULES["mon_control"] =
 
     Cyberdemon = { label="Cyberdemon",   choices=MON_CONTROL_CHOICES },
     Mastermind = { label="Mastermind",   choices=MON_CONTROL_CHOICES },
+  },
+}
+
+
+----------------------------------------------------------------
+
+
+WEAP_CONTROL_CHOICES =
+{
+  "default", "DEFAULT",
+  "none",    "None at all",
+  "scarce",  "Scarce",
+  "less",    "Less",
+  "plenty",  "Plenty",
+  "more",    "More",
+  "heaps",   "Heaps",
+  "loveit",  "I LOVE IT",
+}
+
+WEAP_CONTROL_PROBS =
+{
+  none   = 0,
+  scarce = 2,
+  less   = 15,
+  plenty = 50,
+  more   = 120,
+  heaps  = 300,
+  loveit = 1000,
+}
+
+
+function Weap_Control_setup(self)
+  for name,opt in pairs(self.options) do
+    local W = GAME.weapons[name]
+
+    if W and opt.value ~= "default" then
+      local prob = WEAP_CONTROL_PROBS[opt.value]
+
+      W.start_prob = prob
+      W.add_prob   = prob
+
+      -- adjust usage preference as well
+      if W.pref and prob > 0 then
+        W.pref = W.pref * ((prob / 50) ^ 0.6)
+      end
+
+      -- allow it to appear as often as the user wants
+      W.rarity = nil
+    end
+  end -- for opt
+end
+
+
+OB_MODULES["weap_control"] =
+{
+  label = "Weapon Control (DOOM)",
+
+  for_games = { doom1=1, doom2=1, freedoom=1 },
+  for_modes = { sp=1, coop=1 },
+
+  setup_func = Weap_Control_setup,
+
+  options =
+  {
+    berserk  = { label="Berserk",          choices=WEAP_CONTROL_CHOICES },
+    saw      = { label="Saw",              choices=WEAP_CONTROL_CHOICES },
+    shotty   = { label="Shotgun",          choices=WEAP_CONTROL_CHOICES },
+    super    = { label="Super Shotty",     choices=WEAP_CONTROL_CHOICES },
+    chain    = { label="Chaingun",         choices=WEAP_CONTROL_CHOICES },
+    launch   = { label="Rocket Launcher",  choices=WEAP_CONTROL_CHOICES },
+    plasma   = { label="Plasma Rifle",     choices=WEAP_CONTROL_CHOICES },
+    bfg      = { label="B.F.G",            choices=WEAP_CONTROL_CHOICES },
   },
 }
 
