4a4e9396b07756337c964e794c42fe1144b2a530
Andrew Apted <ajapted@users.sf.net>
2009-09-18 10:10:26 +0000

    GUI: version bump after the beta test.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "Oblige Level Maker"
 
-#define OBLIGE_VERSION  "3.46"
-#define OBLIGE_HEX_VER  0x346
+#define OBLIGE_VERSION  "3.47"
+#define OBLIGE_HEX_VER  0x347
 
 extern const char *install_path;
 
