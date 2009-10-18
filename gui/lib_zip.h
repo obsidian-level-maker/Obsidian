e3d7c3fce9d0783622e3c85c66156095f8362246
Andrew Apted <ajapted@users.sf.net>
2009-10-18 13:39:34 +0000

    Zip Archive reader: added proper on-disk structures to header file (raw_zip_trailer_t and raw_zip_entry_t), based on APPNOTE.txt

--- gui/lib_zip.h
+++ gui/lib_zip.h
@@ -19,20 +19,47 @@ void ZARC_ListEntries(void);
 
 /* ----- ZIP file structure ---------------------- */
 
-/*
- * this structure cannot be wildly enlarged...
- */
-typedef struct zipfileinfo
+typedef struct
 {
-  u32_t usize;
-  u32_t csize;
-  u32_t crc32;
-  u32_t offset; /* offset of file in zipfile */
-  u16_t next; /* next zipfileinfo structure (in linked list)*/
-  byte  compr;
-  char  fname[1];
+  char magic[4];
+
+  u16_t disk_stuff[2];
+  u16_t num_entries[2];
+
+  u32_t dir_size;
+  u32_t dir_offset;
+}
+raw_zip_trailer_t;
+
+#define ZARC_EOD_MAGIC  "PK\005\006"
+
+
+typedef struct
+{
+  char magic[4];
+
+  u16_t req_version;
+  u16_t flags;
+  u16_t comp_method;
+
+  u16_t file_time;  // MS-DOS format
+  u16_t file_data;  //
+
+  u32_t crc;
+  u32_t comp_size;
+  u32_t real_size;
+
+  char filename[1];  // variable size
 }
 raw_zip_entry_t;
+
+#define ZARC_ENTRY_MAGIC  "PK\003\004"
+
+#define ZARC_F_ENCRYPTED       (1 << 0)
+#define ZARC_F_POST_DATA_DESC  (1 << 3)
+
+#define ZARC_COMPR_STORE    0
+#define ZARC_COMPR_DEFLATE  8
 
 
 #endif /* __OBLIGE_LIB_ZIP_H__ */
