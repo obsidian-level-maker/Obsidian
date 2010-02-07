02fa8d64d45d1170d8918e15ccbb77901db4be0b
Andrew Apted <ajapted@users.sf.net>
2010-02-07 13:33:25 +0000

    SpaceTest: split a large dimension with some choice (e.g. 9 --> 2/7), and decrease chance of three-way split of small dimensions.

--- misc/space_test.lua
+++ misc/space_test.lua
@@ -26,18 +26,54 @@ ROOM = 1
 
 
 function divide_horiz(x, y, w, h)
+  if (w >= 3) and rand_odds(math.min(50, (w-1)*10)) then
+    local w2 = int(w / 3)
+
+    recursive_fill(x, y, w2, h)
+    recursive_fill(x+w-w2, y, w2, h)
+    recursive_fill(x+w2, y, w-w2-w2, h)
+
+    return
+  end
+
   local w2 = int(w / 2)
 
+  if (w % 2) == 1 and rand_odds(50) then
+    w2 = w2 + 1
+  end
+
+  if w > 4 then
+    w2 = rand_irange(2, w-2)
+  end
+
   recursive_fill(x, y, w2, h)
-  recursive_fill(x+w-w2, y, w2, h)
+  recursive_fill(x+w2, y, w-w2, h)
 end
 
 
 function divide_vert(x, y, w, h)
+  if (h >= 3) and rand_odds(math.min(50, (h-1)*10)) then
+    local h2 = int(w / 3)
+
+    recursive_fill(x, y, w, h2)
+    recursive_fill(x, y+h-h2, w, h2)
+    recursive_fill(x, y+h2, w, h-h2-h2)
+
+    return
+  end
+
   local h2 = int(h / 2)
 
+  if (h % 2) == 1 and rand_odds(50) then
+    h2 = h2 + 1
+  end
+
+  if h > 4 then
+    h2 = rand_irange(2, h-2)
+  end
+
   recursive_fill(x, y, w, h2)
-  recursive_fill(x, y+h-h2, w, h2)
+  recursive_fill(x, y+h2, w, h-h2)
 end
 
 
@@ -46,19 +82,23 @@ function recursive_fill(x, y, w, h)
 
 -- print(x, y, w, h)
 
-  if (w > h) or (w == h and rand_odds(50)) then
-    d = math.min(w, #DIVIDE_ODDS)
+  if math.min(w, h) >= 2 then
 
-    if rand_odds(DIVIDE_ODDS[d]) then
-      return divide_horiz(x, y, w, h)
-    end
-  else
-    d = math.min(h, #DIVIDE_ODDS)
+    if (w > h) or (w == h and rand_odds(50)) then
+      d = math.min(w, #DIVIDE_ODDS)
 
-    if rand_odds(DIVIDE_ODDS[d]) then
-      return divide_vert(x, y, w, h)
+      if rand_odds(DIVIDE_ODDS[d]) then
+        return divide_horiz(x, y, w, h)
+      end
+    else
+      d = math.min(h, #DIVIDE_ODDS)
+
+      if rand_odds(DIVIDE_ODDS[d]) then
+        return divide_vert(x, y, w, h)
+      end
     end
-  end
+
+  end  -- min(w, h) >= 2
 
   -- no subdivision, just fill the space
   for sy = y, y+h-1 do
@@ -71,7 +111,7 @@ function recursive_fill(x, y, w, h)
 end
 
 
-function generate_rand()
+function generate_noise()
   for y = 1,SEED_H do
     for x = 1,SEED_W do
       SEEDS[x][y] = rand_index_by_probs({ 80, 40, 20, 10, 5 }) - 1
@@ -81,8 +121,8 @@ end
 
 
 function write_seeds()
-  for y = 1,SEED_H do
-    for x = 1,SEED_W do
+  for y = 1,SEED_W do
+    for x = 1,SEED_H do
       print(SEEDS[x][y] or 0)
     end
   end
