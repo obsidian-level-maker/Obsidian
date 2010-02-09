954fd8499230ca3c47ff05a076b8d41f08db3a77
Andrew Apted <ajapted@users.sf.net>
2010-02-09 10:38:20 +0000

    Restored GUI colors.

--- gui/main.cc
+++ gui/main.cc
@@ -175,7 +175,7 @@ void Setup_FLTK()
 
   if (custom_colors)
   {
-    Fl::background(236-12, 232-12, 228-12);  ///!!!! FIXME
+    Fl::background(236, 232, 228);
     Fl::background2(255, 255, 255);
     Fl::foreground(0, 0, 0);
   }
