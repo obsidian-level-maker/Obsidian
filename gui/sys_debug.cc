9d3395505f8cb8b8844be861b045ef1cda6154f9
Andrew Apted <ajapted@users.sf.net>
2010-02-11 11:38:47 +0000

    GUI: minor tweak.

--- gui/sys_debug.cc
+++ gui/sys_debug.cc
@@ -35,7 +35,7 @@ void LogInit(const char *filename)
     log_file = fopen(filename, "w");
   }
 
-  LogPrintf("========= START OF OBLIGE LOGS =========\n");
+  LogPrintf("====== START OF OBLIGE LOGS ======\n");
 }
 
 
@@ -60,7 +60,7 @@ void LogEnableTerminal(bool enable)
 
 void LogClose(void)
 {
-  LogPrintf("\n========= END OF OBLIGE LOGS =========\n");
+  LogPrintf("\n====== END OF OBLIGE LOGS ======\n");
 
   if (log_file)
   {
