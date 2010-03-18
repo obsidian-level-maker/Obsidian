d34a6624aca5f304fa4676f893903b2807e60d83
Andrew Apted <ajapted@users.sf.net>
2010-03-18 09:34:22 +0000

    DOOM: tweaked light level of nukage.

--- games/doom.lua
+++ games/doom.lua
@@ -1966,7 +1966,7 @@ DOOM_LIQUIDS =
 {
   water  = { mat="FWATER1", light=0.65, sec_kind=16 },
   blood  = { mat="BLOOD1",  light=0.65, sec_kind=16 }, --  5% damage
-  nukage = { mat="NUKAGE1", light=0.75, sec_kind=16 }, -- 10% damage
+  nukage = { mat="NUKAGE1", light=0.65, sec_kind=16 }, -- 10% damage
   lava   = { mat="LAVA1",   light=0.75, sec_kind=16 }, -- 20% damage
 
   -- Doom II only --
