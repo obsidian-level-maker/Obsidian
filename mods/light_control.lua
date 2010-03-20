406dfe55e7094532b089cafeebaafbdeeac13ec9
Andrew Apted <ajapted@users.sf.net>
2010-03-20 00:33:03 +0000

    Checked in new 'Light Control' module.

---
+++ mods/light_control.lua
@@ -0,0 +1,45 @@
+----------------------------------------------------------------
+--  MODULE: Lighting Control
+----------------------------------------------------------------
+--
+--  Copyright (C) 2010 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+
+function LightControl_setup()
+  -- TODO
+end
+
+
+OB_MODULES["lighting_control"] =
+{
+  label = "Lighting Control",
+
+  setup_func = LightControl_setup,
+
+  options =
+  {
+    precision =
+    {
+      label="Precision",
+      choices=
+      {
+        "low",  "Fastest",
+        "medium", "Medium",
+        "high", "Highest",
+      },
+    },
+  }
+}
+
