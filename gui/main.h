8e998765865fbaa0664d9eb8bd702f312222021f
Andrew Apted <ajapted@users.sf.net>
2010-04-08 11:15:07 +0000

    OBLIGE 3.57 was released, hence bump version.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "3.57"
-#define OBLIGE_HEX_VER  0x357
+#define OBLIGE_VERSION  "3.58"
+#define OBLIGE_HEX_VER  0x358
 
 extern const char *install_path;
 extern const char *working_path;
