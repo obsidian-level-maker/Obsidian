90a3a8dcf614f7b9eedceef0668b223b052448a1
Andrew Apted <ajapted@users.sf.net>
2010-04-18 08:48:34 +0000

    Added DOOR.lua -- prelim prefab for doors.

---
+++ DOOR.lua
@@ -0,0 +1,182 @@
+
+DOOR_PREFAB =
+{
+  brushes =
+  {
+    {
+      x = 192,
+      y = -24,
+      face = { mat = "step" },
+    },
+    {
+      x = 192,
+      y = 24,
+      face = { mat = "step" },
+    },
+    {
+      x = 0,
+      y = 24,
+      face = { mat = "step" },
+    },
+    {
+      x = 0,
+      y = -24,
+      face = { mat = "step" },
+    },
+    {
+      t = 8,
+      face = { mat = "step", light = 0.7 },
+    },
+  },
+
+  {
+    {
+      x = 192,
+      y = -24,
+      face = { mat = "outer" },
+    },
+    {
+      x = 192,
+      y = 24,
+      face = { mat = "inner" },
+    },
+    {
+      x = 0,
+      y = 24,
+      face = { mat = "outer" },
+    },
+    {
+      x = 0,
+      y = -24,
+      face = { mat = "outer" },
+    },
+    {
+      b = 80,
+      face = { mat = "frame_c" },
+    },
+  },
+
+  {
+    {
+      x = 160,
+      y = -8,
+      kind = "@line_kind",
+      face = { mat = "door", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 160,
+      y = 8,
+      kind = "@line_kind",
+      face = { mat = "door", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 32,
+      y = 8,
+      kind = "@line_kind",
+      face = { mat = "door", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 32,
+      y = -8,
+      kind = "@line_kind",
+      face = { mat = "door", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      b = 16,
+      face = { mat = "door", light = 0.7, },
+    },
+  },
+
+  {
+    {
+      x = 32,
+      y = -8,
+      face = { mat = "track", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 32,
+      y = 8,
+      face = { mat = "key", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 14,
+      y = 24,
+      face = { mat = "key", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 14,
+      y = -24,
+      face = { mat = "key", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+  },
+
+  {
+    {
+      x = 14,
+      y = -24,
+      face = { mat = "outer", },
+    },
+    {
+      x = 14,
+      y = 24,
+      face = { mat = "inner", },
+    },
+    {
+      x = 0,
+      y = 24,
+      face = { mat = "outer", },
+    },
+    {
+      x = 0,
+      y = -24,
+      face = { mat = "outer", },
+    },
+  },
+
+  {
+    {
+      x = 160,
+      y = -8,
+      face = { mat = "key", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 178,
+      y = -24,
+      face = { mat = "key", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 178,
+      y = 24,
+      face = { mat = "key", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+    {
+      x = 160,
+      y = 8,
+      face = { mat = "track", peg = 1, x_offset = 0, y_offset = 0, },
+    },
+  },
+
+  {
+    {
+      x = 178,
+      y = -24,
+      face = { mat = "outer", },
+    },
+    {
+      x = 192,
+      y = -24,
+      face = { mat = "outer", },
+    },
+    {
+      x = 192,
+      y = 24,
+      face = { mat = "inner", },
+    },
+    {
+      x = 178,
+      y = 24,
+      face = { mat = "outer", },
+    },
+  },
+}
+
