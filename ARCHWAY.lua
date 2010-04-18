e41656c6dac7b1104184c4739152c734aa21adc3
Andrew Apted <ajapted@users.sf.net>
2010-04-18 05:45:37 +0000

    Checked in ARCHWAY.lua -- prefab test code.

---
+++ ARCHWAY.lua
@@ -0,0 +1,33 @@
+
+ARCHWAY =
+{
+  -- frame
+  {
+    { x = 192, y = -24, face = { tex = "outer" } },
+    { x = 192, y =  24, face = { tex = "inner" } },
+    { x = 0,   y =  24, face = { tex = "outer" } },
+    { x = 0,   y = -24, face = { tex = "outer" } },
+    { b = 128, face = { tex = "outer" } },
+  },
+
+  -- left side
+  {
+    { x = 0,  y = -24, face = { tex = "outer" } },
+    { x = 40, y = -24, face = { tex = "outer" } },
+    { x = 52, y =  -8, face = { tex = "break" } },
+    { x = 52, y =   8, face = { tex = "inner" } },
+    { x = 40, y =  24, face = { tex = "inner" } },
+    { x = 0,  y =  24, face = { tex = "inner" } },
+  },
+
+  -- right side
+  {
+    { x = 192, y =  24, face = { tex = "inner" } },
+    { x = 152, y =  24, face = { tex = "inner" } },
+    { x = 140, y =   8, face = { tex = "break" } },
+    { x = 140, y =  -8, face = { tex = "outer" } },
+    { x = 152, y = -24, face = { tex = "outer" } },
+    { x = 192, y = -24, face = { tex = "inner" } },
+  },
+}
+
