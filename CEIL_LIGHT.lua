ed712a01aaa8b894eeed7c1bc1cdcba8620c1472
Andrew Apted <ajapted@users.sf.net>
2010-04-20 05:04:04 +0000

    Fixes for the CEIL_LIGHT prefab.

--- CEIL_LIGHT.lua
+++ CEIL_LIGHT.lua
@@ -1,24 +1,26 @@
 
 CEIL_LIGHT =
 {
+  ceiling_relative = true,
+
   brushes =
   {
     -- trim around light
     {
       { x = -40, y = -40, mat = "trim" },
+      { x =  40, y = -40, mat = "trim" },
+      { x =  40, y =  40, mat = "trim" },
       { x = -40, y =  40, mat = "trim" },
-      { x = -40, y =  40, mat = "trim" },
-      { x = -40, y = -40, mat = "trim" },
-      { b = -16, light = 0.72, mat = "trim" },
+      { b = -16, mat = "trim" },
     },
 
     -- light itself
     {
+      { x = -32, y = -32, mat = "glowy" },
       { x =  32, y = -32, mat = "glowy" },
       { x =  32, y =  32, mat = "glowy" },
       { x = -32, y =  32, mat = "glowy" },
-      { x = -32, y = -32, mat = "glowy" },
-      { b = -20, delta_z = 8, light = 0.9, mat = "glowy" },
+      { b = -18, delta_z = 10, light = 0.9, mat = "glowy" },
     },
   },
 }
