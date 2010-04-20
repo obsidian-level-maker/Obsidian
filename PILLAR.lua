57724dd55f1f5d554de821ad6b25d659da1af424
Andrew Apted <ajapted@users.sf.net>
2010-04-20 05:21:23 +0000

    Tweaked the PILLAR prefab.

--- PILLAR.lua
+++ PILLAR.lua
@@ -11,21 +11,21 @@ PILLAR =
       { x = -32, y = -32, mat = "pillar", peg=1, x_offset=0, y_offset=0 },
     },
 
-    -- trim closest to stem (TODO: MAKE IT OPTIONAL)
+    -- trim closest to stem
     {
-      { x =  36, y = -36, mat = "trim2" },
-      { x =  36, y =  36, mat = "trim2" },
-      { x = -36, y =  36, mat = "trim2" },
-      { x = -36, y = -36, mat = "trim2" },
-      { t = 32, mat = "trim2" },
+      { x =  40, y = -40, mat = "trim2" },
+      { x =  40, y =  40, mat = "trim2" },
+      { x = -40, y =  40, mat = "trim2" },
+      { x = -40, y = -40, mat = "trim2" },
+      { t = 20, mat = "trim2" },
     },
 
     {
-      { x =  36, y = -36, mat = "trim2" },
-      { x =  36, y =  36, mat = "trim2" },
-      { x = -36, y =  36, mat = "trim2" },
-      { x = -36, y = -36, mat = "trim2" },
-      { b = 96, mat = "trim2" },
+      { x =  40, y = -40, mat = "trim2" },
+      { x =  40, y =  40, mat = "trim2" },
+      { x = -40, y =  40, mat = "trim2" },
+      { x = -40, y = -40, mat = "trim2" },
+      { b = 108, mat = "trim2" },
     },
 
     -- roundish and lowest trim
