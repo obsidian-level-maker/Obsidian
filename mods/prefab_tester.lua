d75d660a51211a0f9edbe829d0db61356db57ce6
Andrew Apted <ajapted@users.sf.net>
2010-04-20 04:19:05 +0000

    Prefab Tester: added scaling and mirroring options, and made the playfield larger.

--- mods/prefab_tester.lua
+++ mods/prefab_tester.lua
@@ -20,28 +20,30 @@ PREFAB_FILE = "STAIR.lua"
 PREFAB_NAME = "STAIR_8_NICHE"
 
 
-function Build_Prefab()
+function Build_Prefab(info)
   gui.printf("Building prefab....\n")
+
+  gui.printf("transform =\n%s\n", table.tostr(info))
 
   local prefab = assert(_G[PREFAB_NAME])
 
   -- empty room
   Trans.brush("solid",
   {
-    { x=-512, y=-512, tex="BLAKWAL1" },
-    { x= 512, y=-512, tex="BLAKWAL1" },
-    { x= 512, y= 512, tex="BLAKWAL1" },
-    { x=-512, y= 512, tex="BLAKWAL1" },
+    { x=-1024, y=-1024, tex="BLAKWAL1" },
+    { x= 1024, y=-1024, tex="BLAKWAL1" },
+    { x= 1024, y= 1024, tex="BLAKWAL1" },
+    { x=-1024, y= 1024, tex="BLAKWAL1" },
     { t=0, tex="CEIL5_1" },
   })
 
   Trans.brush("solid",
   {
-    { x=-512, y=-512, tex="BLAKWAL1" },
-    { x= 512, y=-512, tex="BLAKWAL1" },
-    { x= 512, y= 512, tex="BLAKWAL1" },
-    { x=-512, y= 512, tex="BLAKWAL1" },
-    { b=512, tex="CEIL5_1", light=1.0 },
+    { x=-1024, y=-1024, tex="BLAKWAL1" },
+    { x= 1024, y=-1024, tex="BLAKWAL1" },
+    { x= 1024, y= 1024, tex="BLAKWAL1" },
+    { x=-1024, y= 1024, tex="BLAKWAL1" },
+    { b=512, tex="CEIL5_1", light=0.85 },
   })
 
   -- player start
@@ -57,6 +59,8 @@ function Build_Prefab()
   local mapping = { }
 
 
+  Trans.set(info)
+
   local brushes = assert(prefab.brushes)
 
   for _,B0 in ipairs(brushes) do
@@ -68,7 +72,7 @@ function Build_Prefab()
         if not mapping[C.mat] then
           local idx = table.size(mapping) + 1
           mapping[C.mat] = materials[1 + (idx-1) % #materials]
-          gui.printf("  material %s --> %s\n", C.mat, mapping[C.mat])
+          gui.printf("  mapping %s --> %s\n", C.mat, mapping[C.mat])
         end
 
         C.tex = mapping[C.mat]
@@ -79,16 +83,51 @@ function Build_Prefab()
     Trans.brush("solid", B)
   end
 
+  Trans.clear()
+
   gui.printf("Done prefab\n\n")
 end
 
 
-function Test_Prefab()
-  
+SIZE_CHOICES =
+{
+  "s100", "100%",
+  "s50", "50%",
+  "s25", "25%",
+  "s150", "150%",
+  "s200", "200%",
+  "s300", "300%",
+}
+
+SIZE_VALUES =
+{
+  s100 = 1.00, s25 =  0.25, s50 =  0.50,
+  s150 = 1.50, s200 = 2.00, s300 = 3.00,
+}
+
+MIRROR_CHOICES =
+{
+  "none","NONE", "x","X", "y","Y", "xy","X+Y"
+}
+
+
+function Test_Prefab(self)
   -- load it
   assert(loadfile(PREFAB_FILE))()
 
-  GAME.all_levels[1].build_func = Build_Prefab
+  local LEV = GAME.all_levels[1]
+
+  LEV.build_func = Build_Prefab
+  LEV.build_data =
+  {
+    scale_x = SIZE_VALUES[self.options.scale_x.value] or 1,
+    scale_y = SIZE_VALUES[self.options.scale_y.value] or 1,
+    scale_z = SIZE_VALUES[self.options.scale_z.value] or 1,
+  }
+
+  local mir = self.options.mirror.value
+  if mir == "x" or mir == "xy" then LEV.build_data.mirror_x = 0 end
+  if mir == "y" or mir == "xy" then LEV.build_data.mirror_y = 0 end
 end
 
 
@@ -99,5 +138,13 @@ OB_MODULES["prefab_tester"] =
   for_games = { doom2=1 },
 
   hooks = { get_levels = Test_Prefab },
+
+  options =
+  {
+    scale_x = { label="X Scaling", choices=SIZE_CHOICES },
+    scale_y = { label="Y Scaling", choices=SIZE_CHOICES },
+    scale_z = { label="Z Scaling", choices=SIZE_CHOICES },
+    mirror  = { label="Mirroring", choices=MIRROR_CHOICES, priority=1 },
+  },
 }
 
