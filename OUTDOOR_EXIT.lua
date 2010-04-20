9c502925ab29a115c2f12cf865f3dd4e6cbaf0b9
Andrew Apted <ajapted@users.sf.net>
2010-04-20 04:53:05 +0000

    Fixes for the OUTDOOR_EXIT_SWITCH prefab.

--- OUTDOOR_EXIT.lua
+++ OUTDOOR_EXIT.lua
@@ -1,67 +1,71 @@
 
 OUTDOOR_EXIT_SWITCH =
 {
+  brushes =
+  {
 
+    -- podium
+    {
+      { x =  64, y = -64, mat = "podium" },
+      { x =  64, y =  64, mat = "podium" },
+      { x = -64, y =  64, mat = "podium" },
+      { x = -64, y = -64, mat = "podium" },
+      { t = 12, mat = "podium" },
+    },
 
-{
-  { x = 160, y = 32, mat = "podium", },
-  { x = 160, y = 160, mat = "podium", },
-  { x = 32, y = 160, mat = "podium", },
-  { x = 32, y = 32, mat = "podium", },
-  { t = 188, mat = "podium", },
-},
+    -- base of switch
+    {
+      { x = -36, y = -24, mat = "base" },
+      { x =  36, y = -24, mat = "base" },
+      { x =  44, y = -16, mat = "base" },
+      { x =  44, y =  16, mat = "base" },
+      { x =  36, y =  24, mat = "base" },
+      { x = -36, y =  24, mat = "base" },
+      { x = -44, y =  16, mat = "base" },
+      { x = -44, y = -16, mat = "base" },
+      { t = 22, mat = "base" },
+    },
 
-{
-  { x = -36, y = -24, mat = "base", },
-  { x = 36, y = -24, mat = "base", },
-  { x = 44, y = -16, mat = "base", },
-  { x = 44, y = 16, mat = "base", },
-  { x = 36, y = 24, mat = "base", },
-  { x = -36, y = 24, mat = "base", },
-  { x = -44, y = 16, mat = "base", },
-  { x = -44, y = -16, mat = "base", },
-  { t = 192, mat = "base", },
-},
+    -- switch itself
+    {
+      { x =  32, y = -8, mat = "base" },
+      { x =  32, y =  8, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { x = -32, y =  8, mat = "base" },
+      { x = -32, y = -8, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { t = 86, mat = "base" },
+    },
 
-{
-  { x = 32, y = -8, mat = "base", },
-  { x = 32, y = 8, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11, },
-  { x = -32, y = 8, mat = "base", },
-  { x = -32, y = -8, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11, },
-  { t = 256, mat = "base", },
-},
+    {
+      { x = -40, y = 32, mat = "base" },
+      { x = -12, y = 48, mat = "base" },
+      { x = -20, y = 56, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = -48, y = 40, mat = "base" },
+      { t = 28, mat = "base" },
+    },
 
-{
-  { x = 56, y = 128, mat = "base", },
-  { x = 84, y = 144, mat = "base", },
-  { x = 76, y = 152, mat = "exit", peg = 1, x_offset = 0, y_offset = 0, },
-  { x = 48, y = 136, mat = "base", },
-  { t = 204, mat = "base", },
-},
+    {
+      { x = 40, y = 32, mat = "base" },
+      { x = 48, y = 40, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = 20, y = 56, mat = "base" },
+      { x = 12, y = 48, mat = "base" },
+      { t = 28, mat = "base" },
+    },
 
-{
-  { x = 136, y = 128, mat = "base", },
-  { x = 144, y = 136, mat = "exit", peg = 1, x_offset = 0, y_offset = 0, },
-  { x = 116, y = 152, mat = "base", },
-  { x = 108, y = 144, mat = "base", },
-  { t = 204, mat = "base", },
-},
+    {
+      { x = -40, y = -32, mat = "base" },
+      { x = -48, y = -40, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = -20, y = -56, mat = "base" },
+      { x = -12, y = -48, mat = "base" },
+      { t = 28, mat = "base" },
+    },
 
-{
-  { x = 56, y = 64, mat = "base", },
-  { x = 48, y = 56, mat = "exit", peg = 1, x_offset = 0, y_offset = 0, },
-  { x = 76, y = 40, mat = "base", },
-  { x = 84, y = 48, mat = "base", },
-  { t = 204, mat = "base", },
-},
+    {
+      { x = 40, y = -32, mat = "base" },
+      { x = 12, y = -48, mat = "base" },
+      { x = 20, y = -56, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = 48, y = -40, mat = "base" },
+      { t = 28, mat = "base" },
+    },
+  },
+}
 
-{
-  { x = 136, y = 64, mat = "base", },
-  { x = 108, y = 48, mat = "base", },
-  { x = 116, y = 40, mat = "exit", peg = 1, x_offset = 0, y_offset = 0, },
-  { x = 144, y = 56, mat = "base", },
-  { t = 204, mat = "base", },
-},
-
-
-},
