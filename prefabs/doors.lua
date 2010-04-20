b8721bdceb2ffbf841f0e2e88b1d385ec6b6c9e5
Andrew Apted <ajapted@users.sf.net>
2010-04-20 13:10:09 +0000

    Moved/merged/renamed prefab files into new prefab/ folder.

--- prefabs/doors.lua
+++ prefabs/doors.lua
@@ -1,5 +1,41 @@
 
-DOOR_PREFAB =
+ARCHWAY =
+{
+  brushes =
+  {
+    -- frame
+    {
+      { x = 192, y = -24, mat = "outer" },
+      { x = 192, y =  24, mat = "inner" },
+      { x = 0,   y =  24, mat = "outer" },
+      { x = 0,   y = -24, mat = "outer" },
+      { b = 128, mat = "outer" },
+    },
+
+    -- left side
+    {
+      { x = 0,  y = -24, mat = "outer" },
+      { x = 40, y = -24, mat = "outer" },
+      { x = 52, y =  -8, mat = "break" },
+      { x = 52, y =   8, mat = "inner" },
+      { x = 40, y =  24, mat = "inner" },
+      { x = 0,  y =  24, mat = "inner" },
+    },
+
+    -- right side
+    {
+      { x = 192, y =  24, mat = "inner" },
+      { x = 152, y =  24, mat = "inner" },
+      { x = 140, y =   8, mat = "break" },
+      { x = 140, y =  -8, mat = "outer" },
+      { x = 152, y = -24, mat = "outer" },
+      { x = 192, y = -24, mat = "inner" },
+    },
+  },
+}
+
+
+DOOR =
 {
   brushes =
   {
--- prefabs/exits.lua
+++ prefabs/exits.lua
@@ -1,4 +1,125 @@
 
+EXIT_PILLAR =
+{
+  brushes =
+  {
+    -- pillar itself
+    {
+      { x = -32, y = -32, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { x =  32, y = -32, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { x =  32, y =  32, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { x = -32, y =  32, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { t = 128, mat = "switch" },
+    },
+
+    -- exit signs
+    {
+      { x = -60, y = 44, mat = "exitside" },
+      { x = -32, y = 60, mat = "exitside" },
+      { x = -40, y = 68, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = -68, y = 52, mat = "exitside" },
+      { t = 16, light = 0.82, mat = "exitside" },
+    },
+
+    {
+      { x = 60, y = 44, mat = "exitside" },
+      { x = 68, y = 52, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = 40, y = 68, mat = "exitside" },
+      { x = 32, y = 60, mat = "exitside" },
+      { t = 16, light = 0.82, mat = "exitside" },
+    },
+
+    {
+      { x = -60, y = -44, mat = "exitside" },
+      { x = -68, y = -52, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = -40, y = -68, mat = "exitside" },
+      { x = -32, y = -60, mat = "exitside" },
+      { t = 16, light = 0.82, mat = "exitside" },
+    },
+
+    {
+      { x = 60, y = -44, mat = "exitside" },
+      { x = 32, y = -60, mat = "exitside" },
+      { x = 40, y = -68, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = 68, y = -52, mat = "exitside" },
+      { t = 16, light = 0.82, mat = "exitside" },
+    },
+
+  },
+}
+
+
+OUTDOOR_EXIT_SWITCH =
+{
+  brushes =
+  {
+
+    -- podium
+    {
+      { x =  64, y = -64, mat = "podium" },
+      { x =  64, y =  64, mat = "podium" },
+      { x = -64, y =  64, mat = "podium" },
+      { x = -64, y = -64, mat = "podium" },
+      { t = 12, mat = "podium" },
+    },
+
+    -- base of switch
+    {
+      { x = -36, y = -24, mat = "base" },
+      { x =  36, y = -24, mat = "base" },
+      { x =  44, y = -16, mat = "base" },
+      { x =  44, y =  16, mat = "base" },
+      { x =  36, y =  24, mat = "base" },
+      { x = -36, y =  24, mat = "base" },
+      { x = -44, y =  16, mat = "base" },
+      { x = -44, y = -16, mat = "base" },
+      { t = 22, mat = "base" },
+    },
+
+    -- switch itself
+    {
+      { x =  32, y = -8, mat = "base" },
+      { x =  32, y =  8, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { x = -32, y =  8, mat = "base" },
+      { x = -32, y = -8, mat = "switch", peg = 1, x_offset = 0, y_offset = 0, line_kind = 11 },
+      { t = 86, mat = "base" },
+    },
+
+    {
+      { x = -40, y = 32, mat = "base" },
+      { x = -12, y = 48, mat = "base" },
+      { x = -20, y = 56, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = -48, y = 40, mat = "base" },
+      { t = 28, mat = "base" },
+    },
+
+    {
+      { x = 40, y = 32, mat = "base" },
+      { x = 48, y = 40, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = 20, y = 56, mat = "base" },
+      { x = 12, y = 48, mat = "base" },
+      { t = 28, mat = "base" },
+    },
+
+    {
+      { x = -40, y = -32, mat = "base" },
+      { x = -48, y = -40, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = -20, y = -56, mat = "base" },
+      { x = -12, y = -48, mat = "base" },
+      { t = 28, mat = "base" },
+    },
+
+    {
+      { x = 40, y = -32, mat = "base" },
+      { x = 12, y = -48, mat = "base" },
+      { x = 20, y = -56, mat = "exit", peg = 1, x_offset = 0, y_offset = 0 },
+      { x = 48, y = -40, mat = "base" },
+      { t = 28, mat = "base" },
+    },
+  },
+}
+
+
 SMALL_EXIT =
 {
   brushes =
