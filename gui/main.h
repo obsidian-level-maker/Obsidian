60eec083b36c64e192e1933bd4bcf3a310732e73
Andrew Apted <ajapted@users.sf.net>
2010-05-30 14:10:13 +0000

    Version bump to 3.60 -- in honor of new planning code (for big, shaped and natural rooms), as well as some progress with prefabs.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "3.59"
-#define OBLIGE_HEX_VER  0x359
+#define OBLIGE_VERSION  "3.60"
+#define OBLIGE_HEX_VER  0x360
 
 extern const char *install_path;
 extern const char *working_path;
