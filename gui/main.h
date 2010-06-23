ecfa42722ea006851ca176152f184a6ba1ea7c20
Andrew Apted <ajapted@users.sf.net>
2010-06-23 12:52:24 +0000

    Version bump to 3.61

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "3.60"
-#define OBLIGE_HEX_VER  0x360
+#define OBLIGE_VERSION  "3.61"
+#define OBLIGE_HEX_VER  0x361
 
 extern const char *install_path;
 extern const char *working_path;
