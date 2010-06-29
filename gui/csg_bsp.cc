6075a269d136333ce3f565d76929b4451e6bfb9a
Andrew Apted <ajapted@users.sf.net>
2010-06-29 06:47:50 +0000

    CSG: began work on new CSG method using BSP technique...

---
+++ gui/csg_bsp.cc
@@ -0,0 +1,57 @@
+//------------------------------------------------------------------------
+//  2.5D Constructive Solid Geometry
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker
+//
+//  Copyright (C) 2006-2010 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+#include "hdr_fltk.h"
+#include "hdr_lua.h"
+
+#include <algorithm>
+
+#include "lib_util.h"
+#include "main.h"
+
+#include "csg_main.h"
+#include "g_lua.h"
+#include "ui_dialog.h"
+
+
+void CSG_BSP()
+{
+  
+}
+
+
+//------------------------------------------------------------------------
+
+void CSG_Quantize()
+{
+}
+
+
+//------------------------------------------------------------------------
+
+void CSG_Merge()
+{
+}
+
+
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
