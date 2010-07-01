9e4b2ded1def8b5593d2097716fa08a0d296bb09
Andrew Apted <ajapted@users.sf.net>
2010-07-01 14:45:43 +0000

    Version bump, in honor of working teleporters.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "3.61"
-#define OBLIGE_HEX_VER  0x361
+#define OBLIGE_VERSION  "3.62"
+#define OBLIGE_HEX_VER  0x362
 
 extern const char *install_path;
 extern const char *working_path;
