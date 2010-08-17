d84e10cbfc0eafdb855755a2bbe9a085bd2dfa1d
Andrew Apted <ajapted@users.sf.net>
2010-08-17 05:13:15 +0000

    CSG LIGHT: added empty code files for world tracing : q_trace.cc/h

---
+++ gui/q_trace.cc
@@ -0,0 +1,57 @@
+//------------------------------------------------------------------------
+//  QUAKE LIGHT TRACING
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker
+//
+//  Copyright (C)      2010 Andrew Apted
+//  Copyright (C) 2005-2006 Peter Brett
+//  Copyright (C) 1994-2001 iD Software
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+//
+//  This code based on Quake II lighting tool : q2rad.c
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+
+#include "lib_file.h"
+#include "lib_util.h"
+#include "main.h"
+
+#include "q_common.h"
+#include "q_light.h"
+
+#include "csg_main.h"
+#include "csg_quake.h"
+
+
+void QCOM_MakeTraceNodes()
+{
+}
+
+
+void QCOM_FreeTraceNodes()
+{
+}
+
+
+bool QCOM_TraceLine(float x1, float y1, float z1,
+                    float x2, float y2, float z2)
+{
+}
+
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
---
+++ gui/q_trace.h
@@ -0,0 +1,35 @@
+//------------------------------------------------------------------------
+//  QUAKE LIGHT TRACING
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker
+//
+//  Copyright (C)      2010 Andrew Apted
+//  Copyright (C) 2005-2006 Peter Brett
+//  Copyright (C) 1994-2001 iD Software
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#ifndef __QUAKE_TRACE_H__
+#define __QUAKE_TRACE_H__
+
+void QCOM_MakeTraceNodes();
+void QCOM_FreeTraceNodes();
+
+bool QCOM_TraceLine(float x1, float y1, float z1,
+                    float x2, float y2, float z2);
+
+#endif /* __QUAKE_TRACE_H__ */
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
