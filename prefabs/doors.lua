73ca51ecb83f85e4064e3ba1d31272889699d88b
Andrew Apted <ajapted@users.sf.net>
2010-09-06 12:03:04 +0000

    Prefabs: added QUAKE_V_DOOR, a two-part vertically opening door.

--- prefabs/doors.lua
+++ prefabs/doors.lua
@@ -266,3 +266,64 @@ PREFAB.QUAKE_DOOR =
   },
 }
 
+
+PREFAB.QUAKE_V_DOOR =
+{
+  brushes =
+  {
+    -- door frame
+    {
+      { x = 192, y =  0, mat = "?outer" },
+      { x = 192, y = 48, mat = "?inner" },
+      { x = 0,   y = 48, mat = "?outer" },
+      { x = 0,   y =  0, mat = "?outer" },
+      { b = 128, mat = "?outer" },
+    },
+
+    {
+      { x = 0,  y =  0, mat = "?outer" },
+      { x = 32, y =  0, mat = "?outer" },
+      { x = 32, y = 16, mat = "?track" },
+      { x = 32, y = 32, mat = "?inner" },
+      { x = 32, y = 48, mat = "?inner" },
+      { x = 0,  y = 48, mat = "?inner" },
+    },
+
+    {
+      { x = 192, y = 48, mat = "?inner" },
+      { x = 160, y = 48, mat = "?inner" },
+      { x = 160, y = 32, mat = "?track" },
+      { x = 160, y = 16, mat = "?outer" },
+      { x = 160, y =  0, mat = "?outer" },
+      { x = 192, y =  0, mat = "?inner" },
+    },
+  },
+
+  models =
+  {
+    -- low part
+    {
+      x1 = 32, x2 = 160, x_face = { mat="METAL1_2" },
+      y1 = 16, y2 =  32, y_face = { mat="DR05_2" },
+      z1 =  0, z2 =  64, z_face = { mat="METAL1_2" },
+
+      entity =
+      {
+        ent = "door", angles = "90 0 0", sounds = 2,
+      },
+    },
+
+    -- high part
+    {
+      x1 = 32, x2 = 160, x_face = { mat="METAL1_2" },
+      y1 = 16, y2 =  32, y_face = { mat="DR05_2" },
+      z1 = 64, z2 = 128, z_face = { mat="METAL1_2" },
+
+      entity =
+      {
+        ent = "door", angles = "270 0 0",
+      },
+    },
+  },
+}
+
