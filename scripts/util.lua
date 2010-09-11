2f326a0d0fb752b732986e0f0597796e4a4d6f33
Andrew Apted <ajapted@users.sf.net>
2010-09-11 03:39:05 +0000

    Utils: tweak

--- scripts/util.lua
+++ scripts/util.lua
@@ -612,8 +612,8 @@ end
 function geom.boxes_overlap(x1,y1,x2,y2, x3,y3,x4,y4)
   -- NOTE: mere touching is not enough
 
-  assert(x2 >= x1 and y2 >= y1)
-  assert(x4 >= x3 and y4 >= y3)
+  assert(x1 < x2 and y1 < y2)
+  assert(x3 < x4 and y3 < y4)
 
   if x3 >= x2 or x4 <= x1 then return false end
   if y3 >= y2 or y4 <= y1 then return false end
