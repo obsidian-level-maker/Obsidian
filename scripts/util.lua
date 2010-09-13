5811f3896e2dbb393e47c379c95d0438bd389d6c
Andrew Apted <ajapted@users.sf.net>
2010-09-13 05:24:39 +0000

    Utils: added geom.box_mid() and geom.box_size() functions.

--- scripts/util.lua
+++ scripts/util.lua
@@ -614,6 +614,14 @@ function geom.rough_dir(dx, dy)
 end
 
 
+function geom.box_mid(x1,y1, x2,y2)
+  return int((x1+x2)/2), int((y1+y2)/2)
+end
+
+function geom.box_size(x1,y1, x2,y2)
+  return x2 - x1, y2 - y1
+end
+
 function geom.group_size(x1,y1, x2,y2)
   return x2 - x1 + 1, y2 - y1 + 1
 end
