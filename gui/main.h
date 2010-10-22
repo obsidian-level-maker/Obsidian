5411a56b83c3c3695a6e7cf21e93b461fa0426d2
Andrew Apted <ajapted@users.sf.net>
2010-10-22 03:48:47 +0000

    Version bumped to 4.03

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "4.02"
-#define OBLIGE_HEX_VER  0x402
+#define OBLIGE_VERSION  "4.03"
+#define OBLIGE_HEX_VER  0x403
 
 
 #define RMZ_TITLE   "OBLIGE Randomizer"
