d5a56df24267e3e70ea8a1629c263f0671039d47
Andrew Apted <ajapted@users.sf.net>
2010-10-31 02:54:47 +0000

    tweak.

--- gui/csg_doom.cc
+++ gui/csg_doom.cc
@@ -715,7 +715,7 @@ static void DM_LightingBrushes(doom_sector_c *S, region_c *R,
       if (val > 0)
       {
         effect = val;
-        delta  = B->props.getInt("delta", -64);
+        delta  = B->props.getInt("delta", -63);
       }
     }
   }
