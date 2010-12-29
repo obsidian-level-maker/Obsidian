35d9927dd651c574653406e700249f59d40c4d58
Andrew Apted <ajapted@users.sf.net>
2010-12-29 07:17:08 +0000

    Version bump.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "4.06-WIP"
-#define OBLIGE_HEX_VER  0x406
+#define OBLIGE_VERSION  "4.07-WIP"
+#define OBLIGE_HEX_VER  0x407
 
 
 #define RMZ_TITLE   "OBLIGE Randomizer"
