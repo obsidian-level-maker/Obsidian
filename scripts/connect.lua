3aca7c365c24ae6320fb47c231dcc966dae78499
Andrew Apted <ajapted@users.sf.net>
2011-01-04 12:00:20 +0000

    Hallways: document the HALLWAY objects.

--- scripts/connect.lua
+++ scripts/connect.lua
@@ -34,6 +34,23 @@ class CONN
   conn_h : floor height for connection
 }
 
+
+class HALLWAY
+{
+  start : SEED   -- the starting seed (in a room)
+  dest  : SEED   -- destination seed  (in a room)
+
+  start_dir  --  direction from start --> hallway
+  dest_dir   --  direction from hallway --> dest
+
+  path : list  -- the path between the start and the destination
+               -- (not including either start or dest).
+               -- each element holds the seed and a direction.
+
+  sub_halls   -- number of hallways branching off this one
+              -- (normally zero)
+}
+
 --------------------------------------------------------------]]
 
 require 'defs'
