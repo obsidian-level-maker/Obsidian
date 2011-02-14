89bed90001178b1be13dfecd89eba388ad649408
Andrew Apted <ajapted@users.sf.net>
2011-02-14 01:11:34 +0000

    Mazes: bit of work on generation...

--- scripts/maze.lua
+++ scripts/maze.lua
@@ -19,3 +19,41 @@
 ----------------------------------------------------------------
 
 
+function CAVE_CLASS.maze_generate(maze)
+
+  local function pick_start()
+    -- TODO
+  end
+
+
+  local function trace_next(x, y, dir, len)
+    for i = 1,len do
+      x, y = geom.nudge(x, y, dir)
+
+      maze.cells[x][y] = 1
+    end
+
+    -- TODO
+  end
+
+
+  ---| maze_generate |---
+
+  while true do
+    local x, y, dir, len = pick_start()
+
+    -- stop if nothing is possible
+    if not x then break; end
+
+    repeat
+      x, y, dir, len = trace_next(x, y, dir, len)
+    until not x
+  end
+end
+
+
+
+function CAVE_CLASS.maze_render()
+  -- TODO
+end
+
