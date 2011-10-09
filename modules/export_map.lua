7d6ab9605d2f1ba2eafbea0f53df532d39f3ac45
Andrew Apted <ajapted@users.sf.net>
2011-10-09 09:37:29 +0000

    MODULES: further work on "Export .MAP" module...

--- modules/export_map.lua
+++ modules/export_map.lua
@@ -19,6 +19,66 @@
 EXPORT_MAP = {}
 
 
+function export_printf(fmt, ...)
+  table.insert(EXPORT_MAP.ent_data, string.format(fmt, ...))
+end
+
+
+function EXPORT_MAP.add_brush(coords)
+  local file = EXPORT_MAP.file
+
+  if not file then return end  
+
+  -- FIXME !!!!!!
+
+  fprintf(file, "// brush %s %d\n", coords[1].m or "solid", #coords)
+end
+
+
+function EXPORT_MAP.add_entity(ent)
+  local file = EXPORT_MAP.file
+
+  if not file then return end  
+
+  export_printf("{\n")
+
+  local origin
+
+  each key, value in ent do
+
+    -- special handling for the origin
+    if key == "x" or key == "y" or key == "z" then
+      if not origin then origin = {} end
+      origin[key] = value
+      continue
+    end
+
+    local key2 = key
+
+    if key2 == "id" then key2 = "classname" end
+
+    export_printf("\"%s\" \"%s\"\n", tostring(key2), tostring(value))
+  end
+
+  if origin then
+    export_printf("\"origin\" \"%d %d %d\"\n", origin.x or 0, origin.y or 0, origin.z or 0)
+  end
+
+  export_printf("}\n")
+end
+
+
+function EXPORT_MAP.add_model(model)
+  local file = EXPORT_MAP.file
+
+  if not file then return end  
+
+  -- FIXME !!!
+
+  export_printf("// model %s\n", model.entity.id)
+end
+
+
 function EXPORT_MAP.setup()
   gui.mkdir("debug")  
 
@@ -27,6 +87,11 @@ function EXPORT_MAP.setup()
     EXPORT_MAP.file:close()
     EXPORT_MAP.file = nil
   end
+
+  -- setup hooks
+  GAME.add_brush_func  = EXPORT_MAP.add_brush
+  GAME.add_entity_func = EXPORT_MAP.add_entity
+  GAME.add_model_func  = EXPORT_MAP.add_model
 end
 
 
@@ -35,9 +100,6 @@ function EXPORT_MAP.begin_level()
   if LEVEL.prebuilt then return end  
 
   local filename = string.format("debug/%s.map", LEVEL.name)
-  local error_msg
-
-  gui.printf("Creating new file: %s\n", filename)
 
   local file, error_msg = io.open(filename, "w")
 
@@ -46,10 +108,20 @@ function EXPORT_MAP.begin_level()
     return
   end
 
-  EXPORT_MAP.file = file
+  gui.printf("Started .MAP export to file: %s\n", filename)
 
-  file:write("// this MAP file was created by OBLIGE.\n")
-  file:write("// it is only for the purpose of debugging.\n")
+  EXPORT_MAP.file = file
+  EXPORT_MAP.ent_data = {}
+
+  fprintf(file, "// this MAP file was created by OBLIGE.\n")
+  fprintf(file, "// it is only for the purpose of debugging.\n")
+
+  -- write the worldspawn entity
+  fprintf(file, "{\n")
+  fprintf(file, "\"classname\" \"worldspawn\"\n")
+  fprintf(file, "\"worldtype\" \"0\"\n")  -- FIXME
+ 
+  -- TODO: "message" : LEVEL.description
 end
 
 
@@ -58,11 +130,25 @@ function EXPORT_MAP.end_level()
 
   if not file then return end  
 
-  file:write("// The End\n")
+  -- close off the worldspawn entity
+  fprintf(file, "}\n")
+
+  -- write the entity data
+  each str in EXPORT_MAP.ent_data do
+    fprintf(file, "%s", str)
+  end
+
+  fprintf(file, "// The End\n")
 
   EXPORT_MAP.file:close()
   EXPORT_MAP.file = nil
+  EXPORT_MAP.ent_data = nil
+
+  gui.printf("Finished .MAP export\n")
 end
+
+
+----------------------------------------------------------------
 
 
 OB_MODULES["export_map"] =
