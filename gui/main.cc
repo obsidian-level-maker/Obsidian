939af5dc2b02907739feed40d120154e0fc75184
Andrew Apted <ajapted@users.sf.net>
2012-02-26 01:58:38 +0000

    Fixed batch mode crash due to recent change.

--- gui/main.cc
+++ gui/main.cc
@@ -482,8 +482,6 @@ bool Build_Cool_Shit()
     u32_t total_time = end_time - start_time;
 
     LogPrintf("\nTOTAL TIME: %1.2f seconds\n\n", total_time / 1000.0);
-
-    main_win->game_box->BumpSeed();
   }
 
   if (main_win)
@@ -492,6 +490,9 @@ bool Build_Cool_Shit()
     main_win->build_box->SetAbortButton(false);
 
     main_win->Locked(false);
+
+    if (was_ok)
+      main_win->game_box->BumpSeed();
   }
 
   if (main_action == MAIN_CANCEL)
