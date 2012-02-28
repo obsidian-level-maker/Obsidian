af9cf7c0c5a3a64e78d7bf7d2766c481b0b4f8d5
Andrew Apted <ajapted@users.sf.net>
2012-02-28 01:44:17 +0000

    Restored style settings for Cycles (were modified for debugging).

--- scripts/levels.lua
+++ scripts/levels.lua
@@ -130,7 +130,7 @@ GLOBAL_STYLE_LIST =
   odd_shapes  = { none=10, few=40, some=60, heaps=4 }
   big_rooms   = { none=6, few=60, some=40, heaps=20 }
   caves       = { none=20, few=50, some=30, heaps=5 }
-  cycles      = { heaps=50 } --!!!!  { none=20, some=50, heaps=50 }
+  cycles      = { none=20, some=50, heaps=50 }
   crossovers  = { none=40 } --!!!! , some=40, heaps=40 }
   teleporters = { none=30, few=30, some=30, heaps=5 }
 
