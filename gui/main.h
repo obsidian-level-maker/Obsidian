dfed14d86f48f7eb5c24288e77fc4043925446a1
Andrew Apted <ajapted@users.sf.net>
2012-05-17 04:16:22 +0000

    GUI: version bump for upcoming release candidate.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "4.26-WIP"
-#define OBLIGE_HEX_VER  0x426
+#define OBLIGE_VERSION  "4.27-RC"
+#define OBLIGE_HEX_VER  0x427
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
