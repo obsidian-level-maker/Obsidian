990b29bdc5026bbcaf7cebbdc61dee8da4197b67
Andrew Apted <ajapted@users.sf.net>
2012-09-04 01:45:54 +0000

    EXE: bumped version to 4.28, for upcoming release.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "4.27-RC"
-#define OBLIGE_HEX_VER  0x427
+#define OBLIGE_VERSION  "4.28"
+#define OBLIGE_HEX_VER  0x428
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
