277cfac258e4ddee9e06b0ef107934706b024870
Andrew Apted <ajapted@users.sf.net>
2012-09-29 05:18:55 +0000

    DOOM1: restored the missing room themes (from r8164).

--- games/udoom.lua
+++ games/udoom.lua
@@ -123,6 +123,341 @@ DOOM1.MATERIALS =
 
 DOOM1.ROOM_THEMES =
 {
+  ----- Tech Bases -------------------------
+
+  D1_Tech_room =
+  {
+    walls =
+    {
+      STARTAN3=25, STARG2=20, STARTAN2=18, STARG3=11,
+      STARBR2=5, STARGR2=10, STARG1=5, STARG2=5,
+      SLADWALL=18, GRAY7=10, BROWN1=5,
+      BROWNGRN=10, BROWN96=8, METAL1=1, GRAY5=3,
+
+      COMPOHSO=10, STARTAN1=5, COMPTILE=5,
+    }
+
+    floors =
+    {
+      FLOOR0_1=50, FLOOR0_2=20, FLOOR0_3=50,
+      FLOOR0_7=50, FLOOR3_3=50, FLOOR7_1=10,
+      FLOOR4_5=50, FLOOR4_6=50, FLOOR4_8=50, FLOOR5_2=50,
+      FLAT1=20, FLAT5=20, FLAT9=50, FLAT14=50,
+      CEIL3_2=50,
+    }
+
+    ceilings =
+    {
+      CEIL5_1=50, CEIL5_2=50, CEIL3_3=50, CEIL3_5=50,
+      FLAT1=50, FLAT4=50, FLAT18=50,
+      FLOOR0_2=50, FLOOR4_1=50, FLOOR5_1=50, FLOOR5_4=20,
+      TLITE6_5=2, TLITE6_6=2, CEIL1_2=2,
+    }
+
+    __corners =
+    {
+      STARGR1=40, METAL1=20, ICKWALL3=6,
+      TEKWALL4=6, COMPTALL=3, COMPBLUE=3,
+
+      COMPTILE=10,
+    }
+  }
+
+
+  D1_Tech_hallway =
+  {
+    walls =
+    {
+      BROWN1=33, BROWNGRN=50, GRAY1=50, STARBR2=33
+    }
+
+    floors =
+    {
+      FLAT4=50, CEIL5_1=50, FLOOR1_1=50, FLOOR3_3=50
+    }
+
+    ceilings =
+    {
+      FLAT4=50, CEIL5_1=50, CEIL3_5=50, CEIL3_3=50
+    }
+  }
+
+
+  D1_Tech_cave =
+  {
+    naturals =
+    {
+      ASHWALL=50,
+      SP_ROCK1=50,
+      GRAYVINE=50,
+      TEKWALL4=3,
+    }
+  }
+
+
+  D1_Tech_outdoors =
+  {
+    floors =
+    {
+      BROWN144=30, BROWN1=20, STONE=20,
+      ASHWALL=5, FLAT10=5,
+    }
+
+    naturals =
+    {
+      ASHWALL=50,
+      SP_ROCK1=50,
+      GRAYVINE=50,
+      STONE=50,
+    }
+  }
+
+
+  ----- Hell / Gothic -------------------------
+
+  D1_Marble_room =
+  {
+    walls =
+    {
+      MARBLE1=25, MARBLE2=10, MARBLE3=20,
+      GSTVINE2=20, SLADWALL=10,
+      SKINMET1=3, SKINMET2=3,
+
+      SKINTEK1=15, SKINTEK2=15,
+    }
+
+    floors =
+    {
+      DEM1_5=15, DEM1_6=15, FLAT5_7=10, FLAT10=10,
+      FLOOR7_1=10, FLAT1=10, FLOOR5_2=10,
+    }
+
+    ceilings =
+    {
+      FLAT1=10, FLAT10=10, FLAT5_5=10, FLOOR7_2=10,
+    }
+
+    __corners =
+    {
+      SKULWALL=8, SKULWAL3=7,
+    }
+  }
+
+
+  D1_Marble_outdoors =
+  {
+    floors =
+    {
+      ASHWALL=20,
+      FLAT5_6=10, FLAT10=20,
+      SFLR6_1=10, MFLR8_2=20,
+    }
+
+    naturals =
+    {
+      ASHWALL=50, GRAYVINE=50,
+      SP_ROCK1=50, ROCKRED1=90,
+      SKSNAKE1=10, SKSNAKE2=10,
+    }
+  }
+
+
+  D1_Hot_room =
+  {
+    walls =
+    {
+      SP_HOT1=25, GSTVINE1=17, STONE=10, SKINMET2=5, BROWN1=2,
+      SKINCUT=2,
+
+      SKINTEK1=10, SKINTEK2=10,
+    }
+
+    floors =
+    {
+      FLAT5_7=10, FLAT10=10, FLAT5_3=10,
+      FLOOR7_1=10, FLAT1=10, FLOOR5_2=10,
+    }
+
+    ceilings =
+    {
+      FLAT1=10, FLOOR6_1=10, FLAT10=10, FLAT8=10,
+    }
+
+    __corners =
+    {
+      SKULWALL=10, SKULWAL3=10, REDWALL1=15,
+    }
+  }
+
+
+  D1_Hot_outdoors =
+  {
+    floors =
+    {
+      FLAT5_6=10, ASHWALL=5, FLAT10=5,
+      SFLR6_4=10, MFLR8_2=10,
+    }
+
+    naturals =
+    {
+      ASHWALL=50, GRAYVINE=50,
+      SP_ROCK1=50, ROCKRED1=90,
+      SKSNAKE1=10, SKSNAKE2=10,
+    }
+  }
+
+
+  D1_Hell_cave =
+  {
+    naturals =
+    {
+      ROCKRED1=90,
+      SKIN2=50, SKINFACE=50, SKSNAKE1=35, SKSNAKE2=35,
+      FIREBLU1=50, FIRELAVA=50, 
+    }
+  }
+
+
+  ---- Episode 2 --------------------------
+
+  Deimos_room =
+  {
+    walls =
+    {
+      STARTAN3=10, STARG2=15, BROVINE=20, ICKWALL1=15,
+      STARBR2=15, STARGR2=10, STARG1=5, STARG2=3, ICKWALL3=30,
+      SLADWALL=10, GRAY7=20, BROWN1=5, GRAY1=15, BROVINE2=15,
+      BROWNGRN=10, BROWN96=5, METAL1=15, GRAY5=15, STONE=10,
+
+      STONE2=30, STARTAN1=5, STONE3=20,
+    }
+
+    floors =
+    {
+      FLOOR0_1=30, FLOOR0_2=40, FLOOR0_3=30, CEIL4_1=5,
+      FLOOR0_7=10, FLOOR3_3=20, FLOOR7_1=20, CEIL_4_2=10,
+      FLOOR4_1=30, FLOOR4_6=20, FLOOR4_8=50, FLOOR5_2=35,
+      FLAT1=40, FLAT5=30, FLAT14=10, FLAT1_1=30, FLOOR1_6=3,
+      FLAT1_2=30, FLOOR5_1=50, FLAT3=15, FLAT5_4=15,
+    }
+
+    ceilings =
+    {
+      CEIL5_1=30, CEIL3_3=70, CEIL3_5=50, CEIL4_1=10,
+      FLAT1=30, FLAT4=20, FLAT19=30, FLAT8=15, FLAT5_4=20,
+      FLOOR0_2=20, FLOOR4_1=50, FLOOR5_1=50, FLOOR5_4=10,
+    }
+
+    __corners =
+    {
+      STARGR1=40, METAL1=20, ICKWALL3=6, TEKWALL1=20,
+      TEKWALL4=6, COMPTALL=3, COMPBLUE=3, STARTAN1=7,
+
+      COMPTILE=10,
+    }
+  }
+
+  Deimos_hallway =
+  {
+    walls =
+    {
+      BROWN1=33, BROWNGRN=50, BROVINE=20,
+      GRAY1=50, GRAY5=33, ICKWALL1=30, ICKWALL3=30,
+      STONE2=40, STONE3=50, METAL1=30
+    }
+
+    floors =
+    {
+      FLAT4=30, CEIL5_1=30, FLAT14=20, FLAT5_4=20
+      FLOOR3_3=30, FLOOR4_8=40, FLOOR5_1=25,
+    }
+
+    ceilings =
+    {
+      FLAT4=50, CEIL5_1=50, CEIL3_5=50, CEIL3_3=50, FLAT19=20
+    }
+  }
+
+  Deimos_cave =
+  {
+    naturals =
+    {
+      SP_ROCK1=90,
+      ASHWALL=20, BROWNHUG=15,
+      GRAYVINE=15,
+    }
+  }
+
+  Deimos_outdoors =
+  {
+--Makes sense for high prob for SP_ROCK1 because the intermission screen shows
+--Deimos has a desolate, gray ground.
+    floors = { BROWN144=20, BROWN1=10, STONE=10 }
+
+    naturals = { SP_ROCK1=60, ASHWALL=2, FLAT10=3 }
+  }
+
+
+  ---- Episode 4 --------------------------
+
+  Flesh_room =
+  {
+    walls =
+    {
+      MARBLE1=10, MARBLE2=10, MARBLE3=10, WOOD1=30,
+      GSTVINE2=12, SLADWALL=10, GSTONE1=15, WOOD5=20,
+      SKINMET1=3, SKINMET2=3, GSTVINE1=12, WOOD3=15, STONE2=3,
+
+      SKINTEK1=10, SKINTEK2=10, BROWNGRN=7, BROVINE2=5, STONE3=3,
+    }
+
+    floors =
+    {
+      DEM1_5=10, DEM1_6=10, FLAT5_7=12, FLAT10=12,
+      FLOOR7_1=10, FLAT1=12, FLOOR5_2=10, FLAT5_8=10,
+      FLOOR5_4=10, FLOOR5_3=10, FLAT5=10, FLAT5_4=15,
+      FLOOR7_2=10, FLAT5_1=15, FLAT5_2=15, FLAT8=10,
+    }
+
+    ceilings =
+    {
+      FLAT1=10, FLAT10=10, FLAT5_5=10, FLOOR7_2=6, DEM1_6=10,
+      FLOOR6_1=10, FLOOR6_2=10, MFLR8_1=12, FLAT5_4=10, SFLR6_1=5,
+      SFLR6_2=5, CEIL1_1=5, FLAT5_1=12, FLAT5_2=12, FLAT8=8,
+    }
+
+    __corners =
+    {
+      SKULWALL=8, SKULWAL3=7, SKSNAKE1=5, SKSNAKE2=5, ASHWALL=5, METAL=15
+    }
+  }
+
+  Flesh_cave =
+  {
+    naturals =
+    {
+      ROCKRED1=70, SP_ROCK1=50, BROWNHUG=15,
+      SKIN2=10, SKINFACE=20, SKSNAKE1=5, SKSNAKE2=5,
+      FIREBLU1=10, FIRELAVA=10, 
+    }
+  }
+
+  Flesh_outdoors =
+  {
+    floors =
+    {
+      ASHWALL=12, FLAT5_7=10, FLAT1_1=15, FLAT5_4=10,
+      FLAT10=20, FLAT5_8=10, MFLR8_4=10, FLOOR7_1=15,
+      SFLR6_1=8, MFLR8_2=5, FLAT1_2=10, MFLR8_3=10,
+    }
+
+    naturals =
+    {
+      ASHWALL=30, GRAYVINE=30, SP_ROCK2=15,
+      SP_ROCK1=50, ROCKRED1=50, BROWNHUG=10,
+      SKSNAKE1=10, SKSNAKE2=10,
+    }
+  }
 }
 
 
@@ -280,7 +615,7 @@ DOOM1.LEVEL_THEMES =
 
     __logos = { carve=90, pill=50, neon=5 }
 
-    pictures =
+    __pictures =
     {
       marbface=10, skinface=10, firewall=20,
       spdude1=4, spdude2=4, spdude5=3, spine=2,
@@ -318,7 +653,7 @@ DOOM1.LEVEL_THEMES =
 
     __logos = { carve=90, pill=50, neon=5 }
 
-    pictures =
+    __pictures =
     {
       marbfac2=10, marbfac3=10,
       spface1=2, firewall=20,
@@ -595,7 +930,7 @@ OB_GAMES["ultdoom"] =
 
 ------------------------------------------------------------
 
-UNFINISHED["doom_deimos"] =
+OB_THEMES["doom_deimos"] =
 {
   label = "Deimos"
   priority = 6
@@ -605,7 +940,7 @@ UNFINISHED["doom_deimos"] =
 }
 
 
-UNFINISHED["doom_flesh"] =
+OB_THEMES["doom_flesh"] =
 {
   label = "Thy Flesh"
   priority = 2
