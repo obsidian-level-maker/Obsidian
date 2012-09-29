c0486dd96bcfd96d8932769d7a75f9ab40e789e9
Andrew Apted <ajapted@users.sf.net>
2012-09-29 11:52:13 +0000

    GUI: version bump after 4.28 release.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "4.28"
-#define OBLIGE_HEX_VER  0x428
+#define OBLIGE_VERSION  "4.29"
+#define OBLIGE_HEX_VER  0x429
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
