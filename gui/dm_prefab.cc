dab077b91ae3cf95ab80101d8f5d51cba0908a5d
Andrew Apted <ajapted@users.sf.net>
2012-10-26 05:57:46 +0000

    New code file 'dm_prefab.cc' -- currently empty, will contain code to load a DOOM map from a WAD file and pass the structure to the Lua scripts.

---
+++ gui/dm_prefab.cc
@@ -0,0 +1,43 @@
+//------------------------------------------------------------------------
+//  DOOM PREFAB loader
+//------------------------------------------------------------------------
+//
+//  Oblige Level Maker
+//
+//  Copyright (C) 2012 Andrew Apted
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------
+
+#include "headers.h"
+#include "hdr_fltk.h"
+#include "hdr_lua.h"
+#include "hdr_ui.h"
+
+#include "lib_file.h"
+#include "lib_util.h"
+#include "lib_wad.h"
+
+#include "main.h"
+#include "m_lua.h"
+
+#include "csg_main.h"
+
+#include "dm_prefab.h"
+#include "g_doom.h"
+#include "q_common.h"  // qLump_c
+
+
+// TODO
+
+//--- editor settings ---
+// vi:ts=2:sw=2:expandtab
