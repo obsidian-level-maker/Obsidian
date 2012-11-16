d21349c2516d731afdef6344b1b0f627568d4f50
Andrew Apted <ajapted@users.sf.net>
2012-11-16 06:05:35 +0000

    Improved the pack-source.sh script: 1. changed to run it from top level, added check to be sure 2. simplified script, removed $src variable 3. fixed missing lua_src/ and glbsp_src/ folders 4. fixed missing INSTALL.txt file 5. create empty obj_linux and obj_win32 folders

--- misc/pack-source.sh
+++ misc/pack-source.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
+if [ ! -d glbsp_src ]; then
+  echo "Run this script from the top level."
+  exit 1
+fi
+
 echo "Creating the source package for Oblige..."
 
-cd ../..
+cd ..
 
-src=oblige
-dest=PACK-SRC
+dest=~/PACK-SRC
 
 mkdir $dest
 
@@ -14,62 +18,74 @@ mkdir $dest
 #  Lua scripts
 #
 mkdir $dest/scripts
-cp -av $src/scripts/*.* $dest/scripts
+cp -av scripts/*.* $dest/scripts
 
 mkdir $dest/games
-cp -av $src/games/*.* $dest/games
+cp -av games/*.* $dest/games
 
 mkdir $dest/engines
-cp -av $src/engines/*.* $dest/engines
+cp -av engines/*.* $dest/engines
 
 mkdir $dest/modules
-cp -av $src/modules/*.* $dest/modules
+cp -av modules/*.* $dest/modules
 
 mkdir $dest/prefabs
-cp -av $src/prefabs/*.* $dest/prefabs
+cp -av prefabs/*.* $dest/prefabs
 
 #
 #  Source code
 #
 mkdir $dest/gui
-cp -av $src/gui/*.[chr]* $dest/gui
-cp -av $src/gui/*.ico $dest/gui
-cp -av $src/Makefile.* $dest/
+cp -av gui/*.[chr]* $dest/gui
+cp -av gui/*.ico $dest/gui
+cp -av Makefile.* $dest/
+
+mkdir $dest/lua_src
+cp -av lua_src/*.[chr]* $dest/lua_src
+
+mkdir $dest/glbsp_src
+cp -av glbsp_src/*.[chr]* $dest/glbsp_src
 
 mkdir $dest/tools
 mkdir $dest/tools/qsavetex
-cp -av $src/tools/qsavetex/*.[ch]* $dest/tools/qsavetex
-cp -av $src/tools/qsavetex/Makefile* $dest/tools/qsavetex
+cp -av tools/qsavetex/*.[ch]* $dest/tools/qsavetex
+cp -av tools/qsavetex/Makefile* $dest/tools/qsavetex
 
 mkdir $dest/misc
-cp -av $src/misc/pack*.sh $dest/misc
+cp -av misc/pack*.sh $dest/misc
+
+mkdir $dest/obj_linux
+mkdir $dest/obj_linux/lua
+mkdir $dest/obj_linux/glbsp
+
+mkdir $dest/obj_win32
+mkdir $dest/obj_win32/lua
+mkdir $dest/obj_win32/glbsp
 
 #
 #  Data files
 #
 mkdir $dest/data
-cp -av $src/data/*.lmp $dest/data || true
-cp -av $src/data/*.wad $dest/data || true
-cp -av $src/data/*.pak $dest/data || true
+cp -av data/*.lmp $dest/data || true
+cp -av data/*.wad $dest/data || true
+cp -av data/*.pak $dest/data || true
 
 mkdir $dest/data/doom1_boss
 mkdir $dest/data/doom2_boss
 
-cp -av $src/data/doom1_boss/*.* $dest/data/doom1_boss
-cp -av $src/data/doom2_boss/*.* $dest/data/doom2_boss
+cp -av data/doom1_boss/*.* $dest/data/doom1_boss
+cp -av data/doom2_boss/*.* $dest/data/doom2_boss
 
 #
 #  Documentation
 #
-mkdir $dest/doc
-cp -av $src/doc/*.* $dest/doc
+cp -av *.txt $dest
 
-cp -av $src/GPL.txt $dest
-cp -av $src/TODO.txt $dest
-cp -av $src/README.txt $dest
-cp -av $src/WISHLIST.txt $dest
-cp -av $src/CHANGES.txt $dest
-cp -av $src/AUTHORS.txt $dest
+rm $dest/LOGS.txt
+rm $dest/CONFIG.txt
+
+mkdir $dest/doc
+cp -av doc/*.* $dest/doc
 
 #
 # all done
