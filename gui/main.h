3466f67caa42a13008d39ea339bd3fa7cf734556
Andrew Apted <ajapted@users.sf.net>
2013-01-28 03:33:59 +0000

    Version bump, in honor of new DOOM lighting system.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "4.30"
-#define OBLIGE_HEX_VER  0x430
+#define OBLIGE_VERSION  "4.31"
+#define OBLIGE_HEX_VER  0x431
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
