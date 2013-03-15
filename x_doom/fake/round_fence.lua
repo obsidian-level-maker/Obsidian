f053809601d432f45c45525ac1246409fde87769
Andrew Apted <ajapted@users.sf.net>
2013-03-15 11:44:34 +0000

    Prefabs: created two "round fence" fake wall fabs.

---
+++ x_doom/fake/round_fence.lua
@@ -0,0 +1,20 @@
+--
+-- Rounded fat fences
+--
+
+DOOM.SKINS.Fake_RoundFence_1x1 =
+{
+  file   = "fake/round_fence.wad"
+  where  = "fake_fence"
+  shape  = "I"
+  need_sky = 1
+}
+
+DOOM.SKINS.Fake_RoundFence_2x1 =
+{
+  file   = "fake/round_fence2.wad"
+  where  = "fake_fence"
+  shape  = "I"
+  need_sky = 1
+}
+
