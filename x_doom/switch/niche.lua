9a2bd86767a3adda9f223599b8141d35e21582d7
Andrew Apted <ajapted@users.sf.net>
2013-03-16 01:23:39 +0000

    Prefabs: added skin file for niche switches : niche.lua

---
+++ x_doom/switch/niche.lua
@@ -0,0 +1,22 @@
+--
+--  Niche switches
+--
+
+DOOM.SKINS.Switch_niche1 =
+{
+  file   = "switch/niche1.wad"
+  where  = "closet"
+  switch = "sw_blue"
+  x_fit  = "frame"
+  y_fit  = "top"
+}
+
+DOOM.SKINS.Switch_niche2 =
+{
+  file   = "switch/niche2.wad"
+  where  = "closet2"
+  switch = "sw_blue"
+  x_fit  = "frame"
+  y_fit  = "top"
+}
+
