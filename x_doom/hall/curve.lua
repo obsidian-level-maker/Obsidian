15beb3c6f7c7c0d98b01efa511be0ebe4fc3dbdf
Andrew Apted <ajapted@users.sf.net>
2013-03-18 12:34:23 +0000

    Prefabs: began work on a "Curvey" hallway...

---
+++ x_doom/hall/curve.lua
@@ -0,0 +1,57 @@
+--
+-- Curvey hallway
+--
+
+DOOM.SKINS.Hall_curve1_I =
+{
+  file   = "hall/curve1_i_win.wad"
+  group  = "hall_curve1"
+  shape  = "I"
+}
+
+DOOM.SKINS.Hall_curve1_C =
+{
+  file   = "hall/curve1_c.wad"
+  group  = "hall_curve1"
+  shape  = "C"
+}
+
+DOOM.SKINS.Hall_curve1_T =
+{
+  file   = "hall/curve1_t_lit.wad"
+  group  = "hall_curve1"
+  shape  = "T"
+}
+
+DOOM.SKINS.Hall_curve1_P =
+{
+  file   = "hall/curve1_p.wad"
+  group  = "hall_curve1"
+  shape  = "P"
+}
+
+DOOM.SKINS.Hall_curve1_I_Stair =
+{
+  file   = "hall/curve1_st.wad"
+  group  = "hall_curve1"
+  shape  = "IS"
+}
+
+DOOM.SKINS.Hall_curve1_I_Lift =
+{
+  file   = "hall/curve1_lf.wad"
+  group  = "hall_curve1"
+  shape  = "IL"
+  tags   = 1
+}
+
+--
+-- Group information
+--
+
+DOOM.GROUPS.hall_curve1 =
+{
+  kind = "hall"
+  prob = 999
+}
+
