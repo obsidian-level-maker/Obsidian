49dab9873e1127b77e416bb6ae424daa95535b00
Andrew Apted <ajapted@users.sf.net>
2013-03-18 12:02:51 +0000

    Prefabs: tweaked skin tables for 'trim1' hallway.

--- x_doom/hall/trim.lua
+++ x_doom/hall/trim.lua
@@ -2,35 +2,35 @@
 -- Hallway with a trim
 --
 
-DOOM.SKINS.Hall_Basic_I =
+DOOM.SKINS.Hall_trim1_I =
 {
   file   = "hall/trim1_i_win.wad"
   group  = "hall_trim"
   shape  = "I"
 }
 
-DOOM.SKINS.Hall_Basic_C =
+DOOM.SKINS.Hall_trim1_C =
 {
   file   = "hall/trim1_c.wad"
   group  = "hall_trim"
   shape  = "C"
 }
 
-DOOM.SKINS.Hall_Basic_T =
+DOOM.SKINS.Hall_trim1_T =
 {
   file   = "hall/trim1_t_lit.wad"
   group  = "hall_trim"
   shape  = "T"
 }
 
-DOOM.SKINS.Hall_Basic_P =
+DOOM.SKINS.Hall_trim1_P =
 {
   file   = "hall/trim1_p.wad"
   group  = "hall_trim"
   shape  = "P"
 }
 
-DOOM.SKINS.Hall_Basic_I_Stair =
+DOOM.SKINS.Hall_trim1_I_Stair =
 {
   file   = "hall/trim1_st.wad"
   group  = "hall_trim"
@@ -43,7 +43,7 @@ DOOM.SKINS.Hall_Basic_I_Stair =
 --]]
 }
 
-DOOM.SKINS.Hall_Basic_I_Lift =
+DOOM.SKINS.Hall_trim1_I_Lift =
 {
   file   = "hall/trim1_lf.wad"
   group  = "hall_trim"
