3ba7dbd6509e4b605a1550b2ac4ea3ac64d8e6b0
Andrew Apted <ajapted@users.sf.net>
2013-03-20 05:36:03 +0000

    Prefabs / junctions: minor file rename, and simplified circle.lua

--- x_doom/junction/circle.lua
+++ x_doom/junction/circle.lua
@@ -2,35 +2,9 @@
 -- Big junction : circle floor and a pillar
 --
 
-DOOM.SKINS.Junc_Circle_gothic_P =
+DOOM.SKINS.Junc_circle_gothic =
 {
-  file   = "junction/circle_p.wad"
-  shape  = "P"
-}
-
-DOOM.SKINS.Junc_Circle_gothic_I =
-{
-  copy  = "Junc_Circle_gothic_P"
-  shape = "I"
-
-  east_h = 0
-  west_h = 0
-}
-
-DOOM.SKINS.Junc_Circle_gothic_C =
-{
-  copy  = "Junc_Circle_gothic_P"
-  shape = "C"
-
-  east_h = 0
-  north_h = 0
-}
-
-DOOM.SKINS.Junc_Circle_gothic_T =
-{
-  copy  = "Junc_Circle_gothic_P"
-  shape = "T"
-
-  north_h = 0
+  file   = "junction/circle.wad"
+  shape  = { I=1, C=1, T=1, P=1 }
 }
 
