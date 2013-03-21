b398c1b0f8dca5f8303ab490416543f792f7523e
Andrew Apted <ajapted@users.sf.net>
2013-03-21 03:36:57 +0000

    Prefabs: made stair versions of the curvey big junction.

--- x_doom/junction/curve.lua
+++ x_doom/junction/curve.lua
@@ -7,8 +7,29 @@ DOOM.SKINS.Junc_curve_C =
   file   = "junction/curve_c.wad"
   shape  = "C"
   group  = "hall_curve"
-  prob   = 999
+  prob   = 100
 }
+
+DOOM.SKINS.Junc_curve_stair1 =
+{
+  file   = "junction/curve_stair1.wad"
+  shape  = "C"
+  group  = "hall_curve"
+  prob   = 200
+
+  west = { h=80 }
+}
+
+DOOM.SKINS.Junc_curve_stair2 =
+{
+  file   = "junction/curve_stair2.wad"
+  shape  = "C"
+  group  = "hall_curve"
+  prob   = 9999
+
+  west = { h=-80 }
+}
+
 
 --
 -- Big junction : weird snakey T-shape
