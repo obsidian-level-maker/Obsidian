69bcefeb2ed9fc2c705fa6375f7e2f7a28c97cf1
Andrew Apted <ajapted@users.sf.net>
2013-03-22 02:51:00 +0000

    Prefabs: bit more work on "Y stair" joiner...

---
+++ x_doom/joiner/y_stair.lua
@@ -0,0 +1,15 @@
+--
+-- Joiner with three pillars
+--
+
+DOOM.SKINS.Joiner_Y_stair =
+{
+  file   = "joiner/y_stair.wad"
+  kind   = "unfinished"
+
+  prob   = 9999
+
+  north1 = { h=48 }
+  north3 = { h=48 }
+}
+
