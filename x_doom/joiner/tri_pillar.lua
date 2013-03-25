802ac96ea760655b0ed2f2ff3ea83fe19d652d26
Andrew Apted <ajapted@users.sf.net>
2013-03-25 11:25:10 +0000

    Prefabs: various improvements to the TriPillar joiner.

--- x_doom/joiner/tri_pillar.lua
+++ x_doom/joiner/tri_pillar.lua
@@ -5,9 +5,12 @@
 DOOM.SKINS.Joiner_TriPillar =
 {
   file   = "joiner/tri_pillar.wad"
+  z_fit  = { 64, 72 }
 
   room_kind = "outdoor"
   neighbor  = "outdoor"
+  height    = 160
+  bound_z1  = 0
 
   prob = 200
 }
