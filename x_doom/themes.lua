ca73a967dc4173a609fe1e8050982e74a13991e6
Andrew Apted <ajapted@users.sf.net>
2013-03-25 00:50:33 +0000

    DOOM: more variety of facades in the HELL theme.

--- x_doom/themes.lua
+++ x_doom/themes.lua
@@ -938,6 +938,18 @@ DOOM2.LEVEL_THEMES =
     outdoors  = { Hell2_outdoors=50 }
     caves     = { Hell2_cave=50 }
 
+    facades =
+    {
+      MARBLE2  = 50
+      SP_HOT1  = 50
+      MARBGRAY = 25
+      STONE3   = 25
+      GSTVINE1 = 15
+      GSTVINE2 = 15
+      BRONZE1  = 3
+      BROWN1   = 3
+    }
+
 --!!!!    logos = { Pic_Carve=90, Pic_Pill=50, Pic_Neon=5 }
 
 --[[
