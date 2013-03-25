bca6aa969c4a33ac15e54802cc57d6ef21c9ac2e
Andrew Apted <ajapted@users.sf.net>
2013-03-25 11:58:43 +0000

    Prefabs: created a 'brick gate' joiner (for Urban theme).

---
+++ x_doom/joiner/brick_gate.lua
@@ -0,0 +1,17 @@
+--
+-- Brick gate joiner
+--
+
+DOOM.SKINS.Joiner_brick_gate =
+{
+  file   = "joiner/brick_gate.wad"
+
+  room_kind = "outdoor"
+  neighbor  = "outdoor"
+
+  bound_z1  = 0
+  add_sky   = 1
+
+  prob = 2000
+}
+
