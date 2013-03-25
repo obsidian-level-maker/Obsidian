03212b50de443909ea266e966f9b0ad0c00faaba
Andrew Apted <ajapted@users.sf.net>
2013-03-25 11:42:05 +0000

    Prefabs: created a joiner with round walls and some steps.

---
+++ x_doom/joiner/round_stair.lua
@@ -0,0 +1,19 @@
+--
+-- Joiner with rounded walls + stair
+--
+
+DOOM.SKINS.Joiner_round_stair =
+{
+  file   = "joiner/round_stair.wad"
+
+  room_kind = "outdoor"
+  neighbor  = "outdoor"
+
+  bound_z1  = 0
+  add_sky   = 1
+
+  prob = 2000
+
+  north = { h=80 }
+}
+
