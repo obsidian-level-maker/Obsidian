5a2f7ff85479e87c16c02f9737eb7983d7c89a00
Andrew Apted <ajapted@users.sf.net>
2013-03-26 04:42:27 +0000

    Prefabs: created a bridge-over-lava joiner.

---
+++ x_doom/joiner/bridge.lua
@@ -0,0 +1,16 @@
+--
+-- Joiner with bridge over lava (etc)
+--
+
+DOOM.SKINS.Joiner_bridge =
+{
+  file   = "joiner/bridge.wad"
+
+  room_kind = "outdoor"
+  neighbor  = "outdoor"
+
+  add_sky   = 1
+
+  prob = 80000
+}
+
