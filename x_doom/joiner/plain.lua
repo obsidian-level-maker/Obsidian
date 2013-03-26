987f17447a314e3280b0b189ed38652ed1e776c5
Andrew Apted <ajapted@users.sf.net>
2013-03-26 04:04:47 +0000

    Prefabs: fixed several issues with the 'plain trap' joiner, especially the trigger lines disappearing -- hacked around that by lowering the nearby sectors by 1 unit.

--- x_doom/joiner/plain.lua
+++ x_doom/joiner/plain.lua
@@ -1,11 +1,25 @@
 --
--- Joiner with cage on each side
+-- Plain joiners
 --
 
-DOOM.SKINS.Joiner_cagey =
+DOOM.SKINS.Joiner_plain =
 {
   file = "joiner/cagey.wad"
 
   prob = 9000
+}
+
+DOOM.SKINS.Joiner_plain_cage =
+{
+  file = "joiner/plain_cage.wad"
+
+  prob = 3000
+}
+
+DOOM.SKINS.Joiner_plain_trap =
+{
+  file = "joiner/plain_trap.wad"
+
+  prob = 90000
 }
 
