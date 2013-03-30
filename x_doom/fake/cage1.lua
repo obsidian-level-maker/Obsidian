583db1d4137b293c71fb7622afbaa833d541d5b6
Andrew Apted <ajapted@users.sf.net>
2013-03-30 09:05:40 +0000

    Prefabs: renamed fake-building cages again, added cage1.lua

---
+++ x_doom/fake/cage1.lua
@@ -0,0 +1,24 @@
+--
+-- Cage bordering room
+--
+
+DOOM.SKINS.Fake_cage1 =
+{
+  file   = "fake/cage1.wad"
+  shape  = "F"
+
+  style = "cages"
+}
+
+--
+-- Cage between two rooms
+--
+
+DOOM.SKINS.Fake_cage1_fence =
+{
+  file   = "fake/cage_fence.wad"
+  shape  = "I"
+
+  style  = "cages"
+}
+
--- x_doom/fake/plain.lua
+++ x_doom/fake/plain.lua
@@ -1,10 +1,22 @@
 --
--- Plain fake wall (or corner)
+-- Plain fake wall / corner / fence
 --
 
-DOOM.SKINS.Fake_Plain =
+DOOM.SKINS.Fake_plain_F =
 {
   file   = "fake/plain.wad"
-  where  = "border"
+  shape  = "F"
+}
+
+DOOM.SKINS.Fake_plain_C =
+{
+  file   = "fake/plain.wad"
+  shape  = "C"
+}
+
+DOOM.SKINS.Fake_plain_I =
+{
+  file   = "fake/plain.wad"
+  shape  = "I"
 }
 
