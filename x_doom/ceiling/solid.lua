32f3a9d55183e64524469108bdf61daee5ee99d5
Andrew Apted <ajapted@users.sf.net>
2013-04-02 11:01:54 +0000

    Prefabs: added skin definitions for the basic floor/ceiling fabs.

---
+++ x_doom/ceiling/solid.lua
@@ -0,0 +1,9 @@
+--
+-- Solid ceiling
+--
+
+DOOM.SKINS.Ceiling_solid =
+{
+  file   = "ceiling/solid.wad"
+}
+
---
+++ x_doom/floor/plain.lua
@@ -0,0 +1,9 @@
+--
+-- Plain floor
+--
+
+DOOM.SKINS.Floor_plain =
+{
+  file   = "floor/plain.wad"
+}
+
