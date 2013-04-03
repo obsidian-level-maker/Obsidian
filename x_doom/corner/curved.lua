23627e10ae5b74b8ed91c825246322e091a7e82b
Andrew Apted <ajapted@users.sf.net>
2013-04-03 11:45:50 +0000

    Prefabs: created an "outie" (270 degree) corner prefab.

--- x_doom/corner/curved.lua
+++ x_doom/corner/curved.lua
@@ -5,6 +5,20 @@
 DOOM.SKINS.Corner_curved =
 {
   file   = "corner/curved.wad"
+  shape  = "C"
+
+  bound_z1 = 0
+  bound_z2 = 128
+}
+
+--
+-- Outie corner (270 degrees)
+--
+
+DOOM.SKINS.Corner_curved_outie =
+{
+  file   = "corner/curved_o.wad"
+  shape  = "O"
 
   bound_z1 = 0
   bound_z2 = 128
