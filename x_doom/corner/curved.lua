962b228a423114af78dc615783de5b73b469bd40
Andrew Apted <ajapted@users.sf.net>
2013-04-03 04:47:19 +0000

    Prefabs: created two corner prefabs.

---
+++ x_doom/corner/curved.lua
@@ -0,0 +1,12 @@
+--
+-- Curved corner
+--
+
+DOOM.SKINS.Corner_curved =
+{
+  file   = "corner/curved.wad"
+
+  bound_z1 = 0
+  bound_z2 = 128
+}
+
---
+++ x_doom/corner/support.lua
@@ -0,0 +1,12 @@
+--
+-- Corner with a support pillar
+--
+
+DOOM.SKINS.Corner_support =
+{
+  file   = "corner/support.wad"
+
+  bound_z1 = 0
+  bound_z2 = 128
+}
+
