3c1ec068a9820bf8324ed9c7173af96fc6d42ed2
Andrew Apted <ajapted@users.sf.net>
2013-04-28 06:09:54 +0000

    Prefabs: added Lua definition for "tall_arch".

---
+++ x_doom/joiner/tall_arch.lua
@@ -0,0 +1,22 @@
+--
+-- Tall archway
+--
+
+DOOM.SKINS.Joiner_TallArch =
+{
+  file   = "joiner/tall_arch.wad"
+  z_fit  = "top"
+
+  bound_z1 = 0
+  bound_z2 = 256
+
+  prob = 2000
+
+  room_kind = "outdoor"
+  neighbor  = "outdoor"
+  height    = 256
+
+  no_door = 1
+  add_sky = 1
+}
+
