41c0ffb9255246af984ce7c9e2bdbfb7ae5b338f
Andrew Apted <ajapted@users.sf.net>
2013-05-02 12:14:29 +0000

    Prefabs: created a switched door with see-through bars.

--- x_doom/door/sw_door.lua
+++ x_doom/door/sw_door.lua
@@ -13,3 +13,17 @@ DOOM.SKINS.Door_SW_blue =
   deep   = 48
 }
 
+
+DOOM.SKINS.Door_SW_bars =
+{
+  file   = "door/sw_door2.wad"
+  where  = "edge"
+  switch = "sw_blue"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 32
+
+  prob   = 50
+}
+
