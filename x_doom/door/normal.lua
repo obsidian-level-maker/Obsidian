e732f172111cc8f846577a62c16c13dc5d6827e3
Andrew Apted <ajapted@users.sf.net>
2013-05-03 05:25:07 +0000

    Prefabs / doors: added a wooden door variant for Urban/Hell themes.

--- x_doom/door/normal.lua
+++ x_doom/door/normal.lua
@@ -2,7 +2,7 @@
 -- Normal doors
 --
 
-DOOM.SKINS.Door_normal1 =
+DOOM.SKINS.Door_normal_tech =
 {
   file   = "door/normal1.wad"
   where  = "edge"
@@ -11,7 +11,25 @@ DOOM.SKINS.Door_normal1 =
   long   = 192
   deep   = 48
 
+  theme  = { urban=0.1, hell=0, other=1 }
   prob   = 90
+}
+
+
+DOOM.SKINS.Door_normal_gothic =
+{
+  file   = "door/normal1.wad"
+  where  = "edge"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+
+  theme  = { urban=1, hell=1 }
+  prob   = 90
+
+   tex_BIGDOOR1 = "BIGDOOR5"
+  flat_FLAT23 = "CEIL5_2"
 }
 
 
