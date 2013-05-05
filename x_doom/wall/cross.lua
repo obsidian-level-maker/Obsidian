90e1fb1f0731c39fef32db7d9f71d0ed584e1812
Andrew Apted <ajapted@users.sf.net>
2013-05-05 14:59:54 +0000

    Prefabs: created two hell-ish wall fabs: cross1.wad / torch3.wad

---
+++ x_doom/wall/cross.lua
@@ -0,0 +1,23 @@
+--
+-- Wall with hellish cross
+--
+
+DOOM.SKINS.Wall_cross1 =
+{
+  file   = "wall/cross1.wad"
+  where  = "edge"
+
+  long   = 128
+  deep   = 32
+
+  x_fit  = { 0,32, 96,128 }
+--z_fit  = { 108,128 }
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  theme  = "hell"
+
+  prob   = 999
+}
+
---
+++ x_doom/wall/torch.lua
@@ -0,0 +1,21 @@
+--
+-- Walls with torches
+--
+
+DOOM.SKINS.Wall_torch3 =
+{
+  file   = "wall/torch3.wad"
+  where  = "edge"
+
+  long   = 192
+  deep   = 64
+  x_fit  = { 64,128 }
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  theme  = "hell"
+
+  props_57 = { light=184, _factor=0.8 }
+}
+
