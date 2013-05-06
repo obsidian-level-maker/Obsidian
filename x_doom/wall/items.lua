8a4a0289e60f99780d5ca79d29f08499ae195fed
Andrew Apted <ajapted@users.sf.net>
2013-05-06 05:16:18 +0000

    Prefabs: added 'item3' wall prefab, which can hold pickup items.

---
+++ x_doom/wall/items.lua
@@ -0,0 +1,19 @@
+--
+-- Item porches
+--
+
+DOOM.SKINS.Wall_item3 =
+{
+  file   = "wall/item3.wad"
+  where  = "edge"
+
+  long   = 192
+  deep   = 64
+  x_fit  = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  prob   = 10
+}
+
