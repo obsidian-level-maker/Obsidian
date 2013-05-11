cc0db014d3baa213e6315d4ee6ec0a3fb0aa2c90
Andrew Apted <ajapted@users.sf.net>
2013-05-11 06:05:16 +0000

    Prefabs: created lava pit floor prefab (3x3 seeds).

---
+++ x_doom/floor/L_shape.lua
@@ -0,0 +1,17 @@
+--
+-- L shapes
+--
+
+DOOM.SKINS.Floor_L1_3x3 =
+{
+  file   = "floor/L1.wad"
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = { h=64 }
+  east   = { h=64 }
+  south  = { h=0 }
+  west   = { h=0 }
+}
+
---
+++ x_doom/floor/lava_pit.lua
@@ -0,0 +1,19 @@
+--
+-- Lava/Nukage pits
+--
+
+DOOM.SKINS.Floor_lava_pit1 =
+{
+  file   = "floor/lava_pit.wad"
+
+  seed_w = 3
+  seed_h = 3
+
+  liquid = "any"
+
+  north  = { h=0 }
+  east   = { h=0 }
+  south  = { h=0 }
+  west   = { h=0 }
+}
+
