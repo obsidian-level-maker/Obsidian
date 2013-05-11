3e9ea8c13da82f41fbe16ffff01c5917af88a6f1
Andrew Apted <ajapted@users.sf.net>
2013-05-11 09:39:39 +0000

    Prefabs: renamed bridge junctions (e.g. "bridge1.wad"), and added Lua definitions for them.

---
+++ x_doom/junction/bridge.lua
@@ -0,0 +1,31 @@
+--
+-- Bridge junctions
+--
+
+DOOM.SKINS.Junc_bridge1 =
+{
+  file   = "junction/curve_bridge1.wad"
+  shape  = "I"
+  group  = "hall_curve"
+
+  seed_w = 3
+  seed_h = 3
+  prob   = 100
+
+  liquid = 1
+}
+
+
+DOOM.SKINS.Junc_bridge2 =
+{
+  file   = "junction/curve_bridge2.wad"
+  shape  = "C"
+  group  = "hall_curve"
+
+  seed_w = 3
+  seed_h = 3
+  prob   = 400
+
+  west = { f_h=96 }
+}
+
