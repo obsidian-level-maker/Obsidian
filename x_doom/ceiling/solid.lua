d0d50c7f17010f1143653b6905f5082bd090da53
Andrew Apted <ajapted@users.sf.net>
2013-05-14 11:13:02 +0000

    Prefabs: 1. created a plain 3x3 floor prefab
    2. changed 'kind' field of plain floor and ceiling, to mark these
       prefabs as distinct from normal floors.

--- x_doom/ceiling/solid.lua
+++ x_doom/ceiling/solid.lua
@@ -4,6 +4,7 @@
 
 DOOM.SKINS.Ceiling_solid_1x1 =
 {
-  file   = "ceiling/solid_1x1.wad"
+  file = "ceiling/solid_1x1.wad"
+  kind = "plain_ceiling"
 }
 
--- x_doom/floor/plain.lua
+++ x_doom/floor/plain.lua
@@ -5,10 +5,26 @@
 DOOM.SKINS.Floor_plain_1x1 =
 {
   file   = "floor/plain_1x1.wad"
+  kind   = "plain_floor"
 
   north  = "."
   east   = "."
   south  = "."
   west   = "."
+}
+
+
+DOOM.SKINS.Floor_plain_3x3 =
+{
+  file   = "floor/plain_3x3.wad"
+  kind   = "plain_floor"
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  east   = "..."
+  south  = "..."
+  west   = "..."
 }
 
