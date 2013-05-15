ffad8c928d1e0443bf2e6992e22a8bc52feb664a
Andrew Apted <ajapted@users.sf.net>
2013-05-15 09:58:35 +0000

    Prefabs: created the first "room" prefab : a simple pillar.

---
+++ x_doom/room/pillar1.lua
@@ -0,0 +1,19 @@
+--
+-- Pillar
+--
+
+DOOM.SKINS.Room_pillar1 =
+{
+  file   = "room/pillar1.wad"
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  east   = "..."
+  south  = "..."
+  west   = "..."
+
+  prob   = 999
+}
+
