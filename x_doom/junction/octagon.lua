38b6ddb294d506f58d32c13ae6dbdb204f573f6d
Andrew Apted <ajapted@users.sf.net>
2013-05-16 04:49:11 +0000

    Prefabs: created another big junction : octagon.wad

---
+++ x_doom/junction/octagon.lua
@@ -0,0 +1,20 @@
+--
+-- Octagon-shaped junction
+--
+
+DOOM.SKINS.Junc_octagon =
+{
+  file   = "junction/octagon.wad"
+  shape  = { I=1, C=1, T=1, P=1 }
+
+  prob   = 2000
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "#.#"
+  south  = "#.#"
+  east   = "#.#"
+  west   = "#.#"
+}
+
