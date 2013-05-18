e586f3fec83ede625e2107cff704533fe916a570
Andrew Apted <ajapted@users.sf.net>
2013-05-18 10:48:20 +0000

    Prefabs: created columns_3x1 room fab.

---
+++ x_doom/room/columns_3x1.lua
@@ -0,0 +1,19 @@
+--
+-- 3x1 piece with narrow columns
+--
+
+DOOM.SKINS.Room_columns_3x1 =
+{
+  file   = "room/columns_3x1.wad"
+
+  seed_w = 3
+  seed_h = 1
+
+  north  = "..."
+  south  = "..."
+  east   = "."
+  west   = "."
+
+  prob   = 80
+}
+
