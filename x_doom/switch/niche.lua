8f2f27bc114bcbeb4ab6422ecab2ac6e20944ddf
Andrew Apted <ajapted@users.sf.net>
2013-05-18 13:31:40 +0000

    Prefabs: made the niche switches be theme specific.

--- x_doom/switch/niche.lua
+++ x_doom/switch/niche.lua
@@ -7,18 +7,25 @@ DOOM.SKINS.Switch_niche1 =
   file   = "switch/niche1.wad"
   where  = "closet"
   switch = "sw_blue"
+
   x_fit  = "frame"
   y_fit  = "bottom"
 
+  theme  = { tech=0, other=1 }
+
   props_35 = { light=208, _factor=0.5 }
 }
+
 
 DOOM.SKINS.Switch_niche2 =
 {
   file   = "switch/niche2.wad"
   where  = "closet"
   switch = "sw_blue"
+
   x_fit  = "frame"
   y_fit  = "bottom"
+
+  theme  = "tech"
 }
 
