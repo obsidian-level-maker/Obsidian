263df3cfe299cf76023a319785fd032ed1ef222c
Andrew Apted <ajapted@users.sf.net>
2013-05-18 13:10:33 +0000

    Prefabs: created 'skybm_3x1' ceiling, similar shape to redlt_3x1 but with sky and metal beams -- good for non-tech themes.

---
+++ x_doom/ceiling/skybm_3x1.lua
@@ -0,0 +1,16 @@
+--
+-- 3x1 skylight pair with beams
+--
+
+DOOM.SKINS.Ceiling_skybeams_3x1 =
+{
+  file = "ceiling/skybm_3x1.wad"
+
+  seed_w = 3
+  seed_h = 1
+
+  prob = 800
+
+  theme = { tech=0, other=1 }
+}
+
