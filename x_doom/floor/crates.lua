5e9800fe90b774023d5214de4a0c16699b4792b4
Andrew Apted <ajapted@users.sf.net>
2013-05-19 12:19:54 +0000

    Prefabs: created a 3x3 floor with a bunch of crates.

---
+++ x_doom/floor/crates.lua
@@ -0,0 +1,21 @@
+--
+-- Crates are great!
+--
+
+DOOM.SKINS.Floor_crate1 =
+{
+  file   = "floor/crate1.wad"
+
+  prob   = 999
+
+  seed_w = 3
+  seed_h = 3
+
+  north = "..."
+  south = "..."
+  east  = "..."
+  west  = "..."
+
+  complexity = 2
+}
+
