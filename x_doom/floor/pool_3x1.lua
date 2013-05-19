e54bc7a1e5691dbdf3449da541ce6357c92d9d26
Andrew Apted <ajapted@users.sf.net>
2013-05-19 12:43:28 +0000

    Prefabs: created a 3x1 floor fab with a liquid pool.

---
+++ x_doom/floor/pool_3x1.lua
@@ -0,0 +1,22 @@
+--
+-- Lava/Nukage pits
+--
+
+DOOM.SKINS.Floor_pool_3x1 =
+{
+  file   = "floor/pool_3x1.wad"
+
+  prob = 999
+
+  seed_w = 3
+  seed_h = 1
+
+  liquid = "any"
+
+  north  = "..."
+  south  = "..."
+  east   = "."
+  west   = "."
+
+}
+
