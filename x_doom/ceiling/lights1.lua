f83f5753fa16779a2c84c937bb8abe3a075d4eb8
Andrew Apted <ajapted@users.sf.net>
2013-05-19 06:59:32 +0000

    Prefabs: added another techy ceiling : lights3.wad

--- x_doom/ceiling/lights1.lua
+++ x_doom/ceiling/lights1.lua
@@ -1,16 +1,44 @@
 --
--- Ceiling with fancy light arrangement
+-- Ceilings with electrical lights
 --
 
-DOOM.SKINS.Ceiling_lights1 =
+DOOM.SKINS.Ceil_lights1 =
 {
   file = "ceiling/lights1.wad"
 
   seed_w = 3
   seed_h = 3
 
+  prob = 10
+
   theme = "tech"
 
-  prob = 10
+  complexity = 2
+}
+
+
+DOOM.SKINS.Ceil_lights2 =
+{
+  file = "ceiling/lights2.wad"
+
+  seed_w = 3
+  seed_h = 3
+
+  prob = 40
+
+  theme = { tech=1, hell=0, other=0.3 }
+}
+
+
+DOOM.SKINS.Ceil_lights3 =
+{
+  file = "ceiling/lights3.wad"
+
+  seed_w = 3
+  seed_h = 3
+
+  prob = 40
+
+  theme = "tech"
 }
 
--- x_doom/ceiling/solid.lua
+++ x_doom/ceiling/solid.lua
@@ -2,7 +2,7 @@
 -- Solid ceiling
 --
 
-DOOM.SKINS.Ceiling_solid_1x1 =
+DOOM.SKINS.Ceil_solid_1x1 =
 {
   file = "ceiling/solid_1x1.wad"
   kind = "plain_ceiling"
