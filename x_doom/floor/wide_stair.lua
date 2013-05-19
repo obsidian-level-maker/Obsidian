795c668423f20d399282679540a4627648f03b4a
Andrew Apted <ajapted@users.sf.net>
2013-05-19 13:33:04 +0000

    Prefabs: created a 'wide_stair' 3x1 floor fab.

---
+++ x_doom/floor/wide_stair.lua
@@ -0,0 +1,24 @@
+--
+-- Wide staircase
+--
+
+DOOM.SKINS.Floor_wide_stair =
+{
+  file = "floor/wide_stair.wad"
+
+  prob = 1999
+
+  seed_w = 3
+  seed_h = 1
+
+  edges =
+  {
+    b = { f_h=40 }
+  }
+
+  north  = "bbb"
+  south  = "..."
+  east   = "b"
+  west   = "b"
+}
+
