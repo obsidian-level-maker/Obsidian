5b7e15ff522f0e2c19d2caa608c0b95d824c5acb
Andrew Apted <ajapted@users.sf.net>
2013-05-20 05:00:03 +0000

    Prefabs: created 'cage4' prefabs (two variations for indoor and outdoor).

---
+++ x_doom/room/cage4.lua
@@ -0,0 +1,44 @@
+--
+-- Group of cages
+--
+
+DOOM.SKINS.Room_cage4 =
+{
+  file   = "room/cage4.wad"
+
+  prob   = 999
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  east   = "..."
+  south  = "..."
+  west   = "..."
+
+  style  = "cages"
+}
+
+
+DOOM.SKINS.Room_cage4_outdoor =
+{
+  file   = "room/cage4_o.wad"
+  kind   = "floor"
+
+  prob   = 999
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  east   = "..."
+  south  = "..."
+  west   = "..."
+
+  room_kind = "outdoor"
+
+  height = 192
+
+  style  = "cages"
+}
+
