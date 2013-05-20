576b2feef6172454b82d785f1f11740108ee3381
Andrew Apted <ajapted@users.sf.net>
2013-05-20 12:46:22 +0000

    Prefabs: created two big cages (floor fabs), one of which was based on a design by Derek Braun.

---
+++ x_doom/floor/cage_big.lua
@@ -0,0 +1,44 @@
+--
+-- Big cages
+--
+
+DOOM.SKINS.Floor_cage_big1 =
+{
+  file   = "floor/cage_big1.wad"
+
+  prob   = 3999
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  east   = "..."
+  south  = "..."
+  west   = "..."
+
+  style  = "cages"
+  liquid = "harmful"
+}
+
+
+DOOM.SKINS.Floor_cage_big2 =
+{
+  file   = "floor/cage_big2.wad"
+
+  prob   = 3999
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  east   = "..."
+  south  = "..."
+  west   = "..."
+
+  room_kind = "outdoor"
+
+  height = 256
+
+  style  = "cages"
+}
+
