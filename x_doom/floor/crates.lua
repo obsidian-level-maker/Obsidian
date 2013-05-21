538d1c99b8b8e17209b940e15e5f9d929dc91325
Andrew Apted <ajapted@users.sf.net>
2013-05-21 05:33:38 +0000

    Prefabs: created a 3x3 'pool' floor fab, with a small waterfall and some largish rocks.

--- x_doom/floor/crates.lua
+++ x_doom/floor/crates.lua
@@ -6,7 +6,7 @@ DOOM.SKINS.Floor_crate1 =
 {
   file   = "floor/crate1.wad"
 
-  prob   = 999
+  prob   = 500
 
   seed_w = 3
   seed_h = 3
--- x_doom/floor/pool_3x1.lua
+++ x_doom/floor/pool_3x1.lua
@@ -11,12 +11,32 @@ DOOM.SKINS.Floor_pool_3x1 =
   seed_w = 3
   seed_h = 1
 
-  liquid = "any"
-
   north  = "..."
   south  = "..."
   east   = "."
   west   = "."
 
+  liquid = "any"
+
+}
+
+
+DOOM.SKINS.Floor_pool_3x3 =
+{
+  file   = "floor/pool_3x3.wad"
+
+  prob = 9999
+
+  seed_w = 3
+  seed_h = 3
+
+  north  = "..."
+  south  = "..."
+  east   = "..."
+  west   = "..."
+
+  liquid = "any"
+
+  room_kind = "outdoor"
 }
 
