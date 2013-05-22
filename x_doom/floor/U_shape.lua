5e1245a6acf562b225d058e9d87f67d871b8a07d
Andrew Apted <ajapted@users.sf.net>
2013-05-22 04:36:32 +0000

    Prefabs: created another cratey floor fab.

--- x_doom/floor/U_shape.lua
+++ x_doom/floor/U_shape.lua
@@ -6,7 +6,7 @@ DOOM.SKINS.Floor_3x3_U2 =
 {
   file = "floor/U_shape2.wad"
 
-  prob = 100
+  prob = 20
 
   seed_w = 3
   seed_h = 3
--- x_doom/floor/crates.lua
+++ x_doom/floor/crates.lua
@@ -4,9 +4,27 @@
 
 DOOM.SKINS.Floor_crate1 =
 {
-  file   = "floor/crate1.wad"
+  file = "floor/crate1.wad"
 
-  prob   = 500
+  prob = 500
+
+  seed_w = 3
+  seed_h = 3
+
+  north = "..."
+  south = "..."
+  east  = "..."
+  west  = "..."
+
+  complexity = 2
+}
+
+
+DOOM.SKINS.Floor_crate2 =
+{
+  file = "floor/crate2.wad"
+
+  prob = 9999
 
   seed_w = 3
   seed_h = 3
