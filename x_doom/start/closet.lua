b7d46709dfeb9f905f584d5d5801878a65a5d50b
Andrew Apted <ajapted@users.sf.net>
2013-05-22 05:55:53 +0000

    Prefabs: created a second starting closet, with stairs and two exits.

--- x_doom/start/closet.lua
+++ x_doom/start/closet.lua
@@ -1,10 +1,24 @@
 --
--- Start closet
+-- Start closets
 --
-DOOM.SKINS.Start_Closet =
+
+DOOM.SKINS.Start_Closet1 =
 {
   file   = "start/closet1.wad"
   where  = "closet"
+
+  x_fit  = "frame"
+  y_fit  = "top"
+}
+
+
+DOOM.SKINS.Start_Closet2 =
+{
+  file   = "start/closet2.wad"
+  where  = "closet"
+
+  prob   = 9999
+
   x_fit  = "frame"
   y_fit  = "top"
 }
