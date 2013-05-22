5d9ecf68c3b0cb6933eb3ee7c87433d656c7aa4f
Andrew Apted <ajapted@users.sf.net>
2013-05-22 02:27:27 +0000

    Prefabs: experimental 'U_shape2' floor fab (unfinished).

--- x_doom/floor/L_shape.lua
+++ x_doom/floor/L_shape.lua
@@ -2,7 +2,7 @@
 -- L shapes
 --
 
-DOOM.SKINS.Floor_L1_3x3 =
+DOOM.SKINS.Floor_3x3_L1 =
 {
   file   = "floor/L_shape1.wad"
 
@@ -11,15 +11,15 @@ DOOM.SKINS.Floor_L1_3x3 =
   seed_w = 3
   seed_h = 3
 
+  edges =
+  {
+    b = { f_h=64 }
+  }
+
   north = "bbb"
   south = "..b"
 
   east  = "bbb"
   west  = "..b"
-
-  edges =
-  {
-    b = { f_h=64 }
-  }
 }
 
---
+++ x_doom/floor/U_shape.lua
@@ -0,0 +1,26 @@
+--
+-- U shapes
+--
+
+DOOM.SKINS.Floor_3x3_U2 =
+{
+  file = "floor/U_shape2.wad"
+
+  prob = 100
+
+  seed_w = 3
+  seed_h = 3
+
+  edges =
+  {
+    b = { f_h=64 }
+    c = { f_h=-32 }
+  }
+
+  north = "bcb"
+  south = "..."
+
+  east  = ".bb"
+  west  = ".bb"
+}
+
