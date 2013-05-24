5e5071a2a6c72cf4a73866e7b246b7fc9ec54ea4
Andrew Apted <ajapted@users.sf.net>
2013-05-24 03:14:03 +0000

    Prefabs: created test prefabs for keyed and switched joiners.

---
+++ x_doom/joiner/switched.lua
@@ -0,0 +1,18 @@
+--
+-- Remotely switched joiners
+--
+
+DOOM.SKINS.Joiner_switched_test =
+{
+  file   = "joiner/sw_test.wad"
+  switch = "sw_blue"
+
+  prob = 60
+
+  seed_w = 3
+  seed_h = 1
+
+  north = "#.#"
+  south = "#.#"
+}
+
