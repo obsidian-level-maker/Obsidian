4cf4ed841e2167ab757e5413e96bc2c2b77ce1b2
Andrew Apted <ajapted@users.sf.net>
2013-06-06 09:43:29 +0000

    DOOM: lowered damage of Nukage and Slime liquids --> sector type #5

--- x_doom/materials.lua
+++ x_doom/materials.lua
@@ -34,11 +34,11 @@ DOOM.LIQUIDS =
 {
   water  = { mat="FWATER1", light=168, special=0 }
   blood  = { mat="BLOOD1",  light=168, special=0 }
-  nukage = { mat="NUKAGE1", light=178, special=16, damage=20 }
+  nukage = { mat="NUKAGE1", light=178, special=5,  damage=10 }
   lava   = { mat="LAVA1",   light=178, special=16, damage=20 }
 
   -- Doom II only --
-  slime  = { mat="SLIME01", light=168, special=16, damage=20 }
+  slime  = { mat="SLIME01", light=168, special=5, damage=10 }
 }
 
 
