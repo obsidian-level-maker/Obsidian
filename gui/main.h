d3b144587ea5a83c6149159b471dfc982c0e11d9
Andrew Apted <ajapted@users.sf.net>
2013-07-11 09:05:48 +0000

    Version bump (after WIP package).

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "5.07-WIP"
-#define OBLIGE_HEX_VER  0x507
+#define OBLIGE_VERSION  "5.08-WIP"
+#define OBLIGE_HEX_VER  0x508
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
