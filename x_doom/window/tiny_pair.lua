892b9ae7f1d3cf76a449912dfab9bbfb074766f3
Andrew Apted <ajapted@users.sf.net>
2014-01-08 08:44:46 +0000

    DOOM / wad-fabs: created 'window/tiny_pair' prefab.

---
+++ x_doom/window/tiny_pair.lua
@@ -0,0 +1,17 @@
+--
+-- Window with two 64x64 holes
+--
+
+DOOM.SKINS.Window_tiny_pair =
+{
+  file   = "window/tiny_pair.wad"
+  where  = "edge"
+  long   = 160
+  deep   = 32
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  prob = 90
+}
+
