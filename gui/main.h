4e1f5602a4357bcc460660c6d0a23769205c4101
Andrew Apted <ajapted@users.sf.net>
2014-01-08 01:34:21 +0000

    Version bump, in honor of new quest, zone and weapon logic.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "6.01"
-#define OBLIGE_HEX_VER  0x601
+#define OBLIGE_VERSION  "6.02"
+#define OBLIGE_HEX_VER  0x602
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
