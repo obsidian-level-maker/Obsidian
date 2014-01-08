dc9c66a31fc28dca144cf89f897f2908bbc98a08
Andrew Apted <ajapted@users.sf.net>
2014-01-08 11:47:36 +0000

    DOOM / wad-fabs: created a 'narrow' window prefab.

---
+++ x_doom/window/narrow.lua
@@ -0,0 +1,37 @@
+--
+-- Window, narrow and short
+--
+
+DOOM.SKINS.Window_narrow =
+{
+  file   = "window/narrow.wad"
+  where  = "edge"
+  long   = 192
+  deep   = 32
+
+  bound_z1 = 0
+  bound_z2 = 112
+
+  prob = 90
+}
+
+
+--
+-- Tall version (expands vertically)
+--
+
+DOOM.SKINS.Window_narrow_tall =
+{
+  file   = "window/narrow.wad"
+  where  = "edge"
+  long   = 192
+  deep   = 32
+
+  bound_z1 = 0
+  bound_z2 = 112
+
+  z_fit = { 64, 88 }
+
+  prob = 90
+}
+
