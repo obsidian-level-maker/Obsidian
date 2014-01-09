b82a0491641e9afdcf850fea480c4e9b2007d934
Andrew Apted <ajapted@users.sf.net>
2014-01-09 23:09:37 +0000

    DOOM / wad-fabs: created basic Arch prefab.

---
+++ x_doom/door/arch.lua
@@ -0,0 +1,22 @@
+--
+-- Archway
+--
+
+DOOM.SKINS.Arch_plain =
+{
+  file   = "door/arch.wad"
+  where  = "edge"
+  long   = 192
+  deep   = 16
+  over   = 16
+
+  x_fit  = { 0,4, 84,108, 188,192 }
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  prob = 90
+
+  tex_DOORTRAK = "?track"
+}
+
