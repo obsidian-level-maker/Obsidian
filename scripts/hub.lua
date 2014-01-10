20c6942f3a8408958b1df33741451cb06b455357
Andrew Apted <ajapted@users.sf.net>
2014-01-10 10:51:31 +0000

    Checked-in the V5 code for handling Hubs : scripts/hub.lua

---
+++ scripts/hub.lua
@@ -0,0 +1,244 @@
+----------------------------------------------------------------
+--  HUB HANDLING
+----------------------------------------------------------------
+--
+--  Oblige Level Maker
+--
+--  Copyright (C) 2006-2014 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+----------------------------------------------------------------
+
+
+function Hub_connect_levels(epi, keys)
+
+  local function connect(src, dest, kind)
+    assert(src!= dest)
+
+    local LINK =
+    {
+      kind = kind
+      src  = src
+      dest = dest
+    }
+
+    table.insert( src.hub_links, LINK)
+    table.insert(dest.hub_links, LINK)
+    table.insert( epi.hub_links, LINK)
+  end
+
+
+  local function dump()
+    gui.debugf("\nHub links:\n")
+
+    each link in epi.hub_links do
+      gui.debugf("  %s --> %s\n", link.src.name, link.dest.name)
+    end
+
+    gui.debugf("\n")
+  end
+
+
+  ---| Hub_connect_levels |---
+
+  local levels = table.copy(epi.levels)
+
+  assert(#levels >= 4)
+
+  keys = table.copy(keys)
+
+  rand.shuffle(keys)
+
+  -- setup
+  epi.hub_links = { }
+  epi.used_keys = { }
+
+  each L in levels do
+    L.hub_links = { }
+  end
+
+  -- create the initial chain, which consists of the start level, end
+  -- level and possibly a level or two in between.
+
+  local start_L = table.remove(levels, 1)
+  local end_L   = table.remove(levels, #levels)
+
+  assert(end_L.kind == "BOSS")
+
+  local chain = { start_L }
+
+  for loop = 1, rand.sel(75, 2, 1) do
+    assert(#levels >= 1)
+
+    table.insert(chain, table.remove(levels, 1))
+  end
+
+  table.insert(chain, end_L)
+
+  for i = 1, #chain - 1 do
+    connect(chain[i], chain[i+1], "chain")
+  end
+
+  -- the remaining levels just branch off the current chain
+
+  each L in levels do
+    -- pick existing level to branch from (NEVER the end level)
+    local src = chain[rand.irange(1, #chain - 1)]
+
+    -- prefer an level with no branches so far
+    if #src.hub_links > 0 then
+      src = chain[rand.irange(1, #chain - 1)]
+    end
+
+    connect(src, L, "branch")
+
+    -- assign keys to these branch levels
+
+    if L.kind != "SECRET" and not table.empty(keys) then
+      L.hub_key = rand.key_by_probs(keys)
+
+      keys[L.hub_key] = nil
+
+      table.insert(epi.used_keys, L.hub_key)
+
+      gui.debugf("Hub: assigning key '%s' --> %s\n", L.hub_key, L.name)
+    end
+  end
+
+  dump()
+end
+
+
+
+function Hub_assign_keys(epi, keys)
+  -- determines which keys can be used on which levels
+
+  keys = table.copy(keys)
+
+  local function level_for_key()
+    for loop = 1,999 do
+      local idx = rand.irange(1, #epi.levels)
+      local L = epi.levels[idx]
+
+      if L.kind == "SECRET" then continue end
+
+      if L.hub_key and rand.odds(95) then continue end
+
+      local already = #L.usable_keys
+
+      if already == 0 then return L end
+      if already == 1 and rand.odds(20) then return L end
+      if already >= 2 and rand.odds(4)  then return L end
+    end
+
+    error("level_for_key failed.")
+  end
+
+  each L in epi.levels do
+    L.usable_keys = { }
+  end
+
+  -- take away keys already used in the branch levels
+  each name in epi.used_keys do
+    keys[name] = nil
+  end
+
+  while not table.empty(keys) do
+    local name = rand.key_by_probs(keys)
+    local prob = keys[name]
+
+    keys[name] = nil
+
+    local L = level_for_key()
+
+    L.usable_keys[name] = prob
+
+    gui.debugf("Hub: may use key '%s' --> %s\n", name, L.name)
+  end
+end
+
+
+
+function Hub_assign_weapons(epi)
+
+  -- Hexen and Hexen II only have two pick-up-able weapons per class.
+  -- The normal weapon placement logic does not work well for that,
+  -- instead we pick which levels to place them on.
+
+  local a = rand.sel(75, 2, 1)
+  local b = rand.sel(75, 3, 4)
+
+  epi.levels[a].hub_weapon = "weapon2"
+  epi.levels[b].hub_weapon = "weapon3"
+
+  gui.debugf("Hub: assigning 'weapon2' --> %s\n", epi.levels[a].name)
+  gui.debugf("Hub: assigning 'weapon3' --> %s\n", epi.levels[b].name)
+
+  local function mark_assumes(start, weapon)
+    for i = start, #epi.levels do
+      local L = epi.levels[i]
+      if not L.assume_weapons then L.assume_weapons = { } end
+      L.assume_weapons[weapon] = true
+    end
+  end
+
+  mark_assumes(a, "weapon2")
+  mark_assumes(b, "weapon3")
+
+  mark_assumes(#epi.levels, "weapon4")
+end
+
+
+
+function Hub_assign_pieces(epi, pieces)
+
+  -- assign weapon pieces (for HEXEN's super weapon) to levels
+
+  assert(#pieces < #epi.levels)
+
+  local levels = { }
+
+  each L in epi.levels do
+    if L.kind != "BOSS" and L.kind != "SECRET" then
+      table.insert(levels, L)
+    end
+  end
+
+  assert(#levels >= #pieces)
+
+  rand.shuffle(levels)
+
+  each piece in pieces do
+    local L = levels[_index]
+
+    L.hub_piece = piece
+
+    gui.debugf("Hub: assigning piece '%s' --> %s\n", piece, L.name)
+  end 
+end
+
+
+
+function Hub_find_link(kind)
+  each link in LEVEL.hub_links do
+    if kind == "START" and link.dest.name == LEVEL.name then
+      return link
+    end
+
+    if kind == "EXIT" and link.src.name == LEVEL.name then
+      return link
+    end
+  end
+
+  return nil  -- none
+end
+
