308782b6cd0e2602f72a547c7d0f09106fafff28
Andrew Apted <ajapted@users.sf.net>
2014-01-10 05:16:36 +0000

    DOOM / wad-fabs: added the 'Arch_stair' prefab.

---
+++ x_doom/door/arch_stair.lua
@@ -0,0 +1,22 @@
+--
+-- Archway with stair
+--
+
+DOOM.SKINS.Arch_stair =
+{
+  file   = "door/arch_stair.wad"
+  where  = "edge"
+  long   = 192
+  deep   = 16
+  over   = 16
+
+  x_fit  = { 0,4, 84,108, 188,192 }
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  prob = 90
+
+  tex_DOORTRAK = "?track"
+}
+
