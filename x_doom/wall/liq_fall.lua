69fd3d3c513570c16a3606d15526f4bb98c04c6d
Andrew Apted <ajapted@users.sf.net>
2014-01-11 00:46:44 +0000

    DOOM: created a 'liquid_fall' wall prefab.

---
+++ x_doom/wall/liq_fall.lua
@@ -0,0 +1,21 @@
+--
+-- Wall with a nukage/lava/water fall
+--
+
+DOOM.SKINS.Wall_liquid_fall =
+{
+  file   = "wall/liq_fall.wad"
+  where  = "edge"
+
+  long   = 192
+  deep   = 16
+
+  x_fit  = "frame"
+  z_fit  = { 0,16, 144,152 }
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  prob   = 50
+}
+
