1c65f00da915292f2efec5cf8d7143dcc874010e
Andrew Apted <ajapted@users.sf.net>
2014-01-26 07:37:51 +0000

    DOOM / wad-fabs: created 'lite_3' prefab -- three glowing niches.

---
+++ games/doom/wall/lite_3.lua
@@ -0,0 +1,43 @@
+--
+-- Triple lighting niche
+--
+
+DOOM.SKINS.Pic_lite3_blue =
+{
+  file   = "wall/lite_3.wad"
+  where  = "edge"
+
+  long   = 192
+  deep   = 16
+
+  x_fit  = "frame"
+  z_fit  = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 176
+
+  theme  = "tech"
+  prob   = 30
+}
+
+
+DOOM.SKINS.Pic_lite3_white =
+{
+  file   = "wall/lite_3.wad"
+  where  = "edge"
+
+  long   = 192
+  deep   = 16
+
+  x_fit  = "frame"
+  z_fit  = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 176
+
+  theme  = "tech"
+  prob   = 10
+
+  tex_LITEBLU4 = "LITE5"
+}
+
