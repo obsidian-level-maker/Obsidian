12d09ddc11dc8e93aed146bda7755afccab8aa68
Andrew Apted <ajapted@users.sf.net>
2014-04-09 04:46:27 +0000

    Version bump to 6.05 -- ready for the next release.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "6.04"
-#define OBLIGE_HEX_VER  0x604
+#define OBLIGE_VERSION  "6.05"
+#define OBLIGE_HEX_VER  0x605
 
 
 #define CONFIG_FILENAME  "CONFIG.txt"
