d43f31d57e5438738f528d80946eff5572ff35af
Andrew Apted <ajapted@users.sf.net>
2014-05-01 04:25:49 +0000

    Test-it script : actually invoke OBLIGE.

--- misc/test-it.sh
+++ misc/test-it.sh
@@ -25,21 +25,22 @@ declare -a GAMES
 GAMES=("doom1" "doom2" "freedoom" "ultdoom" "tnt" "plutonia")
 index=$(($RANDOM % ${#GAMES}))
 def_game=${GAMES[$index]}
-echo def_game = $def_game
 
 declare -a SIZES
 SIZES=("prog" "mixed" "tiny" "small" "regular" "large")
 index=$(($RANDOM % ${#SIZES}))
 def_size=${SIZES[$index]}
-echo def_size = $def_size
 
 declare -a THEMES
 THEMES=("mixed" "mixed" "jumble" "original" "psycho")
 index=$(($RANDOM % ${#THEMES}))
 def_theme=${THEMES[$index]}
-echo def_theme = $def_theme
 
-## ./Oblige --nolight seed=${seed} length=${def_length} $@ -b ${base}.out > ${base}.log
+set -x
+./Oblige --nolight seed=${seed} length=${def_length} \
+		game=${def_game} size=${def_size} theme=${def_theme} \
+		$@ -b ${base}.out > ${base}.log
+set +x
 
 # --- editor settings ---
 # vi:ts=4:sw=4:noexpandtab
