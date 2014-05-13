2d03a893a5a82ac6dee70263f3a32c9a8207f757
Andrew Apted <ajapted@users.sf.net>
2014-05-13 11:47:04 +0000

    DOOM: added back the "drop-off" border prefabs, resized from the V5 versions, and using a simpler naming scheme.

---
+++ games/doom/border/dropoff.lua
@@ -0,0 +1,49 @@
+--
+-- Drop-off border prefabs
+--
+
+DOOM.SKINS.Border_dropoff_c =
+{
+  file   = "border/dropoff_c2.wad"
+  group  = "border_dropoff"
+  shape  = "C"
+  seed_w = 3
+  seed_h = 3
+
+  add_sky = 1
+}
+
+
+DOOM.SKINS.Border_dropoff_t =
+{
+  file   = "border/dropoff_t.wad"
+  group  = "border_dropoff"
+  shape  = "T"
+  seed_w = 3
+  seed_h = 3
+
+  add_sky = 1
+}
+
+
+DOOM.SKINS.Border_dropoff_s =
+{
+  file   = "border/dropoff_s.wad"
+  group  = "border_dropoff"
+  shape  = "T"
+  seed_w = 2
+  seed_h = 3
+
+  add_sky = 1
+}
+
+
+--
+-- Group information
+--
+
+DOOM.GROUPS.border_dropoff =
+{
+  kind = "border"
+}
+
