d5e005df03b5818ffcf13850673879c4ef72831f
Andrew Apted <ajapted@users.sf.net>
2014-05-19 02:13:06 +0000

    DOOM / prefabs : created an archway with wooden supports, to be used between cave rooms.

---
+++ games/doom/door/wood_arch.lua
@@ -0,0 +1,25 @@
+--
+-- Archway with wooden supports
+--
+
+DOOM.SKINS.Arch_woody =
+{
+  file   = "door/arch.wad"
+  where  = "edge"
+  long   = 192
+  deep   = 16
+  over   = 16
+
+  x_fit  = { 0,8, 80,112, 184,192 }
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  prob = 90
+
+  room     = "cave"
+  neighbor = "cave"
+
+  rank = 2
+}
+
