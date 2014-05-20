546fc0587da3cf0493f9e8dec3bd61f00ac6a2a3
Andrew Apted <ajapted@users.sf.net>
2014-05-20 08:43:21 +0000

    DOOM / prefabs : added 'large.wad' and 'techy.wad' door prefabs.

---
+++ games/doom/door/large.lua
@@ -0,0 +1,16 @@
+--
+-- Large door (128x120 units)
+--
+
+DOOM.SKINS.Door_large =
+{
+  file   = "door/large.wad"
+  where  = "edge"
+
+  x_fit  = "frame"
+
+  long   = 192
+  deep   = 16
+  over   = 16
+}
+
---
+++ games/doom/door/techy.lua
@@ -0,0 +1,16 @@
+--
+-- Low door with techy texture
+--
+
+DOOM.SKINS.Door_techy =
+{
+  file   = "door/techy.wad"
+  where  = "edge"
+
+  x_fit  = "frame"
+
+  long   = 192
+  deep   = 16
+  over   = 16
+}
+
