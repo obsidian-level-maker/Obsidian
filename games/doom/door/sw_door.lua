109ce779a4061d4ef43a4dc268acce80a8324bdb
Andrew Apted <ajapted@users.sf.net>
2014-05-20 07:04:51 +0000

    DOOM / prefabs : added 'sw_door.wad' switched door, based on the V5 version of the same thing.

---
+++ games/doom/door/sw_door.lua
@@ -0,0 +1,17 @@
+--
+-- Switched doors
+--
+
+DOOM.SKINS.Door_SW_blue =
+{
+  file   = "door/sw_door.wad"
+  where  = "edge"
+  switch = "sw_blue"
+
+  x_fit  = "frame"
+
+  long   = 192
+  deep   = 16
+  over   = 16
+}
+
