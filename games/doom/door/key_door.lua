a2adfc7333f9d7df7b02f00530b9430124e4e5a9
Andrew Apted <ajapted@users.sf.net>
2014-05-20 05:06:17 +0000

    DOOM / prefabs : added 'key_door.wad' and '.lua' -- based on the V5 version but shrunk and modified the general shape.

---
+++ games/doom/door/key_door.lua
@@ -0,0 +1,95 @@
+--
+-- Keyed doors
+--
+
+DOOM.SKINS.Locked_kc_blue =
+{
+  file   = "door/key_door.wad"
+  where  = "edge"
+  key    = "kc_blue"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+  over   = 32
+
+  tex_DOORRED = "DOORBLU"
+  line_33 = 32
+}
+
+
+DOOM.SKINS.Locked_kc_yellow =
+{
+  file   = "door/key_door.wad"
+  where  = "edge"
+  key    = "kc_yellow"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+  over   = 32
+
+  tex_DOORRED = "DOORYEL"
+  line_33 = 34
+}
+
+
+DOOM.SKINS.Locked_kc_red =
+{
+  file   = "door/key_door.wad"
+  where  = "edge"
+  key    = "kc_red"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+  over   = 32
+}
+
+
+DOOM.SKINS.Locked_ks_blue =
+{
+  file   = "door/key_door.wad"
+  where  = "edge"
+  key    = "ks_blue"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+  over   = 32
+
+  tex_DOORRED = "DOORBLU2"
+  line_33 = 32
+}
+
+
+DOOM.SKINS.Locked_ks_yellow =
+{
+  file   = "door/key_door.wad"
+  where  = "edge"
+  key    = "ks_yellow"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+  over   = 32
+
+  tex_DOORRED = "DOORYEL2";
+  line_33 = 34
+}
+
+
+DOOM.SKINS.Locked_ks_red =
+{
+  file   = "door/key_door.wad"
+  where  = "edge"
+  key    = "ks_red"
+
+  x_fit  = "frame"
+  long   = 192
+  deep   = 48
+  over   = 32
+
+  tex_DOORRED = "DOORRED2"
+}
+
