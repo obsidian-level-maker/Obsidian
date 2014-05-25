d21fbb2133489dfbe5bd60e173fde54af8e9e071
Andrew Apted <ajapted@users.sf.net>
2014-05-25 14:00:59 +0000

    DOOM : created a secret fence, for usage between two outdoor rooms.

---
+++ games/doom/door/secr_fence.lua
@@ -0,0 +1,18 @@
+--
+-- Secret outdoor fence
+--
+
+DOOM.SKINS.Fence_secret =
+{
+  file   = "door/secr_fence.wad"
+  where  = "edge"
+  
+  long   = 192
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+
+  secret = 1
+}
+
