0ec161ef1dafb6f139fb34c0b1b7a1b0d3c44a0c
Andrew Apted <ajapted@users.sf.net>
2014-05-25 13:32:49 +0000

    DOOM : created door prefab 'door_w_bars.wad' + lua definition.

---
+++ games/doom/door/door_w_bars.lua
@@ -0,0 +1,20 @@
+--
+-- Door with lowering bars (remote activated)
+--
+
+DOOM.SKINS.Door_with_bars =
+{
+  file   = "door/sw_door.wad"
+  where  = "edge"
+  switch = "sw_blue"
+
+  x_fit  = "frame"
+
+  long   = 192
+  deep   = 16
+  over   = 16
+
+  tex_COMPBLUE = "GRAY1"
+  flat_FLAT14  = "GRAY1"
+}
+
