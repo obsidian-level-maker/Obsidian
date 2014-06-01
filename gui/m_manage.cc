ba435b67fdea8a420e1c03f5e8d27a8ac3e4ef66
Andrew Apted <ajapted@users.sf.net>
2014-06-01 12:02:33 +0000

    Config Manager : 1. added Clear() and Enable() methods, these deactivate and activate    certain buttons like "Save".
    2. implemented ReadCurrentSettings() method, and use it when the
       window is first opened.
    
    3. placed the clipboard buttons in a group, prevents the buttons
       resizing when the window is resized.

--- gui/m_manage.cc
+++ gui/m_manage.cc
@@ -22,7 +22,10 @@
 #include "hdr_fltk.h"
 #include "hdr_ui.h"
 
+#include "lib_util.h"
+
 #include "main.h"
+#include "m_lua.h"
 
 
 #define MANAGE_WIN_W  620
@@ -60,6 +63,70 @@ public:
 		return want_quit;
 	}
 
+	void MarkSource(const char *where)
+	{
+		if (! where)
+			where = "NONE";
+
+		char *full = StringPrintf(" Configuration Text  [%s]", where);
+
+		conf_disp->copy_label(full);
+
+		StringFree(full);
+
+		redraw();
+	}
+
+	void Clear()
+	{
+		MarkSource(NULL);
+
+		text_buf->select(0, text_buf->length());
+		text_buf->remove_selection();
+
+		save_but->deactivate();
+		 use_but->deactivate();
+
+		 cut_but->deactivate();
+		copy_but->deactivate();
+
+		redraw();
+	}
+
+	void Enable()
+	{
+		save_but->activate();
+		 use_but->activate();
+
+		 cut_but->activate();
+		copy_but->activate();
+
+		redraw();
+	}
+
+	void ReadCurrentSettings()
+	{
+		Clear();
+
+		text_buf->append("-- CONFIG FILE : OBLIGE " OBLIGE_VERSION "\n"); 
+		text_buf->append("-- " OBLIGE_TITLE " (C) 2006-2014 Andrew Apted\n");
+		text_buf->append("-- http://oblige.sourceforge.net/\n\n");
+
+		std::vector<std::string> lines;
+
+		ob_read_all_config(&lines);
+
+		for (unsigned int i = 0 ; i < lines.size() ; i++)
+		{
+			text_buf->append(lines[i].c_str());
+			text_buf->append("\n");
+		}
+
+		MarkSource("current settings");
+
+		Enable();
+	}
+
 private:
 	static void callback_Quit(Fl_Widget *w, void *data)
 	{
@@ -88,40 +155,41 @@ UI_Manage_Config::UI_Manage_Config(const char *label) :
 
 
 	text_buf = new Fl_Text_Buffer();
-	text_buf->append("This\nIs\nA\nTest");
 
 
-	conf_disp = new Fl_Text_Display(190, 30, 410, 288, " Configuration : current OBLIGE settings");
+	conf_disp = new Fl_Text_Display(190, 30, 410, 288, " Reading Config....");
 	conf_disp->align(Fl_Align(FL_ALIGN_TOP_LEFT));
 	conf_disp->labelsize(16);
 	conf_disp->buffer(text_buf);
+	conf_disp->textfont(FL_COURIER);
+	conf_disp->textsize(16);
 
 
 	/* Main Buttons */
 
 	Fl_Box * o;
 
-	load_but = new Fl_Button(20, 25, 100, 35, "  Load @-3>");
+	load_but = new Fl_Button(30, 25, 100, 35, "  Load @-3>");
 	load_but->labelsize(FL_NORMAL_SIZE + 0);
 
-	extract_but = new Fl_Button(20, 85, 100, 35, "  Extract @-3>");
+	extract_but = new Fl_Button(30, 85, 100, 35, "  Extract @-3>");
 	extract_but->labelsize(FL_NORMAL_SIZE);
 
 	o = new Fl_Box(15, 116, 171, 40, "from a WAD or PAK file");
 	o->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
 	o->labelsize(14);
 
-	save_but = new Fl_Button(20, 165, 100, 35, "Save");
+	save_but = new Fl_Button(30, 165, 100, 35, "Save");
 	save_but->labelsize(FL_NORMAL_SIZE);
 
-	use_but = new Fl_Button(20, 225, 100, 35, "Use");
+	use_but = new Fl_Button(30, 225, 100, 35, "Use");
 	use_but->labelsize(FL_NORMAL_SIZE);
 
 	o = new Fl_Box(15, 256, 173, 50, "Note: this will replace\nall current settings!");
 	o->align(Fl_Align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE));
 	o->labelsize(14);
 
-	close_but = new Fl_Button(30, 320, 100, 40, "Close");
+	close_but = new Fl_Button(30, 325, 100, 40, "Close");
 	close_but->labelfont(FL_HELVETICA_BOLD);
 	close_but->labelsize(FL_NORMAL_SIZE);
 	close_but->callback(callback_Quit, this);
@@ -129,22 +197,28 @@ UI_Manage_Config::UI_Manage_Config(const char *label) :
 
 	/* Clipboard buttons */
 
-	o = new Fl_Box(215, 318, 355, 29, " Clipboard Operations");
-	o->align(Fl_Align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE));
-	o->labelsize(14);
+	{
+		Fl_Group *g = new Fl_Group(conf_disp->x(), 318, conf_disp->w(), MANAGE_WIN_H - 318);
+		g->resizable(NULL);
 
-	cut_but = new Fl_Button(245, 345, 80, 25, "Cut");
-	cut_but->labelsize(FL_NORMAL_SIZE - 2);
-	cut_but->shortcut(FL_CTRL + 'x');
+		o = new Fl_Box(215, 318, 355, 30, " Clipboard Operations");
+		o->align(Fl_Align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE));
+		o->labelsize(14);
 
-	copy_but = new Fl_Button(360, 345, 80, 25, "Copy");
-	copy_but->labelsize(FL_NORMAL_SIZE - 2);
-	copy_but->shortcut(FL_CTRL + 'c');
+		cut_but = new Fl_Button(245, 345, 80, 25, "Cut");
+		cut_but->labelsize(FL_NORMAL_SIZE - 2);
+		cut_but->shortcut(FL_CTRL + 'x');
 
-	paste_but = new Fl_Button(475, 345, 80, 25, "Paste");
-	paste_but->labelsize(FL_NORMAL_SIZE - 2);
-	paste_but->shortcut(FL_CTRL + 'v');
+		copy_but = new Fl_Button(360, 345, 80, 25, "Copy");
+		copy_but->labelsize(FL_NORMAL_SIZE - 2);
+		copy_but->shortcut(FL_CTRL + 'c');
 
+		paste_but = new Fl_Button(475, 345, 80, 25, "Paste");
+		paste_but->labelsize(FL_NORMAL_SIZE - 2);
+		paste_but->shortcut(FL_CTRL + 'v');
+
+		g->end();
+	}
 
 	end();
 
@@ -170,6 +244,8 @@ void DLG_ManageConfig(void)
 
 	config_window->set_modal();
 	config_window->show();
+
+	config_window->ReadCurrentSettings();
 
 	// run the GUI until the user closes
 	while (! config_window->WantQuit())
