8825360382d289cb04d8f4f6edc5dfcb66b5b64a
Andrew Apted <ajapted@users.sf.net>
2014-06-03 09:21:24 +0000

    Version bump --> 6.07 in honor of the Config Manager, better high-res adaptation, progress with outdoor borders and 3D floors, etc...

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "6.06"
-#define OBLIGE_HEX_VER  0x606
+#define OBLIGE_VERSION  "6.07"
+#define OBLIGE_HEX_VER  0x607
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
