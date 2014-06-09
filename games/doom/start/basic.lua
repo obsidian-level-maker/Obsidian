79de7640b05632ff391dfefc44f770125f8979fc
Andrew Apted <ajapted@users.sf.net>
2014-06-09 03:14:17 +0000

    DOOM : modified the basic START pedestal to include the four extra (non-standard) player entities, to support upto 8 players in CO-OP.

--- games/doom/start/basic.lua
+++ games/doom/start/basic.lua
@@ -9,5 +9,12 @@ DOOM.SKINS.Start_basic =
 
   flat_FLAT22 = "O_BOLT"
   tex_SHAWN2  = "O_BOLT"
+
+  -- pass the non-standard players through the ENTITIES table, to
+  -- allow them to be removed if a game/port does not support them.
+  thing_4001 = "player5"
+  thing_4002 = "player6"
+  thing_4003 = "player7"
+  thing_4004 = "player8"
 }
 
