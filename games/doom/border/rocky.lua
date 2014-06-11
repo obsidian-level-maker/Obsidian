67df88b02c71a259e8082c90286aa0751ed2e1f7
Andrew Apted <ajapted@users.sf.net>
2014-06-11 07:07:53 +0000

    DOOM : added Lua definitions for 'rocky' border pieces.

---
+++ games/doom/border/rocky.lua
@@ -0,0 +1,37 @@
+--
+-- Rocky border prefabs
+--
+
+DOOM.SKINS.Border_rocky_c =
+{
+  file   = "border/rocky_c.wad"
+  group  = "border_rocky"
+  shape  = "C"
+  seed_w = 3
+  seed_h = 3
+
+  add_sky = 1
+}
+
+
+DOOM.SKINS.Border_rocky_t =
+{
+  file   = "border/rocky_t.wad"
+  group  = "border_rocky"
+  shape  = "T"
+  seed_w = 3
+  seed_h = 3
+
+  add_sky = 1
+}
+
+
+--
+-- Group information
+--
+
+DOOM.GROUPS.border_rocky =
+{
+  kind = "border"
+}
+
