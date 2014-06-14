b35aced0d76475e2cc516c08e67b82ecf8bea823
Andrew Apted <ajapted@users.sf.net>
2014-06-14 07:29:02 +0000

    DOOM : added 'teleporter/closet2.wad' prefab, adapted from V5.

---
+++ games/doom/teleporter/closet2.lua
@@ -0,0 +1,16 @@
+--
+-- Teleporter closet
+--
+
+DOOM.SKINS.Teleporter_Closet2 =
+{
+  file  = "teleporter/closet2.wad"
+  where = "closet"
+
+  seed_w = 1
+  seed_h = 1
+
+  tag_1 = "?out_tag"
+  tag_2 = "?in_tag"
+}
+
