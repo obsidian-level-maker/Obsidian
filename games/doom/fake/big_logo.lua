ec11b169159767fc7f1871f540b17931cafe2ec5
Andrew Apted <ajapted@users.sf.net>
2014-06-19 05:02:00 +0000

    DOOM : added definition for 'big_logo.wad' fake building prefab.

---
+++ games/doom/fake/big_logo.lua
@@ -0,0 +1,18 @@
+--
+-- Large fake building with a logo
+--
+
+PREFABS.Fake_big_logo =
+{
+  file   = "fake/big_logo.wad"
+  where  = "border"
+  shape  = "T"
+
+  seed_w = 3
+  seed_h = 3
+
+  bound_z1 = 0
+
+  add_sky = 1
+}
+
