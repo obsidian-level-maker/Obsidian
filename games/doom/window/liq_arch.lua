358937179373981e1e0964b0f7b03d7464b15039
Andrew Apted <ajapted@users.sf.net>
2014-06-20 08:04:21 +0000

    DOOM : created a liquid arch prefab.

---
+++ games/doom/window/liq_arch.lua
@@ -0,0 +1,21 @@
+--
+-- Liquid arch
+--
+
+PREFABS.Window_liquid_arch =
+{
+  file   = "window/liq_arch.wad"
+  where  = "edge"
+
+  long   = 192
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  prob = 90
+}
+
