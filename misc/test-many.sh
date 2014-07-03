686923e586a43897a7a3a88a0c5cef7a760d0901
Andrew Apted <ajapted@users.sf.net>
2014-07-03 05:13:59 +0000

    Added test-many.sh shell script, which repeatly invokes the test-it script.

---
+++ misc/test-many.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ]
+then
+	echo "USAGE: test-many  oblige_options..."
+	exit
+fi
+
+if [ ! -d lua_src ]
+then
+	echo "Run this script from the top level."
+	exit 1
+fi
+
+for (( ; 1 ; )) do
+  misc/test-it.sh $*
+done
+
+# --- editor settings ---
+# vi:ts=4:sw=4:noexpandtab
