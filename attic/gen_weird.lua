a2e9eb99c8f146634a26e55851cdcf648d5c3ac5
Andrew Apted <ajapted@users.sf.net>
2014-10-18 10:59:47 +0000

    gen_weird : implemented create_points, add_edge, save_as_svg functions.

--- attic/gen_weird.lua
+++ attic/gen_weird.lua
@@ -21,7 +21,7 @@ end
 
 -- class POINT
 --[[
-    x, y   : coordinate
+    gx, gy   : grid coordinate
 
     neighbor : table[DIR] --> POINT
 
@@ -34,19 +34,63 @@ end
 
 GRID = {}
 
-GRID_W = 50
-GRID_H = 50
+GRID_W = 48
+GRID_H = 30
 
 
 function create_points()
-  GRID = table.array_2
+  GRID = table.array_2D(GRID_W, GRID_H)
 
-  --....
+  for gx = 1, GRID_W do
+  for gy = 1, GRID_H do
+    local P =
+    {
+      gx = gx,
+      gy = gy,
+      neighbor = {},
+      edge = {},
+      num_edges = 0
+    }
+    
+    GRID[gx][gy] = P
+  end
+  end
+
+  -- link neighbors
+
+  for gx = 1, GRID_W do
+  for gy = 1, GRID_H do
+    local P = GRID[gx][gy]
+
+    for dir = 1, 9 do
+    if dir ~= 5 then
+      local nx, ny = geom.nudge(gx, gy, dir)
+
+      if table.valid_pos(GRID, nx, ny) then
+        local N = GRID[nx][ny]
+        assert(N)
+
+        P.neighbor[dir] = N
+      end
+    end
+    end
+  end
+  end
 end
 
 
 function add_edge(gx, gy, dir)
-  -- TODO
+  local P = GRID[gx][gy]
+
+  P.edge[dir] = true
+  P.num_edges  = P.num_edges + 1
+
+  local N = P.neighbor[dir]
+
+  if N then
+    N.edge[10 - dir] = true
+    N.num_edges = N.num_edges + 1
+  end
 end
 
 
@@ -60,8 +104,53 @@ function remove_dead_ends()
 end
 
 
+function wr_line(fp, x1, y1, x2, y2, color, width)
+  fp:write(string.format('<line x1="%d" y1="%d" x2="%d" y2="%d" stroke="%s" stroke-width="%d" />\n',
+           x1, y1, x2, y2, color, width or 1))
+end
+
+
 function save_as_svg()
-  -- TODO
+  local SIZE = 20
+
+  local fp = io.open("_weird.svg", "w")
+
+  if not fp then error("Cannot create file") end
+
+  -- header
+  fp:write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n')
+
+  -- grid
+  local max_x = GRID_W * SIZE
+  local max_y = GRID_H * SIZE
+
+  for x = 0, GRID_W do
+    wr_line(fp, x * SIZE, 0, x * SIZE, max_y, "#bbb")
+  end
+
+  for y = 0, GRID_H do
+    wr_line(fp, 0, y * SIZE, max_x, y * SIZE, "#bbb")
+  end
+
+  -- points
+  for x = 1, GRID_W do
+  for y = 1, GRID_H do
+    local P = GRID[x][y]
+
+    for dir = 6,9 do
+      if P.edge[dir] then
+        local N = P.neighbor[dir]
+
+        wr_line(fp, x * SIZE, y * SIZE, N.gx * SIZE, N.gy * SIZE, "#00f", 3)
+      end
+    end
+  end
+  end
+
+  -- end
+  fp:write('</svg>\n')
+
+  fp:close()
 end
 
 
