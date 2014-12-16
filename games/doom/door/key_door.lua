3612637ba884e91e4cdd6511cfd3fffb0f3e7990
Andrew Apted <ajapted@users.sf.net>
2014-12-16 02:31:46 +0000

    DOOM / prefabs : added a diagonal version of the keyed door prefab.

--- games/doom/door/key_door.lua
+++ games/doom/door/key_door.lua
@@ -113,3 +113,89 @@ PREFABS.Locked_ks_red =
   flat_FLAT23  = "?big_door"
 }
 
+
+----------------------------------------
+--   DIAGONAL VERSIONS
+----------------------------------------
+
+PREFABS.Locked_kc_blue_diag =
+{
+  file   = "door/key_door_dg.wad"
+  where  = "diagonal"
+  key    = "kc_blue"
+
+  tex_DOORRED  = "DOORBLU"
+  line_33 = 32
+
+  tex_BIGDOOR2 = "?big_door"
+  flat_FLAT23  = "?big_door"
+}
+
+
+PREFABS.Locked_kc_yellow_diag =
+{
+  file   = "door/key_door_dg.wad"
+  where  = "diagonal"
+  key    = "kc_yellow"
+
+  tex_DOORRED  = "DOORYEL"
+  line_33 = 34
+
+  tex_BIGDOOR2 = "?big_door"
+  flat_FLAT23  = "?big_door"
+}
+
+
+PREFABS.Locked_kc_red_diag =
+{
+  file   = "door/key_door_dg.wad"
+  where  = "diagonal"
+  key    = "kc_red"
+
+  -- prefab has DOORRED texture and line special #33
+
+  tex_BIGDOOR2 = "?big_door"
+  flat_FLAT23  = "?big_door"
+}
+
+
+PREFABS.Locked_ks_blue_diag =
+{
+  file   = "door/key_door_dg.wad"
+  where  = "diagonal"
+  key    = "ks_blue"
+
+  tex_DOORRED  = "DOORBLU2"
+  line_33 = 32
+
+  tex_BIGDOOR2 = "?big_door"
+  flat_FLAT23  = "?big_door"
+}
+
+
+PREFABS.Locked_ks_yellow_diag =
+{
+  file   = "door/key_door_dg.wad"
+  where  = "diagonal"
+  key    = "ks_yellow"
+
+  tex_DOORRED  = "DOORYEL2"
+  line_33 = 34
+
+  tex_BIGDOOR2 = "?big_door"
+  flat_FLAT23  = "?big_door"
+}
+
+
+PREFABS.Locked_ks_red_diag =
+{
+  file   = "door/key_door_dg.wad"
+  where  = "diagonal"
+  key    = "ks_red"
+
+  tex_DOORRED  = "DOORRED2"
+
+  tex_BIGDOOR2 = "?big_door"
+  flat_FLAT23  = "?big_door"
+}
+
