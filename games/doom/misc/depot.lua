3114c9f5788b05e7b9d5e06067fdd4356941e0b2
Andrew Apted <ajapted@users.sf.net>
2015-01-21 20:17:39 +1100

    DOOM / prefabs : added lua definition for 'Depot' prefab.

---
+++ games/doom/misc/depot.lua
@@ -0,0 +1,16 @@
+--
+--  Monster depot
+--
+
+PREFABS.Depot =
+{
+  file  = "misc/depot.wad"
+  where = "depot"
+
+  tag_9 = "?trigger_tag"
+
+  tag_1 = "?out_tag1"
+  tag_2 = "?out_tag2"
+  tag_3 = "?out_tag3"
+}
+
