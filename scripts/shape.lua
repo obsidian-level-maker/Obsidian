36b012201e9798d6bd58bf1b2bb5bae5f0c9bcea
Andrew Apted <ajapted@users.sf.net>
2015-02-23 00:03:43 +1100

    Shapes : more work on Shape_fill_gaps() algorithm -- setup the initial temporary areas and logic to look for possible merges.

--- scripts/shape.lua
+++ scripts/shape.lua
@@ -30,10 +30,101 @@ function Shape_fill_gaps()
   -- certain threshhold.
   --
 
+  local temp_areas = {}
+
+  local MIN_SIZE = 5
+  local MAX_SIZE = 16
+
+
+  local function new_temp_area(first_S)
+    local TEMP =
+    {
+      size = 0
+      seeds = { first_S }
+    }
+
+    if first_S.diagonal then
+      TEMP.size = 0.5
+    else
+      TEMP.size = 1.0
+    end
+
+    table.insert(temp_areas, TEMP)
+
+    return TEMP
+  end
+
+
+  local function create_temp_areas()
+    for sx = 1, SEED_W do
+    for sy = 1, SEED_H do
+      local S = SEEDS[sx][sy]
+
+      if not S.diagonal and not S.area then
+        -- whole unused seed : split into two
+
+        S:split(rand.sel(50, 1, 3))
+      end
+
+      S2 = S.top
+
+      if S  and not S.area  then S .temp_area = new_temp_area(S)  end
+      if S2 and not S2.area then S2.temp_area = new_temp_area(S2) end
+    end
+    end
+  end
+
+
+  local function marge_an_area(A1)
+    local best_S
+    local best_dir
+    local best_score = 0
+
+    each S in A1.seeds do
+    each dir in geom.ALL_DIRS do
+      local N = S:neighbor(dir)
+
+      if not (N and N.temp_area) then continue end
+
+      local A2 = N.temp_area
+
+      if A2 == A1 then continue end
+
+      local score = eval_merge(A1, A2)
+
+      if score > best_score then
+        best_S = S
+        best_dir = dir
+        best_score = score
+      end
+    end
+    end
+
+    -- FIXME
+  end
+
+
+  local function merge_temp_areas()
+    rand.shuffle(temp_areas)
+
+    for i = #temp_areas, 1, -1 do
+      local A1 = temp_areas[i]
+      if area.is_dead then continue end
+
+      if A1.size < MIN_SIZE then
+        merge_an_area(A1)
+      end
+    end
+  end
+
 
   ---| Shape_fill_gaps |---
 
-  -- TODO
+  create_temp_areas()
+
+  for loop = 1,30 do
+    merge_temp_areas()
+  end
 end
 
 
