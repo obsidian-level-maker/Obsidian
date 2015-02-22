dcc0c172cba8d3c0dee448523177a7936c7d80cd
Andrew Apted <ajapted@users.sf.net>
2015-02-23 00:30:42 +1100

    Shapes : fleshed out merging logic in Shape_fill_gaps() -- the basics of the algorithm are done now, but still need to create real areas....

--- scripts/shape.lua
+++ scripts/shape.lua
@@ -33,7 +33,7 @@ function Shape_fill_gaps()
   local temp_areas = {}
 
   local MIN_SIZE = 5
-  local MAX_SIZE = 16
+  local MAX_SIZE = 15
 
 
   local function new_temp_area(first_S)
@@ -61,8 +61,7 @@ function Shape_fill_gaps()
       local S = SEEDS[sx][sy]
 
       if not S.diagonal and not S.area then
-        -- whole unused seed : split into two
-
+        -- a whole unused seed : split into two
         S:split(rand.sel(50, 1, 3))
       end
 
@@ -75,9 +74,48 @@ function Shape_fill_gaps()
   end
 
 
-  local function marge_an_area(A1)
-    local best_S
-    local best_dir
+  local function perform_merge(A1, A2)
+    -- merges A2 into A1 (killing A1)
+
+    if A2.size > A1.size then
+      A1, A2 = A2, A1
+    end
+
+    A1.size = A1.size + A2.size
+
+    table.append(A1.seeds, A2.seeds)
+
+    -- fix all seeds
+    for sx = 1, SEED_W do
+    for sy = 1, SEED_H do
+      local S = SEEDS[sx][sy]
+      local S2 = S.top
+
+      if S .temp_area == A2 then S .temp_area = A1 end
+      if S2.temp_area == A2 then S2.temp_area = A1 end
+    end
+    end
+
+    -- mark A2 as dead
+    A2.is_dead = true
+    A2.size  = nil
+    A2.seeds = nil
+  end
+
+
+  local function eval_merge(A1, A2, dir)
+    local score = 1
+
+    if A1.size + A2.size <= MAX_SIZE then
+      score = 2
+    end
+
+    return score + gui.random()    
+  end
+
+
+  local function try_merge_an_area(A1)
+    local best_A2
     local best_score = 0
 
     each S in A1.seeds do
@@ -90,31 +128,44 @@ function Shape_fill_gaps()
 
       if A2 == A1 then continue end
 
-      local score = eval_merge(A1, A2)
+      assert(not A2.is_dead)
+
+      local score = eval_merge(A1, A2, dir)
 
       if score > best_score then
-        best_S = S
-        best_dir = dir
+        best_A2 = A2
         best_score = score
       end
     end
     end
 
-    -- FIXME
+    if best_A2 then
+      perform_merge(A1, A2)
+    end
+  end
+
+
+  local function prune_dead_areas()
+    for i = #temp_areas, 1, -1 do
+      if temp_areas[i].is_dead then
+        table.remove(temp_areas, i)
+      end
+    end
   end
 
 
   local function merge_temp_areas()
     rand.shuffle(temp_areas)
 
-    for i = #temp_areas, 1, -1 do
-      local A1 = temp_areas[i]
-      if area.is_dead then continue end
+    each A1 in temp_areas do
+      if A1.is_dead then continue end
 
       if A1.size < MIN_SIZE then
-        merge_an_area(A1)
+        try_merge_an_area(A1)
       end
     end
+
+    prune_dead_areas()
   end
 
 
