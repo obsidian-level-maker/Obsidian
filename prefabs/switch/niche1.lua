8421b9175c779370ae53b0a7a1c7008d777370b7
Andrew Apted <ajapted@users.sf.net>
2015-04-12 23:18:49 +1000

    Prefabs : added 'niche1' switch fab, converted from V5.

---
+++ prefabs/switch/niche1.lua
@@ -0,0 +1,18 @@
+--
+--  Niche switch w/ candelabras
+--
+
+DOOM.SKINS.Switch_niche1 =
+{
+  file   = "switch/niche1.wad"
+  where  = "closet"
+  switch = "sw_blue"
+
+  seed_w = 1
+  seed_h = 1
+
+  tag_1 = "?lock_tag"
+
+  line_103 = "?special"
+}
+
