d77a58be8c7876d22200b52a5f81604fab8dd974
Andrew Apted <ajapted@users.sf.net>
2015-04-26 19:47:32 +1000

    Prefabs : created diagonal version of 'door_w_bars' prefab, and tweaked the existing one (also fixed the _NOTHING texture on the frame).

--- prefabs/door/door_w_bars.lua
+++ prefabs/door/door_w_bars.lua
@@ -1,10 +1,12 @@
 --
--- Door with lowering bars (remote activated)
+-- Door with raising bars (remote activated)
 --
 
 PREFABS.Door_with_bars =
 {
   file   = "door/door_w_bars.wad"
+  map    = "MAP01"
+
   where  = "edge"
   switch = "sw_blue"
 
@@ -18,5 +20,17 @@ PREFABS.Door_with_bars =
   flat_FLAT14  = "GRAY1"
 
   tag_1 = "?lock_tag"
+}
+
+
+PREFABS.Door_with_bars_diag =
+{
+  file   = "door/door_w_bars.wad"
+  map    = "MAP02"
+
+  where  = "diagonal"
+  switch = "sw_blue"
+
+  tag_1  = "?lock_tag"
 }
 
