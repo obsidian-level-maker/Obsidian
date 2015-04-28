847e0b775b0724964c6c0bab708d0f4e1e5a2bef
Andrew Apted <ajapted@users.sf.net>
2015-04-28 15:30:15 +1000

    Prefabs : added Lua definitions for the straight hallway stair pieces.

---
+++ prefabs/hall/c_stair1.lua
@@ -0,0 +1,10 @@
+--
+-- Hallway stair : ceiling, straight, small step up
+--
+
+PREFABS.Hall_c_stair_small =
+{
+  file   = "hall/c_stair1.wad"
+  where  = "chunk"
+}
+
---
+++ prefabs/hall/f_stair1.lua
@@ -0,0 +1,10 @@
+--
+-- Hallway stair : floor, straight, small step up
+--
+
+PREFABS.Hall_f_stair_small =
+{
+  file   = "hall/f_stair1.wad"
+  where  = "chunk"
+}
+
