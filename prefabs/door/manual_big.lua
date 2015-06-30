037463f3a0c4fbf7794f4eb68e3db68e4d11334f
Andrew Apted <ajapted@users.sf.net>
2015-06-30 12:50:03 +1000

    Prefabs / door : added diagonal version of 'manual_big' door.

--- prefabs/door/manual_big.lua
+++ prefabs/door/manual_big.lua
@@ -2,9 +2,10 @@
 -- Large door (128x120 units)
 --
 
-PREFABS.Door_large =
+PREFABS.Door_manual_big =
 {
-  file   = "door/large.wad"
+  file   = "door/manual_big.wad"
+  map    = "MAP01"
   where  = "edge"
 
   x_fit  = "frame"
@@ -12,5 +13,14 @@ PREFABS.Door_large =
   long   = 192
   deep   = 16
   over   = 16
+}
+
+
+PREFABS.Door_manual_big_diag =
+{
+  file   = "door/manual_big.wad"
+  map    = "MAP02"
+  where  = "diagonal"
+
 }
 
