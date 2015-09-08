cd77d4b80f805ffee8364cf3497e6260d1a21307
Andrew Apted <ajapted@users.sf.net>
2015-09-08 19:34:38 +1000

    Added script file 'shapes.lua' -- so far contains a few minimal room shapes.

---
+++ scripts/shapes.lua
@@ -0,0 +1,157 @@
+------------------------------------------------------------------------
+--  ROOM SHAPES
+------------------------------------------------------------------------
+--
+--  Oblige Level Maker
+--
+--  Copyright (C) 2015 Andrew Apted
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License
+--  as published by the Free Software Foundation; either version 2
+--  of the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+------------------------------------------------------------------------
+
+
+SHAPES =
+{
+ 
+--------------------------------
+--
+--  Hallways
+--
+--------------------------------
+
+HALL_I_1x1 =
+{
+  prob = 10
+
+  structure =
+  {
+    "1"
+  }
+
+  conns =
+  {
+    A = { x=1, y=1, dir=2 }
+    B = { x=1, y=1, dir=8 }
+  }
+}
+
+
+--[[
+HALL_I_2x1 =
+{
+  prob = 10
+
+  structure =
+  {
+    "11"
+  }
+
+  conns =
+  {
+    A = { x=1, y=1, dir=2, w=2 }
+    B = { x=1, y=1, dir=8, w=2 }
+  }
+}
+--]]
+
+
+--[[
+HALL_U_5x3_rounded =
+{
+  prob = 25
+
+  structure =
+  {
+    "1...1"
+    "%%.//"
+    ".%1/."
+  }
+
+  diagonals =
+  {
+    ".1", "1.", ".1", "1."
+    ".1", "1."
+  }
+
+  conns =
+  {
+    A = { x=1, y=3, dir=8 }
+    B = { x=5, y=3, dir=8 }
+    C = { x=5, y=3, dir=8 }
+  }
+
+  conn_
+}
+--]]
+
+
+--------------------------------
+--
+--  Rooms
+--
+--------------------------------
+
+ROOM_RECT_3x2 =
+{
+  prob = 50
+
+  structure =
+  {
+    "111"
+    "111"
+  }
+
+  conns =
+  {
+    A = { x=2, y=3, dir=2 }
+    B = { x=1, y=3, dir=8 }
+    C = { x=3, y=3, dir=8 }
+--  D = { x=1, y=3, dir=4 }
+--  E = { x=3, y=3, dir=6 }
+  }
+}
+
+
+--------------------------------
+--
+--  Large Hubs
+--
+--------------------------------
+
+--[[
+HUB_DONUT_1 =
+{
+  prob = 100
+
+  structure =
+  {
+    "/1111%"
+    "1/22%1"
+    "122221"
+    "122221"
+    "1%22/1"
+    "%1111/"
+  }
+
+  diagonals =
+  {
+    ".1", "1."
+    "1.", ".1"
+    "1.", ".1"
+    ".1", "1."
+  }
+}
+--]]
+
+-- end of SHAPES
+}
+
