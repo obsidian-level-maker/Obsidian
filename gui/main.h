babec3ef2a7d95f876c855bbf296b207cec5b58f
Andrew Apted <ajapted@users.sf.net>
2015-09-14 22:16:29 +1000

    Version bump to 7.20 [ to distinguish from the V7-WEIRD package ]

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "7.16"
-#define OBLIGE_HEX_VER  0x716
+#define OBLIGE_VERSION  "7.20"
+#define OBLIGE_HEX_VER  0x720
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
