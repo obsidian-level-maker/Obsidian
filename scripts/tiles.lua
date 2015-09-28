3e6ccb1ac6e08504e1295008be1af046bf218885
Andrew Apted <ajapted@users.sf.net>
2015-09-28 22:56:27 +1000

    Tiles : enabling numerous tiles and added a 'Y' shape hallway tile.

--- scripts/tiles.lua
+++ scripts/tiles.lua
@@ -45,11 +45,12 @@ HALL_I_1x1 =
   }
 
 }
+--]]
 
 
 HALL_I_1x3 =
 {
-  prob = 20
+  prob = 10
 
   structure =
   {
@@ -64,7 +65,6 @@ HALL_I_1x3 =
     b = { x=1, y=3, dir=8 }
   }
 }
---]]
 
 
 HALL_I_2x1 =
@@ -117,9 +117,41 @@ HALL_2x2 =
 }
 
 
+HALL_U_5x3_a =
+{
+  prob = 300
+
+  structure =
+  {
+    "./1%."
+    "//.%%"
+    "1...1"
+  }
+
+  diagonals =
+  {
+    ".1", "1."
+    ".1", "1.", ".1", "1."
+  }
+
+  conns =
+  {
+    a = { x=1, y=1, dir=2 }
+    b = { x=5, y=1, dir=2 }
+    c = { x=3, y=3, dir=8 }
+  }
+
+  conn_sets =
+  {
+    "a:b"
+--  "a:bc"
+  }
+}
+
+
 HALL_DIAG_I3 =
 {
-  prob = 4099
+  prob = 200
 
   structure =
   {
@@ -143,34 +175,37 @@ HALL_DIAG_I3 =
 }
 
 
---[[
-HALL_U_5x3_rounded =
+HALL_DIAG_Y_4x4 =
 {
-  prob = 25
+  prob = 200
 
   structure =
   {
-    "1...1"
-    "%%.//"
-    ".%1/."
+    ".1.."
+    ".1.."
+    "/111"
+    "%/.."
   }
 
   diagonals =
   {
-    ".1", "1.", ".1", "1."
+    ".1",
     ".1", "1."
   }
 
   conns =
   {
-    a = { x=1, y=3, dir=8 }
-    b = { x=5, y=3, dir=8 }
-    c = { x=5, y=3, dir=8 }
+    a = { x=1, y=1, dir=1 }
+    b = { x=2, y=4, dir=8 }
+    c = { x=4, y=2, dir=6 }
   }
 
-  conn_
+  conn_sets =
+  {
+    "a:bc"
+  }
 }
---]]
+
 
 
 --------------------------------
@@ -272,10 +307,10 @@ ROOM_RECT_3x2 =
 --
 --------------------------------
 
---[[
 START_O_3x3 =
 {
-  start_prob = 1000
+  start_prob = 100
+  prob = 400
 
   structure =
   {
@@ -296,43 +331,63 @@ START_O_3x3 =
     b = { x=1, y=2, dir=4 }
     c = { x=3, y=2, dir=6 }
     d = { x=2, y=3, dir=8 }
+
+    e = { x=1, y=1, dir=1 }
+    f = { x=3, y=1, dir=3 }
+    g = { x=1, y=3, dir=7 }
+    h = { x=3, y=3, dir=9 }
+  }
+
+  conn_sets =
+  {
+    "a:bcd"
+    "a:gh"
+
+    "e:fgh"
+    "e:cd"
   }
 }
---]]
 
 
 START_O_4x4 =
 {
-  start_prob = 1000
-prob = 999
+  start_prob = 500
+  prob = 50
 
   structure =
   {
     "/11%"
-    "1%11"
-    "12%1"
+    "1111"
+    "1111"
     "%11/"
   }
 
   diagonals =
   {
     ".1", "1."
-"21", "21", --!!!
     ".1", "1."
   }
 
   conns =
   {
---[[
     a = { x=3, y=1, w=2, dir=2 }
     b = { x=1, y=2, w=2, dir=4 }
     c = { x=4, y=3, w=2, dir=6 }
     d = { x=2, y=4, w=2, dir=8 }
---]]
+
     e = { x=1, y=1, dir=1 }
     f = { x=4, y=1, dir=3 }
     g = { x=1, y=4, dir=7 }
     h = { x=4, y=4, dir=9 }
+  }
+
+  conn_sets =
+  {
+    "a:bcd"
+    "a:gh"
+
+    "e:fgh"
+    "e:cd"
   }
 }
 
