82b41bd73998368f25da52313616560c91a642a3
Andrew Apted <ajapted@users.sf.net>
2016-02-13 23:08:37 +1100

    Prefabs : updated standing cage Lua defs, and added a temporary ceiling to the wad-fab since the code does not handle that yet.

--- prefabs/cage/stand_cage.lua
+++ prefabs/cage/stand_cage.lua
@@ -5,9 +5,17 @@
 PREFABS.Cage_standing1 =
 {
   file   = "cage/stand_cage.wad"
-  where  = "seed"
+  where  = "area"
+
+  seed_w = 1
+  seed_h = 1
+
+  x_fit = { 56,72 }
+  y_fit = { 56,72 }
 
   bound_z1 = 0
   bound_z2 = 120
+
+  prob = 200
 }
 
