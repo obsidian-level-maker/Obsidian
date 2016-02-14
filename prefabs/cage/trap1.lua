10c4558d84ffa5b70cb3062d616e6b0e9a35626e
Andrew Apted <ajapted@users.sf.net>
2016-02-14 20:53:19 +1100

    Prefabs : added 'trap1.wad' prefab -- a basic monster closet.

---
+++ prefabs/cage/trap1.lua
@@ -0,0 +1,24 @@
+--
+-- Simple trap
+--
+
+PREFABS.Trap_1 =
+{
+  file   = "cage/trap_1.wad"
+  where  = "area"
+--shape  = "T"
+
+  seed_w = 1
+  seed_h = 1
+
+  x_fit = { 56,72 }
+  y_fit = { 56,72 }
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  is_trap = true
+
+  tag_1 = "?tag"
+}
+
