62f52a0fbae3dde2ac2436663e46178809efda33
Andrew Apted <ajapted@users.sf.net>
2016-02-16 22:33:46 +1100

    Prefabs : updated 'stair_32' fab for 128x128 size, use a simpler design.

--- prefabs/stairs/stair_32.lua
+++ prefabs/stairs/stair_32.lua
@@ -5,14 +5,15 @@
 PREFABS.Stair_outie_32 =
 {
   file   = "stairs/stair_32.wad"
-  where  = "chunk"
 
-  bound_x1 = 0
-  bound_y1 = 0
-  bound_x2 = 192
-  bound_y2 = 192
+--??  where  = "chunk"
 
-  z_fit = "stretch"
+--bound_x1 = 0
+--bound_y1 = 0
+--bound_x2 = 128
+--bound_y2 = 128
+
+--?? z_fit = "stretch"
 
   bound_z1 = 0
   bound_z2 = 32
