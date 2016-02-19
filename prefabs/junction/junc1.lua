ae232262bcedd801565a291a074a4059235f50b6
Andrew Apted <ajapted@users.sf.net>
2016-02-19 12:42:17 +1100

    Prefabs : created a simple 2x1 seed junction prefab.

---
+++ prefabs/junction/junc1.lua
@@ -0,0 +1,17 @@
+--
+-- Simple junction
+--
+
+PREFABS.Junction_1 =
+{
+  file   = "junction/junc1.wad"
+  where  = "junction"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit = "frame"
+
+  prob = 100
+}
+
