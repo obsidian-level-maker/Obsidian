3ae67c9877541bca1905d7280c4b77111a30e8e6
Andrew Apted <ajapted@users.sf.net>
2016-02-20 21:07:02 +1100

    Prefabs : created 'stair_test' joiner prefab.

---
+++ prefabs/joiner/stair_test.lua
@@ -0,0 +1,19 @@
+--
+-- Joiner with stairs
+--
+
+PREFABS.Joiner_stair_test =
+{
+  file   = "joiner/stair_test.wad"
+--where  = "joiner"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit = "frame"
+
+  prob = 100
+
+  rank = 9  -- FIXME : TEST ONLY
+}
+
