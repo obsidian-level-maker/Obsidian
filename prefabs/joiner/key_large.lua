35475bd2ad5364d9c23f143072260410268e5568
Andrew Apted <ajapted@users.sf.net>
2016-02-25 14:39:24 +1100

    Prefabs : began work on a keyed joiner prefab: 'joiner/key_large.wad'

---
+++ prefabs/joiner/key_large.lua
@@ -0,0 +1,17 @@
+--
+-- Large keyed door
+--
+
+PREFABS.Joiner_key_large =
+{
+  file   = "joiner/key_large.wad"
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit = "frame"
+
+  prob = 100
+}
+
