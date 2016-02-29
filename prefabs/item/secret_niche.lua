d8ce7d19e68b616024f8256ddbb1588d8b7421ba
Andrew Apted <ajapted@users.sf.net>
2016-02-29 21:03:14 +1100

    Prefabs : created 'item/secret_niche' prefab.

---
+++ prefabs/item/secret_niche.lua
@@ -0,0 +1,20 @@
+--
+--  Secret item niche
+--
+
+PREFABS.Item_secret_niche_small =
+{
+  file  = "item/secret_niche.wad"
+  where = "seeds"
+
+  key   = "secret"
+
+  seed_w = 1
+  seed_h = 1
+
+  x_fit = "frame"
+  y_fit = "top"
+
+  prob  = 50
+}
+
