013d88311de6666282cf1870faa5b7f579406d09
Andrew Apted <ajapted@users.sf.net>
2016-03-07 23:39:15 +1100

    Prefabs : added Lua definition for 'switch/niche2' prefab.

---
+++ prefabs/switch/niche2.lua
@@ -0,0 +1,19 @@
+--
+--  Niche switch
+--
+
+PREFABS.Switch_niche2 =
+{
+  file   = "switch/niche2.wad"
+  where  = "seeds"
+  switch = "any"
+
+  seed_w = 1
+  seed_h = 1
+
+  x_fit = "frame"
+  y_fit = "top"
+
+  tag_1 = "?lock_tag"
+}
+
