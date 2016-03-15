4427c398338f921832c901eb6da71cf802eba8e0
Andrew Apted <ajapted@users.sf.net>
2016-03-15 18:29:03 +1100

    Version bump, in honor of new chunk system.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "7.31-WIP"
-#define OBLIGE_HEX_VER  0x731
+#define OBLIGE_VERSION  "7.32-WIP"
+#define OBLIGE_HEX_VER  0x732
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
