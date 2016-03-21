c83eb4b1e2e8ea6acc86cd1f1d2029bee69baaae
Andrew Apted <ajapted@users.sf.net>
2016-03-21 18:31:24 +1100

    Prefabs : created a variant monster depot which lowers a floor to allow the monsters to teleport.  This will be useful with switches which lower bars (etc).

--- prefabs/misc/depot.lua
+++ prefabs/misc/depot.lua
@@ -2,9 +2,27 @@
 --  Monster depot
 --
 
-PREFABS.Depot =
+PREFABS.Depot_raise =
 {
   file  = "misc/depot.wad"
+  map   = "MAP01"
+  where = "seeds"
+
+  seed_w = 3
+  seed_h = 6
+
+  tag_9 = "?trigger_tag"
+
+  tag_1 = "?out_tag1"
+  tag_2 = "?out_tag2"
+  tag_3 = "?out_tag3"
+}
+
+
+PREFABS.Depot_lower =
+{
+  file  = "misc/depot.wad"
+  map   = "MAP02"
   where = "seeds"
 
   seed_w = 3
