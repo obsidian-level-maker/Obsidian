24d3e73a1244575c8cda5b087bb78a86a2424dc3
Andrew Apted <ajapted@users.sf.net>
2016-03-23 20:12:48 +1100

    Prefabs : created 'switch/wall_box1' prefab, which is used as a closet but is actually just a wall switch.

---
+++ prefabs/switch/wall_box1.lua
@@ -0,0 +1,16 @@
+--
+--  Niche switch
+--
+
+PREFABS.Switch_wall_box1 =
+{
+  file   = "switch/wall_box1.wad"
+  where  = "seeds"
+  switch = "any"
+
+  x_fit = "frame"
+  y_fit = "top"
+
+  tag_1 = "?lock_tag"
+}
+
