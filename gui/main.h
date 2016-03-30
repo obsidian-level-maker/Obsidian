8513300a07db0d0a4cfa0bdffe39f27ab30e6f92
Andrew Apted <ajapted@users.sf.net>
2016-03-31 00:18:37 +1100

    Version bump, in honor of new monster planning (etc).

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "7.32-WIP"
-#define OBLIGE_HEX_VER  0x732
+#define OBLIGE_VERSION  "7.33-WIP"
+#define OBLIGE_HEX_VER  0x733
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
