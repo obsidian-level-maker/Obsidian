a148be6e9bde095ae147eff6fb37919fbbb78643
Andrew Apted <ajapted@users.sf.net>
2016-04-01 01:15:37 +1100

    Prefabs : created 'joiner/barred1' joiner.

---
+++ prefabs/joiner/barred1.lua
@@ -0,0 +1,19 @@
+--
+-- Joiner with opening bars
+--
+
+PREFABS.Joiner_barred1 =
+{
+  file   = "joiner/barred1.wad"
+  where  = "seeds"
+
+  key    = "barred"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit = "frame"
+
+  tag_1 = "?lock_tag"
+}
+
