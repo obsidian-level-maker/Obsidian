65760b8677ba4d346b03269e1eeedcfe422ac9f9
Andrew Apted <ajapted@users.sf.net>
2016-04-01 12:55:14 +1100

    Prefabs ; created a barred archway.

---
+++ prefabs/door/barred_arch.lua
@@ -0,0 +1,42 @@
+--
+-- Archway with bars
+--
+
+PREFABS.Arch_barred =
+{
+  file   = "door/barred_arch.wad"
+  map    = "MAP01"
+
+  kind   = "arch"
+  where  = "edge"
+
+  key    = "barred"
+
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  tag_1 = "?lock_tag"
+}
+
+
+PREFABS.Arch_barred_diag =
+{
+  file   = "door/barred_arch.wad"
+  map    = "MAP02"
+
+  kind   = "arch"
+  where  = "diagonal"
+
+  key    = "barred"
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  tag_1 = "?lock_tag"
+}
+
