ed7896099dedd2879bafa393efaa328c34af8d4e
Andrew Apted <ajapted@users.sf.net>
2016-04-01 15:33:58 +1100

    Prefabs : created 'door/key_medium' keyed door prefab.

---
+++ prefabs/door/key_medium.lua
@@ -0,0 +1,140 @@
+--
+-- Keyed doors, medium size
+--
+
+
+PREFABS.Locked_medium_kc_red =
+{
+  file   = "door/key_medium.wad"
+  map    = "MAP01"
+
+  where  = "edge"
+  key    = "kc_red"
+
+  seed_w = 2
+
+  deep   = 32
+  over   = 32
+
+  x_fit  = "frame"
+
+  rank   = 1
+
+  -- prefab already has DOORRED texture and line special #33
+}
+
+PREFABS.Locked_medium_kc_red_diag =
+{
+  file   = "door/key_medium.wad"
+  map    = "MAP02"
+
+  where  = "diagonal"
+  key    = "kc_red"
+
+  seed_w = 2
+  seed_h = 2
+
+  rank   = 1
+
+  -- prefab already has DOORRED texture and line special #33
+}
+
+
+PREFABS.Locked_medium_ks_red =
+{
+  template = "Locked_medium_kc_red"
+  key      = "ks_red"
+
+  tex_DOORRED = "DOORRED2"
+}
+
+PREFABS.Locked_medium_ks_red_diag =
+{
+  template = "Locked_medium_kc_red_diag"
+  key      = "ks_red"
+
+  tex_DOORRED = "DOORRED2"
+}
+
+
+------------------------------------------
+
+
+PREFABS.Locked_medium_kc_blue =
+{
+  template = "Locked_medium_kc_red"
+  key      = "kc_blue"
+
+  tex_DOORRED = "DOORBLU"
+  line_33     = 32
+}
+
+PREFABS.Locked_medium_kc_blue_diag =
+{
+  template = "Locked_medium_kc_red_diag"
+  key      = "kc_blue"
+
+  tex_DOORRED = "DOORBLU"
+  line_33     = 32
+}
+
+
+PREFABS.Locked_medium_ks_blue =
+{
+  template = "Locked_medium_kc_red"
+  key      = "ks_blue"
+
+  tex_DOORRED = "DOORBLU2"
+  line_33     = 32
+}
+
+PREFABS.Locked_medium_ks_blue_diag =
+{
+  template = "Locked_medium_kc_red_diag"
+  key      = "ks_blue"
+
+  tex_DOORRED = "DOORBLU2"
+  line_33     = 32
+}
+
+
+------------------------------------------
+
+
+PREFABS.Locked_medium_kc_yellow =
+{
+  template = "Locked_medium_kc_red"
+  key      = "kc_yellow"
+
+  tex_DOORRED = "DOORYEL"
+  line_33     = 34
+}
+
+PREFABS.Locked_medium_kc_yellow_diag =
+{
+  template = "Locked_medium_kc_red_diag"
+  key      = "kc_yellow"
+
+  tex_DOORRED = "DOORYEL"
+  line_33     = 34
+}
+
+
+PREFABS.Locked_medium_ks_yellow =
+{
+  template = "Locked_medium_kc_red"
+  key      = "ks_yellow"
+
+  tex_DOORRED = "DOORYEL2"
+  line_33     = 34
+}
+
+PREFABS.Locked_medium_ks_yellow_diag =
+{
+  template = "Locked_medium_kc_red_diag"
+  key      = "ks_yellow"
+
+  tex_DOORRED = "DOORYEL2"
+  line_33     = 34
+}
+
