257283afa7b06065f050deb59ba579e203a6fcbc
Andrew Apted <ajapted@users.sf.net>
2016-04-04 20:52:37 +1000

    Prefabs : added Lua definition for 'decor/pillar1'.

---
+++ prefabs/decor/pillar1.lua
@@ -0,0 +1,16 @@
+--
+-- Simple round pillar
+--
+
+PREFABS.Pillar_round1 =
+{
+  file   = "decor/pillar1.wad"
+  where  = "point"
+  size   = 80
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  z_fit  = { 56,72 }
+}
+
