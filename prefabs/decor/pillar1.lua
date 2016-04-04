ddc5729be3f844fa7ba2e29e6836bf6ec5596eb5
Andrew Apted <ajapted@users.sf.net>
2016-04-04 21:39:06 +1000

    Prefabs : added "height" value to 'decor/pillar1' definition.

--- prefabs/decor/pillar1.lua
+++ prefabs/decor/pillar1.lua
@@ -6,11 +6,15 @@ PREFABS.Pillar_round1 =
 {
   file   = "decor/pillar1.wad"
   where  = "point"
+
   size   = 80
+  height = 128
 
   bound_z1 = 0
   bound_z2 = 128
 
   z_fit  = { 56,72 }
+
+  prob   = 200
 }
 
