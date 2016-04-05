f0c12cdbce5a551488abe646f660feabac2c8f15
Andrew Apted <ajapted@users.sf.net>
2016-04-05 20:07:01 +1000

    Prefabs : created 'wall/low_gap' prefab.

---
+++ prefabs/wall/low_gap.lua
@@ -0,0 +1,43 @@
+--
+-- Wall with gap at bottom
+--
+
+PREFABS.Wall_lowgap =
+{
+  file   = "wall/low_gap.wad"
+  map    = "MAP01"
+  where  = "edge"
+
+  long   = 128
+  deep   = 16
+
+  height = 64
+
+  bound_z1 = 0
+  bound_z2 = 64
+
+  z_fit  = "top"
+
+  rank = 3
+}
+
+
+PREFABS.Wall_lowgap_diag =
+{
+  file   = "wall/low_gap.wad"
+  map    = "MAP02"
+  where  = "diagonal"
+
+  long   = 128
+  deep   = 16
+
+  height = 64
+
+  bound_z1 = 0
+  bound_z2 = 64
+
+  z_fit  = "top"
+
+  rank = 3
+}
+
