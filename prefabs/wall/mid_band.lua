c29c213042af7c5a538c7bc2513f74bed29972b5
Andrew Apted <ajapted@users.sf.net>
2016-04-05 18:27:58 +1000

    Prefabs : created 'wall/mid_band' prefab.

---
+++ prefabs/wall/mid_band.lua
@@ -0,0 +1,43 @@
+--
+-- Wall with horizontal band
+--
+
+PREFABS.Wall_midband =
+{
+  file   = "wall/mid_band.wad"
+  map    = "MAP01"
+  where  = "edge"
+
+  long   = 128
+  deep   = 16
+
+  height = 64
+
+  bound_z1 = 0
+  bound_z2 = 64
+
+  z_fit  = "frame"
+
+  rank = 2
+}
+
+
+PREFABS.Wall_midband_diag =
+{
+  file   = "wall/mid_band.wad"
+  map    = "MAP02"
+  where  = "diagonal"
+
+  long   = 128
+  deep   = 16
+
+  height = 64
+
+  bound_z1 = 0
+  bound_z2 = 64
+
+  z_fit  = "frame"
+
+  rank = 2
+}
+
