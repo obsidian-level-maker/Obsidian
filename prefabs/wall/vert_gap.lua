18f1a4eb8858adf76d3617f8d28756f5c6bacfcc
Andrew Apted <ajapted@users.sf.net>
2016-04-05 20:26:10 +1000

    Prefabs : created 'wall/vert_gap' prefab.

---
+++ prefabs/wall/vert_gap.lua
@@ -0,0 +1,43 @@
+--
+-- Wall with vertical gap
+--
+
+PREFABS.Wall_vertgap =
+{
+  file   = "wall/vert_gap.wad"
+  map    = "MAP01"
+  where  = "edge"
+
+  env    = "outdoors"
+
+  deep   = 16
+  height = 64
+
+  bound_z1 = 0
+  bound_z2 = 4
+
+  z_fit  = "stretch"
+
+  rank = 4
+}
+
+
+PREFABS.Wall_vertgap_diag =
+{
+  file   = "wall/vert_gap.wad"
+  map    = "MAP02"
+  where  = "diagonal"
+
+  env    = "outdoors"
+
+  deep   = 16
+  height = 64
+
+  bound_z1 = 0
+  bound_z2 = 4
+
+  z_fit  = "stretch"
+
+  rank = 4
+}
+
