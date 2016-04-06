c0043bd1c4f437ff2fe8e88bfc5853579a7e90f5
Andrew Apted <ajapted@users.sf.net>
2016-04-06 23:41:37 +1000

    Render : for decorative prefabs, give reqs table "env" of the room.

--- scripts/render.lua
+++ scripts/render.lua
@@ -2214,6 +2214,10 @@ stderrf("***** can_see_dist [%d] --> %d\n", dir, dist)
       height = A.ceil_h - A.floor_h
     }
 
+    if A.room then
+      reqs.env = A.room:get_env()
+    end
+
     local def = Fab_pick(reqs)
     local skin1 = { floor=floor_mat }
     local T = Trans.spot_transform(chunk.mx, chunk.my, A.floor_h, 2)
