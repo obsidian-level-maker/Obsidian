51ccf04c60399627463bd0ee54cf352cb98ed730
Andrew Apted <ajapted@users.sf.net>
2016-04-10 01:24:57 +1000

    Prefabs : created a wide version of 'barred_arch' door.

--- prefabs/door/barred_arch.lua
+++ prefabs/door/barred_arch.lua
@@ -13,12 +13,23 @@ PREFABS.Arch_barred =
   deep   = 16
   over   = 16
 
-  x_fit  = "frame"
+  -- no x_fit, hence the wide version gets used when seed_w >= 2
 
   bound_z1 = 0
   bound_z2 = 128
 
   tag_1 = "?lock_tag"
+}
+
+
+PREFABS.Arch_barred_wide =
+{
+  template = "Arch_barred"
+  map      = "MAP03"
+
+  seed_w = 2
+
+  x_fit  = "frame"
 }
 
 
