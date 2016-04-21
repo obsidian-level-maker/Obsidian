d5dad9cbd5be3411d91ef25c6b3c9d89c90d0f2b
Andrew Apted <ajapted@users.sf.net>
2016-04-21 21:52:53 +1000

    Title gen : for title_prop() API, made "color1" equiv to "color" (for easier iteration over a list of colors).

--- gui/dm_extra.cc
+++ gui/dm_extra.cc
@@ -1574,7 +1574,7 @@ int DM_title_property(lua_State *L)
 
 	const char *propname = luaL_checkstring(L, 1);
 
-	if (strcmp(propname, "color") == 0)
+	if (strcmp(propname, "color") == 0 || strcmp(propname, "color1") == 0)
 		title_drawctx.color[0] = Grab_Color(L, 2);
 	else if (strcmp(propname, "color2") == 0)
 		title_drawctx.color[1] = Grab_Color(L, 2);
