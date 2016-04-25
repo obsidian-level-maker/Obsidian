543c39e6657ebb2c21382d038de12071f84d0e44
Andrew Apted <ajapted@users.sf.net>
2016-04-26 00:01:04 +1000

    debugging blah.

--- scripts/render.lua
+++ scripts/render.lua
@@ -1100,7 +1100,7 @@ function Render_floor(A, S)
 
   local tag = S.tag
 -- tag = A.id
-if A.room then tag = A.room.id end
+-- if A.room then tag = A.room.id end
 -- if A.pool_id then tag = 1000 + A.pool_id end
 
 
