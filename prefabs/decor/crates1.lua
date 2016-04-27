9a8f0fd4bb02966fef9ee175b59ff6abc2323054
Andrew Apted <ajapted@users.sf.net>
2016-04-27 14:36:54 +1000

    Prefabs : added "small" crate to crates1.wad + Lua definition.

--- prefabs/decor/crates1.lua
+++ prefabs/decor/crates1.lua
@@ -3,9 +3,34 @@
 --
 
 
+-- a small crate, 64x64 in size
+
+PREFABS.Crate_small_brown =
+{
+  file   = "decor/crates1.wad"
+  map    = "MAP01"
+
+  where  = "point"
+  size   = 64
+
+  bound_z1 = 0
+
+  prob   = 20
+}
+
+
+PREFABS.Crate_small_gray =
+{
+  template = "Crate_small_brown"
+
+   tex_CRATE1  = "CRATE2"
+  flat_CRATOP2 = "CRATOP1"
+}
+
+
 -- a medium-size crate (96x96)
 
-UNFINISHED.Crate_medium1 =
+PREFABS.Crate_medium_brown =
 {
   file   = "decor/crates1.wad"
   map    = "MAP03"
@@ -14,5 +39,7 @@ UNFINISHED.Crate_medium1 =
   size   = 96
 
   bound_z1 = 0
+
+  prob   = 8
 }
 
