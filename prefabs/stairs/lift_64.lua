bb8b837d9119a10377f840159e0900867d6be182
Andrew Apted <ajapted@users.sf.net>
2016-04-28 13:45:37 +1000

    Prefabs : created 'stairs/lift_64' prefab.

---
+++ prefabs/stairs/lift_64.lua
@@ -0,0 +1,21 @@
+--
+-- basic lift, 64 units high
+--
+
+PREFABS.Lift_64 =
+{
+  file   = "stairs/lift_64.wad"
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+
+  delta_h = 64
+
+  x_fit  = "stretch"
+
+  bound_z1 = 0
+
+  prob = 15
+}
+
