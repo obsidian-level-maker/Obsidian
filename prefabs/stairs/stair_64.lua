5832872c6bccf70a64bf0271373d703fd5aa0e6e
Andrew Apted <ajapted@users.sf.net>
2016-04-28 13:27:53 +1000

    Prefabs : created 'stair_64' prefab.

---
+++ prefabs/stairs/stair_64.lua
@@ -0,0 +1,21 @@
+--
+-- basic stair, 64 units high
+--
+
+PREFABS.Stair_64_red =
+{
+  file   = "stairs/stair_64.wad"
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+
+  delta_h = 64
+
+  x_fit  = "stretch"
+
+  bound_z1 = 0
+
+  prob = 5
+}
+
