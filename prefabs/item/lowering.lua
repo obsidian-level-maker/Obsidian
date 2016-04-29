0d8798a314dfe8dd1cde8f4900871137f4a3161d
Andrew Apted <ajapted@users.sf.net>
2016-04-29 22:18:43 +1000

    Prefabs : created a lowering item pedestal (remote activated).

---
+++ prefabs/item/lowering.lua
@@ -0,0 +1,16 @@
+--
+--  Lowering pedestal (via remote switch)
+--
+
+PREFABS.Item_lowering_remote =
+{
+  file   = "item/lowering.wad"
+  where  = "point"
+  size   = 40
+
+  key    = "barred"
+
+  tag_1  = "?lock_tag"
+  action = "S1_LowerFloor"
+}
+
