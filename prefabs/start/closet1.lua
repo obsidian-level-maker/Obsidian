1750b5fee7cd93bd69eb3e770c355b85a16053fb
Andrew Apted <ajapted@users.sf.net>
2016-04-29 15:34:17 +1000

    Prefabs : added texture variations for the starting closet.

--- prefabs/start/closet1.lua
+++ prefabs/start/closet1.lua
@@ -12,5 +12,11 @@ PREFABS.Start_Closet1 =
 
   x_fit = "frame"
   y_fit = "top"
+
+  -- door variations
+  tex_DOOR1 = { DOOR1=50, DOOR3=50, SPCDOOR1=50, SPCDOOR3=50 }
+
+  -- wall variations
+  tex_BROWN1 = { BROWN1=50, BRONZE3=50, BROWNGRN=50, SLADWALL=50 }
 }
 
