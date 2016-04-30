4399a687e157a7c37b354261aa597b9fad8579ae
Andrew Apted <ajapted@users.sf.net>
2016-04-30 13:18:23 +1000

    Prefabs : created a shorter version for 'item/lowering' which can be used under low ceilings.

--- prefabs/item/lowering.lua
+++ prefabs/item/lowering.lua
@@ -2,18 +2,31 @@
 --  Lowering pedestal (via remote switch)
 --
 
-PREFABS.Item_lowering_remote =
+PREFABS.Item_lowering_remote1 =
 {
   file   = "item/lowering.wad"
+  map    = "MAP01"
   where  = "point"
 
 --!!!  size   = 40
---!!!  height = 144
 
   item_kind = "key"
   key    = "lowering"
 
   tag_1  = "?lock_tag"
   action = "S1_LowerFloor"
+
+  prob   = 1
+}
+
+
+PREFABS.Item_lowering_remote2 =
+{
+  template = "Item_lowering_remote1"
+  map      = "MAP02"
+
+  height = 144
+
+  prob   = 80
 }
 
