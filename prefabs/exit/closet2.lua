bbaa8254bcdc3e2adc27277e6b1f4d78f8cd94ac
Andrew Apted <ajapted@users.sf.net>
2016-05-01 22:52:22 +1000

    Prefabs : added Lua definition for 'exit/closet2' + some tweaks.

---
+++ prefabs/exit/closet2.lua
@@ -0,0 +1,18 @@
+--
+-- Exit closet (no door, flickering light)
+--
+
+PREFABS.Exit_Closet2 =
+{
+  file   = "exit/closet2.wad"
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit = "frame"
+  y_fit = "top"
+
+  prob = 150
+}
+
