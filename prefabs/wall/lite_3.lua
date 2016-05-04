bb2cfb6d992d3e4f94f211e3ebc220ba33903e2b
Andrew Apted <ajapted@users.sf.net>
2016-05-04 15:10:58 +1000

    Prefabs : updated 'lite_3' Lua definition.

--- prefabs/wall/lite_3.lua
+++ prefabs/wall/lite_3.lua
@@ -1,63 +1,48 @@
 --
--- Triple lighting niche
+-- Double lighting niche
 --
 
-UNFINISHED.Pic_lite3_blue =
+GROUPS.wall_lite2 =
 {
-  file   = "wall/lite_3.wad"
+  env = "indoor"
+
+  theme = "tech"
+}
+
+
+UNFINISHED.Wall_lite2_blue =
+{
+  file   = "wall/lite_2.wad"
   where  = "edge"
 
-  long   = 192
   deep   = 16
 
   x_fit  = "frame"
   z_fit  = "frame"
 
   bound_z1 = 0
-  bound_z2 = 176
+  bound_z2 = 128
 
   theme  = "tech"
   prob   = 40
 }
 
 
-UNFINISHED.Pic_lite3_white =
+------------------------------------------------------------
+
+-- TODO : color variations
+
+UNFINISHED.Pic_lite2_white =
 {
-  file   = "wall/lite_3.wad"
-  where  = "edge"
-
-  long   = 192
-  deep   = 16
-
-  x_fit  = "frame"
-  z_fit  = "frame"
-
-  bound_z1 = 0
-  bound_z2 = 176
-
-  theme  = "tech"
-  prob   = 15
+  template = "Wall_lite2_blue"
 
   tex_LITEBLU4 = "LITE5"
 }
 
 
-UNFINISHED.Pic_lite3_red =
+UNFINISHED.Pic_lite2_red =
 {
-  file   = "wall/lite_3.wad"
-  where  = "edge"
-
-  long   = 192
-  deep   = 16
-
-  x_fit  = "frame"
-  z_fit  = "frame"
-
-  bound_z1 = 0
-  bound_z2 = 176
-
-  theme  = "tech"
-  prob   = 5
+  template = "Wall_lite2_blue"
 
   tex_LITEBLU4 = "REDWALL"
 }
