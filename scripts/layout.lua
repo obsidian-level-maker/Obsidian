3cd866aa28228f75bb04a822e02c87d6e576e769
Andrew Apted <ajapted@users.sf.net>
2016-05-05 21:46:34 +1000

    Cages : produce less free-standing cages.

--- scripts/layout.lua
+++ scripts/layout.lua
@@ -1138,7 +1138,8 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
 
     each chunk in R.floor_chunks do
       if not chunk.content_kind and not Chunk_is_slave(chunk) and
-         chunk.sw >= 2 and chunk.sh >= 2 and not chunk.content_kind
+         chunk.sw >= 2 and chunk.sh >= 2 and
+         not chunk.content_kind and rand.odds(50)
       then
         table.insert(locs, chunk)
       end
