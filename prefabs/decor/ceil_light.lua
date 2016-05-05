ad72b57a6848d2a4f57ba3101244e2ac150e3e0c
Andrew Apted <ajapted@users.sf.net>
2016-05-05 14:08:06 +1000

    Prefabs : created Lua definition for 'decor/ceil_light'.

---
+++ prefabs/decor/ceil_light.lua
@@ -0,0 +1,19 @@
+--
+-- Simple ceiling light
+--
+
+PREFABS.Light_1 =
+{
+  file   = "decor/pillar1.wad"
+  kind   = "light"
+  where  = "point"
+
+  env    = "building"
+  height = 96
+
+  bound_z1 = -128
+  bound_z2 = 0
+
+  prob   = 50
+}
+
