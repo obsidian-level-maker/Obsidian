1b51508b1fbbede3b1be587fd084384b5a16e19e
Andrew Apted <ajapted@users.sf.net>
2016-05-07 14:21:10 +1000

    Prefabs : tweaked 'fountain' structure, added Lua definition.

---
+++ prefabs/decor/fountain.lua
@@ -0,0 +1,20 @@
+--
+-- Water Fountain
+--
+
+
+PREFABS.Decor_water_fountain =
+{
+  file   = "decor/fountain.wad"
+  map    = "MAP01"
+
+  where  = "point"
+  size   = 128
+
+  bound_z1 = 0
+
+  env    = "outdoor"
+  theme  = "!hell"
+  prob   = 900
+}
+
