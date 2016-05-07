ced7bbe0f749c7d7e45e0ab2e5e3233a64249694
Andrew Apted <ajapted@users.sf.net>
2016-05-07 16:04:54 +1000

    Prefabs : created Lua definition for 'decor/dh_comp'.

---
+++ prefabs/decor/dh_comp.lua
@@ -0,0 +1,23 @@
+--
+-- Derek's indoor techy computer thing
+--
+
+PREFABS.Decor_dittohead_computer =
+{
+  file   = "decor/dh_comp.wad"
+  map    = "MAP01"
+
+  where  = "point"
+  size   = 128
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = { 128,144 }
+
+  env    = "building"
+  theme  = "tech"
+  prob   = 800
+}
+
