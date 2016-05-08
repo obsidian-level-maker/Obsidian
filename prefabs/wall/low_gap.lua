0a20ef8ba5a8bd8fc801bbe40990008eeaff3562
Andrew Apted <ajapted@users.sf.net>
2016-05-08 23:14:13 +1000

    Prefabs : tweaked definition of 'wall/low_gap'.

--- prefabs/wall/low_gap.lua
+++ prefabs/wall/low_gap.lua
@@ -14,14 +14,12 @@ PREFABS.Wall_lowgap =
 {
   file   = "wall/low_gap.wad"
   map    = "MAP01"
+
   where  = "edge"
+  deep   = 16
+  height = 64
 
   group  = "low_gap"
-
-  long   = 128
-  deep   = 16
-
-  height = 64
 
   bound_z1 = 0
   bound_z2 = 64
@@ -34,14 +32,11 @@ PREFABS.Wall_lowgap_diag =
 {
   file   = "wall/low_gap.wad"
   map    = "MAP02"
+
   where  = "diagonal"
+  height = 64
 
   group  = "low_gap"
-
-  long   = 128
-  deep   = 16
-
-  height = 64
 
   bound_z1 = 0
   bound_z2 = 64
