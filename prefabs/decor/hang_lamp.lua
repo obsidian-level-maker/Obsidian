9bb2c571b4d02df0cab2ce9c460ba91baddf2489
Andrew Apted <ajapted@users.sf.net>
2016-05-09 18:08:31 +1000

    Prefabs : split the hanging lamp off into its own file.

---
+++ prefabs/decor/hang_lamp.lua
@@ -0,0 +1,22 @@
+--
+-- Hanging ceiling lamp
+--
+
+PREFABS.Light_hanging =
+{
+  file   = "decor/ceil_light.wad"
+  map    = "MAP03"
+
+  kind   = "light"
+  where  = "point"
+
+  env    = "building"
+  height = 192
+
+  bound_z1 = -64
+  bound_z2 = 0
+
+  theme  = "urban"
+  prob   = 500
+}
+
