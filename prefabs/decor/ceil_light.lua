66d0f8f779f1b60a0d45fe0fe67d7aa70e58ce12
Andrew Apted <ajapted@users.sf.net>
2016-05-09 16:25:57 +1000

    Prefabs : created a ceil-light prefab using the hanging lamp.

--- prefabs/decor/ceil_light.lua
+++ prefabs/decor/ceil_light.lua
@@ -21,3 +21,22 @@ PREFABS.Light_1 =
   flat_TLITE6_4 = "TLITE6_6"
 }
 
+
+PREFABS.Light_hanging =
+{
+  file   = "decor/ceil_light.wad"
+  map    = "MAP03"
+
+  kind   = "light"
+  where  = "point"
+
+  env    = "building"
+  height = 192
+
+  bound_z1 = -64
+  bound_z2 = 0
+
+  theme  = "urban"
+  prob   = 500
+}
+
