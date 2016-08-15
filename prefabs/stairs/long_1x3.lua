87fae509e44910f503d9053f23e2bd828de7aa77
Andrew Apted <ajapted@users.sf.net>
2016-08-15 23:29:04 +1000

    Prefabs : created a long/narrow staircase (1x3 seeds).

---
+++ prefabs/stairs/long_1x3.lua
@@ -0,0 +1,22 @@
+--
+-- long narrow stair, 3 seeds high
+--
+
+PREFABS.Stair_long_1x3 =
+{
+  file   = "stairs/long_1x3.wad"
+  where  = "seeds"
+  shape  = "I"
+  
+  seed_h = 3
+
+  x_fit  = "stretch"
+
+  bound_z1 = 0
+  bound_z2 = 96
+
+  delta_h = 96
+
+  prob = 90
+}
+
