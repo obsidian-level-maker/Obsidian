67ddf67997cb46cfc17c0b03a24e5cae2e7d721d
Andrew Apted <ajapted@users.sf.net>
2016-08-16 21:00:13 +1000

    Prefabs : added variant of 'wide_3x1' for Urban/Hell themes.

--- prefabs/stairs/wide_3x1.lua
+++ prefabs/stairs/wide_3x1.lua
@@ -5,6 +5,7 @@
 PREFABS.Stair_wide_3x1 =
 {
   file   = "stairs/wide_3x1.wad"
+  map    = "MAP01"
   where  = "seeds"
   shape  = "I"
 
@@ -16,5 +17,25 @@ PREFABS.Stair_wide_3x1 =
   delta_h = 32
 
   prob = 900
+}
+
+
+PREFABS.Stair_wide_3x1_torch =
+{
+  file   = "stairs/wide_3x1.wad"
+  map    = "MAP02"
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 3
+
+  bound_z1 = 0
+  bound_z2 = 32
+
+  delta_h = 32
+
+  theme = "!tech"
+  rank  = 2
+  prob  = 500
 }
 
