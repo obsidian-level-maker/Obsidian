8d02bc0a86d436457f1d8fffead3a33ad4437c00
Andrew Apted <ajapted@users.sf.net>
2016-08-16 16:22:54 +1000

    Prefabs : created a long 1x2 seed staircase.

---
+++ prefabs/stairs/long_1x2.lua
@@ -0,0 +1,22 @@
+--
+-- long narrow stair, 2 seeds high
+--
+
+PREFABS.Stair_long_1x2 =
+{
+  file   = "stairs/long_1x2.wad"
+  where  = "seeds"
+  shape  = "I"
+  
+  seed_h = 2
+
+  x_fit  = "stretch"
+
+  bound_z1 = 0
+  bound_z2 = 64
+
+  delta_h = 64
+
+  prob = 90
+}
+
