75db8821964f437eb12c482269f260afd59dc60f
Andrew Apted <ajapted@users.sf.net>
2016-08-24 21:13:40 +1000

    Prefabs : created a joiner with a computer.

---
+++ prefabs/joiner/computer.lua
@@ -0,0 +1,19 @@
+--
+-- Joiner with a computer
+--
+
+PREFABS.Joiner_computer1 =
+{
+  file   = "joiner/computer.wad"
+  where  = "seeds"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit = "frame"
+  y_fit = "stretch"
+
+  theme = "tech"
+  prob  = 10
+}
+
