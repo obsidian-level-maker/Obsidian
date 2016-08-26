5ef01bfae2c5f3a04345edd4b43a874f8efe1b49
Andrew Apted <ajapted@users.sf.net>
2016-08-26 19:28:45 +1000

    Prefabs : created twin arch variation of "arch_plain".

--- prefabs/door/arch_plain.lua
+++ prefabs/door/arch_plain.lua
@@ -32,3 +32,60 @@ PREFABS.Arch_plain_diag =
   bound_z2 = 128
 }
 
+
+PREFABS.Arch_plain2 =
+{
+  file   = "door/arch_plain.wad"
+  map    = "MAP03"
+
+  kind   = "arch"
+  where  = "edge"
+
+  seed_w = 3
+
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+
+  bound_z1 = 0
+  bound_z2 = 128
+
+  theme  = "tech"
+  prob   = 400
+}
+
+
+PREFABS.Arch_plain2_satyr =
+{
+  template = "Arch_plain2"
+
+  theme  = "urban"
+  rank   = 1
+  prob   = 250
+
+  tex_SILVER2 = "GSTSATYR"
+}
+
+PREFABS.Arch_plain2_garg =
+{
+  template = "Arch_plain2"
+
+  theme  = "urban"
+  rank   = 1
+  prob   = 250
+
+  tex_SILVER2 = "GSTGARG"
+}
+
+PREFABS.Arch_plain2_lion =
+{
+  template = "Arch_plain2"
+
+  theme  = "urban"
+  rank   = 1
+  prob   = 250
+
+  tex_SILVER2 = "GSTLION"
+}
+
