33e7b93f8469629d7d345982f8d61ebf37bc27ec
Andrew Apted <ajapted@users.sf.net>
2016-08-26 12:48:59 +1000

    Prefabs : created a L-shape stairwell joiner: "curve1".

---
+++ prefabs/joiner/curve1.lua
@@ -0,0 +1,19 @@
+--
+-- L-shape Joiner with stairs
+--
+
+PREFABS.Joiner_curve1 =
+{
+  file   = "joiner/curve1.wad"
+  where  = "seeds"
+  shape  = "L"
+
+  seed_w = 2
+  seed_h = 2
+
+  prob = 50
+
+  delta_h  = 64
+  nearby_h = 128
+}
+
