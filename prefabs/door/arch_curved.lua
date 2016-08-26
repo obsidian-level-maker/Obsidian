48b3908e65cabb802ba72a6611807d62f55f0bef
Andrew Apted <ajapted@users.sf.net>
2016-08-26 19:02:17 +1000

    Prefabs : created archways with 2 and 3 arches.

--- prefabs/door/arch_curved.lua
+++ prefabs/door/arch_curved.lua
@@ -2,9 +2,10 @@
 -- Archway with a curved arch
 --
 
-PREFABS.Arch_curved =
+PREFABS.Arch_curved1 =
 {
   file   = "door/arch_curved.wad"
+  map    = "MAP01"
   kind   = "arch"
   where  = "edge"
 
@@ -21,5 +22,29 @@ PREFABS.Arch_curved =
 
   rank   = 1
   prob   = 50
+}
+
+
+PREFABS.Arch_curved2 =
+{
+  template = "Arch_curved1"
+
+  map    = "MAP02"
+
+  seed_w = 3
+
+  prob   = 200
+}
+
+
+PREFABS.Arch_curved3 =
+{
+  template = "Arch_curved1"
+
+  map    = "MAP03"
+
+  seed_w = 4
+
+  prob   = 800
 }
 
