b4f1b076e68c496654f2594eb7eec6965d8f0f83
Andrew Apted <ajapted@users.sf.net>
2016-08-26 12:49:30 +1000

    Prefabs : shrunk the y_stair joiner from attic --> "y3.wad"

---
+++ prefabs/joiner/y3.lua
@@ -0,0 +1,20 @@
+--
+-- another Y-shapish 3x2 joiner 
+--
+
+PREFABS.Joiner_y3 =
+{
+  file   = "joiner/y3.wad"
+  where  = "seeds"
+
+  seed_w = 3
+  seed_h = 2
+
+  theme = "!tech"
+  prob  = 90000
+
+  delta_h  = 32
+  nearby_h = 160
+  can_flip = true
+}
+
