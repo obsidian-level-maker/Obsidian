120ce928b78602fa16133f677d34666d5942832a
Andrew Apted <ajapted@users.sf.net>
2016-08-27 15:49:33 +1000

    Modules : updated Level Control module for recent stuff, added back "big_rooms", "doors" and "secrets" settings.

--- modules/level_control.lua
+++ modules/level_control.lua
@@ -55,24 +55,25 @@ OB_MODULES["level_control"] =
 
   options =
   {
+    big_rooms   = { label=_("Big Rooms"),      choices=LEVEL_CONTROL.CHOICES }
     cages       = { label=_("Cages"),          choices=LEVEL_CONTROL.CHOICES }
     darkness    = { label=_("Dark Outdoors"),  choices=LEVEL_CONTROL.CHOICES }
+    doors       = { label=_("Doors"),          choices=LEVEL_CONTROL.CHOICES }
 
     keys        = { label=_("Keyed Doors"),    choices=LEVEL_CONTROL.CHOICES }
     liquids     = { label=_("Liquids"),        choices=LEVEL_CONTROL.CHOICES }
     mon_variety = { label=_("Monster Variety"),choices=LEVEL_CONTROL.CHOICES, tooltip="Setting this to NONE will make each level use a single monster type" }
 
+    secrets     = { label=_("Secrets"),        choices=LEVEL_CONTROL.CHOICES }
     steepness   = { label=_("Steepness"),      choices=LEVEL_CONTROL.CHOICES }
     switches    = { label=_("Switched Doors"), choices=LEVEL_CONTROL.CHOICES }
+
     teleporters = { label=_("Teleporters"),    choices=LEVEL_CONTROL.CHOICES }
     traps       = { label=_("Traps"),          choices=LEVEL_CONTROL.CHOICES }
     windows     = { label=_("Windows"),        choices=LEVEL_CONTROL.CHOICES }
 
---  secrets     = { label=_("Secrets"),        choices=LEVEL_CONTROL.CHOICES }
---  big_rooms   = { label=_("Big Rooms"),      choices=LEVEL_CONTROL.CHOICES }
 --  symmetry    = { label=_("Symmetry"),       choices=LEVEL_CONTROL.CHOICES }
 
---  doors       = { label=_("Doors"),          choices=LEVEL_CONTROL.CHOICES }
 --  hallways    = { label=_("Hallways"),       choices=LEVEL_CONTROL.CHOICES }
 --  pictures    = { label=_("Pictures"),       choices=LEVEL_CONTROL.CHOICES }
 --  crates      = { label=_("Crates"),         choices=LEVEL_CONTROL.CHOICES }
