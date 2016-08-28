2c6bedd84aaf26f2b18f9dd32e9687e59cdd9d20
Andrew Apted <ajapted@users.sf.net>
2016-08-28 22:04:56 +1000

    Prefabs : created a variation of the "closet1" exit.

--- prefabs/exit/closet1.lua
+++ prefabs/exit/closet1.lua
@@ -2,7 +2,7 @@
 -- Exit closet
 --
 
-PREFABS.Exit_Closet1 =
+PREFABS.Exit_closet1 =
 {
   file   = "exit/closet1.wad"
   where  = "seeds"
@@ -14,12 +14,14 @@ PREFABS.Exit_Closet1 =
   y_fit = "top"
 
   theme = "!tech"
+
+  prob  = 100
 }
 
 
-PREFABS.Exit_Closet1_tech =
+PREFABS.Exit_closet1_tech =
 {
-  template = "Exit_Closet1"
+  template = "Exit_closet1"
 
   tex_STEP3 = "STEP1"
 
@@ -27,5 +29,38 @@ PREFABS.Exit_Closet1_tech =
   tex_SW1VINE  = "SW1BLUE"
 
   theme = "tech"
+}
+
+
+------- Trappy variation -------------------------
+
+PREFABS.Exit_closet1_trap =
+{
+  template = "Exit_closet1"
+  map      = "MAP02"
+
+  seed_w = 1
+  seed_h = 2
+
+  prob   = 20
+}
+
+
+PREFABS.Exit_closet1_trap_tech =
+{
+  template = "Exit_closet1"
+  map      = "MAP02"
+
+  seed_w = 1
+  seed_h = 2
+
+  tex_STEP3 = "STEP1"
+
+  tex_GRAYVINE = "COMPBLUE"
+  tex_SW1VINE  = "SW1BLUE"
+
+  theme  = "tech"
+
+  prob   = 20
 }
 
