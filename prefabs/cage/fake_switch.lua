5596c9ecce41ee7df48ce496df069b1691e02f7e
Andrew Apted <ajapted@users.sf.net>
2016-08-28 19:49:45 +1000

    Prefabs : began work on "fake_switch" cage...

---
+++ prefabs/cage/fake_switch.lua
@@ -0,0 +1,20 @@
+--
+-- Trap looking like a normal switch
+--
+
+PREFABS.Cage_fake_switch =
+{
+  file   = "cage/fake_switch.wad"
+  where  = "seeds"
+  shape  = "U"
+
+  seed_w = 2
+  seed_h = 1
+
+  y_fit = "top"
+
+  bound_z1 = 0
+
+  prob  = 999
+}
+
