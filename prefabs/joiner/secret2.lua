22dba116b9d36c4a7a786b9d848e5531f14a0e9c
Andrew Apted <ajapted@users.sf.net>
2016-08-28 23:05:57 +1000

    Prefabs : simplified the new "secret2" joiner.

---
+++ prefabs/joiner/secret2.lua
@@ -0,0 +1,23 @@
+--
+-- Secret joiners
+--
+
+PREFABS.Joiner_secret2_A =
+{
+  file   = "joiner/secret2.wad"
+  map    = "MAP01"
+
+  where  = "seeds"
+  shape  = "I"
+
+  key    = "secret"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit  = "frame"
+  y_fit  = "frame"
+
+  prob   = 100
+}
+
