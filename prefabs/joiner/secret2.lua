2ce2c638b7a73547e1be99e01174a5e7aebd8e33
Andrew Apted <ajapted@users.sf.net>
2016-08-29 15:17:26 +1000

    Prefabs : created two variations of the "secret2" joiner.

--- prefabs/joiner/secret2.lua
+++ prefabs/joiner/secret2.lua
@@ -16,14 +16,14 @@ PREFABS.Joiner_secret2_A =
   seed_h = 1
 
   x_fit  = "frame"
-  y_fit  = "frame"
+  y_fit  = "stretch"
 
-  prob   = 100
+  prob   = 150
 
   -- pick some different objects for the hint, often none
-  thing_34 =
+  thing_33 =
   {
-    nothing = 40
+    nothing = 30
     barrel = 10
     dead_player = 10
     dead_imp = 10
@@ -31,5 +31,43 @@ PREFABS.Joiner_secret2_A =
     gibbed_player = 5
     pool_brains = 5
   }
+}
+
+
+-- hint is a small gap
+PREFABS.Joiner_secret2_B =
+{
+  template = "Joiner_secret2_A"
+
+  map    = "MAP02"
+  y_fit  = "top"
+
+  prob   = 50
+}
+
+
+-- this one looks like a picture
+PREFABS.Joiner_secret2_C1 =
+{
+  template = "Joiner_secret2_A"
+
+  map    = "MAP03"
+  y_fit  = "top"
+
+  theme  = "tech"
+  prob   = 100
+}
+
+PREFABS.Joiner_secret2_C2 =
+{
+  template = "Joiner_secret2_A"
+
+  map    = "MAP03"
+  y_fit  = "top"
+
+  theme  = "!tech"
+  prob   = 100
+
+  tex_SILVER2 = "MARBFAC3"
 }
 
