1447e01cbd34e71d94c593cc12e207888f45e610
Andrew Apted <ajapted@users.sf.net>
2016-08-29 23:28:40 +1000

    Prefabs : finished the "secret3" joiner.

---
+++ prefabs/joiner/secret3.lua
@@ -0,0 +1,23 @@
+--
+-- More secret joiners
+--
+
+PREFABS.Joiner_secret3_cage =
+{
+  file   = "joiner/secret3.wad"
+  map    = "MAP01"
+
+  where  = "seeds"
+  shape  = "I"
+
+  key    = "secret"
+
+  seed_w = 2
+  seed_h = 1
+
+  x_fit  = "frame"
+--y_fit  = "top"
+
+  prob   = 40
+}
+
