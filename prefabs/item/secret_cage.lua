a0b27315649563959ab443d3a3da0920e4390c31
Andrew Apted <ajapted@users.sf.net>
2016-08-30 16:33:48 +1000

    Prefabs : created "secret_cage" item closet.

---
+++ prefabs/item/secret_cage.lua
@@ -0,0 +1,21 @@
+--
+--  Secret item masquerading as a cage
+--
+
+PREFABS.Item_secret_cage =
+{
+  file  = "item/secret_cage.wad"
+  where = "seeds"
+
+  key   = "secret"
+
+  seed_w = 2
+  seed_h = 2
+  height = 120
+
+  x_fit = "frame"
+
+  prob  = 200
+  env   = "building"
+}
+
