da7424c4c31566023465e7187161345576e37690
Andrew Apted <ajapted@users.sf.net>
2016-09-02 20:41:16 +1000

    Version bump, in honor of good handling of secrets.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "7.40"
-#define OBLIGE_HEX_VER  0x740
+#define OBLIGE_VERSION  "7.42"
+#define OBLIGE_HEX_VER  0x742
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
