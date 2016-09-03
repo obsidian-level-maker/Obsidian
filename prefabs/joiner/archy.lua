4f6252cb1d78526c2abe0b17d5a0415bc72ad3b6
Andrew Apted <ajapted@users.sf.net>
2016-09-03 23:19:39 +1000

    Prefabs : created a double-arch variation of the "archy" joiner.

--- prefabs/joiner/archy.lua
+++ prefabs/joiner/archy.lua
@@ -5,16 +5,35 @@
 PREFABS.Joiner_archy1 =
 {
   file   = "joiner/archy.wad"
+  map    = "MAP01"
+
   where  = "seeds"
   shape  = "I"
 
   seed_w = 2
   seed_h = 1
 
-  x_fit = "frame"
   y_fit = "stretch"
 
   theme = "!tech"
   prob  = 140
+}
+
+
+PREFABS.Joiner_archy1_wide =
+{
+  file   = "joiner/archy.wad"
+  map    = "MAP02"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 3
+  seed_h = 1
+
+  x_fit = "frame"
+
+  theme = "!tech"
+  prob  = 400
 }
 
