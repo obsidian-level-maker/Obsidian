40f5c57f3acbe03d2870435cabdbf081891977bb
Andrew Apted <ajapted@users.sf.net>
2016-09-04 20:17:13 +1000

    Prefabs : got the "secret_ledge" prefab working.

---
+++ prefabs/item/secret_ledge.lua
@@ -0,0 +1,21 @@
+--
+-- Closet with a secret item visible from outside
+--
+
+PREFABS.Item_secret_ledge =
+{
+  file  = "item/secret_ledge.wad"
+  where = "seeds"
+
+  key   = "secret"
+
+  seed_w = 3
+  seed_h = 1
+  height = 160
+
+  x_fit = "frame"
+  y_fit = "frame"
+
+  prob  = 5000
+}
+
