4010c4358035e202dbf09c09cdb7b59cf148b2a5
Andrew Apted <ajapted@users.sf.net>
2016-09-11 16:59:24 +1000

    Prefabs : tested the "waterfall" prefab, doesn't work very well since the liquid part rarely connects nicely with the liquid in the cave.  There are other issues too, hence disabled it for now.

---
+++ prefabs/decor/waterfall.lua
@@ -0,0 +1,22 @@
+--
+-- Waterfall for caves
+--
+
+UNFINISHED.Decor_cave_waterfall =
+{
+  file   = "decor/waterfall.wad"
+
+  prob   = 20*99
+  env    = "cave"
+
+  where  = "seeds"
+  shape  = "U"
+
+  seed_w = 3
+  seed_h = 1
+
+  deep   = 192
+
+  y_fit  = "top"
+}
+
