6c041a4b2594ac5c08af210dbcbcd965dbfbcab5
Andrew Apted <ajapted@users.sf.net>
2016-09-12 21:09:28 +1000

    Prefabs : improved the cavey "stalag" prefabs, added defs.

---
+++ prefabs/decor/stalag.lua
@@ -0,0 +1,33 @@
+--
+-- Cavey stalagmite / stalactite
+--
+
+PREFABS.Decor_stalag1 =
+{
+  file   = "decor/stalag.wad"
+  map    = "MAP01"
+
+  prob   = 200
+  env    = "cave"
+
+  where  = "point"
+  size   = 104  -- NOTE: a hack, it is really 128
+  height = 160
+
+  bound_z1 = 0
+  bound_z2 = 160
+
+  z_fit  = "stretch"
+
+  sink_mode = "never"
+}
+
+
+PREFABS.Decor_stalag2 =
+{
+  template = "Decor_stalag1"
+  map      = "MAP02"
+
+  prob   = 100
+}
+
