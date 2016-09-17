3d1f149fd074bd304ba642765d43bc8a8d4f1bf7
Andrew Apted <ajapted@users.sf.net>
2016-09-17 23:33:06 +1000

    Prefabs : tidy up definition of lowering key pedestal.

--- prefabs/item/lowering.lua
+++ prefabs/item/lowering.lua
@@ -6,17 +6,15 @@ PREFABS.Item_lowering_remote1 =
 {
   file   = "item/lowering.wad"
   map    = "MAP01"
-  where  = "point"
 
---!!!  size   = 40
-
-  item_kind = "key"
+  prob   = 1
   key    = "lowering"
+  item_kind = "key"
+
+  where  = "point"
 
   tag_1  = "?lock_tag"
   action = "S1_LowerFloor"
-
-  prob   = 1
 }
 
 
@@ -25,8 +23,8 @@ PREFABS.Item_lowering_remote2 =
   template = "Item_lowering_remote1"
   map      = "MAP02"
 
-  height = 144
-
   prob   = 80
+
+  height = 144
 }
 
