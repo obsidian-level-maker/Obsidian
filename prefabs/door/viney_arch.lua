85140b8f33a716ddb16e06666278aac933143183
Andrew Apted <ajapted@users.sf.net>
2016-09-19 19:04:36 +1000

    Prefabs : more work on definitions for "viney_arch"....

--- prefabs/door/viney_arch.lua
+++ prefabs/door/viney_arch.lua
@@ -2,11 +2,20 @@
 -- Cavey archway with vines
 --
 
-PREFABS.Arch_viney =
+UNFINISHED.Arch_viney1 =
 {
   file   = "door/viney_arch.wad"
+  map    = "MAP02"
+
+  rank = 2
+  prob = 50
+
+  env      = "cave"
+  neighbor = "any"
+
   where  = "edge"
-  long   = 192
+  seed_w = 2
+
   deep   = 16
   over   = 16
 
@@ -14,12 +23,64 @@ PREFABS.Arch_viney =
 
   bound_z1 = 0
   bound_z2 = 132
+}
 
-  prob = 90
 
-  room     = "cave"
-  neighbor = "outdoor"
+UNFINISHED.Arch_viney1_B =
+{
+  template = "Arch_viney1"
 
-  rank = 2
+  env      = "any"
+  neighbor = "cave"
+}
+
+
+---------- JOINER VERSIONS -----------------------
+
+
+UNFINISHED.Joiner_viney1_CC =
+{
+  file   = "door/viney_arch.wad"
+  map    = "MAP04"
+
+  rank   = 2
+  prob   = 50
+
+  env      = "cave"
+  neighbor = "cave"
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 2
+  seed_h = 1
+
+  deep   = 16
+  over   = 16
+
+  x_fit  = "frame"
+  y_fit  = "stretch"
+}
+
+
+UNFINISHED.Joiner_viney1_CN =
+{
+  template = "Joiner_viney1"
+  map      = "MAP05"
+
+  env      = "cave"
+  neighbor = "!cave"
+}
+
+
+UNFINISHED.Joiner_viney1_NC =
+{
+  template = "Joiner_viney1"
+  map      = "MAP05"
+
+  env      = "!cave"
+  neighbor = "cave"
+
+  must_flip = true
 }
 
