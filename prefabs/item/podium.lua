1c1f7cc59f91f201402d81a79fd0df81ac426278
Andrew Apted <ajapted@users.sf.net>
2016-09-20 11:57:24 +1000

    Prefabs : created a better reskin of the tech podium for DOOM 1.

--- prefabs/item/podium.lua
+++ prefabs/item/podium.lua
@@ -26,3 +26,21 @@ PREFABS.Item_podium_tech =
   theme = "tech"
 }
 
+
+PREFABS.Item_podium_doom1 =
+{
+  template = "Item_podium"
+
+  map = "MAP02"
+
+  rank  = 4
+  game  = { doom1=1, ultdoom=1 }
+  theme = "tech"
+
+  tex_TEKLITE  = "COMPSPAN"
+  flat_CEIL3_1 = "CEIL5_1"
+
+  tex_METAL6   = "LITE3"
+--flat_CEIL3_5 = "FLAT1"
+}
+
