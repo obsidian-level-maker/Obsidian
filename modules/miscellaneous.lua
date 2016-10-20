5131eefe84596909e2a40340af5247106f38b4f5
Andrew Apted <ajapted@users.sf.net>
2016-10-20 20:35:05 +1100

    MODULES : removed the "Extra Settings" module, merging its two options into the "Miscellaneous" module.

--- modules/miscellaneous.lua
+++ modules/miscellaneous.lua
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------
---  MODULE: Level Control
+--  MODULE: Miscellaneous Stuff
 ------------------------------------------------------------------------
 --
 --  Copyright (C) 2009      Enhas
@@ -17,20 +17,42 @@
 --
 ------------------------------------------------------------------------
 
-LEVEL_CONTROL = { }
+MISC_STUFF = { }
 
-function LEVEL_CONTROL.begin_level(self)
+MISC_STUFF.YES_NO =
+{
+  "no",  _("No"),
+  "yes", _("Yes"),
+}
+
+
+MISC_STUFF.variety_tip = _(
+    "Affects how many different monster types can "..
+    "appear in each room.  "..
+    "Setting this to NONE will make each level use a single monster type")
+
+
+function MISC_STUFF.begin_level(self)
   for name,opt in pairs(self.options) do
-    local factor = self.options[name].value
+    local value = self.options[name].value
 
-    if factor != "mixed" then
-      STYLE[name] = factor
+    if opt.choices == STYLE_CHOICES then
+      if value != "mixed" then
+        STYLE[name] = value
+      end
+
+    else
+      -- pistol_starts, or other YES/NO stuff
+
+      if value != "no" then
+        PARAM[name] = value
+      end
     end
   end
 end
 
 
-OB_MODULES["level_control"] =
+OB_MODULES["miscellaneous"] =
 {
   label = _("Miscellaneous")
 
@@ -39,31 +61,38 @@ OB_MODULES["level_control"] =
 
   hooks =
   {
-    begin_level = LEVEL_CONTROL.begin_level
+    begin_level = MISC_STUFF.begin_level
   }
 
   options =
   {
+    pistol_starts =
+    {
+      label=_("Pistol Starts")
+      choices=EXTRA_SETTINGS.YES_NO
+      tooltip=_("Ensure every map can be completed from a pistol start (ignore weapons obtained from earlier maps)")
+    }
+
+    start_together =
+    {
+      label=_("Start Together")
+      choices=EXTRA_SETTINGS.YES_NO
+      tooltip=_("For Co-operative games, ensure all players start in the same room (disable the separated start rooms)")
+    }
+
     big_rooms   = { label=_("Big Rooms"),      choices=STYLE_CHOICES }
---  cages       = { label=_("Cages"),          choices=STYLE_CHOICES }
     darkness    = { label=_("Dark Outdoors"),  choices=STYLE_CHOICES }
     doors       = { label=_("Doors"),          choices=STYLE_CHOICES }
 
     keys        = { label=_("Keyed Doors"),    choices=STYLE_CHOICES }
---  liquids     = { label=_("Liquids"),        choices=STYLE_CHOICES }
---  mon_variety = { label=_("Monster Variety"),choices=STYLE_CHOICES, tooltip="Setting this to NONE will make each level use a single monster type" }
+    mon_variety = { label=_("Monster Variety"),choices=STYLE_CHOICES, tooltip=MISC_STUFF.variety_tip }
 
---  secrets     = { label=_("Secrets"),        choices=STYLE_CHOICES }
---  steepness   = { label=_("Steepness"),      choices=STYLE_CHOICES }
     switches    = { label=_("Switched Doors"), choices=STYLE_CHOICES }
 
---  teleporters = { label=_("Teleporters"),    choices=STYLE_CHOICES }
---  traps       = { label=_("Traps"),          choices=STYLE_CHOICES }
     windows     = { label=_("Windows"),        choices=STYLE_CHOICES }
 
 ---- PLANNED (UNFINISHED) STUFF ----
 
---  hallways    = { label=_("Hallways"),       choices=STYLE_CHOICES }
 --  symmetry    = { label=_("Symmetry"),       choices=STYLE_CHOICES }
 --  pictures    = { label=_("Pictures"),       choices=STYLE_CHOICES }
 --  cycles      = { label=_("Multiple Paths"), choices=STYLE_CHOICES }
@@ -72,8 +101,6 @@ OB_MODULES["level_control"] =
 --  barrels     = { label=_("Barrels"),        choices=STYLE_CHOICES }
 --  porches     = { label=_("Porches"),        choices=STYLE_CHOICES }
 --  lakes       = { label=_("Lakes"),          choices=STYLE_CHOICES }
---  bridges     = { label=_("3D Bridges"),     choices=STYLE_CHOICES }
---  crossovers  = { label=_("Cross-Overs"),    choices=STYLE_CHOICES }
   }
 }
 
