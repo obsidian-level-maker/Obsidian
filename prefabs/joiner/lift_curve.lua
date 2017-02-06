519de6a2d606a7aac74e043de9a3c692e4e24cf9
Andrew Apted <ajapted@users.sf.net>
2017-02-06 22:24:58 +1100

    Prefabs : added theme-appropiate variations of the "lift_curve" joiner, courtesy Glaice.

--- prefabs/joiner/lift_curve.lua
+++ prefabs/joiner/lift_curve.lua
@@ -5,9 +5,11 @@
 PREFABS.Joiner_lift_curve =
 {
   file   = "joiner/lift_curve.wad"
+  map    = "MAP01"
 
   prob   = 30
   style  = "steepness"
+  theme  = "tech"
 
   env      = "!cave"
   neighbor = "!cave"
@@ -20,5 +22,45 @@ PREFABS.Joiner_lift_curve =
 
   delta_h  = 128
   nearby_h = 128
+}
+
+
+PREFABS.Joiner_lift_curve_urban =
+{
+  template = "Joiner_lift_curve"
+
+  map    = "MAP02"
+
+  theme  = "urban"
+}
+
+
+PREFABS.Joiner_lift_curve_hell =
+{
+  template = "Joiner_lift_curve"
+
+  map    = "MAP03"
+
+  theme  = "hell"
+}
+
+
+PREFABS.Joiner_lift_curve_deimos =
+{
+  template = "Joiner_lift_curve"
+
+  map    = "MAP01"
+
+  theme  = "deimos"
+}
+
+
+PREFABS.Joiner_lift_curve_flesh =
+{
+  template = "Joiner_lift_curve"
+
+  map    = "MAP04"
+
+  theme  = "flesh"
 }
 
