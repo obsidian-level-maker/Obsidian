24ea97655b036f4feb9e57ad129120e232caeb40
Andrew Apted <ajapted@users.sf.net>
2017-02-11 22:26:05 +1100

    CSG QUAKE3 : created "g_quake3.cc" code file, simply a copy of the "g_quake2.cc" code with various variables and functions renamed (Q2_XXX --> Q3_XXX, etc).

--- gui/main.h
+++ gui/main.h
@@ -151,6 +151,7 @@ game_interface_c * Doom_GameObject();
 game_interface_c * Nukem_GameObject();
 game_interface_c * Quake1_GameObject();
 game_interface_c * Quake2_GameObject();
+game_interface_c * Quake3_GameObject();
 
 
 #endif /* __OBLIGE_MAIN_H__ */
