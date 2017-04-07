0a1d0d81b9aac63fbe07ece4d87340e45183929d
Andrew Apted <ajapted@users.sf.net>
2017-04-07 16:41:48 +1000

    GUI : version bump, in honor of Q3 support + better windows.

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "7.52"
-#define OBLIGE_HEX_VER  0x752
+#define OBLIGE_VERSION  "7.53"
+#define OBLIGE_HEX_VER  0x753
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
