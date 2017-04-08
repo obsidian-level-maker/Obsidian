d82a8f3ca5e0afa7d8bc475d2d05dc9a463dec8d
Andrew Apted <ajapted@users.sf.net>
2017-04-08 22:44:05 +1000

    Prefabs : created a fence-post prefab with a lamp on top.

---
+++ prefabs/fence/posts.lua
@@ -0,0 +1,35 @@
+--
+-- Various fence posts
+--
+
+
+PREFABS.Post_tech_1 =
+{
+  file   = "fence/posts.wad"
+  map    = "MAP01"
+  kind   = "post"
+
+  prob   = 50
+
+  where  = "point"
+  size   = 48
+
+  bound_z1 = 0
+}
+
+
+PREFABS.Post_tech_2 =
+{
+  file   = "fence/posts.wad"
+  map    = "MAP02"
+  kind   = "post"
+
+  prob   = 50
+
+  where  = "point"
+  size   = 64
+  height = 128
+
+  bound_z1 = 0
+}
+
