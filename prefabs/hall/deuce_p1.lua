9ea5f651cb05dd410c26118e5c55cb1f50bb6b80
Andrew Apted <ajapted@users.sf.net>
2017-04-30 16:15:37 +1000

    Prefabs : created Lua definition for "deuce_p1" hallway piece.

---
+++ prefabs/hall/deuce_p1.lua
@@ -0,0 +1,18 @@
+--
+-- 2-seed-wide hallway : four-way "+" junction
+--
+
+PREFABS.Hallway_deuce_p1 =
+{
+  file   = "hall/deuce_p1.wad"
+  map    = "MAP01"
+
+  where  = "seeds"
+  shape  = "P"
+
+  seed_w = 2
+  seed_h = 2
+
+  prob   = 50
+}
+
