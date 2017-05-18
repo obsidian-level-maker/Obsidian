e09b14e0a9d65da1fb0eb80bea8484c18883656d
Andrew Apted <ajapted@users.sf.net>
2017-05-18 15:09:51 +1000

    Prefabs : created a cagey piece for the vent hallway.

--- prefabs/hall/vent_u.lua
+++ prefabs/hall/vent_u.lua
@@ -14,3 +14,17 @@ PREFABS.Hallway_vent_u1 =
   shape  = "U"
 }
 
+
+PREFABS.Hallway_vent_cage1 =
+{
+  file   = "hall/vent_u.wad"
+  map    = "MAP02"
+
+  group  = "vent"
+  style  = "cages"
+  prob   = 50
+
+  where  = "seeds"
+  shape  = "U"
+}
+
