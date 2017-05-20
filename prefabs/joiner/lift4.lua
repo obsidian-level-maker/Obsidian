d3502723173dbd4a410d03f2076794ccaf9331f4
Andrew Apted <ajapted@users.sf.net>
2017-05-20 21:24:38 +1000

    Prefabs : made a non-tech retexturing of "lift4" joiner.

--- prefabs/joiner/lift4.lua
+++ prefabs/joiner/lift4.lua
@@ -6,8 +6,11 @@ PREFABS.Joiner_lift4 =
 {
   file   = "joiner/lift4.wad"
 
-  prob   = 150
+  prob   = 150*9
   style  = "steepness"
+  theme  = "tech"
+
+  env      = "!cave"
   neighbor = "!cave"
 
   where  = "seeds"
@@ -23,5 +26,18 @@ PREFABS.Joiner_lift4 =
 
   delta_h  = 128
   nearby_h = 128
+}
+
+
+PREFABS.Joiner_lift4_hell =
+{
+  template = "Joiner_lift4"
+
+  theme = "!tech"
+
+  tex_SUPPORT2  = "SUPPORT3"
+  tex_TEKWALL4  = "STONE3"
+  flat_MFLR8_2  = "MFLR8_1"
+  flat_TLITE6_5 = "TLITE6_6"
 }
 
