2deda932aa6f5e925248f59b93eee56d8d0a7afb
Andrew Apted <ajapted@users.sf.net>
2017-05-28 00:11:51 +1000

    Prefabs : created "simple2" joiner, inspired by a V5 prefab.

---
+++ prefabs/joiner/simple2.lua
@@ -0,0 +1,24 @@
+--
+-- Simple joiner II
+--
+
+PREFABS.Joiner_simple2 =
+{
+  file   = "joiner/simple2.wad"
+  map    = "MAP01"
+
+  prob   = 1400
+
+  where  = "seeds"
+  shape  = "I"
+
+  seed_w = 3
+  seed_h = 1
+
+  deep   = 16
+  over   = 16
+
+  x_fit = "frame"
+--y_fit = "frame"
+}
+
