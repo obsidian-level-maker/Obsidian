6e49929b72fb55978f8e8499323606c235009ad4
Andrew Apted <ajapted@users.sf.net>
2017-06-24 17:01:13 +1000

    Prefabs : the "blob_lamp" decor was over-used, reduced prob.

--- prefabs/decor/blob_lamp.lua
+++ prefabs/decor/blob_lamp.lua
@@ -7,8 +7,8 @@ PREFABS.Decor_blob_lamp =
   file   = "decor/blob_lamp.wad"
   map    = "MAP01"
 
-  prob   = 500
-  skip_prob = 50
+  prob   = 150
+  skip_prob = 75
 
   theme  = "!hell"
   env    = "outdoor"
