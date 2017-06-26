85a416c09e032578d6d872b2da4c38e5f25ce976
Andrew Apted <ajapted@users.sf.net>
2017-06-26 23:22:05 +1000

    Prefabs : added hell theme version of the start sky-pad.

--- prefabs/start/sky_pad.lua
+++ prefabs/start/sky_pad.lua
@@ -9,6 +9,7 @@ PREFABS.Start_sky_pad =
 
   rank   = 2
   prob   = 50
+  theme  = "tech"
 
   env    = "outdoor"
   open_to_sky = 1
@@ -18,5 +19,23 @@ PREFABS.Start_sky_pad =
   seed_h = 2
 
   x_fit  = { 72,80, 176,184 }
+}
+
+
+PREFABS.Start_sky_pad_hell =
+{
+  template = "Start_sky_pad"
+
+  theme  = "!tech"
+
+  -- replace the lamps
+  thing_2028 = "candelabra"
+
+  -- texture replacements
+  tex_STEP1   = "STEP4"
+  tex_STARBR2 = "GSTONE1"
+  tex_BROWN1  = "STONE2"
+
+  flat_FLOOR4_1 = "MFLR8_1"
 }
 
