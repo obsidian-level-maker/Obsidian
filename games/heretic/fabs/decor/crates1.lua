dacb6edf6bd92744bafd25fcbc6c589f5082effd
Andrew Apted <ajapted@users.sf.net>
2017-08-07 22:30:38 +1000

    HER : disabled the crate prefabs for now....

--- games/heretic/fabs/decor/crates1.lua
+++ games/heretic/fabs/decor/crates1.lua
@@ -5,12 +5,11 @@
 
 -- a small crate, 64x64 in size
 
-PREFABS.Crate_small_brown =
+UNFINISHED.Crate_small_brown =
 {
   file   = "decor/crates1.wad"
   map    = "MAP01"
 
-  theme  = "!hell"
   prob   = 20
 
   where  = "point"
@@ -18,7 +17,7 @@ PREFABS.Crate_small_brown =
 }
 
 
-PREFABS.Crate_small_gray =
+UNFINISHED.Crate_small_gray =
 {
   template = "Crate_small_brown"
 
@@ -27,37 +26,15 @@ PREFABS.Crate_small_gray =
 }
 
 
--- ones for Urban and Hell themes
-
-PREFABS.Crate_small_woodmet =
-{
-  file   = "decor/crates1.wad"
-  map    = "MAP11"
-
-  theme  = "!tech"
-  prob   = 20
-
-  where  = "point"
-  size   = 64
-}
-
-PREFABS.Crate_small_wood3 =
-{
-  template = "Crate_small_woodmet"
-
-  map    = "MAP10"
-}
-
 
 -- a tall narrow crate
 
-PREFABS.Crate_tall_brown =
+UNFINISHED.Crate_tall_brown =
 {
   file   = "decor/crates1.wad"
   map    = "MAP02"
 
   prob   = 8
-  theme  = "!hell"
 
   where  = "point"
   size   = 64
@@ -67,13 +44,12 @@ PREFABS.Crate_tall_brown =
 
 -- a medium-size crate (96x96)
 
-PREFABS.Crate_medium_gray =
+UNFINISHED.Crate_medium_gray =
 {
   file   = "decor/crates1.wad"
   map    = "MAP03"
 
   prob   = 8
-  theme  = "!hell"
   env    = "!cave"
 
   where  = "point"
@@ -81,31 +57,13 @@ PREFABS.Crate_medium_gray =
 }
 
 
--- a group of three and a half crates
-
-PREFABS.Crate_group_medium =
-{
-  file   = "decor/crates1.wad"
-  map    = "MAP04"
-
-  theme  = "!hell"
-  env    = "!cave"
-  prob   = 300
-
-  where  = "point"
-  size   = 128
-  height = 160
-}
-
-
 -- a large, tall, wooden crate
 
-PREFABS.Crate_large_wooden =
+UNFINISHED.Crate_large_wooden =
 {
   file   = "decor/crates1.wad"
   map    = "MAP12"
 
-  theme  = "!tech"
   prob   = 400
 
   where  = "point"
