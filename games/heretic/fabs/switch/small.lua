e06a8ab9fc9032f878be8a50a72bc30f61702a07
Andrew Apted <ajapted@users.sf.net>
2017-08-08 19:20:42 +1000

    HER : updated the small free-standing switch prefab.

--- games/heretic/fabs/switch/small.lua
+++ games/heretic/fabs/switch/small.lua
@@ -2,40 +2,16 @@
 -- Small switch
 --
 
-PREFABS.Switch_small_sw_blue =
+PREFABS.Switch_small =
 {
   file   = "switch/small.wad"
+  map    = "MAP01"
 
-  key    = "sw_blue"
+  key    = "sw_metal"
   prob   = 50
 
   where  = "point"
---???  size   = 64
 
   tag_1  = "?switch_tag"
-
-  -- prefab has COMPBLUE / FLAT14 textures
-}
-
-
-PREFABS.Switch_small_sw_red =
-{
-  template = "Switch_small_sw_blue"
-
-  key = "sw_red"
-
-  tex_COMPBLUE = "REDWALL"
-  flat_FLAT14  = "REDWALL"
-}
-
-
-PREFABS.Switch_small_sw_metal =
-{
-  template = "Switch_small_sw_blue"
-
-  key = "sw_metal"
-
-  tex_COMPBLUE = "METAL"
-  flat_FLAT14  = "METAL"
 }
 
