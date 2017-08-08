6815d48c1a97f04c190172053bd5166a6221fd7d
Andrew Apted <ajapted@users.sf.net>
2017-08-08 23:11:40 +1000

    HER : added definition for a "secret_exit" closet (a re-skin of the closet1 exit prefab).

--- games/heretic/fabs/exit/closet1.lua
+++ games/heretic/fabs/exit/closet1.lua
@@ -5,6 +5,7 @@
 PREFABS.Exit_closet1 =
 {
   file   = "exit/closet1.wad"
+  map    = "MAP01"
 
   prob   = 100
 
@@ -17,5 +18,23 @@ PREFABS.Exit_closet1 =
 
   x_fit  = "frame"
   y_fit  = "top"
+}
+
+
+----------------------------------------------------------------------
+
+PREFABS.Exit_closet1_secret =
+{
+  template = "Exit_closet1"
+
+  kind = "secret_exit"
+
+  -- replace normal exit special with "exit to secret" special
+  line_11 = 51
+
+  -- retexture to a red color
+  tex_METL2 = "REDWALL"
+  flat_FLAT502 = "FLOOR09"
+  flat_FLOOR30 = "FLOOR09"
 }
 
