b82051e1ab2721d03ed3b7b7930f07368fa63f8f
Andrew Apted <ajapted@users.sf.net>
2017-10-20 13:29:40 +1100

    Version bump --> 7.70

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "7.666"
-#define OBLIGE_HEX_VER  0x766
+#define OBLIGE_VERSION  "7.70"
+#define OBLIGE_HEX_VER  0x770
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
