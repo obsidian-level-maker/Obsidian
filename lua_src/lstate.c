223fb4c6f4083de9e57ace5e6df9fb8344fc8911
dashodanger <dashodanger@outlook.com>
2021-03-02 20:14:01 +0430

    -Switched Lua state seed to fixed value for predictable table access.

--- lua_src/lstate.c
+++ lua_src/lstate.c
@@ -64,19 +64,9 @@ typedef struct LG {
 ** Rely on Address Space Layout Randomization (if present) and
 ** current time.
 */
-#define addbuff(b,p,e) \
-  { size_t t = cast_sizet(e); \
-    memcpy(b + p, &t, sizeof(t)); p += sizeof(t); }
 
 static unsigned int luai_makeseed (lua_State *L) {
-  char buff[3 * sizeof(size_t)];
-  unsigned int h = cast_uint(time(NULL));
-  int p = 0;
-  addbuff(buff, p, L);  /* heap variable */
-  addbuff(buff, p, &h);  /* local variable */
-  addbuff(buff, p, &lua_newstate);  /* public function */
-  lua_assert(p == sizeof(buff));
-  return luaS_hash(buff, p, h);
+  return 770;
 }
 
 #endif
