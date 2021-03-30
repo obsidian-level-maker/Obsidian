1f2b0c1e74b79f7dff8ed97f3ab21f5741a4d5ff
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-03-30 11:30:04 +0800

    - Added new generic group wall piece for all themes based on 'modquake' architecture.

--- games/doom/fabs/wall/gtd_wall_generic_modquake_set.lua
+++ games/doom/fabs/wall/gtd_wall_generic_modquake_set.lua
@@ -1,4 +1,3 @@
---
 -- uses slope lines... a lot
 --
 
@@ -38,13 +37,13 @@ PREFABS.Wall_modquake_set_hell =
   tex_COMPBLUE = "CRACKLE2"
 }
 
--- LIMIT-safe versions
+-- LIMIT-SAFE:
 
 PREFABS.Wall_modquake_set_industrial_boom =
 {
   template = "Wall_modquake_set_industrial",
 
-  engine = "nolimit",
+  engine = "limit",
 
   rank = 2,
 
@@ -57,7 +56,7 @@ PREFABS.Wall_modquake_set_hell_boom =
 
   theme = "hell",
 
-  engine = "nolimit",
+  engine = "limit",
   rank = 1,
 
   line_342 = 0,
@@ -66,8 +65,7 @@ PREFABS.Wall_modquake_set_hell_boom =
   tex_COMPBLUE = "CRACKLE2"
 }
 
---
--- like a JAW!
+-- lights arranged like a JAW!
 --
 
 PREFABS.Wall_modquake_set_jawlike =
@@ -81,14 +79,14 @@ PREFABS.Wall_modquake_set_jawlike =
   group = "gtd_modquake_jawlike"
 }
 
--- LIMIT-safe versions
+-- LIMIT-SAFE:
 
 PREFABS.Wall_modquake_set_jawlike_boom =
 {
   template = "Wall_modquake_set_industrial",
   map = "MAP02",
 
-  engine = "nolimit",
+  engine = "limit",
 
   theme = "any",
   rank = 1,
@@ -99,7 +97,6 @@ PREFABS.Wall_modquake_set_jawlike_boom =
   line_341 = 0
 }
 
---
 -- in reference to Quake's brutalist wall pillars
 --
 
@@ -113,12 +110,14 @@ PREFABS.Wall_modquake_top_heavy_brace_set =
   group = "gtd_modquake_top_heavy_brace"
 }
 
+-- LIMIT-SAFE:
+
 PREFABS.Wall_modquake_top_heavy_brace_set_limit =
 {
   template = "Wall_modquake_set_industrial",
   map = "MAP03",
 
-  engine = "nolimit",
+  engine = "limit",
 
   rank = 1,
 
@@ -127,8 +126,7 @@ PREFABS.Wall_modquake_top_heavy_brace_set_limit =
   line_344 = 0
 }
 
---
--- 
+-- sloped brace textured with the eponymous TEKWALL4
 --
 
 PREFABS.Wall_modquake_tek_slope_brace =
@@ -143,12 +141,14 @@ PREFABS.Wall_modquake_tek_slope_brace =
   group = "gtd_modquake_tek_slope_brace"
 }
 
+-- LIMIT-SAFE:
+
 PREFABS.Wall_modquake_tek_slope_brace_limit =
 {
   template = "Wall_modquake_set_industrial",
   map = "MAP04",
 
-  engine = "nolimit",
+  engine = "limit",
 
   rank = 1,
 
@@ -159,6 +159,7 @@ PREFABS.Wall_modquake_tek_slope_brace_limit =
   line_341 = 0
 }
 
+-- exit light inset into a sloped brace
 --
 
 PREFABS.Wall_modquake_ex_light_slope_brace =
@@ -173,12 +174,14 @@ PREFABS.Wall_modquake_ex_light_slope_brace =
   group = "gtd_modquake_ex_light_slope_brace"
 }
 
+-- LIMIT-SAFE:
+
 PREFABS.Wall_modquake_ex_light_slope_brace_limit =
 {
   template = "Wall_modquake_set_industrial",
   map = "MAP05",
 
-  engine = "nolimit",
+  engine = "limit",
 
   rank = 1,
 
@@ -187,4 +190,60 @@ PREFABS.Wall_modquake_ex_light_slope_brace_limit =
   group = "gtd_modquake_ex_light_slope_brace",
 
   line_341 = 0
+}
+
+-- round pillar inset into a wall surrounded
+-- by a slope-ish brace
+
+PREFABS.Wall_modquake_round_braced_lit_pillar_industrial =
+{
+  template = "Wall_modquake_set_industrial",
+  map = "MAP06",
+
+  theme = "!hell"
+
+  rank = 2,
+
+  deep = 20,
+
+  group = "gtd_modquake_round_braced_lit_pillar",
+
+  flat_CEIL5_2 = "FLAT23",
+  tex_METAL = "SHAWN2",
+  tex_SUPPORT3 = "SUPPORT2"
+}
+
+PREFABS.Wall_modquake_round_braced_lit_pillar_hell =
+{
+  template = "Wall_modquake_set_industrial",
+  map = "MAP06",
+
+  theme = "hell"
+
+  rank = 3,
+
+  deep = 20,
+
+  group = "gtd_modquake_round_braced_lit_pillar",
+
+  tex_LITEBLU1 = "FIREWALA",
+  tex_LITEBLU4 = "FIREMAG1"
+}
+
+-- LIMIT-SAFE:
+
+PREFABS.Wall_modquake_round_braced_lit_pillar_limit =
+{
+  template = "Wall_modquake_set_industrial",
+  map = "MAP06",
+
+  theme = "any"
+
+  rank = 1,
+
+  deep = 20,
+
+  group = "gtd_modquake_round_braced_lit_pillar",
+
+  line_342 = 0
 }
