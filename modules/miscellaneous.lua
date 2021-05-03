60d35faec2aa00fa2d912b258a21963c19aceb0b
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-05-03 11:06:42 +0800

    - Fixed missing parameter passes from miscellaneous modules.

--- modules/miscellaneous.lua
+++ modules/miscellaneous.lua
@@ -157,6 +157,8 @@ end
 
 function MISC_STUFF.begin_level(self)
   for _,opt in pairs(self.options) do
+    if opt.valuator then goto continue end
+
     local name  = assert(opt.name)
     local value = opt.value
 
@@ -174,6 +176,8 @@ function MISC_STUFF.begin_level(self)
         PARAM[name] = value
       end
     end
+
+    ::continue::
   end
 end
 
