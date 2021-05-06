d87d359302212ac8252cd58c97d6e37296d48772
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-05-06 17:13:39 +0800

    - Added new window set - bunker-styled narrow slits with MIDSPACE.

---
+++ games/doom/fabs/window/gtd_window_bunker_mid_space.lua
@@ -0,0 +1,21 @@
+PREFABS.Window_thin_mid_space_1 =
+{
+  file   = "window/gtd_window_bunker_mid_space.wad",
+  map    = "MAP01",
+
+  group  = "gtd_window_bunker_mid_space",
+  prob   = 50,
+
+  where  = "edge",
+  seed_w = 1,
+
+  height = 96,
+  deep   = 16,
+  over   = 16,
+
+  bound_z1 = 0,
+  bound_z2 = 96,
+
+  x_fit = { 88,168 },
+  z_fit = "top"
+}
