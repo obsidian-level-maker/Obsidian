248e5abc8589e8e03f32576112c3a3b4668003f4
Dasho Danger <dashodanger@outlook.com>
2021-05-07 08:16:34 +0430

    Fixes for Nature Mode and Steets Mode activation

--- games/doom/levels.lua
+++ games/doom/levels.lua
@@ -263,7 +263,7 @@ function DOOM.get_levels()
       end
 
       -- nature mode
-      if OB_CONFIG.nature_mode and not LEV.has_streets then
+      if PARAM.float_nature_mode and not LEV.has_streets then
         if rand.odds(PARAM.float_nature_mode) then
           LEV.is_nature = true
         end
