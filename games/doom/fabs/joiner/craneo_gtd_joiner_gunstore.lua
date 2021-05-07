da1fb26aaabe7a23f2fab96c965af98d231e6e8e
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-05-07 11:10:03 +0800

    - Added Craneo's joiner-type gun store.

---
+++ games/doom/fabs/joiner/craneo_gtd_joiner_gunstore.lua
@@ -0,0 +1,34 @@
+PREFABS.Joiner_craneo_gun_store =
+{
+  file = "joiner/craneo_gtd_joiner_gunstore.wad",
+  map = "MAP01",
+
+  theme = "urban",
+  texture_pack = "armaetus",
+
+  where = "seeds",
+  shape  = "I",
+
+  prob = 2500,
+
+  seed_w = 3,
+  seed_h = 2,
+
+  deep = 16,
+  over = 16,
+
+  x_fit = "frame",
+  y_fit = "frame",
+
+  tex_CRGNRCK1 =
+  {
+    CRGNRCK1 = 5,
+    CRGNRCK3 = 5
+  },
+
+  tex_CRGNRCK3 =
+  {
+    CRGNRCK1 = 5,
+    CRGNRCK3 = 5
+  }
+}
