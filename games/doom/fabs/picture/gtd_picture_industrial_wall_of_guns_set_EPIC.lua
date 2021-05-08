00b1373e86f3271b05a2e6816aea17f7ae19591a
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-05-08 17:41:30 +0800

    - Added pic fabs for wall of guns wall set.

---
+++ games/doom/fabs/picture/gtd_picture_industrial_wall_of_guns_set_EPIC.lua
@@ -0,0 +1,27 @@
+PREFABS.Pic_indust_wall_of_guns_1 =
+{
+  file = "picture/gtd_picture_industrial_wall_of_guns_set_EPIC.wad",
+  map = "MAP01",
+
+  prob = 150,
+
+  where = "seeds",
+  height = 96,
+
+  seed_h = 1,
+  seed_w = 2,
+
+  bound_z1 = 0,
+  bound_z2 = 96,
+
+  deep = 16,
+
+  x_fit = "frame",
+  y_fit = "top",
+}
+
+PREFABS.Pic_indust_wall_of_guns_2 =
+{
+  template = "Pic_indust_wall_of_guns_1",
+  map = "MAP02"
+}
