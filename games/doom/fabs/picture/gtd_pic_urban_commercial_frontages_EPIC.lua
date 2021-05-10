031ec16569aca91e4c280473fdf0616c7db35d9d
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-05-10 11:46:55 +0800

    - Added new TV store fab using different relevant textures.

---
+++ games/doom/fabs/picture/gtd_pic_urban_commercial_frontages_EPIC.lua
@@ -0,0 +1,61 @@
+
+PREFABS.Pic_TV_store_static =
+{
+  file   = "picture/gtd_pic_urban_commercial_frontages_EPIC.wad",
+  map    = "MAP01",
+
+  prob   = 25,
+  theme = "urban",
+
+  texture_pack = "armaetus",
+
+  where  = "seeds",
+  height = 128,
+
+  env = "!cave",
+
+  seed_w = 3,
+  seed_h = 1,
+
+  bound_z1 = 0,
+  bound_z2 = 128,
+
+  deep   =  16,
+
+  x_fit = "frame",
+  y_fit = "top",
+
+  tex_NOISE1 =
+  {
+    NOISE1 = 5,
+    NOISE2A = 5,
+    NOISE3A = 5,
+    COMPFUZ1 = 5,
+    COMPSD1 = 5,
+    DECMP04A = 5,
+    COMPSA1 = 5,
+    TVSNOW01 = 5
+  },
+
+  tex_NOISE2A =
+  {
+    NOISE2A = 5,
+    NOISE3A = 5,
+    COMPFUZ1 = 5,
+    COMPSD1 = 5,
+    DECMP04A = 5,
+    COMPSA1 = 5,
+    TVSNOW01 = 5
+  },
+
+  tex_NOISE3A =
+  {
+    NOISE2A = 5,
+    NOISE3A = 5,
+    COMPFUZ1 = 5,
+    COMPSD1 = 5,
+    DECMP04A = 5,
+    COMPSA1 = 5,
+    TVSNOW01 = 5
+  }
+}
