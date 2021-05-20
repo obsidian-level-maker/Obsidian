1ebe42cda75d63e8d4b61d9b7eaaebc4a90faa46
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-05-20 11:50:25 +0800

    - Revised probs for objects on the new planter fence set.

--- games/doom/fabs/fence/gtd_fence_gothic.lua
+++ games/doom/fabs/fence/gtd_fence_gothic.lua
@@ -1,35 +1,35 @@
 PREFABS.Fence_gothic =
 {
-  file   = "fence/gtd_fence_gothic.wad",
-  map    = "MAP01",
+  file = "fence/gtd_fence_gothic.wad",
+  map = "MAP01",
 
-  prob   = 50,
+  prob = 50,
 
-  group  = "fence_gothic",
+  group = "fence_gothic",
 
   post_offset_h = 96,
 
-  where  = "edge",
+  where = "edge",
 
-  deep   = 16,
-  over   = 16,
+  deep = 16,
+  over = 16,
 
-  fence_h  = 32,
+  fence_h = 32,
   bound_z1 = 0,
 }
 
 PREFABS.Fence_gothic_diag =
 {
-  file   = "fence/gtd_fence_gothic.wad",
-  map    = "MAP02",
+  file = "fence/gtd_fence_gothic.wad",
+  map = "MAP02",
 
-  prob   = 50,
+  prob = 50,
 
-  group  = "fence_gothic",
+  group = "fence_gothic",
 
   post_offset_h = 96,
 
-  where  = "diagonal",
+  where = "diagonal",
 
   fence_h = 32,
 
