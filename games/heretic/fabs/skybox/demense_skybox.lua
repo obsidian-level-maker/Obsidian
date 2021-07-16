0520bc03edd2ee46a2f1692fafe2b15bcc3136d8
dashodanger <>
2021-07-15 18:27:19 -0600

    Addition of new Heretic skyboxes by Cubebert Addition of themes based on Oblige's various GUI revisions

---
+++ games/heretic/fabs/skybox/demense_skybox.lua
@@ -0,0 +1,14 @@
+PREFABS.Skybox_demense_skybox =
+{
+  file  = "skybox/demense_skybox.wad",
+  map   = "E1M1",
+
+  prob  = 50,
+
+  where = "point",
+
+  kind  = "skybox",
+
+  bound_z1 = -4,
+  bound_z2 = 320,
+}
