d99203569c25a8d734d2797dac73a3bdf126a226
dashodanger <>
2021-09-23 18:26:22 -0600

    Teleporter fixes for extra games

--- games/generic/fabs/teleporter/pad2.lua
+++ games/generic/fabs/teleporter/pad2.lua
@@ -7,11 +7,23 @@ PREFABS.Teleporter2 =
   file   = "teleporter/pad2.wad",
   map    = "MAP01",
 
+  kind = "teleporter",
+  game = "heretic",
+
   prob   = 50,
 
   where  = "point",
 
   tag_1 = "?out_tag",
   tag_2 = "?in_tag",
+}
+
+PREFABS.Teleporter2_no_sparkles =
+{
+  template = "Teleporter2",
+
+  game = "!heretic",
+
+  thing_74 = 0,
 }
 
