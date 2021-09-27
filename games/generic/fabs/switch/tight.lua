f3177709bdcd5a48a74572affb956619e0bf90f0
dashodanger <>
2021-09-27 12:16:50 -0600

    Fixes for tight generic wall switch

--- games/generic/fabs/switch/tight.lua
+++ games/generic/fabs/switch/tight.lua
@@ -3,7 +3,7 @@ PREFABS.Switch_wall_tight =
   file   = "switch/tight.wad",
 
   map = "MAP01",
-  game = { heretic=1,hacx=1,chex3=0,strife=1,harmony=1 },
+  game = "heretic",
 
   prob   = 18,
 
@@ -25,61 +25,52 @@ PREFABS.Switch_wall_tight =
 
 PREFABS.Switch_wall_tight_chex3 =
 {
-  file   = "switch/tight.wad",
+  template   = "Switch_wall_tight",
 
-  map = "MAP01",
   game = "chex3",
-
-
-  prob   = 18,
-
-  where  = "seeds",
-  seed_w = 1,
-  seed_h = 1,
-
-  deep   =  16,
-  over   = -16,
-
-  x_fit = "frame",
-  y_fit = "top",
-
-  tag_1 = "?switch_tag",
-
-  sector_1  = { [0]=70, [1]=15, [2]=5, [3]=5, [8]=10, [12]=3, [13]=3 },
-
-  tex__SWITCH = "SW1SLAD",
-
   forced_offsets = 
   {
-    [9] = { x=47,y=11 }
+    [8] = { x=4,y=76 },
+    [9] = { x=4,y=76 },
+    [10] = { x=4,y=76 },
   }
-
 }
 
-PREFABS.Switch_wall_tight2 =
+PREFABS.Switch_wall_tight_hacx =
 {
-  template = "Switch_wall_tight",
+  template   = "Switch_wall_tight",
 
-  map = "MAP02",
+  game = "hacx",
+  forced_offsets = 
+  {
+    [8] = { x=68,y=65 },
+    [9] = { x=68,y=65 },
+    [10] = { x=68,y=65 },
+  }
 }
 
-PREFABS.Switch_wall_tight2_chex3 =
+PREFABS.Switch_wall_tight_harmony =
 {
-  template = "Switch_wall_tight_chex3",
+  template   = "Switch_wall_tight",
 
-  map = "MAP02",
+  game = "harmony",
+  forced_offsets = 
+  {
+    [8] = { x=4,y=80 },
+    [9] = { x=4,y=80 },
+    [10] = { x=4,y=80 },
+  }
 }
 
-PREFABS.Switch_wall_tight3 =
+PREFABS.Switch_wall_tight_strife =
 {
-  template = "Switch_wall_tight",
+  template   = "Switch_wall_tight",
 
-  map = "MAP03",
-}
-
-PREFABS.Switch_wall_tight3_chex3 =
-{
-  template = "Switch_wall_tight_chex3",
-
-  map = "MAP03",
+  game = "strife",
+  forced_offsets = 
+  {
+    [8] = { x=4,y=62 },
+    [9] = { x=4,y=62 },
+    [10] = { x=4,y=62 },
+  }
 }
