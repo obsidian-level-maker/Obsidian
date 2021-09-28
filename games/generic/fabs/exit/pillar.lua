aa049556b734646fb708ce2969ec0f82e4954e25
dashodanger <>
2021-09-27 20:40:33 -0600

    Added secret variant to generic point exit

--- games/generic/fabs/exit/pillar.lua
+++ games/generic/fabs/exit/pillar.lua
@@ -13,8 +13,11 @@ PREFABS.Exit_pillar =
 
 PREFABS.Exit_pillar_hacx =
 {
-  template = "Exit_pillar",
-  game = "hacx",
+  file   = "exit/pillar.wad",
+  map    = "MAP01",
+  game   = "hacx",
+  prob   = 5,
+  where  = "point",
 
   forced_offsets =
   {
@@ -23,5 +26,25 @@ PREFABS.Exit_pillar_hacx =
     [4] = { x=-64,y=7 },
     [6] = { x=-64,y=7 },
   }  
+}
+
+PREFABS.Exit_pillar_secret =
+{
+  template = "Exit_pillar",
+
+  kind = "secret_exit",
+
+  -- replace normal exit special with "exit to secret" special
+  line_11 = 51,
+}
+
+PREFABS.Exit_pillar_secret_hacx =
+{
+  template = "Exit_pillar_hacx",
+
+  kind = "secret_exit",
+
+  -- replace normal exit special with "exit to secret" special
+  line_11 = 51,
 }
 
