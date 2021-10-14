0122b770aa830f113d714e5844d43c7b5fb81d8d
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-10-14 16:57:00 +0800

    - Added point version for bunker beds.

---
+++ games/doom/fabs/decor/gtd_decor_bunkerbeds.lua
@@ -0,0 +1,19 @@
+PREFABS.Decor_square_planter_grass =
+{
+  file = "decor/gtd_custom_tree_planters_EPIC.wad",
+  map = "MAP01",
+
+  prob = 5000,
+  where = "point",
+  engine = "zdoom",
+
+  group = "cran_bunkbeds",
+
+  height = 128,
+  size = 112,
+
+  bound_z1 = 0,
+  bound_z2 = 128,
+
+  z_fit = "top",
+}
