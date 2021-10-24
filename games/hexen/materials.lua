a1432d6d50c2a669ef08dbb006ddb6911cce1962
dashodanger <none@none.com>
2021-10-24 00:41:34 -0600

    Doom->Hexen generic compatibility progress; need to add SPAC flags when doing line substitution

--- games/hexen/materials.lua
+++ games/hexen/materials.lua
@@ -485,6 +485,10 @@ HEXEN.MATERIALS =
 
 HEXEN.PREFAB_FIELDS =
 {
+  doom_line_to_hexen_700 = { special=13, arg1=0, arg2=16, arg3=150, arg4=1, arg5=0 },
+  doom_line_to_hexen_701 = { special=13, arg1=0, arg2=16, arg3=150, arg4=2, arg5=0 },
+  doom_line_to_hexen_702 = { special=13, arg1=0, arg2=16, arg3=150, arg4=3, arg5=0 },
+
   -- These are used for converting generic fab things --
   thing_11000 = 8100, -- Barrel
   thing_11001 = 17, -- Ceiling light
