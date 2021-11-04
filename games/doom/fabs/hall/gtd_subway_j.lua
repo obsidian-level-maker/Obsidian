b274ea61cfe66133d191a8d2b870fc5235383d9b
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-11-04 20:40:01 +0800

    - Added temporary fallback for subway set secret joiner.

--- games/doom/fabs/hall/gtd_subway_j.lua
+++ games/doom/fabs/hall/gtd_subway_j.lua
@@ -14,3 +14,10 @@ PREFABS.Hallway_subway_basic_joiner =
   seed_w = 2,
   seed_h = 1,
 }
+
+PREFABS.Hallway_subway_basic_joiner_secret = -- replace with real secret
+{
+  template = "Hallway_subway_basic_joiner",
+
+  key = "secret"
+}
