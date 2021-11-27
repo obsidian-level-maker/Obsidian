5ea1bdf0cec3e01a12360939e7ecccc42528b361
dashodanger <none@none.com>
2021-11-27 10:02:49 -0700

    Updated Armaetus niche generic switch

--- games/generic/fabs/switch/armaetus_niche1.lua
+++ games/generic/fabs/switch/armaetus_niche1.lua
@@ -1,9 +1,9 @@
 PREFABS.Switch_armaetus_niche1 =
 {
   file   = "switch/armaetus_niche1.wad",
+  map = "MAP01",
   prob   = 25,
-
-  theme  = "tech",
+  game = { chex3=0, doom1=1, doom2=1, hacx=0, harmony=0, heretic=0, strife=0 },
 
   where  = "seeds",
 
@@ -16,369 +16,118 @@ PREFABS.Switch_armaetus_niche1 =
 
   x_fit = "frame",
   y_fit = "top",
-
-  tex_SW1GARG =
-  {
-    SW1GARG = 50,
-    SW1BLUE = 50,
-    SW1BRIK = 50,
-    SW1BRN2 = 50,
-    SW1BRNGN = 50,
-    SW1CMT = 50,
-    SW1COMP = 50,
-    SW1GRAY = 50,
-    SW1GRAY1 = 50,
-    SW1METAL = 50,
-    SW1SLAD = 50,
-    SW1STRTN = 50,
-    SW1TEK = 50,
-  },
-
-  tex_BROWNGRN =
-  {
-    BROWNGRN = 50,
-    BROWNHUG = 50,
-    COMPBLUE = 50,
-    METAL1   = 50,
-    SLADWALL = 50,
-    STARG1   = 50,
-    STARGR1  = 50,
-    GRAY1    = 50,
-    GRAY4    = 50,
-    GRAY5    = 50,
-    SHAWN2   = 50,
-    CEMENT7  = 50,
-    CEMENT9  = 50,
-    COMPSPAN = 50,
-  },
 }
 
-PREFABS.Switch_armaetus_niche1_urban =
+PREFABS.Switch_armaetus_niche1_chex3 =
 {
   template = "Switch_armaetus_niche1",
-
-  theme = "urban",
-
-  tex_SW1GARG =
-  {
-    SW1BLUE = 50,
-    SW1BRIK = 50,
-    SW1BRN2 = 50,
-    SW1BRNGN = 50,
-    SW1CMT = 50,
-    SW1GRAY = 50,
-    SW1GRAY1 = 50,
-    SW1METAL = 50,
-    SW1SLAD = 50,
-    SW1STRTN = 50,
-    SW1TEK = 50,
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1HOT = 50,
-    SW1LION = 50,
-    SW1MARB = 50,
-    SW1PANEL = 50,
-    SW1ROCK = 50,
-    SW1SATYR = 50,
-    SW1WDMET = 50,
-    SW1WOOD = 50,
-    SW1ZIM = 50,
-  },
-
-  tex_BROWNGRN =
-  {
-    BROWNGRN = 50,
-    BROWNHUG = 50,
-    METAL1   = 50,
-    METAL2   = 50,
-    MODWALL1 = 50,
-    PANCASE2 = 50,
-    PANEL7   = 50,
-    PANEL8   = 50,
-    PANEL9   = 50,
-    PANEL2   = 50,
-    PANEL3   = 50,
-    STONE4   = 50,
-    STONE6   = 50,
-    METAL    = 50,
-    STUCCO1  = 50,
-    STUCCO3  = 50,
-    WOODVERT = 50,
-    WOOD12   = 50,
-    BIGBRIK1 = 50,
-    BIGBRIK2 = 50,
-    BLAKWAL1 = 50,
-    BLAKWAL2 = 50,
-  },
-
+  game = "chex3",
+  map = "MAP02"
 }
 
-PREFABS.Switch_armaetus_niche1_hell =
+PREFABS.Switch_armaetus_niche1_hacx =
 {
   template = "Switch_armaetus_niche1",
-
-  theme = "hell",
-
-  tex_SW1GARG =
+  game = "hacx",
+  forced_offsets =
   {
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1HOT = 50,
-    SW1LION = 50,
-    SW1MARB = 50,
-    SW1PANEL = 50,
-    SW1ROCK = 50,
-    SW1SATYR = 50,
-    SW1WDMET = 50,
-    SW1WOOD = 50,
-    SW1ZIM = 50,
-  },
-
-  tex_BROWNGRN =
-  {
-    BROWNGRN = 50,
-    BROWNHUG = 50,
-    METAL1   = 50,
-    SLADWALL = 50,
-    ICKWALL1 = 50,
-    ICKWALL3 = 50,
-    ASHWALL2 = 50,
-    ASHWALL4 = 50,
-    BSTONE1  = 50,
-    BSTONE2  = 50,
-    CRACKLE2 = 50,
-    CRACKLE4 = 50,
-    MARBGRAY = 50,
-    ROCK1    = 50,
-    ROCK2    = 50,
-    ROCK3    = 50,
-    SKSNAKE1 = 50,
-    SKSNAKE2 = 50,
-    SP_FACE2 = 50,
-    STONE4   = 50,
-    STONE6   = 50,
-    METAL    = 50,
-    STUCCO   = 50,
-    STUCCO1  = 50,
-    STUCCO3  = 50,
-    WOOD4    = 50,
-    WOOD8    = 50,
-    WOOD9    = 50,
-    WOOD12   = 50,
-    WOODVERT = 50,
-    ZIMMER4  = 50,
-    ZIMMER5  = 50,
-    ZIMMER7  = 50,
-    ZIMMER8  = 50,
-  },
-
+    [62] = { x=0, y=0 }
+  }
 }
 
-PREFABS.Switch_armaetus_niche1_flesh =
+PREFABS.Switch_armaetus_niche1_harmony =
 {
   template = "Switch_armaetus_niche1",
-
-  theme = "flesh",
-
-  tex_SW1GARG =
+  game = "harmony",
+  forced_offsets =
   {
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1HOT = 50,
-    SW1LION = 50,
-    SW1SATYR = 50,
-    SW1WOOD = 50,
-    SW1SKIN = 50,
-    SW1BRN2 = 50,
-    SW1METAL = 50,
-    SW1SLAD  = 50,
-  },
-
-  tex_BROWNGRN =
-  {
-    BROWNGRN = 50,
-    BROWNHUG = 50,
-    METAL1   = 50,
-    SLADWALL = 50,
-    WOOD4    = 50,
-    GRAY1    = 50,
-    GRAY4    = 50,
-    ICKWALL1 = 50,
-    ICKWALL3 = 50,
-    METAL    = 50,
-  },
-
+    [62] = { x=16, y=16 }
+  }
 }
 
-PREFABS.Switch_armaetus_niche1_deimos =
+PREFABS.Switch_armaetus_niche1_heretic =
 {
   template = "Switch_armaetus_niche1",
-
-  theme = "deimos",
-
-  tex_SW1GARG =
+  game = "heretic",
+  forced_offsets =
   {
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1LION = 50,
-    SW1SATYR = 50,
-    SW1WOOD = 50,
-    SW1SKIN = 50,
-    SW1BRN1 = 50,
-    SW1BRN2 = 50,
-    SW1METAL = 50,
-    SW1COMP  = 50,
-    SW1STON1 = 50,
-    SW1STON2 = 50,
-    SW1STRTN = 50,
-  },
-
-  tex_BROWNGRN =
-  {
-    BROWNGRN = 50,
-    BROWNHUG = 50,
-    COMPBLUE = 50,
-    METAL1   = 50,
-    SLADWALL = 50,
-    STARG1   = 50,
-    STARGR1  = 50,
-    METAL    = 50,
-    ICKWALL1 = 50,
-    ICKWALL3 = 50,
-    GRAY1    = 50,
-    GRAY4    = 50,
-    GRAY5    = 50,
-  },
+    [62] = { x=16, y=49 }
+  }
 }
 
-PREFABS.Switch_armaetus_niche1_up_tech =
+PREFABS.Switch_armaetus_niche1_strife =
 {
   template = "Switch_armaetus_niche1",
-
-  map = "MAP02",
-
-  tex_SW1GARG =
+  game = "strife",
+  forced_offsets =
   {
-    SW1GARG = 50,
-    SW1BLUE = 50,
-    SW1BRIK = 50,
-    SW1BRN2 = 50,
-    SW1BRNGN = 50,
-    SW1CMT = 50,
-    SW1COMP = 50,
-    SW1GRAY = 50,
-    SW1GRAY1 = 50,
-    SW1METAL = 50,
-    SW1SLAD = 50,
-    SW1STRTN = 50,
-    SW1TEK = 50,
-  },
+    [62] = { x=0, y=72 }
+  }
 }
 
-PREFABS.Switch_armaetus_niche1_up_urban =
+PREFABS.Switch_armaetus_niche1_up =
 {
-  template = "Switch_armaetus_niche1",
+  file  = "switch/armaetus_niche1.wad",
+  map = "MAP03",
+  prob   = 25,
+  game = { chex3=0, doom1=1, doom2=1, hacx=0, harmony=0, heretic=0, strife=0 },
 
-  map = "MAP02",
+  where  = "seeds",
 
-  theme = "urban",
+  seed_w = 1,
+  seed_h = 2,
 
-  tex_SW1GARG =
-  {
-    SW1BLUE = 50,
-    SW1BRIK = 50,
-    SW1BRN2 = 50,
-    SW1BRNGN = 50,
-    SW1CMT = 50,
-    SW1GRAY = 50,
-    SW1GRAY1 = 50,
-    SW1METAL = 50,
-    SW1SLAD = 50,
-    SW1STRTN = 50,
-    SW1TEK = 50,
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1HOT = 50,
-    SW1LION = 50,
-    SW1MARB = 50,
-    SW1PANEL = 50,
-    SW1ROCK = 50,
-    SW1SATYR = 50,
-    SW1WDMET = 50,
-    SW1WOOD = 50,
-    SW1ZIM = 50,
-  },
+  deep = 16,
+
+  tag_1 = "?switch_tag",
+
+  x_fit = "frame",
+  y_fit = "top",
 }
 
-PREFABS.Switch_armaetus_niche1_up_hell =
+PREFABS.Switch_armaetus_niche1_up_chex3 =
 {
-  template = "Switch_armaetus_niche1",
-
-  map = "MAP02",
-
-  theme = "hell",
-
-  tex_SW1GARG =
-  {
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1HOT = 50,
-    SW1LION = 50,
-    SW1MARB = 50,
-    SW1PANEL = 50,
-    SW1ROCK = 50,
-    SW1SATYR = 50,
-    SW1WDMET = 50,
-    SW1WOOD = 50,
-    SW1ZIM = 50,
-  },
+  template = "Switch_armaetus_niche1_up",
+  game = "chex3",
+  map = "MAP04"
 }
 
-PREFABS.Switch_armaetus_niche1_up_flesh =
+PREFABS.Switch_armaetus_niche1_up_hacx =
 {
-  template = "Switch_armaetus_niche1",
-
-  map = "MAP02",
-
-  theme = "flesh",
-
-  tex_SW1GARG =
+  template = "Switch_armaetus_niche1_up",
+  game = "hacx",
+  forced_offsets =
   {
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1HOT = 50,
-    SW1LION = 50,
-    SW1SATYR = 50,
-    SW1WOOD = 50,
-    SW1SKIN = 50,
-    SW1BRN2 = 50,
-    SW1METAL = 50,
-    SW1SLAD  = 50,
-  },
+    [62] = { x=0, y=0 }
+  }
 }
 
-PREFABS.Switch_armaetus_niche1_up_deimos =
+PREFABS.Switch_armaetus_niche1_up_harmony =
 {
-  template = "Switch_armaetus_niche1",
-
-  map = "MAP02",
-
-  theme = "deimos",
-
-  tex_SW1GARG =
+  template = "Switch_armaetus_niche1_up",
+  game = "harmony",
+  forced_offsets =
   {
-    SW1GARG = 50,
-    SW1GSTON = 50,
-    SW1LION = 50,
-    SW1SATYR = 50,
-    SW1WOOD = 50,
-    SW1SKIN = 50,
-    SW1BRN1 = 50,
-    SW1BRN2 = 50,
-    SW1METAL = 50,
-    SW1COMP  = 50,
-    SW1STON1 = 50,
-    SW1STON2 = 50,
-    SW1STRTN = 50,
-  },
+    [62] = { x=16, y=16 }
+  }
+}
+
+PREFABS.Switch_armaetus_niche1_up_heretic =
+{
+  template = "Switch_armaetus_niche1_up",
+  game = "heretic",
+  forced_offsets =
+  {
+    [62] = { x=16, y=49 }
+  }
+}
+
+PREFABS.Switch_armaetus_niche1_up_strife =
+{
+  template = "Switch_armaetus_niche1_up",
+  game = "strife",
+  forced_offsets =
+  {
+    [62] = { x=0, y=72 }
+  }
 }
