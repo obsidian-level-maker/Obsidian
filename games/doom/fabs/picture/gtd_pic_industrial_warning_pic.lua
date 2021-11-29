588375587f4d6a90801a3228500f33abe1ab91af
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-11-30 00:10:30 +0800

    - Added new resource pack texture and pic fab.

---
+++ games/doom/fabs/picture/gtd_pic_industrial_warning_pic.lua
@@ -0,0 +1,22 @@
+PREFABS.Pic_gtd_warning_days_since_last =
+{
+  file = "picture/gtd_pic_industrial_warning_pic.wad",
+  map = "MAP01",
+
+  theme = "!hell",
+
+  prob = 50,
+  skip_prob = 75,
+  texture_pack = "armaetus",
+
+  where = "seeds",
+  height = 128,
+
+  seed_w = 2,
+  seed_h = 1,
+
+  bound_z1 = 0,
+  bound_z2 = 128,
+
+  x_fit = "stretch",
+}
