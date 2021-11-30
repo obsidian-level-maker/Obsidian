d1473e360594803f07cfaf475a51bf6c59c8e6f1
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-11-30 12:16:14 +0800

    - Separated mix fine tune for room size and area count into a different option and added new choices.

--- scripts/level.lua
+++ scripts/level.lua
@@ -266,13 +266,17 @@ function Episode_determine_map_sizes()
     LEV.size_consistency = "normal"
     local mix_type = "normal"
 
-    if PARAM.room_sa_mix_type then
-      mix_type = PARAM.room_sa_mix_type
+    if PARAM.room_size_mix_type then
+      size_mix_type = PARAM.room_size_mix_type
+    end
+
+    if PARAM.room_area_mix_type then
+      area_mix_type = PARAM.room_area_mix_type
     end
 
     if PARAM.room_size_multiplier then
       if PARAM.room_size_multiplier == "mixed" then
-        LEV.size_multiplier = rand.key_by_probs(ROOM_SIZE_MULTIPLIER_MIXED_PROBS[mix_type])
+        LEV.size_multiplier = rand.key_by_probs(ROOM_SIZE_MULTIPLIER_MIXED_PROBS[size_mix_type])
       elseif PARAM.room_size_multiplier ~= "vanilla" then
         LEV.size_multiplier = tonumber(PARAM.room_size_multiplier)
       end
@@ -280,7 +284,7 @@ function Episode_determine_map_sizes()
 
     if PARAM.room_area_multiplier then
       if PARAM.room_area_multiplier == "mixed" then
-        LEV.area_multiplier = rand.key_by_probs(ROOM_AREA_MULTIPLIER_MIXED_PROBS[mix_type])
+        LEV.area_multiplier = rand.key_by_probs(ROOM_AREA_MULTIPLIER_MIXED_PROBS[area_mix_type])
       elseif PARAM.room_area_multiplier ~= "vanilla" then
         LEV.area_multiplier = tonumber(PARAM.room_area_multiplier)
       end
