ef78cd75e759cd2b84ae3ef4baea00859a3d4ebb
Armaetus <6969122+Armaetus@users.noreply.github.com>
2022-01-01 12:24:14 -0500

    Copyright 2022: games/doom directory
    And changing real name to alias.

--- games/doom/shapes.lua
+++ games/doom/shapes.lua
@@ -5,7 +5,7 @@
 --  // Obsidian //
 --
 --  Copyright (C) 2015-2017 Andrew Apted
---  Copyright (C) 2018-2021 MsrSgtShooterPerson
+--  Copyright (C) 2018-2022 MsrSgtShooterPerson
 --
 --  This program is free software; you can redistribute it and/or
 --  modify it under the terms of the GNU General Public License
