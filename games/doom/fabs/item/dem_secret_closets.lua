e690b5f5a6d2827ef3e9d9c7ebc3b7cec8b51dcd
Demiosis <5500931+Demiosis@users.noreply.github.com>
2022-02-23 00:17:14 -0500

    2 more secrets added to hell levels

--- games/doom/fabs/item/dem_secret_closets.lua
+++ games/doom/fabs/item/dem_secret_closets.lua
@@ -942,11 +942,104 @@ PREFABS.Item_dem_cavein7_secret =
   }
 }
 
--- Secret cavenook with liquid
+-- Secret cavenook with liquid --
 
 PREFABS.Item_dem_cavein23_secret =
 {
   template = "Item_dem_cavein7_secret",
 
   map = "MAP30",
+
+
+}
+
+
+
+--Sgt's new flesh machine secret --
+PREFABS.Item_secret_hellmachine2_closet =
+{
+  file  = "item/dem_secret_closets.wad",
+  map   = "MAP31",
+  engine = "zdoom",
+
+  theme = "hell",
+  env = "!nature",
+
+  prob  = 100,
+
+  key   = "secret",
+
+  where  = "seeds",
+  seed_w = 2,
+  seed_h = 1,
+
+  deep = 16,
+  over = -16,
+
+  x_fit = "frame",
+  y_fit = "top",
+
+  can_flip = true,
+
+  texture_pack = "armaetus",
+}
+
+--a shrine in a hell rift without an item, an eye hidden will reveal something.--
+PREFABS.Item_secret_dem_rift_closet =
+{
+  file  = "item/dem_secret_closets.wad",
+  map   = "MAP32",
+
+  engine = "zdoom",
+
+  theme = "hell",
+  prob  = 100,
+
+  key   = "secret",
+
+  where  = "seeds",
+  seed_w = 3,
+  seed_h = 2,
+
+  deep = 16,
+  over = -16,
+
+  x_fit = "frame",
+  y_fit  = "frame",
+
+  texture_pack = "armaetus",
+
+  thing_63 =
+  {
+    hang_twitching = 50,
+    hang_torso = 50,
+    hang_leg   = 50,
+    hang_leg_gone = 50,
+  },
+
+  thing_28 =
+  {
+    skull_pole = 50,
+    skull_kebab = 50,
+    skull_cairn = 50,
+    impaled_human = 50,
+    impaled_twitch = 50,
+    evil_eye = 50,
+    skull_rock = 50,
+    big_tree = 50,
+    burnt_tree = 50,
+  },
+
+  thing_25 =
+  {
+    skull_pole = 50,
+    skull_kebab = 50,
+    skull_cairn = 50,
+    impaled_human = 50,
+    impaled_twitch = 50,
+    evil_eye = 50,
+    skull_rock = 50,
+    big_tree = 50,
+    burnt_tree = 50,
+  }
 }
