b088a796226febd44e1b685d32134049f8830131
dashodanger <none@none.com>
2022-03-05 00:08:18 -0700

    Removed Hexen Weapon Control Module; commented out unused monsters from Hexen Monster Control Module

--- modules/hexen/ctl_hexen.lua
+++ modules/hexen/ctl_hexen.lua
@@ -151,7 +151,7 @@ OB_MODULES["hexen_mon_control"] =
       randomize_group = "monsters"
      },
 
-     {
+     --[[{
       name = "float_serpent1",
       label = _("Stalker"),
       valuator = "slider",
@@ -189,7 +189,7 @@ OB_MODULES["hexen_mon_control"] =
       "3:3 (Heaps)," ..
       "20:20 (INSANE),",
       randomize_group = "monsters"
-     },
+     },]]--
 
      {
       name = "float_wendigo",
@@ -311,7 +311,7 @@ OB_MODULES["hexen_mon_control"] =
       randomize_group = "monsters"
      },
 	 
-     {
+     --[[{
       name = "float_Wyvern",
       label = _("Death Wyvern"),
       valuator = "slider",
@@ -329,7 +329,7 @@ OB_MODULES["hexen_mon_control"] =
       "3:3 (Heaps)," ..
       "20:20 (INSANE),",
       randomize_group = "monsters"
-     },
+     },]]--
 	 
      {
       name = "float_Heresiarch",
@@ -430,258 +430,5 @@ OB_MODULES["hexen_mon_control"] =
       "20:20 (INSANE),",
       randomize_group = "monsters"
      }
-  },
-}
-
-
-----------------------------------------------------------------
-
-CTL_HEXEN.WEAPON_PREF_CHOICES =
-{
-  "normal",  _("Normal"),
-  "vanilla", _("Vanilla"),
-  "none",    _("NONE"),
-}
-
-
-function CTL_HEXEN.weapon_setup(self)
-
-  module_param_up(self)
-
-  for _,opt in pairs(self.options) do
-
-    local W = GAME.WEAPONS[string.sub(opt.name, 7)] -- Strip the float_ prefix from the weapon name for table lookup
-
-    if W and PARAM[opt.name] ~= "Default" then
-      W.add_prob = PARAM[opt.name] * 100
-      W.pref     = W.add_prob * 0.28 + 1 -- Complete guesswork right now - Dasho
-
-      -- loosen some of the normal restrictions
-      W.level = 1
-    end
-  end -- for opt
-
-  if PARAM.weapon_prefs == "vanilla"
-  or PARAM.weapon_prefs == "none" then
-    for _,mon in pairs(GAME.MONSTERS) do
-      mon.weapon_prefs = nil
-    end
-  end
-
-  if PARAM.weapon_prefs == "vanilla" then
-	-- Nothing for now
-  end
-
-end
-
-
-OB_MODULES["hexen_weapon_control"] =
-{
-
-  name = "hexen_weapon_control",
-
-  label = _("Hexen Weapon Control"),
-
-  game = "hexen",
-  engine = "!vanilla",
-
-  hooks =
-  {
-    setup = CTL_HEXEN.weapon_setup
-  },
-
-  options =
-  {
-
-     {
-      name = "float_c_staff",
-      label = _("Serpent Staff"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-
-     {
-      name = "float_c_fire",
-      label = _("Firestorm"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-
-     {
-      name = "float_c_wraith",
-      label = _("Wraithverge"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-
-     {
-      name = "float_f_axe",
-      label = _("Timon's Axe"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-	 
-     {
-      name = "float_f_hammer",
-      label = _("Hammer of Retribution"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-	 
-     {
-      name = "float_f_quietus",
-      label = _("Quietus"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-	 
-     {
-      name = "float_m_cone",
-      label = _("Frost Shards"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-	 
-     {
-      name = "float_m_blitz",
-      label = _("Arcs of Death"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-	 
-     {
-      name = "float_m_scourge",
-      label = _("Bloodscourge"),
-      valuator = "slider",
-      units = "",
-      min = 0,
-      max = 10,
-      increment = .02,
-      default = "Default",
-      nan = "Default,", 
-      presets = "0:0 (None)," ..
-      ".02:0.02 (Scarce)," ..
-      ".14:0.14 (Less)," ..
-      ".5:0.5 (Plenty)," ..
-      "1.2:1.2 (More)," ..
-      "3:3 (Heaps)," ..
-      "10:10 (I LOVE IT),",
-      randomize_group = "pickups"
-     },
-
-    {
-      name="weapon_prefs",
-      label=_("Weapon Preferences"),
-      choices=CTL_HEXEN.WEAPON_PREF_CHOICES,
-      tooltip="Alters selection of weapons that are prefered to show up depending on enemy palette for a chosen map.\n\n" ..
-      "Normal: Monsters have weapon preferences. Stronger weapons and ammo are more likely to appear directly with stronger enemies.\n\n" ..
-      "Vanilla: Vanilla Oblige-style preferences. For now, it doesn't do anything. \n\n" ..
-      "NONE: No preferences at all. For those who like to live life dangerously with bishops and only axes.",
-      default="normal",
-    },
   },
 }
