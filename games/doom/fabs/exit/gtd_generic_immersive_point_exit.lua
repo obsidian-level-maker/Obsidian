164b85438cba4410612fc9fa29752faa437072b2
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-03-13 16:23:28 +0800

    - Added new point exit fab.

--- games/doom/fabs/exit/gtd_generic_immersive_point_exit.lua
+++ games/doom/fabs/exit/gtd_generic_immersive_point_exit.lua
@@ -5,23 +5,36 @@ PREFABS.Exit_immersive_point_exit1 =
 
   where  = "point",
 
-  prob   = 1000,
+  prob   = 1000
 }
 
 PREFABS.Exit_immersive_point_exit2 =
 {
   template = "Exit_immersive_point_exit1",
-
-  map = "MAP02",
+  map = "MAP02"
 }
 
 PREFABS.Exit_immersive_point_portable_nuke =
 {
   template = "Exit_immersive_point_exit1",
-
   map = "MAP03",
 
   height = 136,
 
-  size = 64,
+  size = 64
+}
+
+PREFABS.Exit_immersive_point_exit_door =
+{
+  template = "Exit_immersive_point_exit1",
+  map = "MAP04",
+
+  size = 72,
+
+  tex_DOOR1 =
+  {
+    DOOR1 = 5,
+    DOOR3 = 5,
+    EXITDOOR = 10
+  }
 }
