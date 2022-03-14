4818786f04f3280aa16e631ac035b46c961c9670
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-03-14 15:19:32 +0800

    - Minor correction on whole names Debug option.

--- scripts/naming.lua
+++ scripts/naming.lua
@@ -8657,9 +8657,9 @@ end
 
 
 function namelib.choose_one(DEF, max_len)
-  if PARAM.bool_whole_names_only then
-    whole_name = rand.key_by_probs(DEF.lexicon.s)
-    DEF.lexicon.s[whole_name] = 0
+  if PARAM.bool_whole_names_only and PARAM.bool_whole_names_only ~= 0 then
+    local whole_name = rand.key_by_probs(DEF.lexicon.s)
+    DEF.lexicon.s[whole_name] = DEF.lexicon.s[whole_name] / 10
     return whole_name
   end
 
