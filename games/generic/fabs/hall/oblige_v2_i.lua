f080d7bfd155263356ecc52711c0973e95f891be
dashodanger <none@none.com>
2022-03-14 12:48:22 -0600

    Added initial fabs and definitions for Oblige V2 hallway set

---
+++ games/generic/fabs/hall/oblige_v2_i.lua
@@ -0,0 +1,19 @@
+--
+-- 2-seed-wide hallway : straight piece
+--
+
+PREFABS.Hallway_oblige_v2_i1 =
+{
+  file   = "hall/oblige_v2_i.wad",
+  map    = "MAP01",
+
+  group  = "oblige_v2",
+  prob   = 50,
+
+  where  = "seeds",
+  shape  = "I",
+
+  seed_w = 2,
+  seed_h = 2,
+}
+
---
+++ games/generic/fabs/hall/oblige_v2_j.lua
@@ -0,0 +1,31 @@
+--
+-- 2-seed-wide hallway : terminators
+--
+
+PREFABS.Hallway_oblige_v2_term =
+{
+  file   = "hall/oblige_v2_j.wad",
+  map    = "MAP01",
+  kind   = "terminator",
+
+  group  = "oblige_v2,
+  prob   = 50,
+
+  where  = "seeds",
+  shape  = "I",
+
+  seed_w = 2,
+  seed_h = 1,
+
+  deep   = 16,
+}
+
+
+PREFABS.Hallway_oblige_v2_secret =
+{
+  template = "Hallway_oblige_v2_term",
+
+  map  = "MAP05",
+  key  = "secret",
+}
+
