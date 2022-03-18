8ae41eeef2fb3c1351c45c197a36dd53c42944bf
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-03-18 11:16:34 +0800

    - Added funnel entrance for tall keyed joiner fab.

--- games/doom/fabs/joiner/key_tall.lua
+++ games/doom/fabs/joiner/key_tall.lua
@@ -7,7 +7,7 @@ PREFABS.Locked_joiner_key1_tall =
   file   = "joiner/key_tall.wad",
   map    = "MAP01",
 
-  prob   = 100,
+  prob   = 40,
 
   key    = "k_red",
   where  = "seeds",
@@ -39,6 +39,44 @@ PREFABS.Locked_joiner_key2_tall =
 PREFABS.Locked_joiner_key3_tall =
 {
   template = "Locked_joiner_key1_tall",
+
+  key = "k_yellow",
+
+  tex_DOORRED = "DOORYEL",
+
+  line_33 = 34
+}
+
+--
+
+PREFABS.Locked_joiner_key1_tall_funnel =
+{
+  template = "Locked_joiner_key1_tall",
+  map = "MAP02",
+
+  seed_w = 2
+}
+
+PREFABS.Locked_joiner_key2_tall_funnel =
+{
+  template = "Locked_joiner_key1_tall",
+  map = "MAP02",
+
+  seed_w = 2,
+
+  key = "k_blue",
+
+  tex_DOORRED = "DOORBLU",
+
+  line_33 = 32
+}
+
+PREFABS.Locked_joiner_key3_tall_funnel =
+{
+  template = "Locked_joiner_key1_tall",
+  map = "MAP02",
+
+  seed_w = 2,
 
   key = "k_yellow",
 
