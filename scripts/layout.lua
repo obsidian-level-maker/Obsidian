8fae5c3df9f3bed047b27a3ec2ea04ea815b2760
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-03-18 22:55:24 +0800

    - Attempted to fix issue with road sink being applied to non-street areas in streets mode.

--- scripts/layout.lua
+++ scripts/layout.lua
@@ -2161,7 +2161,7 @@ stderrf("Cages in %s [%s pressure] --> any_prob=%d  per_prob=%d\n",
       end
 
       -- for streets
-      if R.is_street and R.svolume > 16 then
+      if fg.is_road then
         if not GAME.THEMES[LEVEL.theme_name].street_sinks then
           gui.printf("WARNING! No street sinks for theme " .. LEVEL.theme_name .. ".\n")
           return
