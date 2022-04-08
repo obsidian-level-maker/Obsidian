30e6c851a10f5038534b6d4d39c823fec51256de
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-04-09 00:17:27 +0800

    - Freedom from want

--- games/doom/fabs/item/gtd_item_closed_armory.lua
+++ games/doom/fabs/item/gtd_item_closed_armory.lua
@@ -28,10 +28,27 @@ PREFABS.Item_sealed_armory_room_keyed =
   template = "Item_sealed_armory_room_regular",
   map      = "MAP01",
 
+  theme = "zdoom"
   item_kind = "key",
 
   rank   = 2,
   prob   = 100,
 
   sound = "Toilet_Running",
+}
+
+PREFABS.Item_sealed_armory_room_keyed_limit =
+{
+  template = "Item_sealed_armory_room_regular",
+  map      = "MAP01",
+
+  theme = "!zdoom"
+  item_kind = "key",
+
+  rank   = 2,
+  prob   = 100,
+
+  sound = "Toilet_Running",
+
+  line_21018 = 41
 }
