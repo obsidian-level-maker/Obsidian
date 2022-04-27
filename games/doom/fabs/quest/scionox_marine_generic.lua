1200f87bf4a7609b3e6d46318ff887f568df9c2b
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-04-27 12:17:28 +0800

    - Added 1x1 version prefab for marine closets.

--- games/doom/fabs/quest/scionox_marine_generic.lua
+++ games/doom/fabs/quest/scionox_marine_generic.lua
@@ -12,7 +12,6 @@ PREFABS.Quest_scionox_marine_generic =
   seed_h = 1,
 
   deep   = 16,
-  over   = -16,
 
   kind = "sec_quest",
 
@@ -41,4 +40,29 @@ PREFABS.Quest_scionox_marine_generic_3 =
 
   seed_w = 1,
   seed_h = 2,
+}
+
+PREFABS.Quest_scionox_marine_generic_smol =
+{
+  file   = "quest/scionox_marine_generic.wad",
+  map    = "MAP01",
+
+  prob   = 250,
+
+  where  = "seeds",
+  shape  = "U",
+
+  seed_w = 1,
+  seed_h = 1,
+
+  deep   = 16,
+
+  kind = "sec_quest",
+
+  group = "marine_closet",
+
+  mmin = 1,
+  mmax = 4,
+
+  y_fit = "top",
 }
