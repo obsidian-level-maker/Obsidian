abb1013b94fc08f9c8b197b728ceb0858f0bbfcf
dashodanger <none@none.com>
2022-08-09 09:32:06 -0600

    Added EpicTyphlosion's new start fabs; generics updates

---
+++ games/doom/fabs/start/typh_basic_hell.lua
@@ -0,0 +1,21 @@
+PREFABS.Start_typh_basic_hell =
+{
+  file   = "start/typh_basic_hell.wad",
+  map   = "MAP01",
+  
+  theme = "hell",
+
+  prob   = 30,
+
+  where  = "point",
+
+  flat_GATE1 = { GATE1=50, GATE2=50, GATE3=70, GATE4=20 },
+}
+
+PREFABS.Start_typh_basic_hell_wide =
+{
+  template = "Start_typh_basic_hell",
+  map   = "MAP02",
+  
+  theme = "hell",
+}
