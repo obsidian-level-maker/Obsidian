d5675df0e7e54e9b98b29243af517866b09f64bf
Kyle Coffey <me@phytolizer.dev>
2022-09-10 19:25:18 -0500

    refactor filename_formatter

--- source_files/filename_formatter/ff_main.h
+++ source_files/filename_formatter/ff_main.h
@@ -1,2 +1,3 @@
-const char *ff_main(const char *levelcount, const char *game, const char *engine, const char *theme,
-                    const char *version, const char *format);
+const char *ff_main(const char *levelcount, const char *game,
+                    const char *engine, const char *theme, const char *version,
+                    const char *format);
