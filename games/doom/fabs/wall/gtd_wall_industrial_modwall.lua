d0635141e8b4da4ae3438504ac5070e6337185b3
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-09-16 16:56:12 +0800

    - Added modwall-inspired by wall group for tech and urban.

---
+++ games/doom/fabs/wall/gtd_wall_industrial_modwall.lua
@@ -0,0 +1,33 @@
+PREFABS.Wall_industrial_modwall_1 =
+{
+  file = "wall/gtd_wall_industrial_modwall.wad",
+  map = "MAP01",
+
+  prob = 50,
+  group = "gtd_ind_modwall_1",
+
+  where = "edge",
+  deep = 16,
+  height = 96,
+
+  bound_z1 = 0,
+  bound_z2 = 96,
+
+  z_fit = "top"
+}
+
+PREFABS.Wall_industrial_modwall_2a =
+{
+  template = "Wall_mining_dirt_fenced",
+  map = "MAP02",
+
+  prob = 120
+}
+
+PREFABS.Wall_industrial_modwall_1b =
+{
+  template = "Wall_mining_dirt_fenced",
+  map = "MAP03",
+
+  where = "diagonal"
+}
