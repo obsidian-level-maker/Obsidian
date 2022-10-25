7cf79ca4bec83d1997b16e87ec0f039b95afd2dc
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-10-25 22:31:23 +0800

    - Attempted to fix some prefab def issues.

--- games/doom/fabs/window/gtd_windows_tall_octagon.lua
+++ games/doom/fabs/window/gtd_windows_tall_octagon.lua
@@ -9,6 +9,7 @@ PREFABS.Window_tall_octagon_1 =
   prob   = 50,
 
   passable = true,
+  rank = 1,
 
   where  = "edge",
   seed_w = 1,
@@ -43,19 +44,34 @@ PREFABS.Window_tall_octagon_3 =
 
 PREFABS.Window_tall_octagon_1_limit =
 {
-  template = "Window_tall_octagon_1",
-  map = "MAP01",
+  file   = "window/gtd_windows_tall_octagon.wad",
+  map    = "MAP01",
 
+  group  = "gtd_window_tall_octagon",
   port = "!zdoom",
 
+  prob   = 50,
+
+  passable = true,
+
+  where  = "edge",
   seed_w = 1,
+
+  height = 128,
+  deep   = 16,
+  over   = 16,
+
+  z_fit = { 54,90 },
+
+  bound_z1 = 0,
+  bound_z2 = 128,
 
   line_345 = 0
 }
 
 PREFABS.Window_tall_octagon_2_limit =
 {
-  template = "Window_tall_octagon_1",
+  template = "Window_tall_octagon_1_limit",
   map = "MAP02",
 
   port = "!zdoom",
@@ -67,7 +83,7 @@ PREFABS.Window_tall_octagon_2_limit =
 
 PREFABS.Window_tall_octagon_3_limit =
 {
-  template = "Window_tall_octagon_1",
+  template = "Window_tall_octagon_1_limit",
   map = "MAP03",
 
   port = "!zdoom",
