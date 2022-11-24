639c231c17d4a7152de4763d2c1f3f07295fe59c
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2022-11-24 10:44:13 +0800

    - Added new resource pack fence set: low park bars.

---
+++ games/doom/fabs/fence/gtd_fence_park_bars.lua
@@ -0,0 +1,25 @@
+PREFABS.Fence_park_bars =
+{
+  file = "fence/gtd_fence_highway_bars.wad",
+  map = "MAP01",
+
+  prob = 50,
+
+  group = "fence_park_bars",
+
+  where = "edge",
+
+  deep = 16,
+  over = 16,
+
+  fence_h = 40,
+  bound_z1 = 0
+}
+
+PREFABS.Fence_park_bars_diag =
+{
+  template = "Fence_park_bars",
+  map = "MAP02",
+
+  where = "diagonal"
+}
