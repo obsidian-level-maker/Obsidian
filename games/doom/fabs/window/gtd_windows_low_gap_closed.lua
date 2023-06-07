ac4b0dbff6b4c8d80b1c101290e6d70d7eebb22d
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2023-06-07 15:50:56 +0800

    - Added new texture variants for low_gap_closed window set.

--- games/doom/fabs/window/gtd_windows_low_gap_closed.lua
+++ games/doom/fabs/window/gtd_windows_low_gap_closed.lua
@@ -5,6 +5,8 @@ PREFABS.Window_low_gap_closed_1 =
 
   group  = "gtd_window_low_gap_closed",
   prob   = 50,
+  theme = "!hell",
+  rank = 1
 
   where  = "edge",
   seed_w = 1,
@@ -41,4 +43,69 @@ PREFABS.Window_low_gap_closed_4 =
 
   map      = "MAP04",
   seed_w   = 4
+}
+
+--
+
+PREFABS.Window_low_gap_closed_1_hell =
+{
+  template = "Window_low_gap_closed_1",
+  map = "MAP01",
+
+  theme = "hell",
+  rank = 2,
+
+  flat_FLAT1 = "CEIL5_2",
+  flat_FLAT3 = "FLAT10",
+  tex_STEP4 = "STEP3",
+  tex_SHAWN2 = "METAL",
+  tex_DOORSTOP = "BRONZE3"
+}
+
+PREFABS.Window_low_gap_closed_2_hell =
+{
+  template = "Window_low_gap_closed_1",
+  map = "MAP02",
+
+  theme = "hell",
+  rank = 2,
+  seed_w = 2,
+
+  flat_FLAT1 = "CEIL5_2",
+  flat_FLAT3 = "FLAT10",
+  tex_STEP4 = "STEP3",
+  tex_SHAWN2 = "METAL",
+  tex_DOORSTOP = "BRONZE3"
+}
+
+PREFABS.Window_low_gap_closed_3_hell =
+{
+  template = "Window_low_gap_closed_1",
+  map = "MAP03",
+
+  theme = "hell",
+  rank = 2,
+  seed_w = 3,
+
+  flat_FLAT1 = "CEIL5_2",
+  flat_FLAT3 = "FLAT10",
+  tex_STEP4 = "STEP3",
+  tex_SHAWN2 = "METAL",
+  tex_DOORSTOP = "BRONZE3"
+}
+
+PREFABS.Window_low_gap_closed_4_hell =
+{
+  template = "Window_low_gap_closed_1",
+  map = "MAP04",
+
+  theme = "hell",
+  rank = 2,
+  seed_w = 4,
+
+  flat_FLAT1 = "CEIL5_2",
+  flat_FLAT3 = "FLAT10",
+  tex_STEP4 = "STEP3",
+  tex_SHAWN2 = "METAL",
+  tex_DOORSTOP = "BRONZE3"
 }
