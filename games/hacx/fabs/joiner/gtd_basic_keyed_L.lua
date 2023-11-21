a4b0644d22db6b23bf7a2fbd8f37dcdd31a2be22
dashodanger <none@none.com>
2023-11-21 11:26:52 -0700

    More HacX work

--- games/hacx/fabs/joiner/gtd_basic_keyed_L.lua
+++ games/hacx/fabs/joiner/gtd_basic_keyed_L.lua
@@ -1,4 +1,4 @@
-PREFABS.Joiner_gtd_basic_keyed_curve_green =
+PREFABS.Joiner_gtd_basic_keyed_curve_red =
 {
   file   = "joiner/gtd_basic_keyed_L.wad",
   map    = "MAP01",
@@ -7,7 +7,7 @@ PREFABS.Joiner_gtd_basic_keyed_curve_green =
 
   
 
-  key    = "k_green",
+  key    = "kz_red",
 
   where  = "seeds",
   shape  = "L",
@@ -18,20 +18,20 @@ PREFABS.Joiner_gtd_basic_keyed_curve_green =
 
 PREFABS.Joiner_gtd_basic_keyed_curve_blue =
 {
-  template = "Joiner_gtd_basic_keyed_curve_green",
+  template = "Joiner_gtd_basic_keyed_curve_red",
 
-  key = "k_blue",
+  key = "kz_blue",
 
   line_33 = 32,
-  thing_95 = 94,
+  tex_HW510 = "HW512",
 }
 
 PREFABS.Joiner_gtd_basic_keyed_curve_yell =
 {
-  template = "Joiner_gtd_basic_keyed_curve_green",
+  template = "Joiner_gtd_basic_keyed_curve_red",
 
-  key = "k_yellow",
+  key = "kz_yellow",
 
   line_33 = 34,
-  thing_95 = 96,
+  tex_HW510 = "HW511",
 }
--- games/hacx/fabs/joiner/key_large.lua
+++ games/hacx/fabs/joiner/key_large.lua
@@ -2,7 +2,7 @@
 -- Large keyed door
 --
 
-PREFABS.Locked_joiner_green =
+PREFABS.Locked_joiner_red =
 {
   file   = "joiner/key_large.wad",
   map    = "MAP01",
@@ -11,7 +11,7 @@ PREFABS.Locked_joiner_green =
 
   
 
-  key    = "k_green",
+  key    = "kz_red",
   where  = "seeds",
   shape  = "I",
 
@@ -25,32 +25,27 @@ PREFABS.Locked_joiner_green =
   y_fit  = "frame",
 
   nearby_h = 160,
-
-  -- thing is already #95 (green statue)
-  -- line special is already #33,
 }
 
 
 PREFABS.Locked_joiner_blue =
 {
-  template = "Locked_joiner_green",
+  template = "Locked_joiner_red",
 
-  key      = "k_blue",
+  key = "kz_blue",
 
-  -- use the blue statue and correct line special
-  thing_95 = 94,
-  line_33  = 32,
+  line_33 = 32,
+  tex_HW510 = "HW512",
 }
 
 
 PREFABS.Locked_joiner_yellow =
 {
-  template = "Locked_joiner_green",
+  template = "Locked_joiner_red",
 
-  key      = "k_yellow",
+  key = "kz_yellow",
 
-  -- use the yellow statue and correct line special
-  thing_95 = 96,
-  line_33  = 34,
+  line_33 = 34,
+  tex_HW510 = "HW511",
 }
 
