5570f3bab22885af6f56b512d03dee63368fd35a
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2024-01-24 21:49:11 +0800

    - Added non-resource pack compat versions of some prison set fabs.

--- games/doom/fabs/picture/gtd_pic_urban_prison.lua
+++ games/doom/fabs/picture/gtd_pic_urban_prison.lua
@@ -5,6 +5,47 @@ PREFABS.Pic_urban_prison_cell =
 
   prob   = 150,
 
+  rank = 2,
+  group  = "gtd_prison_A",
+  where  = "seeds",
+
+  texture_pack = "armaetus",
+
+  height = 96,
+
+  seed_w = 2,
+  seed_h = 1,
+
+  bound_z1 = 0,
+  bound_z2 = 96,
+
+  deep   =  16,
+
+  x_fit = { 36,40 , 216,220 },
+  y_fit = { 36,40 },
+
+  thing_18 =
+  {
+    [10]=1, [12]=1, [15]=1, [18]=1, [19]=1,
+    [80]=1, [24]=1, [79]=1, [81]=1, [0]=20
+  },
+  thing_19 =
+  {
+    [10]=1, [12]=1, [15]=1, [18]=1, [19]=1,
+    [80]=1, [24]=1, [79]=1, [81]=1, [0]=20
+  },
+
+  tex_MIDBARS1 = "FENCE2"
+}
+
+PREFABS.Pic_urban_prison_cell_compat =
+{
+  file   = "picture/gtd_pic_urban_prison.wad",
+  map    = "MAP01",
+
+  prob   = 150,
+
+  rank = 1,
   group  = "gtd_prison_A",
   where  = "seeds",
 
@@ -20,4 +61,15 @@ PREFABS.Pic_urban_prison_cell =
 
   x_fit = { 36,40 , 216,220 },
   y_fit = { 36,40 },
+
+  thing_18 =
+  {
+    [10]=1, [12]=1, [15]=1, [18]=1, [19]=1,
+    [80]=1, [24]=1, [79]=1, [81]=1, [0]=20
+  },
+  thing_19 =
+  {
+    [10]=1, [12]=1, [15]=1, [18]=1, [19]=1,
+    [80]=1, [24]=1, [79]=1, [81]=1, [0]=20
+  }
 }
